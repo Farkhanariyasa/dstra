4279ed703b647f3ac77f0b27aaf366649f69d14c
LuthfilKA <222011321@stis.ac.id>
2023-01-16 16:42:52 +0700

    M Views : Menambahkan tombol dan memperbaiki tampilan tiap chart

--- app/Views/pvd/pages/dasbor/riset3/dummy_riset3.php
+++ app/Views/pvd/pages/dasbor/riset3/dummy_riset3.php
@@ -2,19 +2,21 @@
     <!-- Visualisasi 1.1 -->
     <div class="container">
         <!-- <button onclick="showmsg();" class="tombol btn-for" >Download image</button> -->
-        <div class="card d-flex justify-content-center align-item-center flex-md-row primary">
+        <div class="card d-flex justify-content-center align-item-center flex-md-row primary pb-5 pt-5">
             <div style="width: 900px; height:500px;">
-                <div class="position-relative">
-                    <button type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                        <i class="fa-solid fa-download"></i>
-                    </button>
-                    <div class="selectBox">
+                <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center ">
+                    <div class="selectBox mt-2 me-2">
                         <form action="">
                             <select id="forBarChart">
                                 <option value="a">Jenis Industri</option>
                                 <option value="b">Pendidikan Tertinggi</option>
                             </select>
                         </form>
+                    </div>
+                    <div class="me-1 mt-1 justify-content-end align-item-end">
+                        <button type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                            <i class="fa-solid fa-download"></i>
+                        </button>
                     </div>
                 </div>
                 <canvas id="ji_batang"></canvas>
@@ -25,16 +27,26 @@
 
 <div class="portfolio-item filter-riset3-hasil2">
     <!-- Visualisasi 1.2 -->
-    <div class="container">
+    <div class="container mt-5">
         <!-- <button class="tombol btn-for" id="btndownload">Download image</button> -->
-        <div class="card d-flex justify-content-center align-item-center flex-md-row primary">
-            <div class="position-relative">
-                <button type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal2">
-                    <i class="fa-solid fa-download"></i>
-                </button>
-            </div>
+        <div class="card d-flex justify-content-center align-item-center flex-md-row primary pb-5">
             <div style="width: 550px; height:600px;">
-                <canvas id="tk_pie"></canvas>
+                <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center ">
+                    <div class="selectBox mt-2 me-2">
+                        <form action="">
+                            <select id="forPieChart">
+                                <option value="a">Jenis Industri</option>
+                                <option value="b">Pendidikan Tertinggi</option>
+                            </select>
+                        </form>
+                    </div>
+                    <div class="me-1 mt-1 justify-content-end align-item-end">
+                        <button type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal2">
+                            <i class="fa-solid fa-download"></i>
+                        </button>
+                    </div>
+                </div>
+                <canvas class="mt-5" id="tk_pie"></canvas>
             </div>
         </div>
     </div>
@@ -44,13 +56,23 @@
     <!-- Visualisasi 1.4 -->
     <div class="container mt-5">
         <!-- <button class="tombol btn-for" id="btndownload2">Download image</button> -->
-        <div class="card d-flex justify-content-center align-item-center flex-md-row primary">
-            <div class="position-relative">
-                <button type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal3">
-                    <i class="fa-solid fa-download"></i>
-                </button>
-            </div>
+        <div class="card d-flex justify-content-center align-item-center flex-md-row primary pt-5">
             <div style="width: 550px; height:600px;">
+                <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center ">
+                    <div class="selectBox mt-2 me-2">
+                        <form action="">
+                            <select id="forDoughnutChart">
+                                <option value="a">Jenis Industri</option>
+                                <option value="b">Pendidikan Tertinggi</option>
+                            </select>
+                        </form>
+                    </div>
+                    <div class="me-1 mt-1 justify-content-end align-item-end">
+                        <button type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal2">
+                            <i class="fa-solid fa-download"></i>
+                        </button>
+                    </div>
+                </div>
                 <canvas id="j_doughnut"></canvas>
             </div>
         </div>
@@ -85,7 +107,6 @@
     <!-- <div id="map" class="mt-4"></div>             -->
 </div>
 
-<?php var_dump($ji) ?>
 
 <!-- Modal Riset 3  -->
 <?php include('modal_riset3.php') ?>
