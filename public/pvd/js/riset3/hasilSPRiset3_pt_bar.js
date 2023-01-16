bb8a5787970338739756112bf080cf6d2e73d2c1
LuthfilKA <222011321@stis.ac.id>
2023-01-16 13:33:48 +0700

    M Assets : Menampilkan data pendidikan, menampilkan data berdasarkan dropdown pada barChart

---
+++ public/pvd/js/riset3/hasilSPRiset3_pt_bar.js
@@ -0,0 +1,106 @@
+const ctx = document.getElementById('ji_batang');
+const batang =new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Tidak/belum pernah sekolah',
+            'Tidak/belum SD',
+            'SD/MI/SDLB/Paket A',
+            'SMP/MTs/SMPLB/Paket B',
+            'SMA/MA/SMLB/Paket C',
+            'SMK/MAK',
+            'Diploma I/II/III',
+            'Diploma IV',
+            'S1/S2/S3'
+        ],
+        datasets: [{
+            label: 'Pendidikan Tertinggi',
+            data: [datapendidikantertinggi.pt1,
+                datapendidikantertinggi.pt2,
+                datapendidikantertinggi.pt3,
+                datapendidikantertinggi.pt4,
+                datapendidikantertinggi.pt5,
+                datapendidikantertinggi.pt6,
+                datapendidikantertinggi.pt7,
+                datapendidikantertinggi.pt8,
+                datapendidikantertinggi.pt9,
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+    scales: {
+        y: {
+        beginAtZero: true
+        }
+    },
+    maintainAspectRatio: false
+    }
+});
+
+const barChart = document.getElementById('forBarChart');
+
+barChart.addEventListener('change', tampilData);
+  
+function tampilData(){
+    console.log(barChart.value);
+    // barChart.value.split(',');
+    // batang.data.datasets[0].data=barChart.value.split(',');
+    // batang.update();
+    if (barChart.value =='a') {
+        batang.data.datasets[0].data = 
+            [   datajenisindustri.ji1,
+                datajenisindustri.ji2,
+                datajenisindustri.ji3,
+                datajenisindustri.ji4,
+                datajenisindustri.ji5,
+                datajenisindustri.ji6,
+                datajenisindustri.ji7,
+                datajenisindustri.ji8,
+                datajenisindustri.ji9,
+                datajenisindustri.ji10,
+                datajenisindustri.ji11,
+            ];
+            batang.data.labels = ['jasa akomodasi',
+            'jasa makan dan minum',
+            'jasa angkutan rel',
+            'jasa angkutan darat',
+            'jasa angkutan air',
+            'jasa angkutan udara',
+            'jasa angkutan udara',
+            'Agen perjalanan dan reservasi lainnya',
+            'Jasa kebudayaan, olahraga dan rekreasi',
+            'Aktivitas perdagangan barang-barang pariwisata',
+            'jasa pariwisata lainnya'];
+            // console.log(batang.data.datasets[0].data)
+            console.log('jenisindustri');
+
+            batang.update();
+    } else {
+            batang.data.datasets[0].data = 
+            [   
+                datapendidikantertinggi.pt1,
+                datapendidikantertinggi.pt2,
+                datapendidikantertinggi.pt3,
+                datapendidikantertinggi.pt4,
+                datapendidikantertinggi.pt5,
+                datapendidikantertinggi.pt6,
+                datapendidikantertinggi.pt7,
+                datapendidikantertinggi.pt8,
+                datapendidikantertinggi.pt9
+            ];
+            batang.data.labels = ['Tidak/belum pernah sekolah',
+            'Tidak/belum SD',
+            'SD/MI/SDLB/Paket A',
+            'SMP/MTs/SMPLB/Paket B',
+            'SMA/MA/SMLB/Paket C',
+            'SMK/MAK',
+            'Diploma I/II/III',
+            'Diploma IV',
+            'S1/S2/S3'
+            ];
+            // console.log(batang.data.datasets[0].data)
+            console.log('pendidikan tertinggi');
+
+            batang.update();
+    }
+}
