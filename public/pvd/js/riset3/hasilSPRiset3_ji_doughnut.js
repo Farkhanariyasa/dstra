020c7f5c0fcb42a5e3466476d2df1ee603b1aada
LuthfilKA <222011321@stis.ac.id>
2023-01-16 13:32:33 +0700

    M Assets : Menampilkan data pendidikapada doughnutchart

--- public/pvd/js/riset3/hasilSPRiset3_ji_doughnut.js
+++ public/pvd/js/riset3/hasilSPRiset3_ji_doughnut.js
@@ -2,33 +2,30 @@ const ctx5 = document.getElementById('j_doughnut');
 new Chart(ctx5, {
   type: 'doughnut',
   data: {
-    labels: ['jasa akomodasi',
-            'jasa makan dan minum',
-            'jasa angkutan rel',
-            'jasa angkutan darat',
-            'jasa angkutan air',
-            'jasa angkutan udara',
-            'jasa angkutan udara',
-            'Agen perjalanan dan reservasi lainnya',
-            'Jasa kebudayaan, olahraga dan rekreasi',
-            'Aktivitas perdagangan barang-barang pariwisata',
-            'jasa pariwisata lainnya'
-    ],
-    datasets: [{
-      label: '# of Votes',
-      data: [datajenisindustri.ji1,
-            datajenisindustri.ji2,
-            datajenisindustri.ji3,
-            datajenisindustri.ji4,
-            datajenisindustri.ji5,
-            datajenisindustri.ji6,
-            datajenisindustri.ji7,
-            datajenisindustri.ji8,
-            datajenisindustri.ji9,
-            datajenisindustri.ji10,
-            datajenisindustri.ji11,],
-      borderWidth: 1
-    }]
+    labels: ['Tidak/belum pernah sekolah',
+            'Tidak/belum SD',
+            'SD/MI/SDLB/Paket A',
+            'SMP/MTs/SMPLB/Paket B',
+            'SMA/MA/SMLB/Paket C',
+            'SMK/MAK',
+            'Diploma I/II/III',
+            'Diploma IV',
+            'S1/S2/S3'
+        ],
+        datasets: [{
+            label: 'Pendidikan Tertinggi',
+            data: [datapendidikantertinggi.pt1,
+                datapendidikantertinggi.pt2,
+                datapendidikantertinggi.pt3,
+                datapendidikantertinggi.pt4,
+                datapendidikantertinggi.pt5,
+                datapendidikantertinggi.pt6,
+                datapendidikantertinggi.pt7,
+                datapendidikantertinggi.pt8,
+                datapendidikantertinggi.pt9,
+            ],
+            borderWidth: 1
+        }]
   },
   options:{
     scales:{
