3b9925dcf226849de9d3c05d5e8f294049dc4122
main <222011356@stis.ac.id>
2023-01-16 20:55:47 +0700

    M Views : tambah grafik pie riset 4

--- app/Views/pvd/pages/dasbor/riset4/dummy_riset4.php
+++ app/Views/pvd/pages/dasbor/riset4/dummy_riset4.php
@@ -1,24 +1,4 @@
-
 <!-- Riset 4 Hasil 1 -->
-<div class="portfolio-item filter-hasil2">
-<div class="container mt-5">
-        <!-- <button onclick="showmsg();" class="tombol btn-for" >Download image</button> -->
-        <div class="card d-flex justify-content-center flex-column align-item-center flex-md primary pb-5 pt-5 ">
-            <div style="width: 900px; height:500px;" class="d-flex flex-row align-item-center justify-content-center">
-                <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center">
-                    <div class="me-1 mt-1 justify-content-end align-item-end">
-                        <button type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                            <i class="fa-solid fa-download"></i>
-                        </button>
-                    </div>
-                </div>
-                <div id="map"></div>
-            </div>
-        </div>
-    </div>
-</div>
-
-<!-- Riset 4 Hasil 2 -->
 <div class="portfolio-item filter-hasil3">
     <div class="container mt-5">
         <div class="card d-flex justify-content-center align-item-center flex-md-row primary">
@@ -32,7 +12,26 @@
                         </button>
                     </div>
                 </div>
-                <canvas id="riset1_hasil3"></canvas>
+                <canvas id="hasil1"></canvas>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Riset 4 Hasil 2 -->
+<div class="portfolio-item filter-hasil2">
+<div class="container mt-5">
+        <!-- <button onclick="showmsg();" class="tombol btn-for" >Download image</button> -->
+        <div class="card d-flex justify-content-center flex-column align-item-center flex-md primary pb-5 pt-5 ">
+            <div style="width: 900px; height:500px;" class="d-flex flex-row align-item-center justify-content-center">
+                <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center">
+                    <div class="me-1 mt-1 justify-content-end align-item-end">
+                        <button type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                            <i class="fa-solid fa-download"></i>
+                        </button>
+                    </div>
+                </div>
+                <div id="map"></div>
             </div>
         </div>
     </div>
