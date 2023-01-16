101a66eff5dd319cf074aa6cf4a75e80998b623d
Thomson <222011365@stis.ac.id>
2023-01-16 18:32:22 +0700

    M Views: menampilkan bar chart, pie chart, dan dropdown

--- app/Views/pvd/pages/dasbor/riset1/dummy_riset1.php
+++ app/Views/pvd/pages/dasbor/riset1/dummy_riset1.php
@@ -1,15 +1,20 @@
 <!-- Riset 1 Hasil 1 -->
 <div class="portfolio-item filter-hasil1">
     <div class="container mt-5">
-        <div class="card d-flex justify-content-center align-item-center flex-md-row primary" style="height: 600px;">
-            <div class="content pl-4 mt-2" style="width: 800px; height:500px;">
-                <div class="position-relative">
-                    <div class="position-absolute top-0 end-0">
-                        <!-- ganti modal -->
-                        <!-- <button class="tombol btn-for" id="btndownload">
-                            <i class="fa-solid fa-download"></i>
-                        </button> --> 
-                        <button type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal1-1">
+        <!-- <button onclick="showmsg();" class="tombol btn-for" >Download image</button> -->
+        <div class="card d-flex justify-content-center flex-column align-item-center flex-md primary pb-5 pt-5 ">
+            <div style="width: 900px; height:500px;" class="d-flex flex-row align-item-center justify-content-center">
+                <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center">
+                    <div class="selectBox mt-2 me-2">
+                        <form action="">
+                            <select id="forBarChart">
+                                <option value="a">Jumlah ART Perjalanan Wisata</option>
+                                <option value="b">Jenis Kelamin</option>
+                            </select>
+                        </form>
+                    </div>
+                    <div class="me-1 mt-1 justify-content-end align-item-end">
+                        <button type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal">
                             <i class="fa-solid fa-download"></i>
                         </button>
                     </div>
@@ -22,19 +27,18 @@
 
 <!-- Riset 1 Hasil 2 -->
 <div class="portfolio-item filter-hasil2">
-    <div class="content pl-4 mt-2">
-        <div class="card d-flex justify-content-center align-item-center flex-md-row primary">
-            <div class="content pl-4 mt-2" style="width: 800px; height:500px;">
-                <div class="position-relative">
-                    <div class="position-absolute top-0 end-0">
-                         <!-- tambah modal -->
-                        <!-- <button class="tombol btn-for" id="btndownload2"><i class="fa-solid fa-download"></i></button> -->
-                        <button type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal1-2">
+<div class="container mt-5">
+        <!-- <button onclick="showmsg();" class="tombol btn-for" >Download image</button> -->
+        <div class="card d-flex justify-content-center flex-column align-item-center flex-md primary pb-5 pt-5 ">
+            <div style="width: 900px; height:500px;" class="d-flex flex-row align-item-center justify-content-center">
+                <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center">
+                    <div class="me-1 mt-1 justify-content-end align-item-end">
+                        <button type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal">
                             <i class="fa-solid fa-download"></i>
                         </button>
                     </div>
                 </div>
-                <canvas id="riset1_hasil2"></canvas>
+                <canvas id="jk_batang"></canvas>
             </div>
         </div>
     </div>
@@ -47,7 +51,7 @@
             <div class="content pl-4 mt-2" style="width: 800px; height:500px;">
                 <div class="position-relative">
                     <div class="position-absolute top-0 end-0">
-                         <!-- tambah modal -->
+                        <!-- tambah modal -->
                         <!-- <button class="tombol btn-for" id="btndownload3"><i class="fa-solid fa-download"></i></button> -->
                         <button type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal1-3">
                             <i class="fa-solid fa-download"></i>
@@ -82,15 +86,16 @@
                                 </tr>
                             </thead>
                             <tbody>
-                                
-                                    <tr>
-                                        <th scope="row"></th>
-                                        <td></td>
-                                        <td></td>
-                                        <td></td>
-                                        <td><</td>
-                                    </tr>
-                                
+
+                                <tr>
+                                    <th scope="row"></th>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td>
+                                        << /td>
+                                </tr>
+
                             </tbody>
                         </table>
                     </div>
