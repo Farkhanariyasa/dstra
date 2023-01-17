8807de12ad26067e4208a94ee109cef950d1f48d
LuthfilKA <222011321@stis.ac.id>
2023-01-17 21:13:34 +0700

    M Views : Modif modal dashboard riset 2

--- app/Views/pvd/pages/dasbor/riset2/index.php
+++ app/Views/pvd/pages/dasbor/riset2/index.php
@@ -3,7 +3,7 @@
 <?= $this->section('content'); ?>
 
 <section id="portfolio" class="portfolio section-bg">
-  <div class="container" data-aos="fade-up">
+  <div class="container">
 
     <br><br>
     <header class="section-header">
@@ -11,8 +11,8 @@
     </header>
     <hr class="hrjudul">
 
-    <!-- <div class="row">
-      <div class="col-lg-12 d-flex justify-content-center" data-aos="fade-up" data-aos-delay="100">
+    <div class="row">
+      <div class="col-lg-12 d-flex justify-content-center">
         <ul id="portfolio-flters">
           <li data-filter="*" class="filter-active">Semua</li>
           <li data-filter=".filter-riset2-hasil1">Hasil 1</li>
@@ -21,9 +21,9 @@
           <li data-filter=".filter-riset2-tabel">Tabel dummy</li>
         </ul>
       </div>
-    </div> -->
+    </div>
 
-    <div class="row portfolio-container" data-aos="fade-up" data-aos-delay="200">
+    <div class="row portfolio-container" >
       <?php include('dummy_riset2.php') ?>
     </div>
 </section>
