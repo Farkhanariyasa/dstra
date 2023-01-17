cd3bfe02c96ac1187dd4774fa8f36b80b389fd07
renatadelarosa <222011432@stis.ac.id>
2023-01-17 21:37:41 +0700

    M Views : perbaiki dasbor

--- app/Views/pvd/pages/dasbor/riset2/index.php
+++ app/Views/pvd/pages/dasbor/riset2/index.php
@@ -12,7 +12,7 @@
     
     <hr class="hrjudul">
 
-    <div class="row">
+    <!-- <div class="row">
       <div class="col-lg-12 d-flex justify-content-center">
         <ul id="portfolio-flters">
           <li data-filter="*" class="filter-active">Semua</li>
@@ -23,6 +23,40 @@
         </ul>
       </div>
     </div> -->
+
+    <div class="row">
+      <div class="col-sm-6 my-2">
+        <div class="card">
+          <div class="card-body">
+            <p class="card-text text-center">Grafik<br><br><br><br><br><br><br><br><br><br><br></p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-sm-6 my-2">
+        <div class="card">
+          <div class="card-body">
+            <p class="card-text text-center">Grafik<br><br><br><br><br><br><br><br><br><br><br></p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-sm-6 my-2">
+        <div class="card">
+          <div class="card-body">
+            <p class="card-text text-center">Grafik<br><br><br><br><br><br><br><br><br><br><br></p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-sm-6 my-2">
+        <div class="card">
+          <div class="card-body">
+            <p class="card-text text-center">Grafik<br><br><br><br><br><br><br><br><br><br><br></p>
+          </div>
+        </div>
+      </div>
+    </div>
 
     <div class="row portfolio-container" >
       <?php include('dummy_riset2.php') ?>
