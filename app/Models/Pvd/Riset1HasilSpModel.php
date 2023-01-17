a2f8c18caf9b13f30f7e598eba7d9a4d4e8509df
Thomson <222011365@stis.ac.id>
2023-01-17 22:51:35 +0700

    M Model: merging model dan sp1 sp2

--- app/Models/Pvd/Riset1HasilSpModel.php
+++ app/Models/Pvd/Riset1HasilSpModel.php
@@ -14,4 +14,39 @@ class Riset1HasilSpModel extends Model
     {
         return $this->where(["b203" => $ja])->countAllResults();
     }
+    public function getJenisKelamin($jk)
+    {
+        return $this->where(["b504"=>$jk])->countAllResults();
+    }
+
+    public function getMelakukanPerjalananWisata1($b524)
+    {
+        return $this->where(["b524" => $b524])->countAllResults();
+
+    }
+    public function getMelakukanPerjalananWisata2($b525)
+    {
+        return $this->where(["b525" => $b525])->countAllResults();
+
+    }
+    public function getMelakukanPerjalananWisata3($b526)
+    {
+        return $this->where(["b526" => $b526])->countAllResults();
+
+    }
+    public function getMelakukanPerjalananWisata4($b527)
+    {
+        return $this->where(["b527" => $b527])->countAllResults();
+
+    }
+    public function getMelakukanPerjalananWisata5($b528)
+    {
+        return $this->where(["b528" => $b528])->countAllResults();
+
+    }
+    public function getMelakukanPerjalananWisata6($b529)
+    {
+        return $this->where(["b529" => $b529])->countAllResults();
+
+    }
 }
