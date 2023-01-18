451fecb2cf2b02ff56606d70b1e43abfa79d6efe
imams <222011830@gmail.com>
2023-01-18 19:55:54 +0700

    M View : Footer

--- app/Views/pvd/layout/landing_page/footer.php
+++ app/Views/pvd/layout/landing_page/footer.php
@@ -2,14 +2,15 @@
 <footer style="background-color: #7382AB;">
   <div class="footer-top">
     <div class="container">
-       <div class="row justify-content-center pt-3">
+       <div class="row justify-content-center pt-3 pb-3">
           <a class="w-auto" href="<?= base_url("hasil-pkl/beranda") ?>">
-            <img src="<?= base_url('pvd/img/LogoPKL.png') ?>" alt="" width="100px">
+            <img src="<?= base_url('pvd/img/LogoPKL.png') ?>" alt="" width="75px">
             <!-- <span>Tentang PKL 62</span> -->
           </a>
-         <div class="col-lg-12 text-center text-white my-4">
-           <h5>Dibuat Dengan Sepenuh Hati</h5>
-           <p>&copy; Divisi PVD TI PKL 62 2022/2023</p>
+         <div class="col-lg-12 text-center text-white mt-1">
+           <p class="pb-0 mb-0" style="font-size:14px">Dibuat Dengan Sepenuh Hati<br>
+            &copy; Divisi PVD TI PKL 62 2022/2023
+          </p>
          </div>
       </div>
     </div>
