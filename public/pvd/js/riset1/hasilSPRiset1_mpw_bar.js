8450fe7899467310d2efa8e5f6e11219af538219
Thomson <222011365@stis.ac.id>
2023-01-19 17:17:07 +0700

    M Assets: nambar chart baru

--- public/pvd/js/riset1/hasilSPRiset1_mpw_bar.js
+++ public/pvd/js/riset1/hasilSPRiset1_mpw_bar.js
@@ -1,5 +1,5 @@
 const ctx = document.getElementById('jk_batang');
-new Chart(ctx, {
+const hasil2 = new Chart(ctx, {
     type:'bar',
     data: {
         labels: ['Ke Objek Wisata dalam Kota Sama',
@@ -8,7 +8,7 @@ new Chart(ctx, {
                 'Ke Objek Wisata di Luar Kota',
                 'Menginap di Akomodasi Komersil di Luar Kota',
                 'Menuju Kota Lain'
-            ],
+        ],
         datasets: [{
             label: 'Ya',
             data:[
@@ -53,7 +53,7 @@ new Chart(ctx, {
             },
             legend:{
                 display:true,
-                position:'bottom',
+                position:'top',
             },
             tooltip:{
                 enabled:true,
@@ -65,3 +65,65 @@ new Chart(ctx, {
         },maintainAspectRatio: false
     },plugins:[ChartDataLabels]
 });
+
+const barchart = document.getElementById('forhasil2Chart');
+barchart.addEventListener('change',tampilData);
+
+function tampilData(){
+    if (barchart.value =='a') {
+        hasil2.data.datasets[0].data = 
+            [   
+                mpw1.ya,
+                mpw2.ya,
+                mpw3.ya,
+                mpw4.ya,
+                mpw5.ya,
+                mpw6.ya,
+            ];
+            hasil2.data.datasets[1].data = 
+            [   
+                mpw1.tidak,
+                mpw2.tidak,
+                mpw3.tidak,
+                mpw4.tidak,
+                mpw5.tidak,
+                mpw6.tidak,
+            ];
+        hasil2.data.labels = [
+                'Ke Objek Wisata dalam Kota Sama',
+                'Menginap di Akomodasi Komersil di Kota Sama',
+                'Menuju Kecamatan lain dalam Kota Sama',
+                'Ke Objek Wisata di Luar Kota',
+                'Menginap di Akomodasi Komersil di Luar Kota',
+                'Menuju Kota Lain'
+            ];
+            hasil2.data.datasets[0].label='Ya';
+            hasil2.data.datasets[1].label='Tidak';
+            hasil2.options.plugins.title.text = 'Tindakan Perjalanan Tidak Lebih dari 1 Tahun dan Telah Kembali dalam Periode Oktober 2022 - Desember 2022';
+            // console.log(batang.data.datasets[0].data)
+            hasil2.update();
+    } else {
+            
+            hasil2.data.datasets.data = 
+            [   
+                indosat.ya,
+                xl.ya,
+                tri.ya,
+                smartfren.ya,
+                lainnya.ya
+            ];
+            hasil2.data.labels = [
+                'Indosat',
+                'XL Axiata',
+                '3 / Tri',
+                'Smartfren',
+                'Lainnya'
+            ];
+
+            hasil2.options.plugins.title.text = 'Jenis Simcard yang digunakanf0';
+            // console.log(batang.data.datasets[0].data)
+            hasil2.update();
+    }
+}
+
+
