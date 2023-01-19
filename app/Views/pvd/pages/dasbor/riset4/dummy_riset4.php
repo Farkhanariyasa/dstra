5cd03c473a8e6f87550516b4d2445f4d3ff9a79b
main <222011356@stis.ac.id>
2023-01-19 12:52:13 +0700

    M Views : grafik boxplot

--- app/Views/pvd/pages/dasbor/riset4/dummy_riset4.php
+++ app/Views/pvd/pages/dasbor/riset4/dummy_riset4.php
@@ -19,6 +19,26 @@
 </div>
 
 <!-- Riset 4 Hasil 2 -->
+<div class="portfolio-item filter-hasil3">
+    <div class="container mt-5">
+        <div class="card d-flex justify-content-center align-item-center flex-md-row primary">
+            <div class="content pl-4 mt-2" style="width: 800px; height:500px;">
+                <div class="position-relative">
+                    <div class="position-absolute top-0 end-0">
+                        <!-- tambah modal -->
+                        <!-- <button class="tombol btn-for" id="btndownload3"><i class="fa-solid fa-download"></i></button> -->
+                        <button type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal1-3">
+                            <i class="fa-solid fa-download"></i>
+                        </button>
+                    </div>
+                </div>
+                <canvas id="hasil2"></canvas>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Riset 4 Hasil 2 -->
 <div class="portfolio-item filter-hasil2">
 <div class="container mt-5">
         <!-- <button onclick="showmsg();" class="tombol btn-for" >Download image</button> -->
