7d509f79eb487bec8f9a61124f032fe43dfd8748
I Putu Agus Wahyu Dupayana <75474419+putuwahyu29@users.noreply.github.com>
2023-01-19 12:24:34 +0700

    M View : Rapiin depedensi

--- app/Views/pvd/layout/dasbor/header.php
+++ app/Views/pvd/layout/dasbor/header.php
@@ -2,59 +2,46 @@
 <html lang="en">
 
 <head>
-  <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <!-- plugins:css -->
-  <link rel="stylesheet" href="<?= base_url('pvd/vendors/mdi/css/materialdesignicons.min.css'); ?>">
-  <link rel="stylesheet" href="<?= base_url('pvd/vendors/css/vendor.bundle.base.css'); ?>">
-  <link rel="stylesheet" href="<?= base_url('pvd/css/riset1/style.css') ?>">
-  <link rel="stylesheet" href="<?= base_url('pvd/css/home.css') ?>">
-  <link rel="stylesheet" href="<?= base_url('pvd/css/bootstrap.css') ?>">
-  <link rel="stylesheet" href="<?= base_url('pvd/css/riset1/style.css') ?>">
-  <link rel="stylesheet" href="<?= base_url('pvd/css/fontawesome/fontawesome.min.css') ?>">
-  <!-- endinject -->
-  <!-- Plugin css for this page -->
-  <!-- End plugin css for this page -->
-  <!-- inject:css -->
-  <!-- endinject -->
-  <!-- Layout styles -->
-  <link rel="stylesheet" href="<?= base_url('pvd/css/dasbor.css'); ?>">
-  <!-- End layout styles -->
   <title><?= $judul; ?> | Hasil PKL</title>
-  <link rel="stylesheet" href="<?= base_url('pvd/css/fontawesome/fontawesome.min.css') ?>">
-
-  <!-- fontawesome -->
-    <script src="https://kit.fontawesome.com/e0bb680df5.js" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-
-    <!-- isotope untuk dasbor -->
-    <script src="https://unpkg.com/isotope-layout@3/dist/isotope.pkgd.js"></script>
-
-    <!-- chart -->
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/chartjs-plugin-datalabels/2.2.0/chartjs-plugin-datalabels.min.js" integrity="sha512-JPcRR8yFa8mmCsfrw4TNte1ZvF1e3+1SdGMslZvmrzDYxS69J7J49vkFL8u6u8PlPJK+H3voElBtUCzaXj+6ig==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-
-    <!-- leaflet -->
-    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css" integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI=" crossorigin="" />
-    <script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js" integrity="sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM=" crossorigin=""></script>
-
-    <!-- export table -->
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.1/css/jquery.dataTables.min.css">
-    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.3.2/css/buttons.dataTables.min.css">
-    <link rel="stylesheet" href="<?= base_url('pvd/js/table2excel.js') ?>">
-
-    <!-- sankey -->
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/chartjs-chart-sankey"></script>
-
-    <!-- fontawesome -->
-    <script src="https://kit.fontawesome.com/e0bb680df5.js" crossorigin="anonymous"></script>
-
   <!-- Favicon icon -->
   <link rel="icon" type="image/png" href="<?= base_url('pvd/img/logoPKL.png') ?>">
+
+  <!-- Bootstrap -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+
+  <!-- Vendor -->
+  <link rel="stylesheet" href="<?= base_url('pvd/vendors/mdi/css/materialdesignicons.min.css'); ?>">
+  <link rel="stylesheet" href="<?= base_url('pvd/vendors/css/vendor.bundle.base.css'); ?>">
+
+  <!-- Dasbor CSS -->
+  <link rel="stylesheet" href="<?= base_url('pvd/css/home.css') ?>">
+  <link rel="stylesheet" href="<?= base_url('pvd/css/dasbor.css'); ?>">
+
+  <!-- Fontawesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <script src="https://kit.fontawesome.com/e0bb680df5.js" crossorigin="anonymous"></script>
+
+  <!-- Chart JS -->
+  <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/chartjs-chart-sankey"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/chartjs-plugin-datalabels/2.2.0/chartjs-plugin-datalabels.min.js" integrity="sha512-JPcRR8yFa8mmCsfrw4TNte1ZvF1e3+1SdGMslZvmrzDYxS69J7J49vkFL8u6u8PlPJK+H3voElBtUCzaXj+6ig==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+  <!-- AOS -->
+  <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+
+  <!-- Isotope -->
+  <script src="https://unpkg.com/isotope-layout@3/dist/isotope.pkgd.js"></script>
+
+  <!-- Leaflet Js -->
+  <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css" integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI=" crossorigin="" />
+  <script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js" integrity="sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM=" crossorigin=""></script>
+
+  <!-- Datatables -->
+  <link rel="stylesheet" href="https://cdn.datatables.net/1.13.1/css/jquery.dataTables.min.css">
+  <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.3.2/css/buttons.dataTables.min.css">
+  <link rel="stylesheet" href="<?= base_url('pvd/js/table2excel.js') ?>">
 </head>
 
 <body>
