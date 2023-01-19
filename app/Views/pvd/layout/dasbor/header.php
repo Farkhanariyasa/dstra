9bd5556c3ca8e66ab0a9c187216bc0c86f4d3fc5
LuthfilKA <222011321@stis.ac.id>
2023-01-19 17:10:45 +0700

    M Views : Add css buat isotop

--- app/Views/pvd/layout/dasbor/header.php
+++ app/Views/pvd/layout/dasbor/header.php
@@ -33,7 +33,8 @@
 
   <!-- Isotope -->
   <script src="https://unpkg.com/isotope-layout@3/dist/isotope.pkgd.js"></script>
-
+  <link rel="stylesheet" href="<?= base_url('pvd/css/riset1/style.css') ?>">
+  
   <!-- Leaflet Js -->
   <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css" integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI=" crossorigin="" />
   <script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js" integrity="sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM=" crossorigin=""></script>
