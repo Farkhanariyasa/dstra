5ecb1536684c3c7a27ed146e4a6d76e6367da4c9
Thomson <222011365@stis.ac.id>
2023-01-19 12:49:33 +0700

    M Assets: chart riset1 hasil1 dan hasil2

--- public/pvd/js/riset1/hasilSPRiset1_mpw_bar.js
+++ public/pvd/js/riset1/hasilSPRiset1_mpw_bar.js
@@ -36,16 +36,24 @@ new Chart(ctx, {
         }]
     },options:{
         responsive: true,
+        maintainAspectRatio: false,
         plugins: {
             title:{
                 display:true,
                 text:'Tindakan Perjalanan Tidak Lebih dari 1 Tahun dan Telah Kembali dalam Periode Oktober 2022 - Desember 2022',
                 font: {
-                    size:20
+                    size:16
+                },
+                padding:{
+                    top:5,
+                    bottom:5,
+                    right:10,
+                    left:10
                 }
             },
             legend:{
                 display:true,
+                position:'bottom',
             },
             tooltip:{
                 enabled:true,
@@ -57,91 +65,3 @@ new Chart(ctx, {
         },maintainAspectRatio: false
     },plugins:[ChartDataLabels]
 });
-
-const batang1 = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels:['Laki-laki',
-                'Perempuan'],
-        datasets:[{
-            label: 'Responden',
-            data: [jeniskelamin.laki,
-                jeniskelamin.perempuan],
-            borderWidth:1,
-            backgroundColor:[
-                "#ffab00"
-            ]
-        }]
-    },
-    options: {
-        scales:{
-            y: {
-                beginAtZero: true
-            }
-        },
-        responsive: true,
-        plugins:{
-            title:{
-                display:true,
-                text: 'Jenis Kelamin ART',
-                font:{
-                    size:20
-                }
-            },
-            legend:{
-                display:true
-            },
-            tooltip:{
-                enabled:true,
-                // callbacks:{
-                //     label: function(context){
-                //         var label = context.label,
-                //             currentvalue = context.raw;
-
-                //             return label
-                //     }
-                // }
-            },
-            datalabels:{
-                display:true,
-                color:'black'
-            }
-        },
-        maintainAspectRatio: false
-    },plugins:[ChartDataLabels]
-});
-
-
-// const barchart = document.getElementById('forBarChart');
-// barchart.addEventListener('change',tampilData);
-
-// function tampilData(){
-//     if (barchart.value =='a') {
-//         console.log(barchart.value);
-//         // batang.data.datasets[0].data = 
-//         //     [   jeniskelamin.laki,
-//         //         jeniskelamin.perempuan
-//         //     ];
-//         //     batang.data.labels = [
-//         //         'Laki-laki',
-//         //         'Perempuan'
-//         //     ];
-//             // console.log(batang.data.datasets[0].data)
-//             batang1.update();
-//     } else {
-//             console.log(barchart.value);
-//             batang.data.datasets[0].data = 
-//             [   
-//                 mpw1.ya,
-//                 mpw2.ya,
-//                 mpw3.ya,
-//                 mpw4.ya,
-//                 mpw5.ya,
-//                 mpw6.ya,
-//             ];
-//             batang.data.datasets[0].label = 'Ya';
-        
-//             // console.log(batang.data.datasets[0].data)
-//             batang.update();
-//     }
-// }
