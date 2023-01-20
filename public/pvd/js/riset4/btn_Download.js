4239558daeedf82798f485d4a17056861ec9aeb4
LuthfilKA <222011321@stis.ac.id>
2023-01-20 15:42:57 +0700

    M Assets : Merapihkan modal pada riset 4 sementara

--- public/pvd/js/riset4/btn_Download.js
+++ public/pvd/js/riset4/btn_Download.js
@@ -1,8 +1,68 @@
 
 
-document.getElementById('btndownload').addEventListener('click', function(e) {
+document.getElementById('btndownload4-doughnut').addEventListener('click', function(e) {
     // Convert our canvas to a data URL
-    let canvasUrl = document.getElementById('tk_pie').toDataURL();
+    let canvasUrl = document.getElementById('hasil1').toDataURL();
+    // Create an anchor, and set the href value to our data URL
+    const createEl = document.createElement('a');
+    createEl.href = canvasUrl;
+
+    // This is the name of our downloaded file
+    createEl.download = "hasil-sp-riset-4-doughnut-Unit Usaha TIK";
+
+    // Click the download button, causing a download, and then remove it
+    createEl.click();
+    createEl.remove();
+});
+
+document.getElementById('btndownload4-boxplot').addEventListener('click', function(e) {
+    // Convert our canvas to a data URL
+    let canvasUrl = document.getElementById('hasil2').toDataURL();
+    // Create an anchor, and set the href value to our data URL
+    const createEl = document.createElement('a');
+    createEl.href = canvasUrl;
+
+    // This is the name of our downloaded file
+    createEl.download = "hasil-sp-riset-4-boxplot-Sebaran Pendapatan Usaha";
+
+    // Click the download button, causing a download, and then remove it
+    createEl.click();
+    createEl.remove();
+    
+});
+document.getElementById('btndownload4-scatter').addEventListener('click', function(e) {
+    // Convert our canvas to a data URL
+    let canvasUrl = document.getElementById('hasil4').toDataURL();
+    // Create an anchor, and set the href value to our data URL
+    const createEl = document.createElement('a');
+    createEl.href = canvasUrl;
+
+    // This is the name of our downloaded file
+    createEl.download = "hasil-sp-riset-4-scatter-Umur dan Pendapatan";
+
+    // Click the download button, causing a download, and then remove it
+    createEl.click();
+    createEl.remove();
+});
+
+document.getElementById('btndownload4-barplot').addEventListener('click', function(e) {
+    // Convert our canvas to a data URL
+    let canvasUrl = document.getElementById('hasil5').toDataURL();
+    // Create an anchor, and set the href value to our data URL
+    const createEl = document.createElement('a');
+    createEl.href = canvasUrl;
+
+    // This is the name of our downloaded file
+    createEl.download = "hasil-sp-riset-4-barplot-Penggunaan TIK";
+
+    // Click the download button, causing a download, and then remove it
+    createEl.click();
+    createEl.remove();
+});
+
+document.getElementById('btndownload4-heatmap').addEventListener('click', function(e) {
+    // Convert our canvas to a data URL
+    let canvasUrl = document.getElementById('hasil5').toDataURL();
     // Create an anchor, and set the href value to our data URL
     const createEl = document.createElement('a');
     createEl.href = canvasUrl;
@@ -15,24 +75,9 @@ document.getElementById('btndownload').addEventListener('click', function(e) {
     createEl.remove();
 });
 
-document.getElementById('btndownload2').addEventListener('click', function(e) {
+document.getElementById('btndownload4-peta').addEventListener('click', function(e) {
     // Convert our canvas to a data URL
-    let canvasUrl = document.getElementById('j_doughnut').toDataURL();
-    // Create an anchor, and set the href value to our data URL
-    const createEl = document.createElement('a');
-    createEl.href = canvasUrl;
-
-    // This is the name of our downloaded file
-    createEl.download = "hasil-sp";
-
-    // Click the download button, causing a download, and then remove it
-    createEl.click();
-    createEl.remove();
-});
-
-document.getElementById('btndownload3').addEventListener('click', function(e) {
-    // Convert our canvas to a data URL
-    let canvasUrl = document.getElementById('ji_batang').toDataURL();
+    let canvasUrl = document.getElementById('map').toDataURL();
     // Create an anchor, and set the href value to our data URL
     const createEl = document.createElement('a');
     createEl.href = canvasUrl;
