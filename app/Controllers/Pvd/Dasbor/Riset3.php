205713b59728efbd592b6e1cb8f83098ce5503ad
I Putu Agus Wahyu Dupayana <75474419+putuwahyu29@users.noreply.github.com>
2023-01-20 12:49:00 +0700

    M Controller : Tambah Controller

---
+++ app/Controllers/Pvd/Dasbor/Riset3.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers\Pvd\Dasbor;
+
+use App\Controllers\BaseController;
+
+class Riset3 extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+}
