d04d47871c58b893c20334655df274b88087eabb
NickoRagasukma <gabrielprimaditya@gmail.com>
2023-01-20 20:07:14 +0700

    M View : Tambah tabel, datatable belum terbaca

--- app/Views/pvd/pages/dasbor/riset2/menu3/submenu3.php
+++ app/Views/pvd/pages/dasbor/riset2/menu3/submenu3.php
@@ -2,7 +2,129 @@
 
 <?= $this->section('content'); ?>
 
-<h1>Menu 3 submenu 3</h1>
+<section id="portfolio" class="portfolio section-bg">
+    <div class="container" data-aos="fade-up">
+        <header class="section-header">
+            <p>Pilot Survei Wisatawan Nusantara</p>
+        </header>
+        
+        <hr class="hrjudul">
+
+        <!-- <div class="row">
+        <div class="col-lg-12 d-flex justify-content-center">
+            <ul id="portfolio-flters">
+            <li data-filter="*" class="filter-active">Semua</li>
+            <li data-filter=".filter-riset2-hasil1">Hasil 1</li>
+            <li data-filter=".filter-riset2-hasil2">Hasil 2</li>
+            <li data-filter=".filter-riset2-hasil3">Hasil 3</li>
+            <li data-filter=".filter-riset2-tabel">Tabel dummy</li>
+            </ul>
+        </div>
+        </div> -->
+    </div>
+</section>
+
+<table id="example" class="table table-striped table-bordered" style="width:100%">
+    <thead>
+        <tr>
+            <th>Name</th>
+            <th>Position</th>
+            <th>Office</th>
+            <th>Age</th>
+            <th>Start date</th>
+            <th>Salary</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Tiger Nixon</td>
+            <td>System Architect</td>
+            <td>Edinburgh</td>
+            <td>61</td>
+            <td>2011-04-25</td>
+            <td>$320,800</td>
+        </tr>
+        <tr>
+            <td>Garrett Winters</td>
+            <td>Accountant</td>
+            <td>Tokyo</td>
+            <td>63</td>
+            <td>2011-07-25</td>
+            <td>$170,750</td>
+        </tr>
+        <tr>
+            <td>Ashton Cox</td>
+            <td>Junior Technical Author</td>
+            <td>San Francisco</td>
+            <td>66</td>
+            <td>2009-01-12</td>
+            <td>$86,000</td>
+        </tr>
+        <tr>
+            <td>Cedric Kelly</td>
+            <td>Senior Javascript Developer</td>
+            <td>Edinburgh</td>
+            <td>22</td>
+            <td>2012-03-29</td>
+            <td>$433,060</td>
+        </tr>
+        <tr>
+            <td>Airi Satou</td>
+            <td>Accountant</td>
+            <td>Tokyo</td>
+            <td>33</td>
+            <td>2008-11-28</td>
+            <td>$162,700</td>
+        </tr>
+        <tr>
+            <td>Brielle Williamson</td>
+            <td>Integration Specialist</td>
+            <td>New York</td>
+            <td>61</td>
+            <td>2012-12-02</td>
+            <td>$372,000</td>
+        </tr>
+        <tr>
+            <td>Herrod Chandler</td>
+            <td>Sales Assistant</td>
+            <td>San Francisco</td>
+            <td>59</td>
+            <td>2012-08-06</td>
+            <td>$137,500</td>
+        </tr>
+        <tr>
+            <td>Rhona Davidson</td>
+            <td>Integration Specialist</td>
+            <td>Tokyo</td>
+            <td>55</td>
+            <td>2010-10-14</td>
+            <td>$327,900</td>
+        </tr>
+        <tr>
+            <td>Colleen Hurst</td>
+            <td>Javascript Developer</td>
+            <td>San Francisco</td>
+            <td>39</td>
+            <td>2009-09-15</td>
+            <td>$205,500</td>
+        </tr>
+        <tr>
+            <td>Donna Snider</td>
+            <td>Customer Support</td>
+            <td>New York</td>
+            <td>27</td>
+            <td>2011-01-25</td>
+            <td>$112,000</td>
+        </tr>
+    </tbody>
+</table>
+
+
+<script>
+    $(document).ready(function () {
+        $('#example').DataTable();
+    });
+</script>
 
 <?= $this->endSection(); ?>
 
