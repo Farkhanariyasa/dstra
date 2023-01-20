35afb6072d975de63d076b6058d08026fd1fb31a
imams <222011830@gmail.com>
2023-01-20 22:04:33 +0700

    M View : melengkapi fungsi

--- public/pvd/js/collapse.js
+++ public/pvd/js/collapse.js
@@ -1,14 +1,23 @@
 // jika tekan Tampilkan Semua
 function ubahKelas1(id){
-    if(id="latarBelakangAtas"){
+    if(id=="latarBelakangAtas"){
         document.getElementById("latarBelakangAtas").innerHTML = "";
+    }
+
+    if(id=="metodologiAtas"){
+        document.getElementById("metodologiAtas").innerHTML = "";
     }
 }
 
 // jika tekan Tampilkan Sebagian
 function ubahKelas2(id){
-    if(id="latarBelakangBawah"){
+    if(id=="latarBelakangBawah"){
         document.getElementById("latarBelakangAtas").innerHTML = "[<strong><i>Tampilkan Semua</i></strong>]";
         document.getElementById("tombol").scrollIntoView();
+    }
+
+    if(id=="metodologiBawah"){
+        document.getElementById("metodologiAtas").innerHTML = "[<strong><i>Tampilkan Semua</i></strong>]";
+        document.getElementById("goToMetodologi").scrollIntoView();
     }
 }
