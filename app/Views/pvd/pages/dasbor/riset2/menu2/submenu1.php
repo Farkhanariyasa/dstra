610d7ec772d0855b3ccfc308954c4e9e8b5f5f33
imams <222011830@gmail.com>
2023-01-21 05:47:04 +0700

    M View :  kecilin heading riset 2

--- app/Views/pvd/pages/dasbor/riset2/menu2/submenu1.php
+++ app/Views/pvd/pages/dasbor/riset2/menu2/submenu1.php
@@ -4,7 +4,7 @@
 
 <div class="container px-0">
       <header class="section-header">
-        <h4 style="color: #4b395f;"><strong>Pilot Survei Wisatawan Nusantara</strong></h4>
+        <h6 style="color: #4b395f;"><strong>Pilot Survei Wisatawan Nusantara</strong></h6>
       </header>
     <hr class="hrjudul">
 
