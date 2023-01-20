62a1ba0ea9695e7f44d6a83d93754e69433d3477
Thomson <222011365@stis.ac.id>
2023-01-20 11:03:21 +0700

    A Assets: map lokus sby

---
+++ public/pvd/js/riset1/hasilSPRiset1_peta.js
@@ -0,0 +1,14 @@
+
+var map = L.map('map').setView([-7.24917,112.75083], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+var map = L.map('map2').setView([-7.24917,112.75083], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
