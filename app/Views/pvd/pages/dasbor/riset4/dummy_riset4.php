80cb306c1d42da5536782a1a5e087f66a317be7c
stevenfss <221910702@stis.ac.id>
2023-01-20 13:40:16 +0700

    M View: recommit dummy_riset4.php

--- app/Views/pvd/pages/dasbor/riset4/dummy_riset4.php
+++ app/Views/pvd/pages/dasbor/riset4/dummy_riset4.php
@@ -1,4 +1,4 @@
-
+<!-- Riset 4 Hasil DOughnut -->
 <div class="portfolio-item filter-riset4-hasil1">
     <div class="row">
         <div class="col-lg-12 grid-margin stretch-card">
@@ -8,9 +8,9 @@
                     <div class="selectBox mt-2 me-2">
                         <form action="">
                             <select class="dropdownunduh1" id="pieChart">
-                                <option value="uu">uu</option>
-                                <option value="b401a">b401a</option>
-                                <option value="b407a">b407a</option>
+                                <option value="uu">Unit Usaha</option>
+                                <option value="b401a">Adanya Gadget</option>
+                                <option value="b407a">Pemanfaatan TIK</option>
                             </select>
                         </form>
                     </div>
@@ -33,7 +33,7 @@
 
 
 
-<!-- Riset 4 Hasil 2 -->
+<!-- Riset 4 Hasil Boxplot -->
 <body onresize="responsivefonts()">   
     <div class="portfolio-item filter-riset4-hasil2">
         <div class="col-12 grid-margin stretch-card">
@@ -55,52 +55,7 @@
     </div>
 </body>
 
-
-<!-- Riset 4 Hasil 3 (pie chart kedua) -->
-<!-- <body onresize="responsivefonts()">   
-    <div class="portfolio-item filter-riset4-hasil1">
-        <div class="col-12 grid-margin stretch-card">
-            <div class="card shadow">
-                <div class="card-body">
-                    <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center ">
-                        <div class="me-1 mt-1 justify-content-end align-item-end">
-                            <button type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                                <i class="fa-solid fa-download"></i>
-                            </button>
-                        </div>
-                    </div>
-                    <div style=" height:500px;">
-                        <canvas id="hasil3"></canvas>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</body> -->
-
-<!-- Riset 4 Hasil ? -->
-<!-- <body onresize="responsivefonts()">   
-    <div class="portfolio-item filter-riset4-hasil3">
-        <div class="col-12 grid-margin stretch-card">
-            <div class="card shadow">
-                <div class="card-body">
-                    <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center ">
-                        <div class="me-1 mt-1 justify-content-end align-item-end">
-                            <button type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                                <i class="fa-solid fa-download"></i>
-                            </button>
-                        </div>
-                    </div>
-                    <div style=" height:500px;">
-                        <canvas id="hasil3"></canvas>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</body> -->
-
-<!-- Riset 4 Hasil 4 -->
+<!-- Riset 4 Hasil ScatterPlot -->
 <body onresize="responsivefonts()">   
     <div class="portfolio-item filter-riset4-hasil4">
         <div class="col-12 grid-margin stretch-card">
@@ -122,17 +77,21 @@
     </div>
 </body>
 
-<!-- Riset 4 Hasil 5 -->
-<div class="portfolio-item filter-riset4-hasil3">
-<div class="container mt-5">
-        <!-- <button onclick="showmsg();" class="tombol btn-for" >Download image</button> -->
-        <div class="card d-flex justify-content-center flex-column align-item-center flex-md primary pb-5 pt-5 ">
-            <div  class="d-flex flex-row align-item-center justify-content-center">
-                <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center">
-                    <div class="me-1 mt-1 justify-content-end align-item-end">
-                        <button type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                            <i class="fa-solid fa-download"></i>
-                        </button>
+<!-- Riset 4 Hasil Bar Plot -->
+<body onresize="responsivefonts()">   
+    <div class="portfolio-item filter-riset4-hasil5">
+        <div class="col-12 grid-margin stretch-card">
+            <div class="card shadow">
+                <div class="card-body">
+                    <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center ">
+                        <div class="me-1 mt-1 justify-content-end align-item-end">
+                            <button type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                                <i class="fa-solid fa-download"></i>
+                            </button>
+                        </div>
+                    </div>
+                    <div style=" height:500px;">
+                        <canvas id="hasil5"></canvas>
                     </div>
                 </div>
             </div>
@@ -140,38 +99,37 @@
     </div>
 </body>
 
-<!-- Riset 4 Hasil 6 -->
-<div class="portfolio-item filter-riset4-hasil2">
-    <div class="container mt-5">
-        <div class="card d-flex justify-content-center align-item-center flex-md-row primary">
-            <div class="content pl-4 mt-2" style="width: 95%; height:80vh;">
-                <div class="position-relative">
-                    <div class="position-absolute top-0 end-0">
-                        <!-- tambah modal -->
-                        <!-- <button class="tombol btn-for" id="btndownload3"><i class="fa-solid fa-download"></i></button> -->
-                        <button type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal1-3">
-                            <i class="fa-solid fa-download"></i>
-                        </button>
+<!-- Riset 4 Hasil Heat Map -->
+<body onresize="responsivefonts()">   
+    <div class="portfolio-item filter-riset4-hasil6">
+        <div class="col-12 grid-margin stretch-card">
+            <div class="card shadow">
+                <div class="card-body">
+                    <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center ">
+                        <div class="me-1 mt-1 justify-content-end align-item-end">
+                            <button type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                                <i class="fa-solid fa-download"></i>
+                            </button>
+                        </div>
                     </div>
                     <div style=" height:500px;">
                         <figure class="heatmap-figure">
                             <div id="container"></div>
                         </figure>
                     </div>
-
                 </div>
             </div>
         </div>
     </div>
 </body>
 
-<!-- Riset 4 Hasil 7 -->
+<!-- Riset 4 Hasil Peta Batu -->
 <body onresize="responsivefonts()">   
     <div class="portfolio-item filter-riset4-hasil7">
         <div class="col-12 grid-margin stretch-card">
             <div class="card shadow">
                 <div class="card-body">
-                <p class="card-text text-center">Kota Surabaya</p>
+                <p class="card-text text-center">Kota Batu</p>
                     <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center ">
                         <div class="me-1 mt-1 justify-content-end align-item-end">
                             <button type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal">
