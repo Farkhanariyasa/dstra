f159c878272160b9b0b72a22013844d72d08a951
LuthfilKA <222011321@stis.ac.id>
2023-01-21 06:57:05 +0700

    M Assets : tombol unduh

--- public/pvd/js/riset3/btn_Download.js
+++ public/pvd/js/riset3/btn_Download.js
@@ -8,7 +8,7 @@ document.getElementById('btndownload').addEventListener('click', function(e) {
     createEl.href = canvasUrl;
 
     // This is the name of our downloaded file
-    createEl.download = "hasil-sp";
+    createEl.download = "hasil-sp-riset-3-piechart";
 
     // Click the download button, causing a download, and then remove it
     createEl.click();
@@ -23,7 +23,7 @@ document.getElementById('btndownload2').addEventListener('click', function(e) {
     createEl.href = canvasUrl;
 
     // This is the name of our downloaded file
-    createEl.download = "hasil-sp";
+    createEl.download = "hasil-sp-riset-3-doughnut";
 
     // Click the download button, causing a download, and then remove it
     createEl.click();
@@ -38,37 +38,7 @@ document.getElementById('btndownload3').addEventListener('click', function(e) {
     createEl.href = canvasUrl;
 
     // This is the name of our downloaded file
-    createEl.download = "hasil-sp";
-
-    // Click the download button, causing a download, and then remove it
-    createEl.click();
-    createEl.remove();
-});
-
-document.getElementById('btndownload4').addEventListener('click', function(e) {
-    // Convert our canvas to a data URL
-    let canvasUrl = document.getElementById('stacked_barchart').toDataURL();
-    // Create an anchor, and set the href value to our data URL
-    const createEl = document.createElement('a');
-    createEl.href = canvasUrl;
-
-    // This is the name of our downloaded file
-    createEl.download = "hasil-sp";
-
-    // Click the download button, causing a download, and then remove it
-    createEl.click();
-    createEl.remove();
-});
-
-document.getElementById('btndownload5').addEventListener('click', function(e) {
-    // Convert our canvas to a data URL
-    let canvasUrl = document.getElementById('radar').toDataURL();
-    // Create an anchor, and set the href value to our data URL
-    const createEl = document.createElement('a');
-    createEl.href = canvasUrl;
-
-    // This is the name of our downloaded file
-    createEl.download = "hasil-sp";
+    createEl.download = "hasil-sp-riset-3-barplot";
 
     // Click the download button, causing a download, and then remove it
     createEl.click();
