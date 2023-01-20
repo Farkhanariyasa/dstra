fc556e592d2ceae9f83d16dfcf54f1fbfe7b157c
LuthfilKA <222011321@stis.ac.id>
2023-01-20 08:53:45 +0700

    M Assets : tambahin js untuk download chart

--- public/pvd/js/riset3/btn_Download.js
+++ public/pvd/js/riset3/btn_Download.js
@@ -44,3 +44,33 @@ document.getElementById('btndownload3').addEventListener('click', function(e) {
     createEl.click();
     createEl.remove();
 });
+
+document.getElementById('btndownload4').addEventListener('click', function(e) {
+    // Convert our canvas to a data URL
+    let canvasUrl = document.getElementById('stacked_barchart').toDataURL();
+    // Create an anchor, and set the href value to our data URL
+    const createEl = document.createElement('a');
+    createEl.href = canvasUrl;
+
+    // This is the name of our downloaded file
+    createEl.download = "hasil-sp";
+
+    // Click the download button, causing a download, and then remove it
+    createEl.click();
+    createEl.remove();
+});
+
+document.getElementById('btndownload5').addEventListener('click', function(e) {
+    // Convert our canvas to a data URL
+    let canvasUrl = document.getElementById('radar').toDataURL();
+    // Create an anchor, and set the href value to our data URL
+    const createEl = document.createElement('a');
+    createEl.href = canvasUrl;
+
+    // This is the name of our downloaded file
+    createEl.download = "hasil-sp";
+
+    // Click the download button, causing a download, and then remove it
+    createEl.click();
+    createEl.remove();
+});
