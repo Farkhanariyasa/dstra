2f8ece4e46c15dad125f5a2c925cb8d79dacaf4a
LuthfilKA <222011321@stis.ac.id>
2023-01-20 15:42:42 +0700

    M Views : Merapihkan modal pada riset 4 sementara

--- app/Views/pvd/pages/dasbor/riset4/modal_riset4.php
+++ app/Views/pvd/pages/dasbor/riset4/modal_riset4.php
@@ -1,72 +1,202 @@
 <!-- Modal Riset 4  -->
 <!-- 1 -->
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="exampleModal4-doughnut" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Unduh Hasil PKL</h5>
+                <h5 class="modal-title" id="exampleModalLabel">Unduh Hasil PKL donut</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <div class="modal-body">
-                <form>
-                    <div class="mb-3">
-                        <label for="recipient-name" class="col-form-label">Inputkan Email Anda</label>
-                        <input type="email" class="form-control" name="email" id="recipient-name" placeholder="Email Anda">
+            <form name="unduhVisualisasi" action="<?= base_url(); ?>/hasil-pkl/riset4/unduh" method="POST">
+                <div class="modal-body">
+                    <div class="mb-1">
+                        <label for="recipient-name" class="col-form-label">Nama <span class="text-danger">*</span></label>
+                        <input type="text" class="form-control" name="nama" id="recipient-name" placeholder="Nama Anda" required>
                     </div>
-                </form>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="button"id="btndownload3" class="tombol btn-for" data-bs-dismiss="modal" aria-label="Close">Konfirmasi</button>
-            </div>
+                    <div class="mb-1">
+                        <label for="recipient-name" class="col-form-label" name="emailunduh">Email <span class="text-danger">*</span></label>
+                        <input type="email" class="form-control" name="email" id="recipient-name" placeholder="Email Anda" required>
+                    </div>
+                    <div class="mb-1">
+                        <label for="recipient-name" class="col-form-label">Instansi <span class="text-danger">*</span></label>
+                        <input type="text" class="form-control" name="instansi" id="recipient-name" placeholder="Instansi Asal" required>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="tombol btn-secondary" data-bs-dismiss="modal">Kembali</button>
+                    <button type="submit" id="btndownload4-doughnut" class="tombol btn-for" data-bs-dismiss="modal">Konfirmasi</button>
+                </div>
+            </form>
         </div>
     </div>
 </div>   
+
 
 <!-- 2 -->
-<div class="modal fade" id="exampleModal2" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Unduh Hasil PKL</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <form>
-                    <div class="mb-3">
-                        <label for="recipient-name" class="col-form-label">Inputkan Email Anda</label>
-                        <input type="email" class="form-control" name="email" id="recipient-name" placeholder="Email Anda">
-                    </div>
-                </form>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="button"id="btndownload" class="tombol btn-for" data-bs-dismiss="modal">Konfirmasi</button>
-            </div>
-        </div>
-    </div>
-</div> 
 
-<!-- 3 -->
-<div class="modal fade" id="exampleModal3" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="exampleModal4-boxplot" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="exampleModalLabel">Unduh Hasil PKL</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <div class="modal-body">
-                <form>
-                    <div class="mb-3">
-                        <label for="recipient-name" class="col-form-label">Inputkan Email Anda</label>
-                        <input type="email" class="form-control" name="email" id="recipient-name" placeholder="Email Anda">
+            <form name="unduhVisualisasi" action="<?= base_url(); ?>/hasil-pkl/riset4/unduh" method="POST">
+                <div class="modal-body">
+                    <div class="mb-1">
+                        <label for="recipient-name" class="col-form-label">Nama <span class="text-danger">*</span></label>
+                        <input type="text" class="form-control" name="nama" id="recipient-name" placeholder="Nama Anda" required>
                     </div>
-                </form>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="button"id="btndownload2" class="tombol btn-for" data-bs-dismiss="modal">Konfirmasi</button>
-            </div>
+                    <div class="mb-1">
+                        <label for="recipient-name" class="col-form-label" name="emailunduh">Email <span class="text-danger">*</span></label>
+                        <input type="email" class="form-control" name="email" id="recipient-name" placeholder="Email Anda" required>
+                    </div>
+                    <div class="mb-1">
+                        <label for="recipient-name" class="col-form-label">Instansi <span class="text-danger">*</span></label>
+                        <input type="text" class="form-control" name="instansi" id="recipient-name" placeholder="Instansi Asal" required>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="tombol btn-secondary" data-bs-dismiss="modal">Kembali</button>
+                    <button type="submit" id="btndownload4-boxplot" class="tombol btn-for" data-bs-dismiss="modal">Konfirmasi</button>
+                </div>
+            </form>
         </div>
     </div>
 </div>   
+
+
+
+<!-- 3 -->
+<div class="modal fade" id="exampleModal4-scatter" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Unduh Hasil PKL</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <form name="unduhVisualisasi" action="<?= base_url(); ?>/hasil-pkl/riset4/unduh" method="POST">
+                <div class="modal-body">
+                    <div class="mb-1">
+                        <label for="recipient-name" class="col-form-label">Nama <span class="text-danger">*</span></label>
+                        <input type="text" class="form-control" name="nama" id="recipient-name" placeholder="Nama Anda" required>
+                    </div>
+                    <div class="mb-1">
+                        <label for="recipient-name" class="col-form-label" name="emailunduh">Email <span class="text-danger">*</span></label>
+                        <input type="email" class="form-control" name="email" id="recipient-name" placeholder="Email Anda" required>
+                    </div>
+                    <div class="mb-1">
+                        <label for="recipient-name" class="col-form-label">Instansi <span class="text-danger">*</span></label>
+                        <input type="text" class="form-control" name="instansi" id="recipient-name" placeholder="Instansi Asal" required>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="tombol btn-secondary" data-bs-dismiss="modal">Kembali</button>
+                    <button type="submit" id="btndownload4-scatter" class="tombol btn-for" data-bs-dismiss="modal">Konfirmasi</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>   
+
+<!-- 4 -->
+
+<div class="modal fade" id="exampleModal4-barplot" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Unduh Hasil PKL</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <form name="unduhVisualisasi" action="<?= base_url(); ?>/hasil-pkl/riset4/unduh" method="POST">
+                <div class="modal-body">
+                    <div class="mb-1">
+                        <label for="recipient-name" class="col-form-label">Nama <span class="text-danger">*</span></label>
+                        <input type="text" class="form-control" name="nama" id="recipient-name" placeholder="Nama Anda" required>
+                    </div>
+                    <div class="mb-1">
+                        <label for="recipient-name" class="col-form-label" name="emailunduh">Email <span class="text-danger">*</span></label>
+                        <input type="email" class="form-control" name="email" id="recipient-name" placeholder="Email Anda" required>
+                    </div>
+                    <div class="mb-1">
+                        <label for="recipient-name" class="col-form-label">Instansi <span class="text-danger">*</span></label>
+                        <input type="text" class="form-control" name="instansi" id="recipient-name" placeholder="Instansi Asal" required>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="tombol btn-secondary" data-bs-dismiss="modal">Kembali</button>
+                    <button type="submit" id="btndownload4-barplot" class="tombol btn-for" data-bs-dismiss="modal">Konfirmasi</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>   
+
+
+<!-- 5 -->
+<div class="modal fade" id="exampleModal4-heatmap" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Unduh Hasil PKL</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <form name="unduhVisualisasi" action="<?= base_url(); ?>/hasil-pkl/riset4/unduh" method="POST">
+                <div class="modal-body">
+                    <div class="mb-1">
+                        <label for="recipient-name" class="col-form-label">Nama <span class="text-danger">*</span></label>
+                        <input type="text" class="form-control" name="nama" id="recipient-name" placeholder="Nama Anda" required>
+                    </div>
+                    <div class="mb-1">
+                        <label for="recipient-name" class="col-form-label" name="emailunduh">Email <span class="text-danger">*</span></label>
+                        <input type="email" class="form-control" name="email" id="recipient-name" placeholder="Email Anda" required>
+                    </div>
+                    <div class="mb-1">
+                        <label for="recipient-name" class="col-form-label">Instansi <span class="text-danger">*</span></label>
+                        <input type="text" class="form-control" name="instansi" id="recipient-name" placeholder="Instansi Asal" required>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="tombol btn-secondary" data-bs-dismiss="modal">Kembali</button>
+                    <button type="submit" id="btndownload4-barplot" class="tombol btn-for" data-bs-dismiss="modal">Konfirmasi</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>   
+
+
+
+<!-- 6 -->
+<div class="modal fade" id="exampleModal4-peta" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Unduh Hasil PKL</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <form name="unduhVisualisasi" action="<?= base_url(); ?>/hasil-pkl/riset4/unduh" method="POST">
+                <div class="modal-body">
+                    <div class="mb-1">
+                        <label for="recipient-name" class="col-form-label">Nama <span class="text-danger">*</span></label>
+                        <input type="text" class="form-control" name="nama" id="recipient-name" placeholder="Nama Anda" required>
+                    </div>
+                    <div class="mb-1">
+                        <label for="recipient-name" class="col-form-label" name="emailunduh">Email <span class="text-danger">*</span></label>
+                        <input type="email" class="form-control" name="email" id="recipient-name" placeholder="Email Anda" required>
+                    </div>
+                    <div class="mb-1">
+                        <label for="recipient-name" class="col-form-label">Instansi <span class="text-danger">*</span></label>
+                        <input type="text" class="form-control" name="instansi" id="recipient-name" placeholder="Instansi Asal" required>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="tombol btn-secondary" data-bs-dismiss="modal">Kembali</button>
+                    <button type="submit" id="btndownload4-doughnut" class="tombol btn-for" data-bs-dismiss="modal">Konfirmasi</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>   
+
+
