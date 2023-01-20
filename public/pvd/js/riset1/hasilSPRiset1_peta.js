39445f8ea45cf8d0498592c3549d123307ca6a17
Thomson <222011365@stis.ac.id>
2023-01-20 13:33:53 +0700

    M Assets: map kota malang

--- public/pvd/js/riset1/hasilSPRiset1_peta.js
+++ public/pvd/js/riset1/hasilSPRiset1_peta.js
@@ -6,7 +6,7 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-var map = L.map('map2').setView([-7.24917,112.75083], 13);
+var map = L.map('map2').setView([-7.9797, 112.6304], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
