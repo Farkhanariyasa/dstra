ac050d9e1d230de55de470f26a33847fcaaaa224
imams <222011830@gmail.com>
2023-01-20 18:33:09 +0700

    M View : revisi heading riset 2

--- app/Views/pvd/pages/dasbor/riset2/menu2/submenu1.php
+++ app/Views/pvd/pages/dasbor/riset2/menu2/submenu1.php
@@ -4,20 +4,20 @@
 
 <div class="container px-0">
       <header class="section-header">
-        <h1 style="color: #4b395f;"><strong>Pilot Survei Wisatawan Nusantara</strong></h1>
+        <h4 style="color: #4b395f;"><strong>Pilot Survei Wisatawan Nusantara</strong></h4>
       </header>
     <hr class="hrjudul">
 
     <div class="accordion" id="accordionExample">
       <div class="accordion-item my-3">
-        <h2 class="accordion-header" id="headingOne">
+        <h3 class="accordion-header" id="headingOne">
           <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
             Gambaran Umum
           </button>
-        </h2>
+        </h3>
         <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
           <div class="accordion-body">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed viverra neque ligula, at vehicula orci viverra at. 
+            <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed viverra neque ligula, at vehicula orci viverra at. 
               Sed at lorem at tortor dapibus ultricies ut sed ipsum. Maecenas molestie elementum justo, in convallis urna eleifend et. 
               Vestibulum mollis, tortor ut luctus scelerisque, eros est ullamcorper leo, quis tempor urna arcu sit amet tortor. Sed rhoncus 
               odio non ipsum tempus porttitor. Nulla sagittis tincidunt aliquam. Cras commodo egestas commodo. Praesent consectetur mauris sed 
@@ -30,14 +30,14 @@
         </div>
       </div>
       <div class="accordion-item my-3">
-        <h2 class="accordion-header" id="headingTwo">
+        <h3 class="accordion-header" id="headingTwo">
           <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
             Tujuan 1
           </button>
         </h2>
         <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
           <div class="accordion-body">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed viverra neque ligula, at vehicula orci viverra at. 
+            <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed viverra neque ligula, at vehicula orci viverra at. 
               Sed at lorem at tortor dapibus ultricies ut sed ipsum. Maecenas molestie elementum justo, in convallis urna eleifend et. 
               Vestibulum mollis, tortor ut luctus scelerisque, eros est ullamcorper leo, quis tempor urna arcu sit amet tortor. Sed rhoncus 
               odio non ipsum tempus porttitor. Nulla sagittis tincidunt aliquam. Cras commodo egestas commodo. Praesent consectetur mauris sed 
@@ -50,14 +50,14 @@
         </div>
       </div>
       <div class="accordion-item my-3">
-        <h2 class="accordion-header" id="headingThree">
+        <h3 class="accordion-header" id="headingThree">
           <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
             Tujuan 2
           </button>
         </h2>
         <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
           <div class="accordion-body">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed viverra neque ligula, at vehicula orci viverra at. 
+            <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed viverra neque ligula, at vehicula orci viverra at. 
               Sed at lorem at tortor dapibus ultricies ut sed ipsum. Maecenas molestie elementum justo, in convallis urna eleifend et. 
               Vestibulum mollis, tortor ut luctus scelerisque, eros est ullamcorper leo, quis tempor urna arcu sit amet tortor. Sed rhoncus 
               odio non ipsum tempus porttitor. Nulla sagittis tincidunt aliquam. Cras commodo egestas commodo. Praesent consectetur mauris sed 
@@ -70,14 +70,14 @@
         </div>
       </div>
       <div class="accordion-item my-3">
-        <h2 class="accordion-header" id="headingFour">
+        <h3 class="accordion-header" id="headingFour">
           <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
             Tujuan 3
           </button>
         </h2>
         <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#accordionExample">
           <div class="accordion-body">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed viverra neque ligula, at vehicula orci viverra at. 
+            <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed viverra neque ligula, at vehicula orci viverra at. 
               Sed at lorem at tortor dapibus ultricies ut sed ipsum. Maecenas molestie elementum justo, in convallis urna eleifend et. 
               Vestibulum mollis, tortor ut luctus scelerisque, eros est ullamcorper leo, quis tempor urna arcu sit amet tortor. Sed rhoncus 
               odio non ipsum tempus porttitor. Nulla sagittis tincidunt aliquam. Cras commodo egestas commodo. Praesent consectetur mauris sed 
@@ -90,14 +90,14 @@
         </div>
       </div>
       <div class="accordion-item my-3">
-        <h2 class="accordion-header" id="headingFive">
+        <h3 class="accordion-header" id="headingFive">
           <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
             Tujuan 4
           </button>
         </h2>
         <div id="collapseFive" class="accordion-collapse collapse" aria-labelledby="headingFive" data-bs-parent="#accordionExample">
           <div class="accordion-body">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed viverra neque ligula, at vehicula orci viverra at. 
+            <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed viverra neque ligula, at vehicula orci viverra at. 
               Sed at lorem at tortor dapibus ultricies ut sed ipsum. Maecenas molestie elementum justo, in convallis urna eleifend et. 
               Vestibulum mollis, tortor ut luctus scelerisque, eros est ullamcorper leo, quis tempor urna arcu sit amet tortor. Sed rhoncus 
               odio non ipsum tempus porttitor. Nulla sagittis tincidunt aliquam. Cras commodo egestas commodo. Praesent consectetur mauris sed 
