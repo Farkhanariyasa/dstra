2e538ea68db6a67674387bcd667cde9ca1e01347
Thomson <222011365@stis.ac.id>
2023-01-21 21:39:45 +0700

    M Views: nambah map tpi masi belom diedit

--- app/Views/pvd/pages/dasbor/riset1/menu3/submenu4.php
+++ app/Views/pvd/pages/dasbor/riset1/menu3/submenu4.php
@@ -2,7 +2,27 @@
 
 <?= $this->section('content'); ?>
 
-<h1>Menu 3 submenu 4</h1>
+<div class="container mt-5">
+    <header class="section-header">
+        <p>Kajian Pemanfaatan MPD dalam Menunjang Statistik Pariwisata</p>
+    </header>
+    <hr class="hrjudul">
+    <!-- <button onclick="showmsg();" class="tombol btn-for" >Download image</button> -->
+    <div class="card d-flex justify-content-center flex-column align-item-center flex-md primary pb-5 pt-5 ">
+        <p class="card-text text-center">Kota Surabaya</p>
+        <div class="d-flex flex-row align-item-center justify-content-center">
+            <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center">
+                <div class="me-1 mt-1 justify-content-end align-item-end">
+                    <button type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                        <i class="fa-solid fa-download"></i>
+                    </button>
+                </div>
+            </div>
+            <div id="map" style="width: 95%; height:80vh;"></div>
+        </div>
+    </div>
+</div>
 
+<script src="<?= base_url('pvd/js/riset1/btn-Download.js') ?>"></script>
+<script src="<?= base_url('pvd/js/riset1/hasilSPRiset1_peta.js') ?>"></script>
 <?= $this->endSection(); ?>
-
