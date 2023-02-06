2c50cc0c7402f44d0dae5d21c7501902db9f11c8
stevenfss <221910702@stis.ac.id>
2023-02-06 21:59:00 +0700

    M View : Menambahkan deskripsi dan tujuan riset 4

--- app/Views/pvd/pages/dasbor/riset4/menu2/submenu1.php
+++ app/Views/pvd/pages/dasbor/riset4/menu2/submenu1.php
@@ -14,19 +14,16 @@
       <div class="accordion-item my-3">
         <h3 class="accordion-header" id="headingOne">
           <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-            Gambaran Umum
+            Gambaran Umum Penelitian Riset 4
           </button>
         </h3>
         <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
           <div class="accordion-body">
-            <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed viverra neque ligula, at vehicula orci viverra at. 
-              Sed at lorem at tortor dapibus ultricies ut sed ipsum. Maecenas molestie elementum justo, in convallis urna eleifend et. 
-              Vestibulum mollis, tortor ut luctus scelerisque, eros est ullamcorper leo, quis tempor urna arcu sit amet tortor. Sed rhoncus 
-              odio non ipsum tempus porttitor. Nulla sagittis tincidunt aliquam. Cras commodo egestas commodo. Praesent consectetur mauris sed 
-              ultrices dignissim. Suspendisse mollis magna auctor enim posuere interdum. Phasellus suscipit, dolor vitae mattis consectetur, dui 
-              ex viverra nibh, sit amet elementum ligula lectus et quam. Sed et cursus neque. Morbi maximus in ipsum nec aliquam. Integer porttitor 
-              blandit augue, ut posuere dolor gravida vel. Nulla rutrum eu augue non semper. Proin sit amet dui felis. Sed vestibulum tempor urna, 
-              auctor viverra erat. 
+            <p class="text-justify">Kota Batu memiliki posisi dan peran sebagai Kota Wisata sehingga akan dilihat kesiapannya dalam menerapkan 
+              Smart Tourism Destination dari sisi enterprise atau dalam penelitian ini adalah unit usaha pariwisata.
+              Oleh karena itu, dilakukan analisis mengenai kesiapan pemanfaatan TIK dari sisi unit usaha pariwisata di Kota Batu dalam menerapkan 
+              Smart Tourism Destination. Analisis kesiapan dilakukan melalui Enterprise E-Readiness Assessment Model atau Model Kesiapan Unit Usaha 
+              Pariwisata yang terbentuk dari beberapa dimensi dan akan menghasilkan suatu indeks yang dapat diukur tingkat kesiapannya.
             </p>           
           </div>
         </div>
@@ -39,14 +36,8 @@
         </h3>
         <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
           <div class="accordion-body">
-            <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed viverra neque ligula, at vehicula orci viverra at. 
-              Sed at lorem at tortor dapibus ultricies ut sed ipsum. Maecenas molestie elementum justo, in convallis urna eleifend et. 
-              Vestibulum mollis, tortor ut luctus scelerisque, eros est ullamcorper leo, quis tempor urna arcu sit amet tortor. Sed rhoncus 
-              odio non ipsum tempus porttitor. Nulla sagittis tincidunt aliquam. Cras commodo egestas commodo. Praesent consectetur mauris sed 
-              ultrices dignissim. Suspendisse mollis magna auctor enim posuere interdum. Phasellus suscipit, dolor vitae mattis consectetur, dui 
-              ex viverra nibh, sit amet elementum ligula lectus et quam. Sed et cursus neque. Morbi maximus in ipsum nec aliquam. Integer porttitor 
-              blandit augue, ut posuere dolor gravida vel. Nulla rutrum eu augue non semper. Proin sit amet dui felis. Sed vestibulum tempor urna, 
-              auctor viverra erat. 
+            <p class="text-justify">Mengetahui karakteristik tiga unit usaha pariwisata skala mikro dan kecil di Kota Batu yang meliputi Jasa 
+              Transportasi Wisata, Jasa Makanan dan Minuman, serta Penyediaan Akomodasi dalam menerapkan Smart Tourism Destination. 
             </p> 
           </div>
         </div>
@@ -59,14 +50,8 @@
         </h3>
         <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
           <div class="accordion-body">
-            <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed viverra neque ligula, at vehicula orci viverra at. 
-              Sed at lorem at tortor dapibus ultricies ut sed ipsum. Maecenas molestie elementum justo, in convallis urna eleifend et. 
-              Vestibulum mollis, tortor ut luctus scelerisque, eros est ullamcorper leo, quis tempor urna arcu sit amet tortor. Sed rhoncus 
-              odio non ipsum tempus porttitor. Nulla sagittis tincidunt aliquam. Cras commodo egestas commodo. Praesent consectetur mauris sed 
-              ultrices dignissim. Suspendisse mollis magna auctor enim posuere interdum. Phasellus suscipit, dolor vitae mattis consectetur, dui 
-              ex viverra nibh, sit amet elementum ligula lectus et quam. Sed et cursus neque. Morbi maximus in ipsum nec aliquam. Integer porttitor 
-              blandit augue, ut posuere dolor gravida vel. Nulla rutrum eu augue non semper. Proin sit amet dui felis. Sed vestibulum tempor urna, 
-              auctor viverra erat. 
+            <p class="text-justify">Menganalisis tingkat kesiapan pemanfaatan TIK dari tiga unit usaha pariwisata skala mikro dan kecil di Kota 
+              Batu yang meliputi Jasa Transportasi Wisata, Jasa Makanan dan Minuman, serta Penyediaan Akomodasi dalam menerapkan Smart Tourism Destination 
             </p>           
           </div>
         </div>
@@ -79,14 +64,8 @@
         </h3>
         <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#accordionExample">
           <div class="accordion-body">
-            <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed viverra neque ligula, at vehicula orci viverra at. 
-              Sed at lorem at tortor dapibus ultricies ut sed ipsum. Maecenas molestie elementum justo, in convallis urna eleifend et. 
-              Vestibulum mollis, tortor ut luctus scelerisque, eros est ullamcorper leo, quis tempor urna arcu sit amet tortor. Sed rhoncus 
-              odio non ipsum tempus porttitor. Nulla sagittis tincidunt aliquam. Cras commodo egestas commodo. Praesent consectetur mauris sed 
-              ultrices dignissim. Suspendisse mollis magna auctor enim posuere interdum. Phasellus suscipit, dolor vitae mattis consectetur, dui 
-              ex viverra nibh, sit amet elementum ligula lectus et quam. Sed et cursus neque. Morbi maximus in ipsum nec aliquam. Integer porttitor 
-              blandit augue, ut posuere dolor gravida vel. Nulla rutrum eu augue non semper. Proin sit amet dui felis. Sed vestibulum tempor urna, 
-              auctor viverra erat. 
+            <p class="text-justify">Menganalisis faktor-faktor yang memengaruhi kesiapan unit usaha pariwisata skala mikro dan kecil di Kota Batu 
+              dalam pemanfaatan TIK. 
             </p>           
           </div>
         </div>
@@ -99,14 +78,8 @@
         </h3>
         <div id="collapseFive" class="accordion-collapse collapse" aria-labelledby="headingFive" data-bs-parent="#accordionExample">
           <div class="accordion-body">
-            <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed viverra neque ligula, at vehicula orci viverra at. 
-              Sed at lorem at tortor dapibus ultricies ut sed ipsum. Maecenas molestie elementum justo, in convallis urna eleifend et. 
-              Vestibulum mollis, tortor ut luctus scelerisque, eros est ullamcorper leo, quis tempor urna arcu sit amet tortor. Sed rhoncus 
-              odio non ipsum tempus porttitor. Nulla sagittis tincidunt aliquam. Cras commodo egestas commodo. Praesent consectetur mauris sed 
-              ultrices dignissim. Suspendisse mollis magna auctor enim posuere interdum. Phasellus suscipit, dolor vitae mattis consectetur, dui 
-              ex viverra nibh, sit amet elementum ligula lectus et quam. Sed et cursus neque. Morbi maximus in ipsum nec aliquam. Integer porttitor 
-              blandit augue, ut posuere dolor gravida vel. Nulla rutrum eu augue non semper. Proin sit amet dui felis. Sed vestibulum tempor urna, 
-              auctor viverra erat. 
+            <p class="text-justify">Menganalisis perbedaan rata-rata omzet berdasarkan kategori kesiapan pemanfaatan TIK dari unit usaha 
+              pariwisata skala mikro dan kecil di Kota Batu.
             </p>           
           </div>
         </div>
