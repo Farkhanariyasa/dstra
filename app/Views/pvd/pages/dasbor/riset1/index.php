8d2d32c089dcff03c27d96901550d0c904d2274c
Thomson <222011365@stis.ac.id>
2023-02-07 11:53:06 +0700

    M Views: delete pengulangan

--- app/Views/pvd/pages/dasbor/riset1/index.php
+++ app/Views/pvd/pages/dasbor/riset1/index.php
@@ -5,14 +5,14 @@
 
 
 <section id="portfolio" class="portfolio section-bg">
-  <div class="container" >
-      <header class="section-header">
-        <p>Kajian Pemanfaatan MPD dalam Menunjang Statistik Pariwisata</p>
-      </header>
+  <div class="container">
+    <header class="section-header">
+      <p>Kajian Pemanfaatan MPD dalam Menunjang Statistik Pariwisata</p>
+    </header>
     <hr class="hrjudul">
 
     <div class="row">
-      <div class="col-lg-12 d-flex justify-content-center" >
+      <div class="col-lg-12 d-flex justify-content-center">
         <ul id="portfolio-flters">
           <li data-filter="*" class="filter-active">Semua</li>
           <li data-filter=".filter-riset1-hasil1">Hasil 1</li>
@@ -25,60 +25,46 @@
         </ul>
       </div>
     </div>
-    <div class="row portfolio-container" >
+    <div class="row portfolio-container">
       <?php include('dummy_riset1.php') ?>
     </div>
   </div>
-  
+
 
 </section>
 <script>
-        $(document).ready(function() {
-            $('#example').DataTable({
-                dom: 'Bfrtip',
-                buttons: [
-                    'copyHtml5',
-                    'excelHtml5',
-                    'csvHtml5',
-                    'pdfHtml5'
-                ]
-            });
-        });
-    </script>
-
-<script>
-
-    $(document).ready(function() {
-        $('#example').DataTable( {
-            dom: 'Bfrtip',
-            buttons: [
-                'copyHtml5',
-                'excelHtml5',
-                'csvHtml5',
-                'pdfHtml5'
-            ]
-        } );
-    } );
+  $(document).ready(function() {
+    $('#example').DataTable({
+      dom: 'Bfrtip',
+      buttons: [
+        'copyHtml5',
+        'excelHtml5',
+        'csvHtml5',
+        'pdfHtml5'
+      ]
+    });
+  });
 </script>
+
 
 <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/chartjs-plugin-datalabels/2.2.0/chartjs-plugin-datalabels.min.js" integrity="sha512-JPcRR8yFa8mmCsfrw4TNte1ZvF1e3+1SdGMslZvmrzDYxS69J7J49vkFL8u6u8PlPJK+H3voElBtUCzaXj+6ig==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
 <!-- const for chart -->
 <script>
-    const jumlahanggota = <?= json_encode($ja) ?>;
-    const jeniskelamin = <?= json_encode($jk) ?>;
-    const mpw1 = <?= json_encode($b524) ?>;
-    const mpw2 = <?= json_encode($b525) ?>;
-    const mpw3 = <?= json_encode($b526) ?>;
-    const mpw4 = <?= json_encode($b527) ?>;
-    const mpw5 = <?= json_encode($b528) ?>;
-    const mpw6 = <?= json_encode($b529) ?>;
-    const indosat = <?= json_encode($indosat) ?>;
-    const xl = <?= json_encode($xl) ?>;
-    const tri = <?= json_encode($tri) ?>;
-    const smartfren = <?= json_encode($smartfren) ?>;
-    const lainnya = <?= json_encode($lainnya) ?>;
+  const jumlahanggota = <?= json_encode($ja) ?>;
+  const jeniskelamin = <?= json_encode($jk) ?>;
+  const mpw1 = <?= json_encode($b524) ?>;
+  const mpw2 = <?= json_encode($b525) ?>;
+  const mpw3 = <?= json_encode($b526) ?>;
+  const mpw4 = <?= json_encode($b527) ?>;
+  const mpw5 = <?= json_encode($b528) ?>;
+  const mpw6 = <?= json_encode($b529) ?>;
+  const indosat = <?= json_encode($indosat) ?>;
+  const xl = <?= json_encode($xl) ?>;
+  const tri = <?= json_encode($tri) ?>;
+  const smartfren = <?= json_encode($smartfren) ?>;
+  const lainnya = <?= json_encode($lainnya) ?>;
 </script>
 
 <!-- chart -->
@@ -103,4 +89,3 @@
 </section><!-- End -->
 
 <?= $this->endSection(); ?>
-
