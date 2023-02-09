9f2a307fca99598c3728759262dd8b28713addfc
NickoRagasukma <gabrielprimaditya@gmail.com>
2023-02-09 22:01:07 +0700

    A Assets : Tambah peta js riset 2

---
+++ public/pvd/js/riset2/hasilSPRiset2_peta.js
@@ -0,0 +1,7 @@
+var malang = L.map("malang").setView([-7.9797, 112.6304], 13);
+
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(malang);
