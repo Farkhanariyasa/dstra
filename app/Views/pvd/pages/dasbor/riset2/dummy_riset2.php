5214b9770db5352c7d71871798479b203d7b003a
NickoRagasukma <gabrielprimaditya@gmail.com>
2023-02-11 17:53:08 +0700

    M Views : Modif sintaks dummy riset 2

--- app/Views/pvd/pages/dasbor/riset2/dummy_riset2.php
+++ app/Views/pvd/pages/dasbor/riset2/dummy_riset2.php
@@ -1,6 +1,5 @@
+<!-- Visualisasi 1 -->
 <div class="portfolio-item filter-riset2-hasil1">
-    <!-- Visualisasi 1.1 -->
-
     <!-- <div class="row mt-5">
         <div class="col-sm-6 my-2">
             <div class="card">
@@ -24,21 +23,22 @@
             <div class="card shadow">
                 <div class="card-body">
                     <div class="position-absolute top-0 end-0 d-flex flex-row justify-content-center align-item-center ">
-                    <!-- <div class="selectBox mt-2 me-2">
-                        <form action="">
-                            <select class="dropdownunduh1" id="forBarChart">
-                                <option value="a">Jenis Industri</option>
-                                <option value="b">Pendidikan Tertinggi</option>
-                                <option value="c">Jenis Kelamin</option>
-                            </select>
-                        </form>
-                    </div> -->
-                    <div class="me-1 mt-1 justify-content-end align-item-end">
-                        <button type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                            <i class="fa-solid fa-download"></i>
-                        </button>
+                        <!-- <div class="selectBox mt-2 me-2">
+                            <form action="">
+                                <select class="dropdownunduh1" id="forBarChart">
+                                    <option value="a">Jenis Industri</option>
+                                    <option value="b">Pendidikan Tertinggi</option>
+                                    <option value="c">Jenis Kelamin</option>
+                                </select>
+                            </form>
+                        </div> -->
+                        <div class="me-1 mt-1 justify-content-end align-item-end">
+                            <button type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                                <i class="fa-solid fa-download"></i>
+                            </button>
+                        </div>
                     </div>
-                </div>
+                    
                     <div class="chartBox" style="height:500px;">
                         <canvas class="mt1" id="pw_sankey"></canvas>
                     </div>
@@ -48,61 +48,8 @@
     </div>
 </div>
 
+<!-- Visualisasi 2 -->
 <div class="portfolio-item filter-riset2-hasil2">
-    <!-- Visualisasi 1.2 -->
-
-    <!-- <div class="row mt-5">
-        <div class="col-lg-6 grid-margin stretch-card">
-            <div class="card shadow">
-                <div class="card-body">
-                    <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center "> -->
-                        <!-- <div class="selectBox mt-2 me-2">
-                            <form action="">
-                                <select class="dropdownunduh1" id="forPieChart">
-                                    <option value="a">Jenis Industri</option>
-                                    <option value="b">Pendidikan Tertinggi</option>
-                                    <option value="c">Jenis Kelamin</option>
-                                </select>
-                            </form>
-                        </div> -->
-                        <!-- <div class="me-1 mt-1 justify-content-end align-item-end">
-                            <button type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal2">
-                                <i class="fa-solid fa-download"></i>
-                            </button>
-                        </div>
-                    </div>
-                    <div style=" height:500px;">
-                        <canvas class="mt1" id="jk_bar"></canvas>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-6 grid-margin stretch-card">
-            <div class="card shadow">
-                <div class="card-body">
-                    <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center "> -->
-                        <!-- <div class="selectBox mt-2 me-2">
-                            <form action="">
-                                <select class="dropdownunduh1" id="forDoughnutChart">
-                                    <option value=1>Jenis Pekerjaan Utama</option>
-                                    <option value=2>Tingkat Pendidikan Tertinggi</option>
-                                </select>
-                            </form>
-                        </div> -->
-                        <!-- <div class="me-1 mt-1 justify-content-end align-item-end">
-                            <button type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal2">
-                                <i class="fa-solid fa-download"></i>
-                            </button>
-                        </div>
-                    </div>
-                    <div style=" height:500px;">
-                        <canvas  class="mt1" id="jk_bar"></canvas>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div> -->
-
     <!-- <div class="row mt-5">
         <div class="col-sm-6 my-2">
             <div class="card">
@@ -126,21 +73,22 @@
             <div class="card shadow">
                 <div class="card-body">
                     <div class="position-absolute top-0 end-0 d-flex flex-row justify-content-center align-item-center ">
-                    <!-- <div class="selectBox mt-2 me-2">
-                        <form action="">
-                            <select class="dropdownunduh1" id="forBarChart">
-                                <option value="a">Jenis Industri</option>
-                                <option value="b">Pendidikan Tertinggi</option>
-                                <option value="c">Jenis Kelamin</option>
-                            </select>
-                        </form>
-                    </div> -->
-                    <div class="me-1 mt-1 justify-content-end align-item-end">
-                        <button type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal2">
-                            <i class="fa-solid fa-download"></i>
-                        </button>
+                        <!-- <div class="selectBox mt-2 me-2">
+                            <form action="">
+                                <select class="dropdownunduh1" id="forBarChart">
+                                    <option value="a">Jenis Industri</option>
+                                    <option value="b">Pendidikan Tertinggi</option>
+                                    <option value="c">Jenis Kelamin</option>
+                                </select>
+                            </form>
+                        </div> -->
+                        <div class="me-1 mt-1 justify-content-end align-item-end">
+                            <button type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal2">
+                                <i class="fa-solid fa-download"></i>
+                            </button>
+                        </div>
                     </div>
-                </div>
+
                     <div style=" height:500px;">
                         <canvas class="mt1" id="jk_bar"></canvas>
                     </div>
@@ -150,9 +98,8 @@
     </div>
 </div>
 
+<!-- Visualisasi 3 -->
 <div class="portfolio-item filter-riset2-hasil3">
-    <!-- Visualisasi 1.4 -->
-
     <!-- <div class="row mt-5">
         <div class="col-sm-6 my-2">
             <div class="card">
@@ -176,20 +123,22 @@
             <div class="card shadow">
                 <div class="card-body">
                     <div class="position-absolute top-0 end-0 d-flex flex-row justify-content-center align-item-center ">
-                    <div class="selectBox mt-2 me-2">
-                        <form action="">
-                            <select class="dropdownunduh1" id="forBarChart">
-                                <option value = 1>Jenis Pekerjaan Utama</option>
-                                <option value = 2>Tingkat Pendidikan Tertinggi</option>
-                            </select>
-                        </form>
+                        <div class="selectBox mt-2 me-2">
+                            <form action="">
+                                <select class="dropdownunduh1" id="forBarChart">
+                                    <option value = 1>Jenis Pekerjaan Utama</option>
+                                    <option value = 2>Tingkat Pendidikan Tertinggi</option>
+                                </select>
+                            </form>
+                        </div>
+
+                        <div class="me-1 mt-1 justify-content-end align-item-end">
+                            <button type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal3">
+                                <i class="fa-solid fa-download"></i>
+                            </button>
+                        </div>
                     </div>
-                    <div class="me-1 mt-1 justify-content-end align-item-end">
-                        <button type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal3">
-                            <i class="fa-solid fa-download"></i>
-                        </button>
-                    </div>
-                </div>
+
                     <div style=" height:500px;">
                         <canvas class="mt1" id="bar_horizontal"></canvas>
                     </div>
@@ -199,6 +148,7 @@
     </div>
 </div>
 
+<!-- Visualisasi 4 -->
 <div class="portfolio-item filter-riset2-peta" onresize="responsivefonts()">
     <div class="container mt-5">
         <div class="card d-flex justify-content-center flex-column align-item-center flex-md primary pb-5 pt-5 ">
@@ -218,47 +168,6 @@
         </div>
     </div>
 </div>
-
-
-<!-- Tabel dummy -->
-<!-- <div class="portfolio-item filter-riset2-tabel">
-    <div class="container-fluid turun">
-        <div class="kontenH mx-auto position-relative" data-aos="zoom-in" data-aos-duration="1000">
-            <img src="" class="d-md-block d-none ornamen3" alt="">
-            <div class="row py-5 d-flex justify-content-center">
-                <div class="mx-3 my-3 my-lg-0 d-flex align-items-center justify-content-center info">
-                    <h1 class="text-center fw-bold">Tabel</h1>
-                </div>
-                <div class="card mx-3 p-1 p-md-3 kgambar">
-                    <div class="card-body kontener">
-                        <table id="example" class="table">
-                            <thead>
-                                <tr>
-                                    <th scope="col">No</th>
-                                    <th scope="col">Nama</th>
-                                    <th scope="col">Jenis Kelamin</th>
-                                    <th scope="col">Jurusan</th>
-                                    <th scope="col">Tingkat</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                
-                                    <tr>
-                                        <th></th>
-                                        <td></td>
-                                        <td></td>
-                                        <td></td>
-                                        <td></td>
-                                    </tr>
-                                
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div> -->
 
 <!-- Modal Riset 2  -->
 <?php include('modal_riset2.php') ?>
