620b1b6886e2fb56fe7bca34f93090cb34498b38
LuthfilKA <222011321@stis.ac.id>
2023-02-11 15:24:49 +0700

    M Views : Tambah css riset 3

--- app/Views/pvd/layout/dasbor/header.php
+++ app/Views/pvd/layout/dasbor/header.php
@@ -12,6 +12,8 @@
   <!-- Plugin CSS -->
   <link rel="stylesheet" href="<?= base_url('pvd/vendors/mdi/css/materialdesignicons.min.css'); ?>">
   <link rel="stylesheet" href="<?= base_url('pvd/vendors/css/vendor.bundle.base.css'); ?>">
+  <!-- Riset1 CSS -->
+  <link rel="stylesheet" href="<?= base_url('pvd/css/riset1/style.css'); ?>">
   <!-- Fontawesome -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <!-- Leaflet CSS -->
@@ -22,6 +24,7 @@
   <link rel="stylesheet" href="<?= base_url('pvd/css/dasbor.css'); ?>">
   <!-- Jquery -->
   <script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
+
 </head>
 
 <body>
