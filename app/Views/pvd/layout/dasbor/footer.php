cbfcc04e26f6cb86189ee05c235d47b4ed9268ef
I Putu Agus Wahyu Dupayana <iputuagus.wahyu@yahoo.com>
2023-02-11 13:10:29 +0700

    M View : Perbaiki templating header footer

--- app/Views/pvd/layout/dasbor/footer.php
+++ app/Views/pvd/layout/dasbor/footer.php
@@ -9,43 +9,16 @@
       </div>
     </div>
   </div>
-
 </footer><!-- End Footer -->
 </div>
 </div>
 </div>
+<!-- Font Awesome -->
+<script src="https://kit.fontawesome.com/e0bb680df5.js" crossorigin="anonymous"></script>
+<!-- Plugin JS -->
 <script src="<?= base_url('pvd/vendors/js/vendor.bundle.base.js'); ?>"></script>
 <script src="<?= base_url('pvd/js/misc.js'); ?>"></script>
-
-<!-- Datatables -->
-<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.2/css/jquery.dataTables.css">
-<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.13.2/js/jquery.dataTables.js"></script>
-<link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.3.4/css/buttons.dataTables.min.css">
-<script src="<?= base_url('pvd/js/table2excel.js'); ?>"></script>
-
-<script>
-  $(document).ready(function () {
-    $('#table_id').DataTable({
-      "responsive": true,
-      "scrollX": true,
-      // dom: 'Bfrtip',
-      // buttons: [
-      //   'copyHtml5',
-      //   'excelHtml5',
-      //   'csvHtml5',
-      //   'pdfHtml5'
-      // ]
-      // "scrollY": "200px",
-    });
-  });
-</script>
-
-<!-- Untuk dasbor -->
 <script src="<?= base_url('pvd/js/off-canvas.js'); ?>"></script>
 <script src="<?= base_url('pvd/js/main.js') ?>"></script>
-<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/chartjs-chart-sankey"></script>
-
 </body>
-
 </html>
--- app/Views/pvd/layout/dasbor/header.php
+++ app/Views/pvd/layout/dasbor/header.php
@@ -7,38 +7,17 @@
   <title><?= $judul; ?> | Hasil PKL</title>
   <!-- Favicon icon -->
   <link rel="icon" type="image/png" href="<?= base_url('pvd/img/logo_pkl.png') ?>">
-
   <!-- Bootstrap -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-
-  <!-- Vendor -->
+  <!-- Plugin CSS -->
   <link rel="stylesheet" href="<?= base_url('pvd/vendors/mdi/css/materialdesignicons.min.css'); ?>">
   <link rel="stylesheet" href="<?= base_url('pvd/vendors/css/vendor.bundle.base.css'); ?>">
-
   <!-- Fontawesome -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-  <script src="https://kit.fontawesome.com/e0bb680df5.js" crossorigin="anonymous"></script>
-
-  <!-- Chart JS -->
-  <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/chartjs-chart-sankey"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/chartjs-plugin-datalabels/2.2.0/chartjs-plugin-datalabels.min.js" integrity="sha512-JPcRR8yFa8mmCsfrw4TNte1ZvF1e3+1SdGMslZvmrzDYxS69J7J49vkFL8u6u8PlPJK+H3voElBtUCzaXj+6ig==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-
-  <!-- AOS -->
-  <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-
-  <!-- Isotope -->
-  <script src="https://unpkg.com/isotope-layout@3/dist/isotope.pkgd.js"></script>
-
-  <!-- Leaflet Js -->
+  <!-- Leaflet CSS -->
   <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css" integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI=" crossorigin="" />
-  <script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js" integrity="sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM=" crossorigin=""></script>
-
-  <!-- Datatables -->
-  <link rel="stylesheet" href="https://cdn.datatables.net/1.13.2/css/jquery.dataTables.min.css">
-  <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.3.4/css/buttons.dataTables.min.css">
-  <script src="<?= base_url('pvd/js/table2excel.js'); ?>"></script>
-
+  <!-- Datatables CSS -->
+  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.2/css/jquery.dataTables.css">
   <!-- Dasbor CSS -->
   <link rel="stylesheet" href="<?= base_url('pvd/css/dasbor.css'); ?>">
 
