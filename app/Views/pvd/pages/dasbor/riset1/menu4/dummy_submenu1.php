aefe6db5c822d04c3c4a1056df2f04780ca19611
Thomson <222011365@stis.ac.id>
2023-02-12 22:09:48 +0700

    A Views: tampilan menu dan submenu baru

---
+++ app/Views/pvd/pages/dasbor/riset1/menu4/dummy_submenu1.php
@@ -0,0 +1,141 @@
+<div class="portfolio-item filter-riset1-hasil1" onresize="responsivefonts()">
+    <div class="col-12 grid-margin stretch-card">
+        <div class="card shadow">
+            <div class="card-body">
+                <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center ">
+                    <div class="selectBox mt-2 me-2">
+                        <form action="">
+                            <select class="dropdownunduh1 w-100" id="forBarChart">
+                                <option value="a">Jumlah ART Melakukan Perjalanan Wisata</option>
+                                <option value="b">Jenis Kelamin</option>
+                            </select>
+                        </form>
+                    </div>
+                    <div class="me-1 mt-1 justify-content-end align-item-end">
+                        <button type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                            <i class="fa-solid fa-download"></i>
+                        </button>
+                    </div>
+                </div>
+                <div style=" height:500px;">
+                    <canvas class="mt1" id="riset1_hasil1"></canvas>
+                </div>
+
+                <div class="position-absolute bot-0  end-0 d-flex flex-row justify-content-center align-item-center ">
+                    <div class="form-check form-switch me-3">
+                        <input class="form-check-input" onchange="update_value2(this);" type="checkbox" id="forPieChartLegend" value="show" checked>
+                        <label class="form-check-label" style="margin-left:-3px;" for="flexSwitchCheckChecked">Tampilkan Legend</label>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- <div class="row mt-5">
+        <div class="col-sm-6 my-2">
+            <div class="card shadow">
+                <div class="card-body">
+                    <p class="card-text text-center">Grafik<br><br><br><br><br><br><br><br><br><br><br></p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-6 my-2">
+            <div class="card shadow">
+                <div class="card-body">
+                    <p class="card-text text-center">Grafik<br><br><br><br><br><br><br><br><br><br><br></p>
+                </div>
+            </div>
+        </div>
+    </div> -->
+</div>
+
+
+<div class="portfolio-item filter-riset1-hasil2" onresize="responsivefonts()">
+    <div class="row">
+        <div class="col-lg-12 grid-margin stretch-card">
+            <div class="card shadow">
+                <div class="card-body">
+                    <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center ">
+                        <div class="me-1 mt-1 justify-content-end align-item-end">
+                            <button type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal2">
+                                <i class="fa-solid fa-download"></i>
+                            </button>
+                        </div>
+                    </div>
+                    <div style=" height:500px;">
+                        <canvas class="mt1" id="riset1_hasil2"></canvas>
+                    </div>
+                    <div class="position-absolute bot-0  end-0 d-flex flex-row justify-content-center align-item-center ">
+                        <div class="form-check form-switch me-3">
+                            <input class="form-check-input" onchange="update_value_mpw(this);" type="checkbox" id="forBarChartAxis_mpw" value="show" checked>
+                            <label class="form-check-label" style="margin-left:-3px;" for="flexSwitchCheckChecked">Tampilkan Sumbu</label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- <div class="row mt-5">
+        <div class="col-sm-6 my-2">
+            <div class="card shadow">
+                <div class="card-body">
+                    <p class="card-text text-center">Grafik<br><br><br><br><br><br><br><br><br><br><br></p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-6 my-2">
+            <div class="card shadow">
+                <div class="card-body">
+                    <p class="card-text text-center">Grafik<br><br><br><br><br><br><br><br><br><br><br></p>
+                </div>
+            </div>
+        </div>
+    </div> -->
+</div>
+
+
+<div class="portfolio-item filter-riset1-hasil3" onresize="responsivefonts()">
+    <div class="row">
+        <div class="col-lg-12 grid-margin stretch-card">
+            <div class="card shadow">
+                <div class="card-body">
+                    <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center ">
+                        <div class="me-1 mt-1 justify-content-end align-item-end">
+                            <button type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal3">
+                                <i class="fa-solid fa-download"></i>
+                            </button>
+                        </div>
+                    </div>
+                    <div style=" height:500px;">
+                        <canvas class="mt1" id="riset1_hasil3"></canvas>
+                    </div>
+                    <div class="position-absolute bot-0  end-0 d-flex flex-row justify-content-center align-item-center ">
+                        <div class="form-check form-switch me-3">
+                            <input class="form-check-input" onchange="update_value_sim(this);" type="checkbox" id="forBarChartAxis_sim" value="show" checked>
+                            <label class="form-check-label" style="margin-left:-3px;" for="flexSwitchCheckChecked">Tampilkan Sumbu</label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- <div class="row mt-5">
+        <div class="col-sm-6 my-2">
+            <div class="card shadow">
+                <div class="card-body">
+                    <p class="card-text text-center">Grafik<br><br><br><br><br><br><br><br><br><br><br></p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-6 my-2">
+            <div class="card shadow">
+                <div class="card-body">
+                    <p class="card-text text-center">Grafik<br><br><br><br><br><br><br><br><br><br><br></p>
+                </div>
+            </div>
+        </div>
+    </div> -->
+</div>
---
+++ app/Views/pvd/pages/dasbor/riset1/menu4/submenu1.php
@@ -0,0 +1,58 @@
+<?= $this->extend('pvd/layout/dasbor/template'); ?>
+
+<?= $this->section('content'); ?>
+<section id="portfolio" class="portfolio section-bg">
+    <div class="container">
+        <header class="section-header">
+            <p>Kajian Pemanfaatan MPD dalam Menunjang Statistik Pariwisata</p>
+        </header>
+        <hr class="hrjudul">
+        <div class="row">
+            <div class="col-lg-12 d-flex justify-content-center">
+                <ul id="portfolio-flters">
+                    <li data-filter="*" class="filter-active">Semua</li>
+                    <li data-filter=".filter-riset1-hasil1">Hasil 1</li>
+                    <li data-filter=".filter-riset1-hasil2">Hasil 2</li>
+                    <li data-filter=".filter-riset1-hasil3">Hasil 3</li>
+                </ul>
+            </div>
+        </div>
+        <div class="row portfolio-container">
+            <?php include('dummy_submenu1.php') ?>
+        </div>
+    </div>
+</section>
+
+
+
+<!-- Istope -->
+<script src="https://unpkg.com/isotope-layout@3/dist/isotope.pkgd.js"></script>
+<!-- Leaflet JS -->
+<script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js" integrity="sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM=" crossorigin=""></script>
+<!-- Chart JS -->
+<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/chartjs-plugin-datalabels/2.2.0/chartjs-plugin-datalabels.min.js" integrity="sha512-JPcRR8yFa8mmCsfrw4TNte1ZvF1e3+1SdGMslZvmrzDYxS69J7J49vkFL8u6u8PlPJK+H3voElBtUCzaXj+6ig==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<!-- Data Chart -->
+<script>
+    const jumlahanggota = <?= json_encode($ja) ?>;
+    const jeniskelamin = <?= json_encode($jk) ?>;
+    const mpw1 = <?= json_encode($b524) ?>;
+    const mpw2 = <?= json_encode($b525) ?>;
+    const mpw3 = <?= json_encode($b526) ?>;
+    const mpw4 = <?= json_encode($b527) ?>;
+    const mpw5 = <?= json_encode($b528) ?>;
+    const mpw6 = <?= json_encode($b529) ?>;
+    const indosat = <?= json_encode($indosat) ?>;
+    const xl = <?= json_encode($xl) ?>;
+    const tri = <?= json_encode($tri) ?>;
+    const smartfren = <?= json_encode($smartfren) ?>;
+    const lainnya = <?= json_encode($lainnya) ?>;
+</script>
+<!-- Impor Chart -->
+<script src="<?= base_url('pvd/js/riset1/hasilSPRiset1_ja_pie.js') ?>"></script>
+<script src="<?= base_url('pvd/js/riset1/hasilSPRiset1_mpw_bar.js') ?>"></script>
+<script src="<?= base_url('pvd/js/riset1/hasilSPRiset1_simcard.js') ?>"></script>
+<!-- Fungsi Unduh Chart -->
+<script src="<?= base_url('pvd/js/riset1/btn_Download.js') ?>"></script>
+</section><!-- End -->
+<?= $this->endSection(); ?>
