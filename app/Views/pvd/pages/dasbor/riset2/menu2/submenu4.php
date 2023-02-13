d955c7ecb5c4439a14fe40b5930f51556b3ad395
NickoRagasukma <gabrielprimaditya@gmail.com>
2023-02-13 22:35:20 +0700

    D Views : Rename nama file riset 2

--- app/Views/pvd/pages/dasbor/riset2/menu2/submenu4.php
+++
@@ -1,7 +0,0 @@
-<?= $this->extend('pvd/layout/dasbor/template'); ?>
-
-<?= $this->section('content'); ?>
-
-<h1>Menu 2 submenu 4</h1>
-
-<?= $this->endSection(); ?>
--- app/Views/pvd/pages/dasbor/riset2/menu3/submenu3.php
+++
@@ -1,7 +0,0 @@
-<?= $this->extend('pvd/layout/dasbor/template'); ?>
-
-<?= $this->section('content'); ?>
-
-<h1>Menu 3 submenu 3</h1>
-
-<?= $this->endSection(); ?>
