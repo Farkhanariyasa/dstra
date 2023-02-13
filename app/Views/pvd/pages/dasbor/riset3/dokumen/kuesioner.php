a1fece1f50c931680322e28e31fbf05abee1c569
prasetyofmalik <222011325@stis.ac.id>
2023-02-13 21:24:37 +0700

    A Views: menu submenu riset 3

---
+++ app/Views/pvd/pages/dasbor/riset3/dokumen/kuesioner.php
@@ -0,0 +1,26 @@
+<?= $this->extend('pvd/layout/dasbor/template'); ?>
+
+<?= $this->section('content'); ?>
+
+<h2>Kuesioner Listing</h2>
+
+<div class="tab-pane active p-2" id="tab1" role="tabpanel" aria-labelledby="tab-1" style="height: auto;">
+  <div class="row">
+    <div style="width: 100%;">
+      <embed src="https://drive.google.com/file/d/1xuJ295CpXFn0BJrmJlnbs4wTf-nr3554/preview" type="application/pdf" width=100% height="600px" />
+    </div>
+  </div>
+</div>
+
+<h2>Kuesioner Pencacahan</h2>
+
+<div class="tab-pane active p-2" id="tab1" role="tabpanel" aria-labelledby="tab-1" style="height: auto;">
+  <div class="row">
+    <div style="width: 100%;">
+      <embed src="https://drive.google.com/file/d/1frfAz4IZeK4SxGDQQSk_E8AYdpesqwap/preview" type="application/pdf" width=100% height="600px" />
+    </div>
+  </div>
+</div>
+
+<?= $this->endSection(); ?>
+
---
+++ app/Views/pvd/pages/dasbor/riset3/hasilkajian/tujuan4.php
@@ -0,0 +1,8 @@
+<?= $this->extend('pvd/layout/dasbor/template'); ?>
+
+<?= $this->section('content'); ?>
+
+<h1>Hasil Kajian Tujuan 4</h1>
+
+<?= $this->endSection(); ?>
+
