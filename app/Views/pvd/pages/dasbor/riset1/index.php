760aeeecb6973f2ca2b32cd28d0a130c68b7cd62
Thomson <222011365@stis.ac.id>
2023-02-14 10:43:12 +0700

    M Views: perubahan isi konten dasbor

--- app/Views/pvd/pages/dasbor/riset1/index.php
+++ app/Views/pvd/pages/dasbor/riset1/index.php
@@ -1,60 +1,158 @@
 <?= $this->extend('pvd/layout/dasbor/template'); ?>
 <?= $this->section('content'); ?>
-<section id="portfolio" class="portfolio section-bg">
-  <div class="container">
-    <header class="section-header">
-      <p>Kajian Pemanfaatan MPD dalam Menunjang Statistik Pariwisata</p>
-    </header>
-    <hr class="hrjudul">
-    <div class="row">
-      <div class="col-lg-12 d-flex justify-content-center">
-        <ul id="portfolio-flters">
-          <li data-filter="*" class="filter-active">Semua</li>
-          <li data-filter=".filter-riset1-hasil1">Hasil 1</li>
-          <li data-filter=".filter-riset1-hasil2">Hasil 2</li>
-          <li data-filter=".filter-riset1-hasil3">Hasil 3</li>
-          <li data-filter=".filter-riset1-hasil4">Hasil 4</li>
-          <li data-filter=".filter-riset1-peta">Peta Lokus</li>
-          <li data-filter=".filter-riset1-tabeldummy">Tabel Dummy</li>
-        </ul>
-      </div>
+
+<div class="container px-0">
+  <header class="section-header">
+    <h6 style="color: #4b395f;"><strong>
+        Kajian Pemanfaatan MPD dalam Menunjang Statistik Pariwisata</strong>
+    </h6>
+  </header>
+<hr class="hrjudul">  
+
+  <div class="accordion" id="accordionExample">
+    <div class="accordion-item my-3">
+      <h3 class="accordion-header" id="headingOne">
+        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+          Gambaran Umum
+        </button>
+        </h2>
+        <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+          <div class="accordion-body">
+            <p class="text-justify">Penelitian ini dilakukan untuk mengatasi permasalahan MPD khususnya pada double counting pada data MPS dua nomor berbeda milik Telkomsel dan indosat
+              melalui pattern dari kedua nomor tersebut. Dengan memanfaatkan MPD, kami melakukan penelitian terhadap pengelompokkan individu ang termasuk dalam satu rumah tangga berdasarkan dengan usual environment
+              (family grouping) dan pola perjalanan wisata selama tahun 2022. Selain dengan pemanfaatan MPD, penelitin ini dilakukan untuk mencari tahu permasalahan pada statistik wisatawan nusantara dengan melibatkan jumlah perjalana wisata menggunakan MPD dan survei
+              konvensional.
+            </p>
+          </div>
+        </div>
     </div>
-    <div class="row portfolio-container">
-      <?php include('dummy_riset1.php') ?>
+    <div class="accordion-item my-3">
+      <h3 class="accordion-header" id="headingTwo">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+          Tujuan Umum
+        </button>
+        </h2>
+        <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+          <div class="accordion-body">
+            <ol class="text-justify">
+              <li>
+                Menemukan metode untuk mengelompokkan individu yang termasuk dalam satu
+                rumah tangga berdasarkan usual environment dan pola perjalanan wisata (family
+                grouping).
+              </li>
+              <li>
+                Menemukan metode untuk mengidentifikasi double counting berdasarkan pola
+                perjalanan individu yang menggunakan lebih dari satu nomor Telkomsel dan/atau
+                Indosat berbeda dalam satu perangkat.
+              </li>
+              <li>
+                Menyajikan statistik wisatawan nusantara melalui pendekatan Mobile Positioning
+                Data (MPD) dan survei konvensional di Kota Malang selama Triwulan IV 2022.
+              </li>
+            </ol>
+          </div>
+        </div>
+    </div>
+    <div class="accordion-item my-3">
+      <h3 class="accordion-header" id="headingThree">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+          Riset 1 (Kota Surabaya)
+        </button>
+        </h2>
+        <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+          <div class="accordion-body">
+            <p class="text-justify">
+              Metode Penarikan Sampel
+            </p>
+            <p class="text-justify">
+              Judgement (Purposive) Sampling
+            </p>
+            <p class="text-justify">
+              Metodologi pengambilan sampel secara acak dimana
+              kelompok sampel yang ditargetkan memiliki atribut-atribut tertentu. Cara yang dilakukan pada Judgment (Purposive) Sampling penelitian ini
+              adalah pengambilan sampel yang homogen. Sampel homogen yang ditargetkan pada penelitian ini yaitu sampel dengan pekerjaan sama, yaitu orang ang bekerja di BPS Kota Surabaya dan BPS Provinsi Jawa Timur. 
+            </p>
+          </div>
+        </div>
+    </div>
+    <div class="accordion-item my-3">
+      <h3 class="accordion-header" id="headingFour">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+          Tujuan 3
+        </button>
+        </h2>
+        <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#accordionExample">
+          <div class="accordion-body">
+            <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed viverra neque ligula, at vehicula orci viverra at.
+              Sed at lorem at tortor dapibus ultricies ut sed ipsum. Maecenas molestie elementum justo, in convallis urna eleifend et.
+              Vestibulum mollis, tortor ut luctus scelerisque, eros est ullamcorper leo, quis tempor urna arcu sit amet tortor. Sed rhoncus
+              odio non ipsum tempus porttitor. Nulla sagittis tincidunt aliquam. Cras commodo egestas commodo. Praesent consectetur mauris sed
+              ultrices dignissim. Suspendisse mollis magna auctor enim posuere interdum. Phasellus suscipit, dolor vitae mattis consectetur, dui
+              ex viverra nibh, sit amet elementum ligula lectus et quam. Sed et cursus neque. Morbi maximus in ipsum nec aliquam. Integer porttitor
+              blandit augue, ut posuere dolor gravida vel. Nulla rutrum eu augue non semper. Proin sit amet dui felis. Sed vestibulum tempor urna,
+              auctor viverra erat.
+            </p>
+          </div>
+        </div>
+    </div>
+    <div class="accordion-item my-3">
+      <h3 class="accordion-header" id="headingFive">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+          Tujuan 4
+        </button>
+        </h2>
+        <div id="collapseFive" class="accordion-collapse collapse" aria-labelledby="headingFive" data-bs-parent="#accordionExample">
+          <div class="accordion-body">
+            <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed viverra neque ligula, at vehicula orci viverra at.
+              Sed at lorem at tortor dapibus ultricies ut sed ipsum. Maecenas molestie elementum justo, in convallis urna eleifend et.
+              Vestibulum mollis, tortor ut luctus scelerisque, eros est ullamcorper leo, quis tempor urna arcu sit amet tortor. Sed rhoncus
+              odio non ipsum tempus porttitor. Nulla sagittis tincidunt aliquam. Cras commodo egestas commodo. Praesent consectetur mauris sed
+              ultrices dignissim. Suspendisse mollis magna auctor enim posuere interdum. Phasellus suscipit, dolor vitae mattis consectetur, dui
+              ex viverra nibh, sit amet elementum ligula lectus et quam. Sed et cursus neque. Morbi maximus in ipsum nec aliquam. Integer porttitor
+              blandit augue, ut posuere dolor gravida vel. Nulla rutrum eu augue non semper. Proin sit amet dui felis. Sed vestibulum tempor urna,
+              auctor viverra erat.
+            </p>
+          </div>
+        </div>
     </div>
   </div>
-</section>
-<!-- Istope -->
-<script src="https://unpkg.com/isotope-layout@3/dist/isotope.pkgd.js"></script>
-<!-- Leaflet JS -->
-<script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js" integrity="sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM=" crossorigin=""></script>
-<!-- Chart JS -->
-<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/chartjs-plugin-datalabels/2.2.0/chartjs-plugin-datalabels.min.js" integrity="sha512-JPcRR8yFa8mmCsfrw4TNte1ZvF1e3+1SdGMslZvmrzDYxS69J7J49vkFL8u6u8PlPJK+H3voElBtUCzaXj+6ig==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-<!-- Data Chart -->
-<script>
-  const jumlahanggota = <?= json_encode($ja) ?>;
-  const jeniskelamin = <?= json_encode($jk) ?>;
-  const mpw1 = <?= json_encode($b524) ?>;
-  const mpw2 = <?= json_encode($b525) ?>;
-  const mpw3 = <?= json_encode($b526) ?>;
-  const mpw4 = <?= json_encode($b527) ?>;
-  const mpw5 = <?= json_encode($b528) ?>;
-  const mpw6 = <?= json_encode($b529) ?>;
-  const indosat = <?= json_encode($indosat) ?>;
-  const xl = <?= json_encode($xl) ?>;
-  const tri = <?= json_encode($tri) ?>;
-  const smartfren = <?= json_encode($smartfren) ?>;
-  const lainnya = <?= json_encode($lainnya) ?>;
-</script>
-<!-- Impor Chart -->
-<script src="<?= base_url('pvd/js/riset1/hasilSPRiset1_ja_pie.js') ?>"></script>
-<script src="<?= base_url('pvd/js/riset1/hasilSPRiset1_mpw_bar.js') ?>"></script>
-<script src="<?= base_url('pvd/js/riset1/hasilSPRiset1_simcard.js') ?>"></script>
-<script src="<?= base_url('pvd/js/riset1/hasilSPRiset1_stacked_bar.js') ?>"></script>
-<script src="<?= base_url('pvd/js/riset1/hasilSPRiset1_peta.js') ?>"></script>
-<script src="<?= base_url('pvd/js/riset1/hasilSPRiset1_line.js') ?>"></script>
-<!-- Fungsi Unduh Chart -->
-<script src="<?= base_url('pvd/js/riset1/btn_Download.js') ?>"></script>
-</section><!-- End -->
+</div>
+
+<div class=" portfolio-item filter-riset1-peta" onresize="responsivefonts()">
+    <div class="container mt-5">
+        <!-- <button onclick="showmsg();" class="tombol btn-for" >Download image</button> -->
+        <div class="card d-flex justify-content-center flex-column align-item-center flex-md primary pb-5 pt-5 ">
+            <p class="card-text text-center">Kota Surabaya</p>
+            <div class="d-flex flex-row align-item-center justify-content-center">
+                <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center">
+                    <div class="me-1 mt-1 justify-content-end align-item-end">
+                        <button type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                            <i class="fa-solid fa-download"></i>
+                        </button>
+                    </div>
+                </div>
+                <div id="map" style="width: 95%; height:80vh;"></div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container mt-5">
+        <!-- <button onclick="showmsg();" class="tombol btn-for" >Download image</button> -->
+        <div class="card d-flex justify-content-center flex-column align-item-center flex-md primary pb-5 pt-5 ">
+            <p class="card-text text-center">Kota Malang</p>
+            <div class="d-flex flex-row align-item-center justify-content-center">
+                <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center">
+                    <div class="me-1 mt-1 justify-content-end align-item-end">
+                        <button type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                            <i class="fa-solid fa-download"></i>
+                        </button>
+                    </div>
+                </div>
+                <div id="map2" style="width: 95%; height:80vh;"></div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
 <?= $this->endSection(); ?>
