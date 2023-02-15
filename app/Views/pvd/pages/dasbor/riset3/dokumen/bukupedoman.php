096c2bd379c7453a3b35606c414bb47bc8909086
prasetyofmalik <222011325@stis.ac.id>
2023-02-16 06:37:50 +0700

    M View: revisi link dokumen buku pedoman

--- app/Views/pvd/pages/dasbor/riset3/dokumen/bukupedoman.php
+++ app/Views/pvd/pages/dasbor/riset3/dokumen/bukupedoman.php
@@ -7,7 +7,7 @@
 <div class="tab-pane active p-2" id="tab1" role="tabpanel" aria-labelledby="tab-1" style="height: auto;">
   <div class="row">
     <div style="width: 100%;">
-      <embed src="https://drive.google.com/file/d/132lefz-534kticPfl--JtDlc5hMrzDNu/preview" type="application/pdf" width=100% height="600px" />
+      <embed src="https://drive.google.com/file/d/16QYIS3yoEmI91RYZJ8vVJi0QogDgKAf4/preview" type="application/pdf" width=100% height="600px" />
     </div>
   </div>
 </div>
@@ -17,7 +17,7 @@
 <div class="tab-pane active p-2" id="tab1" role="tabpanel" aria-labelledby="tab-1" style="height: auto;">
   <div class="row">
     <div style="width: 100%;">
-      <embed src="https://drive.google.com/file/d/1xYcDXGgIzlLteK2BdenMsGcdfbcRIF9S/preview" type="application/pdf" width=100% height="600px" />
+      <embed src="https://drive.google.com/file/d/17pXhqRCHhgTCobMJIW_M_76QLpgEP1kE/preview" type="application/pdf" width=100% height="600px" />
     </div>
   </div>
 </div>
