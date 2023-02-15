9953f535251e23b412283a836df3a25f36982552
imams <222011830@gmail.com>
2023-02-15 19:42:00 +0700

    M View: Tambah tabel kontingensi di submenu2

--- app/Views/pvd/pages/dasbor/riset4/menu3/submenu2.php
+++ app/Views/pvd/pages/dasbor/riset4/menu3/submenu2.php
@@ -44,7 +44,7 @@
 
 <div class="card">
   <div class="card-header">
-    <strong>Tabulasi Unit Usaha Pariwisata Berdasarkan Skala Usaha</strong>
+    <strong>Tabulasi Unit Usaha Pariwisata Berdasarkan Penggunaan TIK dalam Pencatatan Keuangan</strong>
     <!-- <button type="button" class="tombol btn-for text-right me-1 mt-1 justify-content-end align-item-end" data-bs-toggle="modal" data-bs-target="#exampleModal4-doughnut">
                             <i class="fa-solid fa-download"></i>
    </button> -->
@@ -57,34 +57,78 @@
       <tr>
         <th rowspan="3">Kecamatan</th>
         <th rowspan="3">Desa/Kelurahan</th>
-        <th colspan="6">Jenis Unit Usaha Pariwisata</th>
+        <th colspan="12">Jenis Unit Usaha Pariwisata</th>
         <th rowspan="3">Total</th>
       </tr>
       <tr>
-        <th colspan="2">Jasa Transportasi Wisata</th>
-        <th colspan="2">Jasa Penyediaan Makanan dan Minuman</th>
-        <th colspan="2">Penyediaan Akomodasi</th>
+        <th colspan="4">Jasa Transportasi Wisata</th>
+        <th colspan="4">Jasa Penyediaan Makanan dan Minuman</th>
+        <th colspan="4">Penyediaan Akomodasi</th>
       </tr>
       <tr>
-        <th>Mikro</th>
-        <th>Kecil</th>
-        <th>Mikro</th>
-        <th>Kecil</th>
-        <th>Mikro</th>
-        <th>Kecil</th>
+        <th>[1]</th>
+        <th>[2]</th>
+        <th>[3]</th>
+        <th>[4]</th>
+        <th>[1]</th>
+        <th>[2]</th>
+        <th>[3]</th>
+        <th>[4]</th>
+        <th>[1]</th>
+        <th>[2]</th>
+        <th>[3]</th>
+        <th>[4]</th>
       </tr>
       </thead>
       <tbody>
       <tr>
         <td rowspan="8">Batu</td>
-        <td>Ngaglik</td>
+        <td>Oro-oro Ombo</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
-        <td>60</td>    
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>120</td>    
+      </tr>
+      <tr>
+        <td>Temas</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>120</td>    
+      </tr>
+      <tr>
+        <td>Sisir</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>120</td>    
       </tr>
       <tr>
         <td>Ngaglik</td>
@@ -94,242 +138,363 @@
         <td>10</td>
         <td>10</td>
         <td>10</td>
-        <td>60</td>    
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>120</td>    
       </tr>
       <tr>
-        <td>Ngaglik</td>
+        <td>Pesanggrahan</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
-        <td>60</td>    
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>120</td>    
       </tr>
       <tr>
-        <td>Ngaglik</td>
+        <td>Songgokerto</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
-        <td>60</td>    
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>120</td>    
       </tr>
       <tr>
-        <td>Ngaglik</td>
+        <td>Sumberejo</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
-        <td>60</td>    
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>120</td>    
       </tr>
       <tr>
-        <td>Ngaglik</td>
+        <td>Sidomulyo</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
-        <td>60</td>    
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>120</td>    
       </tr>
       <tr>
-        <td>Ngaglik</td>
+        <td rowspan="7">Junrejo</td>
+        <td>Tlekung</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
-        <td>60</td>    
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>120</td>    
       </tr>
       <tr>
-        <td>Ngaglik</td>
+        <td>Junrejo</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
-        <td>60</td>    
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>120</td>    
       </tr>
       <tr>
-        <td rowspan="7">Batu</td>
-        <td>Ngaglik</td>
+        <td>Mojorejo</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
-        <td>60</td>    
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>120</td>    
       </tr>
       <tr>
-        <td>Ngaglik</td>
+        <td>Torongrejo</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
-        <td>60</td>    
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>120</td>    
       </tr>
       <tr>
-        <td>Ngaglik</td>
+        <td>Beji</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
-        <td>60</td>    
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>120</td>    
       </tr>
       <tr>
-        <td>Ngaglik</td>
+        <td>Pendem</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
-        <td>60</td>    
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>120</td>    
       </tr>
       <tr>
-        <td>Ngaglik</td>
+        <td>Dadaprejo</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
-        <td>60</td>    
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>120</td>    
       </tr>
       <tr>
-        <td>Ngaglik</td>
+        <td rowspan="9">Bumiaji</td>
+        <td>Pandarejo</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
-        <td>60</td>    
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>120</td>    
       </tr>
       <tr>
-        <td>Ngaglik</td>
+        <td>Bumiaji</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
-        <td>60</td>    
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>120</td>    
       </tr>
       <tr>
-        <td rowspan="9">Batu</td>
-        <td>Ngaglik</td>
+        <td>Bulukerto</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
-        <td>60</td>    
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>120</td>    
       </tr>
       <tr>
-        <td>Ngaglik</td>
+        <td>Gunungsari</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
-        <td>60</td>    
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>120</td>    
       </tr>
       <tr>
-        <td>Ngaglik</td>
+        <td>Punten</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
-        <td>60</td>    
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>120</td>    
       </tr>
       <tr>
-        <td>Ngaglik</td>
+        <td>Tulungrejo</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
-        <td>60</td>    
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>120</td>    
       </tr>
       <tr>
-        <td>Ngaglik</td>
+        <td>Sumbergondo</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
-        <td>60</td>    
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>120</td>    
       </tr>
       <tr>
-        <td>Ngaglik</td>
+        <td>Giripurno</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
-        <td>60</td>    
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>120</td>    
       </tr>
       <tr>
-        <td>Ngaglik</td>
+        <td>Sumber Brantas</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
-        <td>60</td>    
-      </tr>
-      <tr>
-        <td>Ngaglik</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
         <td>10</td>
-        <td>60</td>    
-      </tr>
-      <tr>
-        <td>Ngaglik</td>
-        <td>10</td>
-        <td>10</td>
-        <td>10</td>
-        <td>10</td>
-        <td>10</td>
-        <td>10</td>
-        <td>60</td>    
+        <td>120</td>    
       </tr>
       <tr style="font-weight: bold;">
         <td colspan=2 class="text-center">Total</td>
-        <td>10</td>
-        <td>10</td>
-        <td>10</td>
-        <td>10</td>
-        <td>10</td>
-        <td>60</td> 
-        <td>60</td> 
+        <td>240</td>
+        <td>240</td>
+        <td>240</td>
+        <td>240</td>
+        <td>240</td>
+        <td>240</td>
+        <td>240</td>
+        <td>240</td>
+        <td>240</td>
+        <td>240</td>
+        <td>240</td>
+        <td>240</td> 
+        <td>2880</td> 
       </tr>
       </tbody>
     </table>
+    </div>
+    <div class="card-body" style="padding: 0.5rem 1rem;">
+    <p>
+      Keterangan:<br>	
+      [1]	Tidak memanfaatkan TIK sama sekali<br>
+      [2]	Sudah memanfaatkan, namun lebih banyak dilakukan secara konvensional<br>
+      [3]	Sebagian besar aktivitas pencatatan keuangan sudah memanfaatkan TIK<br>
+      [4]	Semua aktivitas pencatatan keuangan sudah memanfaatkan TIK<br>
+    </p>
     </div>
 </div>
 <?= $this->endSection(); ?>
