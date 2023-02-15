c66ae2d31dd8f58cce67b90e1ba6b46b5f09756f
imams <222011830@gmail.com>
2023-02-15 19:42:38 +0700

    M View: Tambah tabel kontingensi di submenu4

--- app/Views/pvd/pages/dasbor/riset4/menu3/submenu4.php
+++ app/Views/pvd/pages/dasbor/riset4/menu3/submenu4.php
@@ -2,84 +2,457 @@
 
 <?= $this->section('content'); ?>
 
-<div class="container px-0">
-      <header class="section-header">
-        <h6 style="color: #4b395f;"><strong>Kesiapan Pemanfaatan TIK Unit Usaha Pariwisata Skala Mikro dan Kecil di Kota Batu dalam Menerapkan 
-          <i>Smart Tourism Destination</i></strong>
-        </h6>
-      </header>
-    <hr class="hrjudul">
+<header class="section-header">
+  <h6 style="color: #4b395f;"><strong>Kesiapan Pemanfaatan TIK Unit Usaha Pariwisata Skala Mikro dan Kecil di Kota Batu dalam Menerapkan
+      <i>Smart Tourism Destination</i></strong>
+  </h6>
+</header>
+<hr class="hrjudul">
 
-    <div class="accordion" id="accordionExample">
-      <div class="accordion-item my-3">
-        <h3 class="accordion-header" id="headingOne">
-          <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-            Manajemen Organisasi
-          </button>
-        </h3>
-        <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-          <div class="accordion-body">
-            <p class="text-justify">Ilham mana wee
-            </p>           
-          </div>
-        </div>
-      </div>
-      <!-- <div class="accordion-item my-3">
-        <h3 class="accordion-header" id="headingTwo">
-          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-            Tujuan 1
-          </button>
-        </h3>
-        <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
-          <div class="accordion-body">
-            <p class="text-justify">Mengetahui karakteristik tiga unit usaha pariwisata skala mikro dan kecil di Kota Batu yang meliputi Jasa 
-              Transportasi Wisata, Jasa Makanan dan Minuman, serta Penyediaan Akomodasi dalam menerapkan Smart Tourism Destination. 
-            </p> 
-          </div>
-        </div>
-      </div>
-      <div class="accordion-item my-3">
-        <h3 class="accordion-header" id="headingThree">
-          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-            Tujuan 2
-          </button>
-        </h3>
-        <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
-          <div class="accordion-body">
-            <p class="text-justify">Menganalisis tingkat kesiapan pemanfaatan TIK dari tiga unit usaha pariwisata skala mikro dan kecil di Kota 
-              Batu yang meliputi Jasa Transportasi Wisata, Jasa Makanan dan Minuman, serta Penyediaan Akomodasi dalam menerapkan Smart Tourism Destination 
-            </p>           
-          </div>
-        </div>
-      </div>
-      <div class="accordion-item my-3">
-        <h3 class="accordion-header" id="headingFour">
-          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
-            Tujuan 3
-          </button>
-        </h3>
-        <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#accordionExample">
-          <div class="accordion-body">
-            <p class="text-justify">Menganalisis faktor-faktor yang memengaruhi kesiapan unit usaha pariwisata skala mikro dan kecil di Kota Batu 
-              dalam pemanfaatan TIK. 
-            </p>           
-          </div>
-        </div>
-      </div>
-      <div class="accordion-item my-3">
-        <h3 class="accordion-header" id="headingFive">
-          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
-            Tujuan 4
-          </button>
-        </h3>
-        <div id="collapseFive" class="accordion-collapse collapse" aria-labelledby="headingFive" data-bs-parent="#accordionExample">
-          <div class="accordion-body">
-            <p class="text-justify">Menganalisis perbedaan rata-rata omzet berdasarkan kategori kesiapan pemanfaatan TIK dari unit usaha 
-              pariwisata skala mikro dan kecil di Kota Batu.
-            </p>           
-          </div>
-        </div>
-      </div> -->
+<div class="card">
+  <div class="card-header">
+    <strong>Tabulasi Unit Usaha Pariwisata Berdasarkan Upaya Meminta Pekerja/Karyawan untuk Mempelajari dan Memahami Penggunaan TIK Baru yang Akan Digunakan di Unit Usaha"</strong>
+    <!-- <button type="button" class="tombol btn-for text-right me-1 mt-1 justify-content-end align-item-end" data-bs-toggle="modal" data-bs-target="#exampleModal4-doughnut">
+                            <i class="fa-solid fa-download"></i>
+   </button> -->
+  </div>
+  <div class="card-body table-responsive" style="padding: 0.5rem 1rem;">
+    <!-- style="padding:0px 0px 0px 0px;" -->
+    <!-- <blockquote class="blockquote mb-0"> -->
+    <table class="table table-hover table-bordered table-responsive" style="width:100%">
+      <thead style="color: #fff;  font-weight:bold; text-align:center;">
+      <tr>
+        <th rowspan="3">Kecamatan</th>
+        <th rowspan="3">Desa/Kelurahan</th>
+        <th colspan="12">Jenis Unit Usaha Pariwisata</th>
+        <th rowspan="3">Total</th>
+      </tr>
+      <tr>
+        <th colspan="4">Jasa Transportasi Wisata</th>
+        <th colspan="4">Jasa Penyediaan Makanan dan Minuman</th>
+        <th colspan="4">Penyediaan Akomodasi</th>
+      </tr>
+      <tr>
+        <th>Tidak Pernah</th>
+        <th>Jarang</th>
+        <th>Sering</th>
+        <th>Selalu</th>
+        <th>Tidak Pernah</th>
+        <th>Jarang</th>
+        <th>Sering</th>
+        <th>Selalu</th>
+        <th>Tidak Pernah</th>
+        <th>Jarang</th>
+        <th>Sering</th>
+        <th>Selalu</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr>
+        <td rowspan="8">Batu</td>
+        <td>Oro-oro Ombo</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>120</td>    
+      </tr>
+      <tr>
+        <td>Temas</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>120</td>    
+      </tr>
+      <tr>
+        <td>Sisir</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>120</td>    
+      </tr>
+      <tr>
+        <td>Ngaglik</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>120</td>    
+      </tr>
+      <tr>
+        <td>Pesanggrahan</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>120</td>    
+      </tr>
+      <tr>
+        <td>Songgokerto</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>120</td>    
+      </tr>
+      <tr>
+        <td>Sumberejo</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>120</td>    
+      </tr>
+      <tr>
+        <td>Sidomulyo</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>120</td>    
+      </tr>
+      <tr>
+        <td rowspan="7">Junrejo</td>
+        <td>Tlekung</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>120</td>    
+      </tr>
+      <tr>
+        <td>Junrejo</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>120</td>    
+      </tr>
+      <tr>
+        <td>Mojorejo</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>120</td>    
+      </tr>
+      <tr>
+        <td>Torongrejo</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>120</td>    
+      </tr>
+      <tr>
+        <td>Beji</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>120</td>    
+      </tr>
+      <tr>
+        <td>Pendem</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>120</td>    
+      </tr>
+      <tr>
+        <td>Dadaprejo</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>120</td>    
+      </tr>
+      <tr>
+        <td rowspan="9">Bumiaji</td>
+        <td>Pandarejo</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>120</td>    
+      </tr>
+      <tr>
+        <td>Bumiaji</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>120</td>    
+      </tr>
+      <tr>
+        <td>Bulukerto</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>120</td>    
+      </tr>
+      <tr>
+        <td>Gunungsari</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>120</td>    
+      </tr>
+      <tr>
+        <td>Punten</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>120</td>    
+      </tr>
+      <tr>
+        <td>Tulungrejo</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>120</td>    
+      </tr>
+      <tr>
+        <td>Sumbergondo</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>120</td>    
+      </tr>
+      <tr>
+        <td>Giripurno</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>120</td>    
+      </tr>
+      <tr>
+        <td>Sumber Brantas</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>10</td>
+        <td>120</td>    
+      </tr>
+      <tr style="font-weight: bold;">
+        <td colspan=2 class="text-center">Total</td>
+        <td>240</td>
+        <td>240</td>
+        <td>240</td>
+        <td>240</td>
+        <td>240</td>
+        <td>240</td>
+        <td>240</td>
+        <td>240</td>
+        <td>240</td>
+        <td>240</td>
+        <td>240</td>
+        <td>240</td> 
+        <td>2880</td> 
+      </tr>
+      </tbody>
+    </table>
     </div>
 </div>
 
