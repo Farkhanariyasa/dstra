a6d95be9c8bc023e2100e2463a4777c01a526371
NickoRagasukma <gabrielprimaditya@gmail.com>
2023-02-15 07:40:03 +0700

    M Views : Modif kajian riset 2

--- public/pvd/js/riset2/hasilSPRiset2_jk_bar.js
+++ public/pvd/js/riset2/hasilSPRiset2_jk_bar.js
@@ -1,5 +1,6 @@
+// Chart
 const jk_bar = document.getElementById("jk_bar");
-new Chart(jk_bar, {
+const bar_jenis_kelamin = new Chart(jk_bar, {
   type: "bar",
   data: {
     labels: ["Laki-laki", "Perempuan"],
@@ -68,3 +69,21 @@ new Chart(jk_bar, {
   },
   plugins: [ChartDataLabels],
 });
+
+// Chart Legend
+// const barChartAxis = document.getElementById("barChartAxis");
+// barChartAxis.addEventListener("click", update_value(check_axis));
+
+// function update_value(check_axis) {
+//   if (check_axis.checked) {
+//     console.log("check");
+//     bar_jenis_kelamin.options.scales.x.display = true;
+//     bar_jenis_kelamin.scales.y.display = true;
+//     bar_jenis_kelamin.update();
+//   } else {
+//     console.log("uncheck");
+//     bar_jenis_kelamin.options.scales.x.display = false;
+//     bar_jenis_kelamin.options.scales.y.display = false;
+//     bar_jenis_kelamin.update();
+//   }
+// }
