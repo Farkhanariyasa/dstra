5f8c686857963ee4afb5545d661e7f3398fc054e
prasetyofmalik <222011325@stis.ac.id>
2023-02-16 06:37:08 +0700

    M View: revisi link dokumen kuesioner

--- app/Views/pvd/pages/dasbor/riset3/dokumen/kuesioner.php
+++ app/Views/pvd/pages/dasbor/riset3/dokumen/kuesioner.php
@@ -7,7 +7,7 @@
 <div class="tab-pane active p-2" id="tab1" role="tabpanel" aria-labelledby="tab-1" style="height: auto;">
   <div class="row">
     <div style="width: 100%;">
-      <embed src="https://drive.google.com/file/d/1xuJ295CpXFn0BJrmJlnbs4wTf-nr3554/preview" type="application/pdf" width=100% height="600px" />
+      <embed src="https://drive.google.com/file/d/1cwNctVF8pr9cn_PPG27hWVP-cOhw0200/preview" type="application/pdf" width=100% height="600px" />
     </div>
   </div>
 </div>
@@ -17,7 +17,7 @@
 <div class="tab-pane active p-2" id="tab1" role="tabpanel" aria-labelledby="tab-1" style="height: auto;">
   <div class="row">
     <div style="width: 100%;">
-      <embed src="https://drive.google.com/file/d/1frfAz4IZeK4SxGDQQSk_E8AYdpesqwap/preview" type="application/pdf" width=100% height="600px" />
+      <embed src="https://drive.google.com/file/d/11rfm3A1_3I5RGTzZrQoj1GZpnEAOKbMS/preview" type="application/pdf" width=100% height="600px" />
     </div>
   </div>
 </div>
