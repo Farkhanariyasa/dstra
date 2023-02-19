616ec82b69c948e8c6a421b2e7388325ed51eaf9
LuthfilKA <222011321@stis.ac.id>
2023-02-19 10:14:32 +0700

    M Views : Dasboar riset 3 show/hide div pake dropdown

--- app/Views/pvd/pages/dasbor/riset3/hasilkajian/tujuan3.php
+++ app/Views/pvd/pages/dasbor/riset3/hasilkajian/tujuan3.php
@@ -2,7 +2,232 @@
 
 <?= $this->section('content'); ?>
 
-<h1>Hasil Kajian Tujuan 3</h1>
+
+
+<div class="container px-0">
+    <header class="section-header">
+        <h6 style="color: #4b395f;"><strong>Menganalisis faktor-faktor yang memengaruhi jam kerja tenaga kerja lokal di industri pariwisata.</strong>
+        </h6>
+    </header>
+    <hr class="hrjudul">
+
+    <div class="menu">
+        <select class="form-select form-select-sm tujuanfilter" style="border-radius: 15px;" name="tujuan3" id="name">
+            <option selected value="tujuan3-1">Persentase jam kerja menurut jenis kelamin</option>
+            <option value="tujuan3-2">Persentase jam kerja menurut status perkawinan</option>
+            <option value="tujuan3-3">Persentase jam kerja menurut tingkat pendapatan</option>
+            <option value="tujuan3-4">Persentase jam kerja menurut wilayah tempat tinggal</option>
+            <option value="tujuan3-5">Persentase jam kerja menurut tingkat pendidikan</option>
+            <option value="tujuan3-6">Persentase jam kerja menurut keikutsertaan dalam pelatihan</option>
+        </select>
+    </div>
+
+    <div class="isi-tujuan shadow mt-4">
+        <div id="tujuan3-1" class="datatujuan" style="width:100%">
+            <h4 class="card-title judul-card ">Persentase jam kerja menurut jenis kelamin</h4>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-lg-6 grid-margin stretch-card h-100">
+                        <div class="card bg-secondary">
+                            <div class="card-body">
+                                <div class="chartBox">
+                                    <canvas class="mt1" id="tk_pie"></canvas>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 grid-margin stretch-card h-100">
+                        <div class="card bg-secondary">
+                            <div class="card-body">
+                                <div class="chartBox">
+                                    <canvas class="mt1" id="tk_pie"></canvas>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-12 grid-margin stretch-card">
+                        <div class="card bg-secondary">
+                            <div class="card-body">
+                                <div class="chartBox">
+                                    <canvas class="mt1" id="tk_pie"></canvas>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="tujuan3-2" class="datatujuan" style="width:100%">
+            <h4 class="card-title  judul-card">Persentase jam kerja menurut status perkawinan</h4>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-lg-6 grid-margin stretch-card h-100">
+                        <div class="card bg-secondary">
+                            <div class="card-body">
+                                <div class="chartBox">
+                                    <canvas class="mt1" id="tk_pie"></canvas>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 grid-margin stretch-card h-100">
+                        <div class="card bg-secondary">
+                            <div class="card-body">
+                                <div class="chartBox">
+                                    <canvas class="mt1" id="tk_pie"></canvas>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-12 grid-margin stretch-card">
+                        <div class="card bg-secondary">
+                            <div class="card-body">
+                                <div class="chartBox">
+                                    <canvas class="mt1" id="tk_pie"></canvas>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="tujuan3-3" class="datatujuan" style="width:100%">
+            <h4 class="card-title judul-card ">Persentase jam kerja menurut jenis kelamin</h4>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-lg-6 grid-margin stretch-card h-100">
+                        <div class="card bg-secondary">
+                            <div class="card-body">
+                                <div class="chartBox">
+                                    <canvas class="mt1" id="tk_pie"></canvas>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 grid-margin stretch-card h-100">
+                        <div class="card bg-secondary">
+                            <div class="card-body">
+                                <div class="chartBox">
+                                    <canvas class="mt1" id="tk_pie"></canvas>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-12 grid-margin stretch-card">
+                        <div class="card bg-secondary">
+                            <div class="card-body">
+                                <div class="chartBox">
+                                    <canvas class="mt1" id="tk_pie"></canvas>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="tujuan3-4" class="datatujuan" style="width:100%">
+            <h4 class="card-title judul-card ">Persentase jam kerja menurut jenis kelamin</h4>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-lg-6 grid-margin stretch-card h-100">
+                        <div class="card bg-secondary">
+                            <div class="card-body">
+                                <div class="chartBox">
+                                    <canvas class="mt1" id="tk_pie"></canvas>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 grid-margin stretch-card h-100">
+                        <div class="card bg-secondary">
+                            <div class="card-body">
+                                <div class="chartBox">
+                                    <canvas class="mt1" id="tk_pie"></canvas>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-12 grid-margin stretch-card">
+                        <div class="card bg-secondary">
+                            <div class="card-body">
+                                <div class="chartBox">
+                                    <canvas class="mt1" id="tk_pie"></canvas>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="tujuan3-5" class="datatujuan" style="width:100%">
+            <h4 class="card-title judul-card ">Persentase jam kerja menurut jenis kelamin</h4>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-lg-6 grid-margin stretch-card h-100">
+                        <div class="card bg-secondary">
+                            <div class="card-body">
+                                <div class="chartBox">
+                                    <canvas class="mt1" id="tk_pie"></canvas>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 grid-margin stretch-card h-100">
+                        <div class="card bg-secondary">
+                            <div class="card-body">
+                                <div class="chartBox">
+                                    <canvas class="mt1" id="tk_pie"></canvas>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-12 grid-margin stretch-card">
+                        <div class="card bg-secondary">
+                            <div class="card-body">
+                                <div class="chartBox">
+                                    <canvas class="mt1" id="tk_pie"></canvas>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="tujuan3-6" class="datatujuan" style="width:100%">
+            <h4 class="card-title judul-card ">Persentase jam kerja menurut jenis kelamin</h4>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-lg-6 grid-margin stretch-card h-100">
+                        <div class="card bg-secondary">
+                            <div class="card-body">
+                                <div class="chartBox">
+                                    <canvas class="mt1" id="tk_pie"></canvas>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 grid-margin stretch-card h-100">
+                        <div class="card bg-secondary">
+                            <div class="card-body">
+                                <div class="chartBox">
+                                    <canvas class="mt1" id="tk_pie"></canvas>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-12 grid-margin stretch-card">
+                        <div class="card bg-secondary">
+                            <div class="card-body">
+                                <div class="chartBox">
+                                    <canvas class="mt1" id="tk_pie"></canvas>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 <?= $this->endSection(); ?>
-
