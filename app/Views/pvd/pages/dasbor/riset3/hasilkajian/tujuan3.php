2e02b01444eb09e47b5d48f05180240d835e6db2
LuthfilKA <222011321@stis.ac.id>
2023-02-19 10:21:52 +0700

    M Views : Load Jekuer1

--- app/Views/pvd/pages/dasbor/riset3/hasilkajian/tujuan3.php
+++ app/Views/pvd/pages/dasbor/riset3/hasilkajian/tujuan3.php
@@ -229,5 +229,10 @@
         </div>
     </div>
 </div>
+<!-- Jquery -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js" integrity="sha512-STof4xm1wgkfm7heWqFJVn58Hm3EtS31XFaagaa8VMReCXAkQnJZ+jEy8PCC/iT18dFy95WcExNHFTqLyp72eQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
+
+<!-- dropdown show/hide content-->
+<script src="<?= base_url('pvd/js/riset3/dropdown_showhide.js') ?>"></script>
 <?= $this->endSection(); ?>
