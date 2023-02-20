6d408f8f502ec8e3a2d611eb5b06ff56dcec2a30
NickoRagasukma <gabrielprimaditya@gmail.com>
2023-02-20 13:06:02 +0700

    A Assets : Tambah violin plot js, belum fix

---
+++ public/pvd/js/riset2/hasilSPRiset2_violin.js
@@ -0,0 +1,35 @@
+const boxplot_chart = document.getElementById("boxplot_chart");
+
+new Chart(boxplot_chart, {
+    type: "boxplot",
+    data: {
+        labels: ["Pendapatan (Rupiah)"],
+        datasets: [
+        {
+            label: "Pendapatan",
+            data: [datapendapatan_riset3],
+            backgroundColor: ["#4b395f"],
+        },
+        ],
+    },
+    options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+        title: {
+            display: true,
+            text: "Sebaran Pendapatan Tenaga Kerja Pariwisata di Kabupaten Malang",
+            font: {
+            size: 20,
+            },
+        },
+        },
+    },
+    scales: {
+        y: {
+        type: "linear",
+        min: Math.min(datapendapatan_riset3),
+        max: Math.max(datapendapatan_riset3),
+        },
+    },
+});
