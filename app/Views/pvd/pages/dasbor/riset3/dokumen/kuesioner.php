4020d3079d249aaddd38faf828a38f51ad88fb68
prasetyofmalik <222011325@stis.ac.id>
2023-02-27 22:01:04 +0700

    M View: perbaiki tampilan dokumen kues riset 3

--- app/Views/pvd/pages/dasbor/riset3/dokumen/kuesioner.php
+++ app/Views/pvd/pages/dasbor/riset3/dokumen/kuesioner.php
@@ -2,22 +2,24 @@
 
 <?= $this->section('content'); ?>
 
-<h2>Kuesioner Listing</h2>
-
-<div class="tab-pane active p-2" id="tab1" role="tabpanel" aria-labelledby="tab-1" style="height: auto;">
-  <div class="row">
-    <div style="width: 100%;">
-      <embed src="https://drive.google.com/file/d/1cwNctVF8pr9cn_PPG27hWVP-cOhw0200/preview" type="application/pdf" width=100% height="600px" />
+<div class="third5 par m-auto p-3 p-md-5 shadow box rounded justify-content-center" style="line-height: 30px;" data-aos="zoom-in" data-aos-duration="1000">
+  <h2>Kuesioner Listing</h2>
+  <div class="tab-pane active p-2" id="tab1" role="tabpanel" aria-labelledby="tab-1" style="height: auto; width: 100%">
+    <div class="row">
+      <div>
+        <embed src="https://drive.google.com/file/d/1cwNctVF8pr9cn_PPG27hWVP-cOhw0200/preview" type="application/pdf" width=100% height="600px" />
+      </div>
     </div>
   </div>
 </div>
 
-<h2>Kuesioner Pencacahan</h2>
-
-<div class="tab-pane active p-2" id="tab1" role="tabpanel" aria-labelledby="tab-1" style="height: auto;">
-  <div class="row">
-    <div style="width: 100%;">
-      <embed src="https://drive.google.com/file/d/11rfm3A1_3I5RGTzZrQoj1GZpnEAOKbMS/preview" type="application/pdf" width=100% height="600px" />
+<div class="third5 par mx-auto mt-5 p-3 p-md-5 shadow box rounded justify-content-center" style="line-height: 30px;" data-aos="zoom-in" data-aos-duration="1000">
+  <h2>Kuesioner Pencacahan</h2>
+  <div class="tab-pane active p-2" id="tab2" role="tabpanel" aria-labelledby="tab-2" style="height: auto; width: 100%">
+    <div class="row">
+      <div>
+        <embed src="https://drive.google.com/file/d/11rfm3A1_3I5RGTzZrQoj1GZpnEAOKbMS/preview" type="application/pdf" width=100% height="600px" />
+      </div>
     </div>
   </div>
 </div>
