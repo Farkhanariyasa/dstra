4dc3b8220669347a921a5f2432cebabac26da5d7
prasetyofmalik <222011325@stis.ac.id>
2023-02-27 22:00:27 +0700

    M View: perbaiki tampilan dokumen bukped

--- app/Views/pvd/pages/dasbor/riset3/dokumen/bukupedoman.php
+++ app/Views/pvd/pages/dasbor/riset3/dokumen/bukupedoman.php
@@ -2,22 +2,24 @@
 
 <?= $this->section('content'); ?>
 
-<h2>Buku Pedoman Listing</h2>
-
-<div class="tab-pane active p-2" id="tab1" role="tabpanel" aria-labelledby="tab-1" style="height: auto;">
-  <div class="row">
-    <div style="width: 100%;">
-      <embed src="https://drive.google.com/file/d/16QYIS3yoEmI91RYZJ8vVJi0QogDgKAf4/preview" type="application/pdf" width=100% height="600px" />
+<div class="third5 par m-auto p-3 p-md-5 shadow box rounded justify-content-center" style="line-height: 30px;" data-aos="zoom-in" data-aos-duration="1000">
+  <h2>Buku Pedoman Listing</h2>
+  <div class="tab-pane active p-2" id="tab1" role="tabpanel" aria-labelledby="tab-1" style="height: auto; width:100%">
+    <div class="row">
+      <div>
+        <embed src="https://drive.google.com/file/d/16QYIS3yoEmI91RYZJ8vVJi0QogDgKAf4/preview" type="application/pdf" width=100% height="600px" />
+      </div>
     </div>
   </div>
 </div>
 
-<h2>Buku Pedoman Pencacahan</h2>
-
-<div class="tab-pane active p-2" id="tab1" role="tabpanel" aria-labelledby="tab-1" style="height: auto;">
-  <div class="row">
-    <div style="width: 100%;">
-      <embed src="https://drive.google.com/file/d/17pXhqRCHhgTCobMJIW_M_76QLpgEP1kE/preview" type="application/pdf" width=100% height="600px" />
+<div class="third5 par mx-auto mt-5 p-3 p-md-5 shadow box rounded justify-content-center" style="line-height: 30px;" data-aos="zoom-in" data-aos-duration="1000">
+  <h2>Buku Pedoman Pencacahan</h2>
+  <div class="tab-pane active p-2" id="tab2" role="tabpanel" aria-labelledby="tab-2" style="height: auto; width: 100%">
+    <div class="row">
+      <div>
+        <embed src="https://drive.google.com/file/d/17pXhqRCHhgTCobMJIW_M_76QLpgEP1kE/preview" type="application/pdf" width=100% height="600px" />
+      </div>
     </div>
   </div>
 </div>
