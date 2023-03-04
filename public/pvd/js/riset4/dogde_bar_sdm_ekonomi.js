4afc37db069c9fc1c242363bd5a9b436390e1b08
imams <222011830@gmail.com>
2023-03-04 10:22:01 +0700

    M View : Menambah fungsi hapus legend

--- public/pvd/js/riset4/dogde_bar_sdm_ekonomi.js
+++ public/pvd/js/riset4/dogde_bar_sdm_ekonomi.js
@@ -126,4 +126,17 @@ function update_value_axis_dodge_bar_sdm_ekonomi(chk_bx) {
   }
 };
 
+const dodgeBarSdmEkonomiLegend = document.getElementById("forDodgeBarSdmEkonomiLegend");
+dodgeBarSdmEkonomiLegend.addEventListener("click", update_value_legend_dodge_bar_sdm_ekonomi(chk_bx));
 
+function update_value_legend_dodge_bar_sdm_ekonomi(chk_bx) {
+    if (chk_bx.checked) {
+      console.log("check");
+      bar.options.plugins.legend.display=true;
+      bar.update();
+    } else {
+      console.log("uncheck");
+      bar.options.plugins.legend.display=false;
+      bar.update();
+    }
+};
