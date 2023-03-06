059c58f19641c60ffb6b4aca73a916559d9f2f23
NickoRagasukma <gabrielprimaditya@gmail.com>
2023-03-06 22:05:24 +0700

    M Views : Modif id download button riset 2

--- app/Views/pvd/pages/dasbor/riset2/hasil_penelitian/dummy_kajian1.php
+++ app/Views/pvd/pages/dasbor/riset2/hasil_penelitian/dummy_kajian1.php
@@ -1,100 +1,38 @@
-<!-- Visualisasi 1 -->
 <div class="portfolio-item filter-riset2-hasilkajian1-1">
-    <div class="row mt-3">
-        <div class="col-lg-12 grid-margin stretch-card">
-            <div class="card shadow">
-                <div class="card-body">
-                    <div class="position-absolute top-0 end-0 d-flex flex-row justify-content-center align-item-center ">
-                        <div class="me-1 mt-1 justify-content-end align-item-end">
-                            <button id="unduh-2-1" type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                                <i class="fa-solid fa-download"></i>
-                            </button>
-                        </div>
-                    </div>
-
-                    <div style=" height:500px;">
-                        <canvas class="mt1" id="jk_bar"></canvas>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="container mt-3 mb-5">
-        <div class="card d-flex justify-content-center flex-column align-item-center flex-md primary pb-5 pt-5 shadow">
-            <p class="card-text text-center">
-                <strong>Kota Malang</strong>
-            </p>
-            <div class="d-flex flex-row align-item-center justify-content-center">
-                <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center">
+    <div class="col-12 grid-margin stretch-card">
+        <div class="card shadow">
+            <div class="card-body">
+                <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center ">
                     <div class="me-1 mt-1 justify-content-end align-item-end">
-                        <button id="" type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="">
+                        <button id="unduh-2-1" type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal">
                             <i class="fa-solid fa-download"></i>
                         </button>
                     </div>
                 </div>
-                <div id="peta_malang_hasil1" style="width: 95%; height:80vh;"></div>
+                <div style=" height:500px;">
+                    <canvas class="mt1" id="jk_bar"></canvas>
+                </div>
+
             </div>
         </div>
     </div>
 </div>
 
-<!-- Visualisasi 2 -->
+
 <div class="portfolio-item filter-riset2-hasilkajian1-2">
-    <div class="row mt-3">
+    <div class="row">
         <div class="col-lg-12 grid-margin stretch-card">
             <div class="card shadow">
                 <div class="card-body">
-                    <div class="position-absolute top-0 end-0 d-flex flex-row justify-content-center align-item-center ">
+                    <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center ">
                         <div class="me-1 mt-1 justify-content-end align-item-end">
                             <button id="unduh-2-2" type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal2">
                                 <i class="fa-solid fa-download"></i>
                             </button>
                         </div>
                     </div>
-
                     <div style=" height:500px;">
                         <canvas class="mt1" id="bar_horizontal"></canvas>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="row mt-3">
-        <div class="col-lg-12 grid-margin stretch-card">
-            <div class="card shadow">
-                <div class="card-body">
-                    <div class="position-absolute top-0 end-0 d-flex flex-row justify-content-center align-item-center ">
-                        <div class="me-1 mt-1 justify-content-end align-item-end">
-                            <button id="unduh-2-3" type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal3">
-                                <i class="fa-solid fa-download"></i>
-                            </button>
-                        </div>
-                    </div>
-
-                    <div style=" height:500px;">
-                        <canvas class="mt1" id="tu_bar"></canvas>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="row mt-3">
-        <div class="col-lg-12 grid-margin stretch-card">
-            <div class="card shadow">
-                <div class="card-body">
-                    <div class="position-absolute top-0 end-0 d-flex flex-row justify-content-center align-item-center ">
-                        <div class="me-1 mt-1 justify-content-end align-item-end">
-                            <button id="unduh-2-4" type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal4">
-                                <i class="fa-solid fa-download"></i>
-                            </button>
-                        </div>
-                    </div>
-                    
-                    <div class="chartBox" style="height:500px;">
-                        <canvas class="mt1" id="pw_sankey"></canvas>
                     </div>
                 </div>
             </div>
@@ -120,47 +58,28 @@
     </div>
 </div>
 
-<!-- Visualisasi 3 -->
+
 <div class="portfolio-item filter-riset2-hasilkajian1-3">
-    <div class="row mt-3">
+    <div class="row">
         <div class="col-lg-12 grid-margin stretch-card">
             <div class="card shadow">
                 <div class="card-body">
-                    <div class="position-absolute top-0 end-0 d-flex flex-row justify-content-center align-item-center ">
+                    <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center ">
                         <div class="me-1 mt-1 justify-content-end align-item-end">
-                            <button id="unduh-2-5" type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal5">
+                            <button id="unduh-2-3" type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal3">
                                 <i class="fa-solid fa-download"></i>
                             </button>
                         </div>
                     </div>
-
                     <div style=" height:500px;">
-                        <canvas class="mt1" id="pie_chart"></canvas>
+                        <canvas class="mt1" id="tu_bar"></canvas>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 
-    <div class="row mt-3">
-        <div class="col-lg-12 grid-margin stretch-card">
-            <div class="card shadow">
-                <div class="card-body">
-                    <div class="position-absolute top-0 end-0 d-flex flex-row justify-content-center align-item-center ">
-                        <div class="me-1 mt-1 justify-content-end align-item-end">
-                            <button id="unduh-2-6" type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal6">
-                                <i class="fa-solid fa-download"></i>
-                            </button>
-                        </div>
-                    </div>
-
-                    <div style=" height:500px;">
-                        <canvas class="mt1" id="twoway_bar"></canvas>
-                    </div>
-                </div>
-            </div>
-        </div>
 </div>
 
-<!-- Modal Hasil Penelitian Riset 2  -->
+<!-- Modal Riset 1 -->
 <?php include('modal_hp.php') ?>
