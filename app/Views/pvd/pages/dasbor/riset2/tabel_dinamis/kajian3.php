e1316d227782dac211f75723805f1deb23c4a5e1
NickoRagasukma <gabrielprimaditya@gmail.com>
2023-03-06 11:53:50 +0700

    M Views : Modif script datatables js riset 2

--- app/Views/pvd/pages/dasbor/riset2/tabel_dinamis/kajian3.php
+++ app/Views/pvd/pages/dasbor/riset2/tabel_dinamis/kajian3.php
@@ -27,7 +27,7 @@
 </section>
 
 <!-- Datatable Instal -->
-<script src="<?= base_url('pvd/js/datatables_install.js'); ?>"></script>
+<script src="<?= base_url('pvd/js/riset2/hasilSPRiset2_tabelkajian3.js'); ?>"></script>
 <!-- Istope -->
 <script src="https://unpkg.com/isotope-layout@3/dist/isotope.pkgd.js"></script>
 
