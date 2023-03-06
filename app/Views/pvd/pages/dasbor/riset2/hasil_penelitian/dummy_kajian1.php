a46f91d065e9527ac62f66869cf815bc48a2232b
NickoRagasukma <gabrielprimaditya@gmail.com>
2023-03-06 21:48:35 +0700

    M Views : Modif id download button riset 2

--- app/Views/pvd/pages/dasbor/riset2/hasil_penelitian/dummy_kajian1.php
+++ app/Views/pvd/pages/dasbor/riset2/hasil_penelitian/dummy_kajian1.php
@@ -1,23 +1,5 @@
 <!-- Visualisasi 1 -->
 <div class="portfolio-item filter-riset2-hasilkajian1-1">
-    <!-- <div class="row mt-5">
-        <div class="col-sm-6 my-2">
-            <div class="card">
-                <div class="card-body">
-                    <p class="card-text text-center">Grafik<br><br><br><br><br><br><br><br><br><br><br></p>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-sm-6 my-2">
-            <div class="card">
-                <div class="card-body">
-                    <p class="card-text text-center">Grafik<br><br><br><br><br><br><br><br><br><br><br></p>
-                </div>
-            </div>
-        </div>
-    </div> -->
-
     <div class="row mt-3">
         <div class="col-lg-12 grid-margin stretch-card">
             <div class="card shadow">
@@ -65,7 +47,7 @@
                 <div class="card-body">
                     <div class="position-absolute top-0 end-0 d-flex flex-row justify-content-center align-item-center ">
                         <div class="me-1 mt-1 justify-content-end align-item-end">
-                            <button id="" type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="">
+                            <button id="unduh-2-2" type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal2">
                                 <i class="fa-solid fa-download"></i>
                             </button>
                         </div>
@@ -85,7 +67,7 @@
                 <div class="card-body">
                     <div class="position-absolute top-0 end-0 d-flex flex-row justify-content-center align-item-center ">
                         <div class="me-1 mt-1 justify-content-end align-item-end">
-                            <button id="" type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="">
+                            <button id="unduh-2-3" type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal3">
                                 <i class="fa-solid fa-download"></i>
                             </button>
                         </div>
@@ -105,7 +87,7 @@
                 <div class="card-body">
                     <div class="position-absolute top-0 end-0 d-flex flex-row justify-content-center align-item-center ">
                         <div class="me-1 mt-1 justify-content-end align-item-end">
-                            <button id="" type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="">
+                            <button id="unduh-2-4" type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal4">
                                 <i class="fa-solid fa-download"></i>
                             </button>
                         </div>
@@ -146,7 +128,7 @@
                 <div class="card-body">
                     <div class="position-absolute top-0 end-0 d-flex flex-row justify-content-center align-item-center ">
                         <div class="me-1 mt-1 justify-content-end align-item-end">
-                            <button id="" type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="">
+                            <button id="unduh-2-5" type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal5">
                                 <i class="fa-solid fa-download"></i>
                             </button>
                         </div>
@@ -166,7 +148,7 @@
                 <div class="card-body">
                     <div class="position-absolute top-0 end-0 d-flex flex-row justify-content-center align-item-center ">
                         <div class="me-1 mt-1 justify-content-end align-item-end">
-                            <button id="" type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="">
+                            <button id="unduh-2-6" type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal6">
                                 <i class="fa-solid fa-download"></i>
                             </button>
                         </div>
