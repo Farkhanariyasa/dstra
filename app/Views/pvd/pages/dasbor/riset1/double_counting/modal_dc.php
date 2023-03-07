afbd64880fbb78a7b21e88f655411b4c8baa3186
Thomson <222011365@stis.ac.id>
2023-03-07 11:39:09 +0700

    M Views: modal double counting riset 1

--- app/Views/pvd/pages/dasbor/riset1/double_counting/modal_dc.php
+++ app/Views/pvd/pages/dasbor/riset1/double_counting/modal_dc.php
@@ -103,3 +103,38 @@
         </div>
     </div>
 </div>
+
+<!-- 4 -->
+<div class="modal fade" id="exampleModal4" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title" id="exampleModalLabel">Unduh Hasil PKL</h4>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <form name="unduhVisualisasi" action="<?= base_url(); ?>/hasil-pkl/riset1/unduh" method="POST">
+                <div class="modal-body">
+                    <div class="mb-1">
+                        <label for="recipient-name" class="col-form-label">Nama <span class="text-danger">*</span></label>
+                        <input oninput="getvalue()" type="text" class="form-control nama" name="nama" id="nama" placeholder="Nama Anda" required>
+                        <div id="errornama" class="errorvlds"></div>
+                    </div>
+                    <div class="mb-1">
+                        <label for="recipient-name" class="col-form-label" name="emailunduh">Email <span class="text-danger">*</span></label>
+                        <input oninput="getvalue()" type="email" class="form-control email" name="email" id="email" placeholder="Email Anda" required>
+                        <div id="erroremail" class="errorvlds"></div>
+                    </div>
+                    <div class="mb-1">
+                        <label for="recipient-name" class="col-form-label">Instansi <span class="text-danger">*</span></label>
+                        <input oninput="getvalue()" type="text" class="form-control instansi" name="instansi" id="instansi" placeholder="Instansi Asal" required>
+                        <div id="errorinstansi" class="errorvlds"></div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="tombol btn-danger" data-bs-dismiss="modal">Kembali</button>
+                    <button disabled type="submit" id="btndownload4" class="tombolkonf" data-bs-dismiss="modal">Konfirmasi</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
