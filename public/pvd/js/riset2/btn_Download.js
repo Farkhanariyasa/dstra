6aa2d55710c77bf346cc9719f038d6edbbe22803
NickoRagasukma <gabrielprimaditya@gmail.com>
2023-03-08 20:14:50 +0700

    M Assets : Modif download js riset 2

--- public/pvd/js/riset2/btn_Download.js
+++ public/pvd/js/riset2/btn_Download.js
@@ -66,7 +66,7 @@ document.getElementById("btndownload1").addEventListener("click", function (e) {
   createEl.href = canvasUrl;
 
   // This is the name of our downloaded file
-  createEl.download = "Riset 2";
+  createEl.download = "Hasil SP Riset 2 - Hasil 1";
 
   // Click the download button, causing a download, and then remove it
   createEl.click();
@@ -89,6 +89,21 @@ document.getElementById("btndownload2").addEventListener("click", function (e) {
 });
 
 document.getElementById("btndownload3").addEventListener("click", function (e) {
+  // Convert our canvas to a data URL
+  let canvasUrl = document.getElementById("tu_bar").toDataURL();
+  // Create an anchor, and set the href value to our data URL
+  const createEl = document.createElement("a");
+  createEl.href = canvasUrl;
+
+  // This is the name of our downloaded file
+  createEl.download = "Riset 2";
+
+  // Click the download button, causing a download, and then remove it
+  createEl.click();
+  createEl.remove();
+});
+
+document.getElementById("btndownload4").addEventListener("click", function (e) {
   // Convert our canvas to a data URL
   let canvasUrl = document.getElementById("tu_bar").toDataURL();
   // Create an anchor, and set the href value to our data URL
