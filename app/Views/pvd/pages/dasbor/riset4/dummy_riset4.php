f8efa7e2a8b6be5e6bfe488bd76fc862be9317e2
imams <222011830@gmail.com>
2023-03-09 19:35:46 +0700

    M View : Menambah div untuk peta

--- app/Views/pvd/pages/dasbor/riset4/dummy_riset4.php
+++ app/Views/pvd/pages/dasbor/riset4/dummy_riset4.php
@@ -209,6 +209,47 @@
             </div>
         </div>
     </div>
+</div>
+
+<div class="portfolio-item filter-riset4-hasil11" onresize="responsivefonts()">
+    <div class="container mt-5 mb-5">
+        <div class="card d-flex justify-content-center flex-column align-item-center flex-md primary pb-5 pt-5 shadow">
+            <p class="card-text text-center">
+                <strong>Kota Malang</strong>
+            </p>
+            <div class="d-flex flex-row align-item-center justify-content-center">
+                <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center">
+                    <div class="me-1 mt-1 justify-content-end align-item-end">
+                        <button id="" type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="">
+                            <i class="fa-solid fa-download"></i>
+                        </button>
+                    </div>
+                </div>
+                <div id="peta1111" style="width: 95%; height:80vh;"></div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="portfolio-item filter-riset4-hasil12" onresize="responsivefonts()">
+    <div class="container mt-5 mb-5">
+        <div class="card d-flex justify-content-center flex-column align-item-center flex-md primary pb-5 pt-5 shadow">
+            <p class="card-text text-center">
+                <strong>Persentase UUP yang Menggunakan TIK dalam Kegiatan Usaha</strong>
+            </p>
+            <div class="d-flex flex-row align-item-center justify-content-center">
+                <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center">
+                    <div class="me-1 mt-1 justify-content-end align-item-end">
+                        <button id="" type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="">
+                            <i class="fa-solid fa-download"></i>
+                        </button>
+                    </div>
+                </div>
+                <div id="peta-batu" style="width: 95%; height:80vh;"></div>
+            </div>
+        </div>
+    </div>
+</div>
 
 <!-- Modal Riset 4  -->
 <?php include('modal_riset4.php') ?>
