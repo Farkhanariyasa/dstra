f8c80888b338a5c59882c5677b27c328b8ffeded
NickoRagasukma <gabrielprimaditya@gmail.com>
2023-03-11 15:31:54 +0700

    M Views : Modif id dan class dummy tujuan 1 riset 2

--- app/Views/pvd/pages/dasbor/riset2/hasil_penelitian/dummy_tujuan1.php
+++ app/Views/pvd/pages/dasbor/riset2/hasil_penelitian/dummy_tujuan1.php
@@ -1,6 +1,6 @@
 <!-- Tab 1 -->
-<div class="portfolio-item filter-riset2-hasilkajian1-1">
-    <div class="col-12 grid-margin stretch-card">
+<div class="portfolio-item filter-riset2-hasiltujuan1-1">
+    <!-- <div class="col-12 grid-margin stretch-card">
         <div class="card shadow">
             <div class="card-body">
                 <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center ">
@@ -16,11 +16,29 @@
 
             </div>
         </div>
+    </div> -->
+
+    <div class="container mt-5 mb-5">
+        <div class="card d-flex justify-content-center flex-column align-item-center flex-md primary pb-5 pt-5 shadow">
+            <p class="card-text text-center">
+                <strong>Sebaran Wisatawan dalam Kota Malang menurut Kecamatan Asal Wisatawan di Kota Malang Bulan Oktober-Desember 2022</strong>
+            </p>
+            <div class="d-flex flex-row align-item-center justify-content-center">
+                <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center">
+                    <div class="me-1 mt-1 justify-content-end align-item-end">
+                        <button id="" type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="">
+                            <i class="fa-solid fa-download"></i>
+                        </button>
+                    </div>
+                </div>
+                <div id="petatematik1" style="width: 95%; height:80vh; z-index:0;"></div>
+            </div>
+        </div>
     </div>
 </div>
 
 <!-- Tab 2 -->
-<div class="portfolio-item filter-riset2-hasilkajian1-2">
+<div class="portfolio-item filter-riset2-hasiltujuan1-2">
     <div class="row">
         <div class="col-lg-12 grid-margin stretch-card">
             <div class="card shadow">
@@ -39,28 +57,10 @@
             </div>
         </div>
     </div>
-
-    <div class="container mt-5 mb-5">
-        <div class="card d-flex justify-content-center flex-column align-item-center flex-md primary pb-5 pt-5 shadow">
-            <p class="card-text text-center">
-                <strong>Kota Malang</strong>
-            </p>
-            <div class="d-flex flex-row align-item-center justify-content-center">
-                <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center">
-                    <div class="me-1 mt-1 justify-content-end align-item-end">
-                        <button id="" type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="">
-                            <i class="fa-solid fa-download"></i>
-                        </button>
-                    </div>
-                </div>
-                <div id="peta_malang_hasil2" style="width: 95%; height:80vh; z-index:0;"></div>
-            </div>
-        </div>
-    </div>
 </div>
 
 <!-- Tab 3 -->
-<div class="portfolio-item filter-riset2-hasilkajian1-3">
+<div class="portfolio-item filter-riset2-hasiltujuan1-3">
     <div class="row">
         <div class="col-lg-12 grid-margin stretch-card">
             <div class="card shadow">
@@ -82,5 +82,5 @@
 
 </div>
 
-<!-- Modal Riset 1 -->
+<!-- Modal Riset 2 -->
 <?php include('modal_hp.php') ?>
