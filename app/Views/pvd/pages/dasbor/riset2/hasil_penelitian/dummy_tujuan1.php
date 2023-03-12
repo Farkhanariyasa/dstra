2dfeeede514dfef85c854b52448a7e1939ec4d5f
NickoRagasukma <gabrielprimaditya@gmail.com>
2023-03-12 15:22:33 +0700

    M Views : Tambah pie chart riset 2

--- app/Views/pvd/pages/dasbor/riset2/hasil_penelitian/dummy_tujuan1.php
+++ app/Views/pvd/pages/dasbor/riset2/hasil_penelitian/dummy_tujuan1.php
@@ -1,23 +1,5 @@
 <!-- Tab 1 -->
-<div class="portfolio-item filter-riset2-hasiltujuan1-1">
-    <!-- <div class="col-12 grid-margin stretch-card">
-        <div class="card shadow">
-            <div class="card-body">
-                <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center ">
-                    <div class="me-1 mt-1 justify-content-end align-item-end">
-                        <button id="unduh-2-1" type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                            <i class="fa-solid fa-download"></i>
-                        </button>
-                    </div>
-                </div>
-                <div style=" height:500px;">
-                    <canvas class="mt1" id="riset2_hasil1"></canvas>
-                </div>
-
-            </div>
-        </div>
-    </div> -->
-
+<div class="portfolio-item filter-riset2-hasiltujuan1-1">    
     <div class="container mt-5">
         <div class="card d-flex justify-content-center flex-column align-item-center flex-md primary pb-5 pt-5 shadow">
             <p class="card-text text-center">
@@ -97,7 +79,7 @@
 
 <!-- Tab 3 -->
 <div class="portfolio-item filter-riset2-hasiltujuan1-3">
-    <div class="row">
+    <!-- <div class="row">
         <div class="col-lg-12 grid-margin stretch-card">
             <div class="card shadow">
                 <div class="card-body">
@@ -110,6 +92,25 @@
                     </div>
                     <div style=" height:500px;">
                         <canvas class="mt1" id="riset2_hasil3"></canvas>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> -->
+
+    <div class="row">
+        <div class="col-lg-12 grid-margin stretch-card">
+            <div class="card shadow">
+                <div class="card-body">
+                    <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center ">
+                        <div class="me-1 mt-1 justify-content-end align-item-end">
+                            <button id="unduh-2-2" type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal2">
+                                <i class="fa-solid fa-download"></i>
+                            </button>
+                        </div>
+                    </div>
+                    <div style=" height:500px;">
+                        <canvas class="mt1" id="tp_pie"></canvas>
                     </div>
                 </div>
             </div>
