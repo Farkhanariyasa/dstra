10fe642554847467adad346084ad90c8eeb6f9fb
NickoRagasukma <gabrielprimaditya@gmail.com>
2023-03-12 14:43:26 +0700

    M Views : Tambah peta tematik riset 2

--- app/Views/pvd/pages/dasbor/riset2/hasil_penelitian/dummy_tujuan1.php
+++ app/Views/pvd/pages/dasbor/riset2/hasil_penelitian/dummy_tujuan1.php
@@ -39,7 +39,7 @@
 
 <!-- Tab 2 -->
 <div class="portfolio-item filter-riset2-hasiltujuan1-2">
-    <div class="row">
+    <!-- <div class="row">
         <div class="col-lg-12 grid-margin stretch-card">
             <div class="card shadow">
                 <div class="card-body">
@@ -54,6 +54,24 @@
                         <canvas class="mt1" id="riset2_hasil2"></canvas>
                     </div>
                 </div>
+            </div>
+        </div>
+    </div> -->
+
+    <div class="container mt-5 mb-5">
+        <div class="card d-flex justify-content-center flex-column align-item-center flex-md primary pb-5 pt-5 shadow">
+            <p class="card-text text-center">
+                <strong>Jumlah Perjalanan dalam Kota Malang menurut Kecamatan Asal Wisatawan di Kota Malang Bulan Oktober-Desember 2022</strong>
+            </p>
+            <div class="d-flex flex-row align-item-center justify-content-center">
+                <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center">
+                    <div class="me-1 mt-1 justify-content-end align-item-end">
+                        <button id="" type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="">
+                            <i class="fa-solid fa-download"></i>
+                        </button>
+                    </div>
+                </div>
+                <div id="petatematik2" style="width: 95%; height:80vh; z-index:0;"></div>
             </div>
         </div>
     </div>
