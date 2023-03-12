5bfa1e6a7b2327a4fcd6dc7184b0dd99ad1b199b
NickoRagasukma <gabrielprimaditya@gmail.com>
2023-03-12 17:44:58 +0700

    M Views : Tambah checbox legend pie chart riset 2

--- app/Views/pvd/pages/dasbor/riset2/hasil_penelitian/dummy_tujuan1.php
+++ app/Views/pvd/pages/dasbor/riset2/hasil_penelitian/dummy_tujuan1.php
@@ -112,6 +112,12 @@
                     <div style=" height:500px;">
                         <canvas class="mt1" id="tp_pie"></canvas>
                     </div>
+                    <div class="position-absolute bot-0  end-0 d-flex flex-row justify-content-center align-item-center ">
+                        <div class="form-check form-switch me-3">
+                            <input class="form-check-input" onchange="update_value(this);" type="checkbox" id="forPieChartLegend" value="show">
+                            <label class="form-check-label" style="margin-left:-3px;" for="flexSwitchCheckChecked">Tampilkan Legend</label>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
