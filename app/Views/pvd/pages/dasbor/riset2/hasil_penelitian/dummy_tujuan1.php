22d34835c73d3fc9a2a19b1dfc69dce593824ff1
NickoRagasukma <gabrielprimaditya@gmail.com>
2023-03-12 15:11:10 +0700

    M Views : Tambah sankey riset 2

--- app/Views/pvd/pages/dasbor/riset2/hasil_penelitian/dummy_tujuan1.php
+++ app/Views/pvd/pages/dasbor/riset2/hasil_penelitian/dummy_tujuan1.php
@@ -18,7 +18,7 @@
         </div>
     </div> -->
 
-    <div class="container mt-5 mb-5">
+    <div class="container mt-5">
         <div class="card d-flex justify-content-center flex-column align-item-center flex-md primary pb-5 pt-5 shadow">
             <p class="card-text text-center">
                 <strong>Sebaran Wisatawan dalam Kota Malang menurut Kecamatan Asal Wisatawan di Kota Malang Bulan Oktober-Desember 2022</strong>
@@ -72,6 +72,24 @@
                     </div>
                 </div>
                 <div id="petatematik2" style="width: 95%; height:80vh; z-index:0;"></div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-12 grid-margin stretch-card">
+        <div class="card shadow">
+            <div class="card-body">
+                <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center ">
+                    <div class="me-1 mt-1 justify-content-end align-item-end">
+                        <button id="unduh-2-1" type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                            <i class="fa-solid fa-download"></i>
+                        </button>
+                    </div>
+                </div>
+                <div style=" height:500px;">
+                    <canvas class="mt1" id="pw_sankey"></canvas>
+                </div>
+
             </div>
         </div>
     </div>
