65e6b8e823da79b165c46705b96f129886e50be1
imams <222011830@gmail.com>
2023-03-14 10:21:45 +0700

    A Assets : File bar chart submenu dimensi

---
+++ app/Views/pvd/pages/dasbor/riset4/ikuptik/bar_chart_dimensi.php
@@ -0,0 +1,152 @@
+<!-- Dodge Bar Chart Menu IKUPTIK submenu Dimensi per Kecamatan (Kiri)-->
+<!-- <div class="portfolio-item filter-riset4-hasil1">
+    <div class="row">
+        <div class="col-lg-12 grid-margin stretch-card">
+            <div class="card shadow">
+                <div class="card-body">
+                    <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center ">
+                        <div class="selectBox mt-2 me-2">
+                            <form action="">
+                                <select class="dropdownunduh1" id="pieChart">
+                                    <option value="dimensi1">dimensi1</option>
+                                    <option value="dimensi2">dimensi2</option>
+                                    <option value="dimensi3">dimensi3</option>
+                                    <option value="dimensi4">dimensi4</option>
+                                    <option value="dimensi5">dimensi5</option>
+                                    <option value="dimensi6">dimensi6</option>
+                                </select>
+                            </form>
+                        </div>
+                        <div class="me-1 mt-1 justify-content-end align-item-end">
+                            <button type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal4-doughnut">
+                                <i class="fa-solid fa-download"></i>
+                            </button>
+                        </div>
+                    </div>
+                    <div class="chartBox" style=" height:500px;">
+                        <canvas class="mt1" id="hasil1"></canvas>
+                    </div>
+                    <div class="position-absolute bot-0  end-0 d-flex flex-row justify-content-center align-item-center ">
+                        <div class="form-check form-switch me-3">
+                            <input class="form-check-input" onchange="update_value(this);" type="checkbox" id="pieChartLegend" value="show" checked>
+                            <label class="form-check-label" style="margin-left:-3px;" for="flexSwitchCheckChecked">Tampilkan Legenda</label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div> -->
+
+<!-- Dodge Bar Chart Menu IKUPTIK submenu Dimensi per Jenis Unit Usaha (Kanan) -->
+<!-- <div class="portfolio-item filter-riset4-hasil1">
+    <div class="row">
+        <div class="col-lg-12 grid-margin stretch-card">
+            <div class="card shadow">
+                <div class="card-body">
+                    <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center ">
+                        <div class="selectBox mt-2 me-2">
+                            <form action="">
+                                <select class="dropdownunduh1" id="barChart">
+                                    <option value="dimensi1">dimensi1</option>
+                                    <option value="dimensi2">dimensi2</option>
+                                    <option value="dimensi3">dimensi3</option>
+                                    <option value="dimensi4">dimensi4</option>
+                                    <option value="dimensi5">dimensi5</option>
+                                    <option value="dimensi6">dimensi6</option>
+                                </select>
+                            </form>
+                        </div>
+                        <div class="me-1 mt-1 justify-content-end align-item-end">
+                            <button type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal4-doughnut">
+                                <i class="fa-solid fa-download"></i>
+                            </button>
+                        </div>
+                    </div>
+                    <div class="chartBox" style=" height:500px;">
+                        <canvas class="mt1" id="hasil2"></canvas>
+                    </div>
+                    <div class="position-absolute bot-0  end-0 d-flex flex-row justify-content-center align-item-center ">
+                        <div class="form-check form-switch me-3">
+                            <input class="form-check-input" onchange="update_value(this);" type="checkbox" id="barChartLegend" value="show" checked>
+                            <label class="form-check-label" style="margin-left:-3px;" for="flexSwitchCheckChecked">Tampilkan Legenda</label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div> -->
+
+<div class="portfolio-item filter-riset4-fg" onresize="responsivefonts()">
+    <div class="row mt-3">
+        <div class="col-sm-6 my-2">
+            <div class="card shadow">
+            <div class="card-body">
+                    <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center ">
+                        <div class="selectBox mt-2 me-2">
+                            <form action="">
+                                <select class="dropdownunduh1" id="pieChart">
+                                    <option value="dimensi1">dimensi1</option>
+                                    <option value="dimensi2">dimensi2</option>
+                                    <option value="dimensi3">dimensi3</option>
+                                    <option value="dimensi4">dimensi4</option>
+                                    <option value="dimensi5">dimensi5</option>
+                                    <option value="dimensi6">dimensi6</option>
+                                </select>
+                            </form>
+                        </div>
+                        <div class="me-1 mt-1 justify-content-end align-item-end">
+                            <button type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal4-doughnut">
+                                <i class="fa-solid fa-download"></i>
+                            </button>
+                        </div>
+                    </div>
+                    <div class="chartBox" style=" height:500px;">
+                        <canvas class="mt1" id="hasil1"></canvas>
+                    </div>
+                    <div class="position-absolute bot-0  end-0 d-flex flex-row justify-content-center align-item-center ">
+                        <div class="form-check form-switch me-3">
+                            <input class="form-check-input" onchange="update_value_legend_bar_kecamatan(this);" type="checkbox" id="pieChartLegend" value="show" checked>
+                            <label class="form-check-label" style="margin-left:-3px;" for="flexSwitchCheckChecked">Tampilkan Legenda</label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6 my-2">
+            <div class="card shadow">
+            <div class="card-body">
+                    <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center ">
+                        <div class="selectBox mt-2 me-2">
+                            <form action="">
+                                <select class="dropdownunduh1" id="barChart">
+                                    <option value="dimensi1">dimensi1</option>
+                                    <option value="dimensi2">dimensi2</option>
+                                    <option value="dimensi3">dimensi3</option>
+                                    <option value="dimensi4">dimensi4</option>
+                                    <option value="dimensi5">dimensi5</option>
+                                    <option value="dimensi6">dimensi6</option>
+                                </select>
+                            </form>
+                        </div>
+                        <div class="me-1 mt-1 justify-content-end align-item-end">
+                            <button type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal4-doughnut">
+                                <i class="fa-solid fa-download"></i>
+                            </button>
+                        </div>
+                    </div>
+                    <div class="chartBox" style=" height:500px;">
+                        <canvas class="mt1" id="hasil2"></canvas>
+                    </div>
+                    <div class="position-absolute bot-0  end-0 d-flex flex-row justify-content-center align-item-center ">
+                        <div class="form-check form-switch me-3">
+                            <input class="form-check-input" onchange="update_value_legend_bar_jenis(this);" type="checkbox" id="barChartLegend" value="show" checked>
+                            <label class="form-check-label" style="margin-left:-3px;" for="flexSwitchCheckChecked">Tampilkan Legenda</label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
