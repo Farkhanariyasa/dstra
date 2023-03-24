bb0023af07fcfe2131f6a73b2f138299fc677789
NickoRagasukma <gabrielprimaditya@gmail.com>
2023-03-24 22:05:15 +0700

    M Views : Modif menu dasbor riset 2

--- app/Views/pvd/pages/dasbor/riset2/index.php
+++ app/Views/pvd/pages/dasbor/riset2/index.php
@@ -9,50 +9,36 @@
 
     <hr class="hrjudul">
 
-    <!-- Gambaran Umum -->
-    <div class="accordion" id="accordionExample">
-      <!-- <div class="accordion-item my-3">
-        <h3 class="accordion-header" id="headingOne">
-          <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-          <span class="fa-solid fa-hotel pe-2"></span>
-            Gambaran Umum
-          </button>
-        </h3>
-        <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-          <div class="accordion-body">
-            <p class="text-justify">Penelitian ini dilakukan untuk mengatasi permasalahan MPD khususnya pada double counting pada data MPS dua nomor berbeda milik Telkomsel dan indosat
-              melalui pattern dari kedua nomor tersebut. Dengan memanfaatkan MPD, kami melakukan penelitian terhadap pengelompokkan individu ang termasuk dalam satu rumah tangga berdasarkan dengan usual environment
-              (family grouping) dan pola perjalanan wisata selama tahun 2022. Selain dengan pemanfaatan MPD, penelitin ini dilakukan untuk mencari tahu permasalahan pada statistik wisatawan nusantara dengan melibatkan jumlah perjalana wisata menggunakan MPD dan survei
-              konvensional.
-            </p>
-          </div>
+    <section>
+      <div class="container-fluid">
+        <div class="shadow box par m-auto p-4 p-md-5 my-4 rounded-5 bg-body" style="line-height: 1.5;">
+          <p class="text-justify">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum reprehenderit voluptatibus optio recusandae enim quis explicabo odit earum pariatur delectus sunt autem facere mollitia, dolor reiciendis! Asperiores, aliquid placeat totam accusamus ad obcaecati, ullam velit, earum voluptatibus laboriosam quia excepturi mollitia quisquam nostrum cupiditate ut pariatur optio necessitatibus? Ut, accusantium. Eum esse dolor rem magni vel enim adipisci, reiciendis voluptatem sunt saepe nostrum laborum eveniet quidem dignissimos omnis quam. Eligendi eveniet accusamus adipisci cum nemo!
+          </p>
         </div>
-      </div> -->
+      </div>
+    </section>
 
+    <div class="accordion" id="accordionExample">
       <!-- Tujuan 1 -->
-      <div class="accordion-item my-3 rounded-3">
-        <h3 class="accordion-header" id="headingTwo">
-          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-          <span class="fa-solid fa-map-location-dot pe-2"></span>  
-          Tujuan 1
+      <div class="accordion-item my-3 rounded-5 shadow">
+        <h3 class="accordion-header" id="headingOne">
+          <button class="accordion-button collapsed rounded-5" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+            <span class="fa-solid fa-map-location-dot pe-2"></span>  
+            Tujuan 1
           </button>
         </h3>
-        <div id="collapseTwo" class="accordion-collapse collapse show" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
-          <div class="accordion-body">
-            <ol class="text-justify">
+        <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+          <div class="accordion-body rounded-bottom">
+            <ol class="text-justify fs-5 ms-3">
               <li>
-                Membangun metode untuk mengelompokkan individu yang termasuk dalam satu
-                rumah tangga berdasarkan usual environment dan pola perjalanan wisata (family
-                grouping).
+                <a href="#" class="text-decoration-none fs-5">Tujuan 1 Tab 1</a>
               </li>
               <li>
-                Membangun metode untuk mengidentifikasi double counting berdasarkan pola
-                perjalanan individu yang menggunakan lebih dari satu nomor Telkomsel dan/atau
-                Indosat berbeda dalam satu perangkat.
+                <a href="#" class="text-decoration-none fs-5">Tujuan 1 Tab 2</a>
               </li>
               <li>
-                Menyajikan statistik wisatawan nusantara melalui pendekatan Mobile Positioning
-                Data (MPD) dan survei konvensional di Kota Malang selama Triwulan IV 2022.
+                <a href="#" class="text-decoration-none fs-5">Tujuan 1 Tab 3</a>
               </li>
             </ol>
           </div>
@@ -60,74 +46,61 @@
       </div>
 
       <!-- Tujuan 2 -->
-      <div class="accordion-item my-3 rounded-3">
-        <h3 class="accordion-header" id="headingThree">
-          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-          <span class="fa-solid fa-calendar-week pe-2"></span>
+      <div class="accordion-item my-3 rounded-5 shadow">
+        <h3 class="accordion-header" id="headingTwo">
+          <button class="accordion-button collapsed rounded-5" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+            <span class="fa-solid fa-calendar-week pe-2"></span>
             Tujuan 2
-            
           </button>
         </h3>
-        <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
-          <div class="accordion-body">
-            <p class="text-justify">
-              Metode Penarikan Sampel
-            </p>
-            <p class="text-justify">
-              Judgement (Purposive) Sampling
-            </p>
-            <p class="text-justify">
-              Metodologi pengambilan sampel secara acak dimana
-              kelompok sampel yang ditargetkan memiliki atribut-atribut tertentu. Cara yang dilakukan pada Judgment (Purposive) Sampling penelitian ini
-              adalah pengambilan sampel yang homogen. Sampel homogen yang ditargetkan pada penelitian ini yaitu sampel dengan pekerjaan sama, yaitu orang ang bekerja di BPS Kota Surabaya dan BPS Provinsi Jawa Timur.
-            </p>
+        <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+          <div class="accordion-body rounded-bottom">
+            <ol class="text-justify fs-5 ms-3">
+              <li>
+                <a href="#" class="text-decoration-none fs-5">Tujuan 2 Tab 1</a>
+              </li>
+              <li>
+                <a href="#" class="text-decoration-none fs-5">Tujuan 2 Tab 2</a>
+              </li>
+            </ol>
           </div>
         </div>
       </div>
 
       <!-- Tujuan 3 -->
-      <div class="accordion-item my-3 rounded-3">
-        <h3 class="accordion-header" id="headingFour">
-          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
-          <span class="fa-solid fa-person-walking-luggage pe-2"></span>
+      <div class="accordion-item my-3 rounded-5 shadow">
+        <h3 class="accordion-header" id="headingThree">
+          <button class="accordion-button collapsed rounded-5" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+            <span class="fa-solid fa-person-walking-luggage pe-2"></span>
             Tujuan 3
-            
           </button>
         </h3>
-        <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#accordionExample">
-          <div class="accordion-body">
-            <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed viverra neque ligula, at vehicula orci viverra at.
-              Sed at lorem at tortor dapibus ultricies ut sed ipsum. Maecenas molestie elementum justo, in convallis urna eleifend et.
-              Vestibulum mollis, tortor ut luctus scelerisque, eros est ullamcorper leo, quis tempor urna arcu sit amet tortor. Sed rhoncus
-              odio non ipsum tempus porttitor. Nulla sagittis tincidunt aliquam. Cras commodo egestas commodo. Praesent consectetur mauris sed
-              ultrices dignissim. Suspendisse mollis magna auctor enim posuere interdum. Phasellus suscipit, dolor vitae mattis consectetur, dui
-              ex viverra nibh, sit amet elementum ligula lectus et quam. Sed et cursus neque. Morbi maximus in ipsum nec aliquam. Integer porttitor
-              blandit augue, ut posuere dolor gravida vel. Nulla rutrum eu augue non semper. Proin sit amet dui felis. Sed vestibulum tempor urna,
-              auctor viverra erat.
-            </p>
+        <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+          <div class="accordion-body rounded-bottom">
+            <ol class="text-justify fs-5 ms-3">
+              <li>
+                <a href="#" class="text-decoration-none fs-5">Tujuan 3 Tab 1</a>
+              </li>
+              <li>
+                <a href="#" class="text-decoration-none fs-5">Tujuan 3 Tab 2</a>
+              </li>
+            </ol>
           </div>
         </div>
       </div>
 
       <!-- Tujuan 4 -->
-      <div class="accordion-item my-3 rounded-3">
-        <h3 class="accordion-header" id="headingFive">
-          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
-          <span class="fa-solid fa-chart-simple pe-2"></span>
+      <div class="accordion-item my-3 rounded-5 shadow">
+        <h3 class="accordion-header" id="headingFour">
+          <button class="accordion-button collapsed rounded-5" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+            <span class="fa-solid fa-chart-simple pe-2"></span>
             Tujuan 4
-            
           </button>
         </h3>
-        <div id="collapseFive" class="accordion-collapse collapse" aria-labelledby="headingFive" data-bs-parent="#accordionExample">
-          <div class="accordion-body">
-            <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed viverra neque ligula, at vehicula orci viverra at.
-              Sed at lorem at tortor dapibus ultricies ut sed ipsum. Maecenas molestie elementum justo, in convallis urna eleifend et.
-              Vestibulum mollis, tortor ut luctus scelerisque, eros est ullamcorper leo, quis tempor urna arcu sit amet tortor. Sed rhoncus
-              odio non ipsum tempus porttitor. Nulla sagittis tincidunt aliquam. Cras commodo egestas commodo. Praesent consectetur mauris sed
-              ultrices dignissim. Suspendisse mollis magna auctor enim posuere interdum. Phasellus suscipit, dolor vitae mattis consectetur, dui
-              ex viverra nibh, sit amet elementum ligula lectus et quam. Sed et cursus neque. Morbi maximus in ipsum nec aliquam. Integer porttitor
-              blandit augue, ut posuere dolor gravida vel. Nulla rutrum eu augue non semper. Proin sit amet dui felis. Sed vestibulum tempor urna,
-              auctor viverra erat.
+        <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#accordionExample">
+          <div class="accordion-body rounded-bottom">
+            <p class="text-justify ms-3">
+              <a href="#" class="text-decoration-none fs-5">Tujuan 4</a>
             </p>
           </div>
         </div>
