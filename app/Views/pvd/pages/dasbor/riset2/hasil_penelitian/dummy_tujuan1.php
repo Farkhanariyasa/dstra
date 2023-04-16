ab504186fc671f113303cd0ca2bef058b5b7e923
NickoRagasukma <gabrielprimaditya@gmail.com>
2023-04-16 13:12:56 +0700

    M Views : Modif layout tujuan 1 riset 2

--- app/Views/pvd/pages/dasbor/riset2/hasil_penelitian/dummy_tujuan1.php
+++ app/Views/pvd/pages/dasbor/riset2/hasil_penelitian/dummy_tujuan1.php
@@ -1,7 +1,67 @@
 <!-- Tab 1 -->
-<div class="portfolio-item filter-riset2-hasiltujuan1-1">    
+<div class="portfolio-item filter-riset2-hasiltujuan1-1">
+    <div class="row mt-3">
+        <div class="col-sm-4 my-2">
+            <div class="card shadow rounded-5">
+                <div class="card-body">
+                    <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center ">
+                        <div class="me-1 mt-1 justify-content-end align-item-end">
+                            <button type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModalfg1">
+                                <i class="fa-solid fa-download"></i>
+                            </button>
+                        </div>
+                    </div>
+                    <div>
+                        <canvas class="mt1" id="riset1_hasil1"></canvas>
+                    </div><br>
+                    <div>
+                        <p class="card-text text-justify">
+                            Grafik menunjukkan banyaknya rumah tangga berdasarkan
+                            jumlah anggota rumah tangga yang melakukan perjalanan wisata.
+                        </p>
+                    </div>
+                    <div class="position-absolute bot-0  end-0 d-flex flex-row justify-content-center align-item-center ">
+                        <div class="form-check form-switch me-3">
+                            <input class="form-check-input" onchange="update_value(this);" type="checkbox" id="forBarChartLegend" value="show" checked>
+                            <label class="form-check-label" style="margin-left:-3px;" for="flexSwitchCheckChecked">Tampilkan Legend</label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-8 my-2">
+            <div class="card shadow rounded-5">
+                <div class="card-body">
+                    <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center ">
+                        <div class="me-1 mt-1 justify-content-end align-item-end">
+                            <button type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModalfg2">
+                                <i class="fa-solid fa-download"></i>
+                            </button>
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            <div class="card">
+                                <div class="card-body konten-teks">
+                                    <p class="card-title">Jumlah RT <br>21 (24,7%) <br></p>
+                                    <p class="card-text subs">Dengan ART sebanyak 4 orang</p>
+                                    <p class="card-text text-justify">
+                                        Banyaknya Rumah Tangga (RT) dengan anggota
+                                        rumah tangga (ART) sebanyak 4 orang yang melakukan perjalanan
+                                        yaitu sebesar 21 rumah tangga (24,7%) dari total 85 rumah tangga yang
+                                        melakukan perjalanan.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <div class="container mt-5">
-        <div class="card d-flex justify-content-center flex-column align-item-center flex-md primary pb-5 pt-5 shadow">
+        <div class="card d-flex justify-content-center flex-column align-item-center flex-md primary pb-5 pt-5 shadow rounded-5">
             <p class="card-text text-center">
                 <strong>Sebaran Wisatawan dalam Kota Malang menurut Kecamatan Asal Wisatawan di Kota Malang Bulan Oktober-Desember 2022</strong>
             </p>
