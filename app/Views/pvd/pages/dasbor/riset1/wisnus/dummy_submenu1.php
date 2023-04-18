af9210ec881384ecbae6b31abaa814e73ccc5535
Thomson <222011365@stis.ac.id>
2023-04-19 01:54:14 +0700

    M Views: perubahan dummy wisnus

--- app/Views/pvd/pages/dasbor/riset1/wisnus/dummy_submenu1.php
+++ app/Views/pvd/pages/dasbor/riset1/wisnus/dummy_submenu1.php
@@ -1,30 +1,30 @@
+<!-- Output 1 -->
 <div class="portfolio-item filter-riset1-hasil1" onresize="responsivefonts()">
     <div class="col-12 grid-margin stretch-card">
         <div class="card shadow">
             <div class="card-body">
                 <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center ">
-                    <div class="selectBox mt-2 me-2">
+                    <!-- <div class="selectBox mt-2 me-2">
                         <form action="">
                             <select class="dropdownunduh1 w-100" id="forBarChart">
                                 <option value="a">Jumlah ART Melakukan Perjalanan Wisata</option>
                                 <option value="b">Jenis Kelamin</option>
                             </select>
                         </form>
-                    </div>
+                    </div> -->
                     <div class="me-1 mt-1 justify-content-end align-item-end">
-                        <button type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                        <button id="wisnus-1" type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal1">
                             <i class="fa-solid fa-download"></i>
                         </button>
                     </div>
                 </div>
                 <div style=" height:500px;">
-                    <canvas class="mt1" id="riset1_hasil1"></canvas>
+                    <canvas class="mt1" id="wisnus_output1"></canvas>
                 </div>
-
                 <div class="position-absolute bot-0  end-0 d-flex flex-row justify-content-center align-item-center ">
                     <div class="form-check form-switch me-3">
-                        <input class="form-check-input" onchange="update_value2(this);" type="checkbox" id="forPieChartLegend" value="show" checked>
-                        <label class="form-check-label" style="margin-left:-3px;" for="flexSwitchCheckChecked">Tampilkan Legend</label>
+                        <input class="form-check-input" onchange="update_value_output1(this);" type="checkbox" id="forBarChartAxis_output1" value="show" checked>
+                        <label class="form-check-label" style="margin-left:-3px;" for="flexSwitchCheckChecked">Tampilkan Sumbu</label>
                     </div>
                 </div>
             </div>
@@ -49,7 +49,7 @@
     </div> -->
 </div>
 
-
+<!-- Output 2 -->
 <div class="portfolio-item filter-riset1-hasil2" onresize="responsivefonts()">
     <div class="row">
         <div class="col-lg-12 grid-margin stretch-card">
@@ -57,7 +57,7 @@
                 <div class="card-body">
                     <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center ">
                         <div class="me-1 mt-1 justify-content-end align-item-end">
-                            <button type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal2">
+                            <button id="wisnus-2" type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal2">
                                 <i class="fa-solid fa-download"></i>
                             </button>
                         </div>
@@ -94,7 +94,7 @@
     </div> -->
 </div>
 
-
+<!-- Output 4 -->
 <div class="portfolio-item filter-riset1-hasil3" onresize="responsivefonts()">
     <div class="row">
         <div class="col-lg-12 grid-margin stretch-card">
@@ -102,7 +102,7 @@
                 <div class="card-body">
                     <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center ">
                         <div class="me-1 mt-1 justify-content-end align-item-end">
-                            <button type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal3">
+                            <button id="wisnus-3" type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal3">
                                 <i class="fa-solid fa-download"></i>
                             </button>
                         </div>
@@ -122,6 +122,7 @@
     </div>
 </div>
 
+<!-- Output 5 -->
 <div class="portfolio-item filter-riset1-hasil4" onresize="responsivefonts()">
     <div class="row">
         <div class="col-lg-12 grid-margin stretch-card">
@@ -129,7 +130,7 @@
                 <div class="card-body">
                     <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center ">
                         <div class="me-1 mt-1 justify-content-end align-item-end">
-                            <button type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal4">
+                            <button id="wisnus-4" type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal4">
                                 <i class="fa-solid fa-download"></i>
                             </button>
                         </div>
@@ -148,3 +149,62 @@
         </div>
     </div>
 </div>
+
+<!-- Output 4 -->
+<div class="portfolio-item filter-riset1-hasil5" onresize="responsivefonts()">
+    <div class="row">
+        <div class="col-lg-12 grid-margin stretch-card">
+            <div class="card shadow">
+                <div class="card-body">
+                    <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center ">
+                        <div class="me-1 mt-1 justify-content-end align-item-end">
+                            <button id="wisnus-5" type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal5">
+                                <i class="fa-solid fa-download"></i>
+                            </button>
+                        </div>
+                    </div>
+                    <div style=" height:500px;">
+                        <canvas class="mt1" id="riset1_hasil3"></canvas>
+                    </div>
+                    <div class="position-absolute bot-0  end-0 d-flex flex-row justify-content-center align-item-center ">
+                        <div class="form-check form-switch me-3">
+                            <input class="form-check-input" onchange="update_value_sim(this);" type="checkbox" id="forBarChartAxis_sim" value="show" checked>
+                            <label class="form-check-label" style="margin-left:-3px;" for="flexSwitchCheckChecked">Tampilkan Sumbu</label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Output 5 -->
+<div class="portfolio-item filter-riset1-hasil6" onresize="responsivefonts()">
+    <div class="row">
+        <div class="col-lg-12 grid-margin stretch-card">
+            <div class="card shadow">
+                <div class="card-body">
+                    <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center ">
+                        <div class="me-1 mt-1 justify-content-end align-item-end">
+                            <button id="wisnus-6" type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal6">
+                                <i class="fa-solid fa-download"></i>
+                            </button>
+                        </div>
+                    </div>
+                    <div style=" height:500px;">
+                        <canvas class="mt1" id="riset1_hasil4"></canvas>
+                    </div>
+                    <div class="position-absolute bot-0  end-0 d-flex flex-row justify-content-center align-item-center ">
+                        <div class="form-check form-switch me-3">
+                            <input class="form-check-input" onchange="update_value_data(this);" type="checkbox" id="forBarChartAxis_data" value="show" checked>
+                            <label class="form-check-label" style="margin-left:-3px;" for="flexSwitchCheckChecked">Tampilkan Sumbu</label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Modal  -->
+<?php include('modal_wisnus.php') ?>
