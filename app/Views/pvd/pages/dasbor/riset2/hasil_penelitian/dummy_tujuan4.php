e50ca384ff8d7ada559d93f97433582f38d836e1
renatadelarosa <222011432@stis.ac.id>
2023-04-21 21:52:01 +0700

    M Views : variabel bebas pada dashboard riset 2

--- app/Views/pvd/pages/dasbor/riset2/hasil_penelitian/dummy_tujuan4.php
+++ app/Views/pvd/pages/dasbor/riset2/hasil_penelitian/dummy_tujuan4.php
@@ -1,25 +1,7 @@
 <!-- Visualisasi 1 -->
-<div class="portfolio-item filter-riset2-hasil1">
-    <!-- <div class="row mt-5">
-        <div class="col-sm-6 my-2">
-            <div class="card">
-                <div class="card-body">
-                    <p class="card-text text-center">Grafik<br><br><br><br><br><br><br><br><br><br><br></p>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-sm-6 my-2">
-            <div class="card">
-                <div class="card-body">
-                    <p class="card-text text-center">Grafik<br><br><br><br><br><br><br><br><br><br><br></p>
-                </div>
-            </div>
-        </div>
-    </div> -->
-
+<!-- <div class="portfolio-item filter-riset2-hasil1">
     <div class="col-12 col-sm-12">
-        <h1 class="headline text-center">Kerangka Pikir</h1>
+        <h1 class="headline text-center">Variabel</h1>
     </div>
 
     <div class="row mt-3">
@@ -39,14 +21,13 @@
                     <div class="chartBox" style="height:500px;">
                         <canvas class="mt1" id="pw_sankey"></canvas>
                     </div>
-                </div>
             </div>
         </div>
     </div>
-</div>
+</div> -->
 
 <!-- Visualisasi 2 -->
-<div class="portfolio-item filter-riset2-hasil2">
+<!-- <div class="portfolio-item filter-riset2-hasil2">
     <div class="col-12 col-sm-12">
         <h1 class="headline text-center">Deskriptif Variabel</h1>
     </div>
@@ -57,15 +38,6 @@
                 <div class="card-body">
                     <div
                         class="position-absolute top-0 end-0 d-flex flex-row justify-content-center align-item-center ">
-                        <!-- <div class="selectBox mt-2 me-2">
-                            <form action="">
-                                <select class="dropdownunduh1" id="forBarChart">
-                                    <option value="a">Jenis Industri</option>
-                                    <option value="b">Pendidikan Tertinggi</option>
-                                    <option value="c">Jenis Kelamin</option>
-                                </select>
-                            </form>
-                        </div> -->
                         <div class="me-1 mt-1 justify-content-end align-item-end">
                             <button id="unduh-3-2" type="button" class="tombol btn-for" data-bs-toggle="modal"
                                 data-bs-target="#exampleModal2">
@@ -81,10 +53,10 @@
             </div>
         </div>
     </div>
-</div>
+</div> -->
 
 <!-- Visualisasi 3 -->
-<div class="portfolio-item filter-riset2-hasil3">
+<!-- <div class="portfolio-item filter-riset2-hasil3">
     <div class="col-12 col-sm-12">
         <h1 class="headline text-center">Persamaan Regresi</h1>
     </div>
@@ -110,14 +82,137 @@
             </div>
         </div>
     </div>
-</div>
+</div> -->
 
 <!-- Visualisasi 4 -->
-<div class="portfolio-item filter-riset2-peta" onresize="responsivefonts()">
+<!-- <div class="portfolio-item filter-riset2-peta" onresize="responsivefonts()">
     <div class="col-12 col-sm-12">
         <h1 class="headline text-center">Interpretasi Koefisien Regresi</h1>
     </div>
+</div> -->
+
+<!-- Visualisasi 1 -->
+<div class="portfolio-item filter-riset2-hasil1">
+    <div class="col-12 col-sm-12">
+        <h1 class="headline text-center">Variabel</h1>
+    </div>
+
+    <div class="row mt-3">
+        <div class="col-lg-12 grid-margin stretch-card">
+            <div class="card shadow">
+                <div class="card-body">
+
+                    <p>
+                        <b>VARIABEL BEBAS</b>
+                    </p>
+
+                        <div class="col varbebas">
+                            <div class="row row-cols-2 justify-content-center">
+                            <!-- Jenis Kelamin -->
+                            <div class="col align-items-center d-flex align-self-center mb-4">
+                                    <div class="card-body batasan2 shadow p-3">
+                                        <div class="col">
+                                            <p class="card-title  ">
+                                            <!-- <i class="fas fa-home col-1 ikon"></i> -->
+                                            <div style="line-height: 20px; color: #4b395f;"><i class="fas fa-city col-1 ikon"></i><strong> Jenis Kelamin</strong></div>
+                                            <!-- <div class="angka">347</div> -->
+                                            <!-- <div style="line-height: 20px;">Blok Sensus</div> -->
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <!-- Jenis Pekerjaan KRT -->
+                                <div class="col align-items-center d-flex align-self-center mb-4">
+                                    <div class="card-body batasan2 shadow p-3">
+                                        <div class="col">
+                                            <p class="card-title  ">
+                                            <!-- <i class="fas fa-city col-1 ikon"></i> -->
+                                            <div style="line-height: 20px; color: #4b395f;"><i class="fas fa-city col-1 ikon"></i><strong> Jenis Pekerjaan KRT</strong></div>
+                                            <!-- <div class="angka">10</div> -->
+                                            <!-- <div style="line-height: 20px;">Rumah Tangga</div> -->
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <!-- Umur -->
+                                <div class="col align-items-center d-flex align-self-center mb-4">
+                                    <div class="card-body   batasan2 shadow p-3">
+                                        <div class="col">
+                                            <p class="card-title  ">
+                                            <!-- <i class="fas fa-home col-1 ikon"></i> -->
+                                            <div style="line-height: 20px; color: #4b395f;"><i class="fas fa-city col-1 ikon"></i><strong> Umur</strong></div>
+                                            <!-- <div class="angka">347</div> -->
+                                            <!-- <div style="line-height: 20px;">Blok Sensus</div> -->
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <!-- Gangguan Disabilitas -->
+                                <div class="col align-items-center d-flex align-self-center mb-4">
+                                    <div class="card-body   batasan2 shadow p-3">
+                                        <div class="col">
+                                            <p class="card-title  ">
+                                            <!-- <i class="fas fa-city col-1 ikon"></i> -->
+                                            <div style="line-height: 20px; color: #4b395f;"><i class="fas fa-city col-1 ikon"></i><strong> Gangguan Disabilitas</strong></div>
+                                            <!-- <div class="angka">10</div> -->
+                                            <!-- <div style="line-height: 20px;">Rumah Tangga</div> -->
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <!-- Tingkat Pendidikan -->
+                                <div class="col align-items-center d-flex align-self-center mb-4">
+                                    <div class="card-body   batasan2 shadow p-3">
+                                        <div class="col">
+                                            <p class="card-title  ">
+                                            <!-- <i class="fas fa-home col-1 ikon"></i> -->
+                                            <div style="line-height: 20px; color: #4b395f;"><i class="fas fa-city col-1 ikon"></i><strong> Tingkat Pendidikan</strong></div>
+                                            <!-- <div class="angka">347</div> -->
+                                            <!-- <div style="line-height: 20px;">Blok Sensus</div> -->
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <!-- Kepemilikan Mobil -->
+                                <div class="col align-items-center d-flex align-self-center mb-4">
+                                    <div class="card-body   batasan2 shadow p-3">
+                                        <div class="col">
+                                            <p class="card-title  ">
+                                            <!-- <i class="fas fa-city col-1 ikon"></i> -->
+                                            <div style="line-height: 20px; color: #4b395f;"><i class="fas fa-city col-1 ikon"></i><strong> Kepemilikan Mobil</strong></div>
+                                            <!-- <div class="angka">10</div> -->
+                                            <!-- <div style="line-height: 20px;">Rumah Tangga</div> -->
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <!-- Kepemilikan Motor -->
+                                <div class="col align-items-center d-flex align-self-center">
+                                    <div class="card-body batasan2 shadow p-3">
+                                        <div class="col">
+                                            <p class="card-title">
+                                            <!-- <i class="fas fa-home col-1 ikon"></i> -->
+                                            <div style="line-height: 20px; color: #4b395f;"><i class="fas fa-city col-1 ikon"></i><strong> Kepemilikan Motor</strong></div>
+                                            <!-- <div class="angka">347</div> -->
+                                            <!-- <div style="line-height: 20px;">Blok Sensus</div> -->
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
+
 
 <!-- Modal Riset 2  -->
 <?php include('modal_hp.php') ?>
