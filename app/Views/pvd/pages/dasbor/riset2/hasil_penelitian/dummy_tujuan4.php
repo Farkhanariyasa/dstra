c55b1bc19d9716e325311a77a3a406c52211ec3e
renatadelarosa <222011432@stis.ac.id>
2023-04-21 23:04:23 +0700

    M Views : card persamaan regresi pada dashboard riset 2

--- app/Views/pvd/pages/dasbor/riset2/hasil_penelitian/dummy_tujuan4.php
+++ app/Views/pvd/pages/dasbor/riset2/hasil_penelitian/dummy_tujuan4.php
@@ -91,7 +91,7 @@
     </div>
 </div> -->
 
-<!-- Visualisasi 1 -->
+<!-- Variabel -->
 <div class="portfolio-item filter-riset2-hasil1">
     <div class="col-12 col-sm-12">
         <h1 class="headline text-center">Variabel</h1>
@@ -107,10 +107,10 @@
                     </p>
 
                         <div class="col varbebas">
-                            <div class="row row-cols-2 justify-content-center">
-                            <!-- Jenis Kelamin -->
-                            <div class="col align-items-center d-flex align-self-center mb-4">
-                                    <div class="card-body batasan2 shadow p-3">
+                            <div class="row justify-content-center ">
+                                <!-- Jenis Kelamin -->
+                                <div class="col align-items-center d-flex align-self-center mb-4 shadow rounded-4 mx-4">
+                                    <div class="card-body p-3">
                                         <div class="col">
                                             <p class="card-title  ">
                                             <!-- <i class="fas fa-home col-1 ikon"></i> -->
@@ -123,8 +123,8 @@
                                 </div>
 
                                 <!-- Jenis Pekerjaan KRT -->
-                                <div class="col align-items-center d-flex align-self-center mb-4">
-                                    <div class="card-body batasan2 shadow p-3">
+                                <div class="col align-items-center d-flex align-self-center mb-4 shadow rounded-4 mx-4">
+                                    <div class="card-body p-3">
                                         <div class="col">
                                             <p class="card-title  ">
                                             <!-- <i class="fas fa-city col-1 ikon"></i> -->
@@ -135,10 +135,12 @@
                                         </div>
                                     </div>
                                 </div>
+                            </div>
 
+                            <div class="row justify-content-center ">
                                 <!-- Umur -->
-                                <div class="col align-items-center d-flex align-self-center mb-4">
-                                    <div class="card-body   batasan2 shadow p-3">
+                                <div class="col align-items-center d-flex align-self-center mb-4 shadow  rounded-4 mx-4">
+                                    <div class="card-body p-3">
                                         <div class="col">
                                             <p class="card-title  ">
                                             <!-- <i class="fas fa-home col-1 ikon"></i> -->
@@ -151,8 +153,8 @@
                                 </div>
 
                                 <!-- Gangguan Disabilitas -->
-                                <div class="col align-items-center d-flex align-self-center mb-4">
-                                    <div class="card-body   batasan2 shadow p-3">
+                                <div class="col align-items-center d-flex align-self-center mb-4 shadow  rounded-4 mx-4">
+                                    <div class="card-body p-3">
                                         <div class="col">
                                             <p class="card-title  ">
                                             <!-- <i class="fas fa-city col-1 ikon"></i> -->
@@ -163,10 +165,12 @@
                                         </div>
                                     </div>
                                 </div>
+                            </div>
 
+                            <div class="row justify-content-center ">
                                 <!-- Tingkat Pendidikan -->
-                                <div class="col align-items-center d-flex align-self-center mb-4">
-                                    <div class="card-body   batasan2 shadow p-3">
+                                <div class="col align-items-center d-flex align-self-center mb-4 shadow  rounded-4 mx-4">
+                                    <div class="card-body p-3">
                                         <div class="col">
                                             <p class="card-title  ">
                                             <!-- <i class="fas fa-home col-1 ikon"></i> -->
@@ -179,8 +183,8 @@
                                 </div>
 
                                 <!-- Kepemilikan Mobil -->
-                                <div class="col align-items-center d-flex align-self-center mb-4">
-                                    <div class="card-body   batasan2 shadow p-3">
+                                <div class="col align-items-center d-flex align-self-center mb-4 shadow  rounded-4 mx-4">
+                                    <div class="card-body p-3">
                                         <div class="col">
                                             <p class="card-title  ">
                                             <!-- <i class="fas fa-city col-1 ikon"></i> -->
@@ -191,10 +195,12 @@
                                         </div>
                                     </div>
                                 </div>
+                            </div>
 
+                            <div class="row justify-content-center ">
                                 <!-- Kepemilikan Motor -->
-                                <div class="col align-items-center d-flex align-self-center">
-                                    <div class="card-body batasan2 shadow p-3">
+                                <div class="col align-items-center d-flex align-self-center mb-4 shadow rounded-4 mx-4">
+                                    <div class="card-body p-3">
                                         <div class="col">
                                             <p class="card-title">
                                             <!-- <i class="fas fa-home col-1 ikon"></i> -->
@@ -205,6 +211,24 @@
                                         </div>
                                     </div>
                                 </div>
+                            </div>    
+                        </div>
+
+                        <!-- Banyak Perjalanan-->
+                        <div class="card konten2 third5 par mx-auto mb-3 p-1 p-md-1 shadow box rounded-5 mt-3 align-items-center d-flex flex-column">
+                            <div class="card-body text-center">
+                                <i class="fas fa-user-alt col-1 ikon" style="font-size: 30px;"></i>
+                                <p class="card-text text-center">
+                                    <div class="text-center">
+                                        Variabel Tidak Bebas
+                                    </div>
+                                </p>
+                                <div class="" style="color: #4b395f; font-size: 30px;"><strong>Banyak Perjalanan</strong></div>
+                                <!-- <p class="card-text text-justify">
+                                    <div class="text-justify" style="line-height: 25px;">
+                                        Kepala atau anggota dari rumah tangga di Kota Malang yang minimal salah satu anggota rumah tangganya pernah melakukan perjalanan wisata dalam kota dan telah kembali pada Triwulan IV tahun 2022.
+                                    </div>
+                                </p> -->
                             </div>
                         </div>
                 </div>
@@ -213,6 +237,24 @@
     </div>
 </div>
 
+<!-- Persamaan Regresi -->
+<div class="portfolio-item filter-riset2-hasil1">
+    <div class="col-12 col-sm-12">
+        <h1 class="headline text-center">Persamaan Regresi</h1>
+    </div>
+
+    <div class="row mt-3">
+        <div class="col-lg-12 grid-margin stretch-card">
+            <div class="card shadow">
+                <div class="card-body">
+                    <div class="text-center" style="color: #4b395f; font-size: 25px;">
+                        <strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, impedit!</strong>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 <!-- Modal Riset 2  -->
 <?php include('modal_hp.php') ?>
