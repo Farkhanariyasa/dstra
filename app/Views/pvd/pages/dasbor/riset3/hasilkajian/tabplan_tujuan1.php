56450410044afae42c34f3d7eeb083c8bcb5e19e
prasetyofmalik <222011325@stis.ac.id>
2023-04-22 21:22:15 +0700

    A Views: Tabulasi tujuan 1 riset 3

---
+++ app/Views/pvd/pages/dasbor/riset3/hasilkajian/tabplan_tujuan1.php
@@ -0,0 +1,67 @@
+<div class="card">
+    <h3><b>Tabulasi</b></h3>
+    <div class="card-body table-responsive border rounded" style="padding: 0.5rem 1rem;">
+        <table class="table table-hover table-bordered table-responsive" id="table_id" style="width:100%">
+            <thead>
+                <tr>
+                    <th scope="col">Jenis Industri Pariwisata</th>
+                    <th scope="col">Milik Sendiri</th>
+                    <th scope="col">Kontrak Sewa</th>
+                    <th scope="col">Bebas Sewa</th>
+                    <th scope="col">Dinas</th>
+                    <th scope="col">Lainnya</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">Penyediaan jasa akomodasi bagi wisatawan</th>
+                    <td>85.33</td>
+                    <td>1.33</td>
+                    <td>13.33</td>
+                    <td>0</td>
+                    <td>0</td>
+                </tr>
+                <tr>
+                    <th scope="row">Penyediaan jasa makan dan minum</th>
+                    <td>82.14</td>
+                    <td>4.46</td>
+                    <td>13.39</td>
+                    <td>0</td>
+                    <td>0</td>
+                </tr>
+                <tr>
+                    <th scope="row">Penyediaan jasa angkutan darat untuk penumpang</th>
+                    <td>80</td>
+                    <td>1.25</td>
+                    <td>18.75</td>
+                    <td>0</td>
+                    <td>0</td>
+                </tr>
+                <tr>
+                    <th scope="row">Penyediaan jasa kebudayaan, olahraga dan rekreasi</th>
+                    <td>88.29</td>
+                    <td>4.5</td>
+                    <td>7.21</td>
+                    <td>0</td>
+                    <td>0</td>
+                </tr>
+                <tr>
+                    <th scope="row">Aktivitas perdagangan barang-barang pariwisata</th>
+                    <td>80.27</td>
+                    <td>4.08</td>
+                    <td>15.65</td>
+                    <td>0</td>
+                    <td>0</td>
+                </tr>
+                <tr>
+                    <th scope="row">Jenis Industri Lainnya</th>
+                    <td>69.23</td>
+                    <td>7.69</td>
+                    <td>23.08</td>
+                    <td>0</td>
+                    <td>0</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
