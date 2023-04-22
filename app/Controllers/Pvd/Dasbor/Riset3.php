df146db079d3772a35007acf64b5c81295df8304
prasetyofmalik <222011325@stis.ac.id>
2023-04-22 21:25:50 +0700

    M Controllers: Grafik tujuan 1 riset 3

--- app/Controllers/Pvd/Dasbor/Riset3.php
+++ app/Controllers/Pvd/Dasbor/Riset3.php
@@ -77,10 +77,46 @@ class Riset3 extends BaseController
     // menu Hasil Kajian
     public function hasilkajian1()
     {
+        // jenis usaha
+        $ji = [
+            'ji1' => $this->jenisindustri->getByJenisIndustri("1"),
+            'ji2' => $this->jenisindustri->getByJenisIndustri("2"),
+            //'ji3' => $this->jenisindustri->getByJenisIndustri("3"),
+            'ji4' => $this->jenisindustri->getByJenisIndustri("4"),
+            //'ji5' => $this->jenisindustri->getByJenisIndustri("5"),
+            //'ji6' => $this->jenisindustri->getByJenisIndustri("6"),
+            //'ji7' => $this->jenisindustri->getByJenisIndustri("7"),
+            //'ji8' => $this->jenisindustri->getByJenisIndustri("8"),
+            'ji9' => $this->jenisindustri->getByJenisIndustri("9"),
+            'ji10' => $this->jenisindustri->getByJenisIndustri("10"),
+            'ji11' => $this->jenisindustri->getByJenisIndustri("11", "3", "5", "6", "7", "8")
+        ];
+        // pendidikan tertinggi
+        $pt = [
+            'pt1' => $this->pendidikantertinggi->getByPendidikanTertinggi("1"),
+            'pt2' => $this->pendidikantertinggi->getByPendidikanTertinggi("2"),
+            'pt3' => $this->pendidikantertinggi->getByPendidikanTertinggi("3"),
+            'pt4' => $this->pendidikantertinggi->getByPendidikanTertinggi("4"),
+            'pt5' => $this->pendidikantertinggi->getByPendidikanTertinggi("5"),
+            'pt6' => $this->pendidikantertinggi->getByPendidikanTertinggi("6"),
+            'pt7' => $this->pendidikantertinggi->getByPendidikanTertinggi("7"),
+            'pt8' => $this->pendidikantertinggi->getByPendidikanTertinggi("8"),
+            'pt9' => $this->pendidikantertinggi->getByPendidikanTertinggi("9"),
+        ];
+        // status pekerja
+        $jk_1 = [
+            'jk_11' => $this->jeniskelamin_riset3->getByJenisKelamin_Riset3("1"),
+            'jk_12' => $this->jeniskelamin_riset3->getByJenisKelamin_Riset3("2")
+        ];
+        $pd_1 = $this->pendapatan_riset3->getPendapatan_Riset3();
         $menu = getMenu();
         $data = [
             'judul' => 'Hasil Kajian Tujuan 1 Riset 3',
             'menu' => $menu['riset3'],
+            'ji' => $ji,
+            'pt' => $pt,
+            'jk_1' => $jk_1,
+            'pd_1' => $pd_1
         ];
         return view('pvd/pages/dasbor/riset3/hasilkajian/tujuan1', $data);
     }
