9ed281f317e70b5ea26c820d04c494668f9d9a31
renatadelarosa <222011432@stis.ac.id>
2023-04-22 11:37:26 +0700

    M Views : menghapus comment tujuan 1 riset 2

--- app/Views/pvd/pages/dasbor/riset2/hasil_penelitian/dummy_tujuan1.php
+++ app/Views/pvd/pages/dasbor/riset2/hasil_penelitian/dummy_tujuan1.php
@@ -50,7 +50,7 @@
     <div class="container mt-5">
         <div class="card d-flex justify-content-center flex-column align-item-center flex-md primary pb-5 pt-5 shadow rounded-5">
             <p class="card-text text-center">
-                <strong>Sebaran Wisatawan dalam Kota Malang menurut Kecamatan Asal Wisatawan di Kota Malang Bulan Oktober-Desember 2022</strong>
+                <strong>Sebaran Wisatawan dalam Kota Malang menurut Kecamatan Asal <br>Wisatawan di Kota Malang Bulan Oktober-Desember 2022</strong>
             </p>
             <div class="d-flex flex-row align-item-center justify-content-center">
                 <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center">
@@ -118,7 +118,7 @@
     <div class="container mt-5">
         <div class="card d-flex justify-content-center flex-column align-item-center flex-md primary pb-5 pt-5 shadow rounded-5">
             <p class="card-text text-center">
-                <strong>Jumlah Perjalanan dalam Kota Malang menurut Kecamatan Asal Wisatawan di Kota Malang Bulan Oktober-Desember 2022</strong>
+                <strong>Jumlah Perjalanan dalam Kota Malang menurut Kecamatan Asal <br>Wisatawan di Kota Malang Bulan Oktober-Desember 2022</strong>
             </p>
             <div class="d-flex flex-row align-item-center justify-content-center">
                 <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center">
@@ -138,32 +138,6 @@
 <div class="portfolio-item filter-riset2-hasiltujuan1-3">
     <div class="row mt-3">
         <div class="col-sm-5 my-2">
-            <!-- <div class="card shadow rounded-5">
-                <div class="card-body">
-                    <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center ">
-                        <div class="me-1 mt-1 justify-content-end align-item-end">
-                            <button type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModalfg1">
-                                <i class="fa-solid fa-download"></i>
-                            </button>
-                        </div>
-                    </div>
-                    <div>
-                        <canvas class="mt1" id="riset1_hasil1"></canvas>
-                    </div><br>
-                    <div>
-                        <p class="card-text text-justify">
-                            Grafik menunjukkan banyaknya rumah tangga berdasarkan
-                            jumlah anggota rumah tangga yang melakukan perjalanan wisata.
-                        </p>
-                    </div>
-                    <div class="position-absolute bot-0  end-0 d-flex flex-row justify-content-center align-item-center ">
-                        <div class="form-check form-switch me-3">
-                            <input class="form-check-input" onchange="update_value(this);" type="checkbox" id="forBarChartLegend" value="show" checked>
-                            <label class="form-check-label" style="margin-left:-3px;" for="flexSwitchCheckChecked">Tampilkan Legend</label>
-                        </div>
-                    </div>
-                </div>
-            </div> -->
             <div class="card shadow rounded-5">
                 <div class="card-body">
                     <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center ">
@@ -185,41 +159,9 @@
                 </div>
             </div>
         </div>
-        <div class="col-sm-7 my-2">
-            <!-- <div class="card shadow rounded-5">
-                <div class="card-body">
-                    <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center ">
-                        <div class="me-1 mt-1 justify-content-end align-item-end">
-                            <button id="unduh-2-2" type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal2">
-                                <i class="fa-solid fa-download"></i>
-                            </button>
-                        </div>
-                    </div>
-                    <div style=" height:500px;">
-                        <canvas class="mt-1" id="tp_pie"></canvas>
-                    </div>
-                    <div class="position-absolute bot-0  end-0 d-flex flex-row justify-content-center align-item-center ">
-                        <div class="form-check form-switch me-3">
-                            <input class="form-check-input" onchange="update_value1(this);" type="checkbox" id="forPieChartLegend" value="show">
-                            <label class="form-check-label" style="margin-left:-3px;" for="flexSwitchCheckChecked">Tampilkan Legend</label>
-                        </div>
-                    </div>
-                </div>
-            </div> -->
-            
+        <div class="col-sm-7 my-2">           
             <div class="card d-flex justify-content-center flex-column align-item-center flex-md primary pb-5 pt-5 shadow rounded-5">
-                <!-- <p class="card-text text-center">
-                    <strong>Jumlah Perjalanan dalam Kota Malang menurut Kecamatan Asal Wisatawan di Kota Malang Bulan Oktober-Desember 2022</strong>
-                </p> -->
                 <div class="d-flex flex-row align-item-center justify-content-center">
-                    <!-- <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center">
-                        <div class="me-1 mt-1 justify-content-end align-item-end">
-                            <button id="" type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="">
-                                <i class="fa-solid fa-download"></i>
-                            </button>
-                        </div>
-                    </div>
-                    <div id="petatematik2" style="width: 95%; height:80vh; z-index:0;"></div> -->
                     <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center ">
                         <div class="me-1 mt-1 justify-content-end align-item-end">
                             <button id="unduh-2-3" type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal3">
@@ -240,40 +182,6 @@
             </div>
         </div>
     </div>
-
-    <!-- <div class="container mt-5"> -->
-        <!-- <div class="card d-flex justify-content-center flex-column align-item-center flex-md primary pb-5 pt-5 shadow rounded-5"> -->
-            <!-- <p class="card-text text-center">
-                <strong>Jumlah Perjalanan dalam Kota Malang menurut Kecamatan Asal Wisatawan di Kota Malang Bulan Oktober-Desember 2022</strong>
-            </p> -->
-            <!-- <div class="d-flex flex-row align-item-center justify-content-center"> -->
-                <!-- <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center">
-                    <div class="me-1 mt-1 justify-content-end align-item-end">
-                        <button id="" type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="">
-                            <i class="fa-solid fa-download"></i>
-                        </button>
-                    </div>
-                </div>
-                <div id="petatematik2" style="width: 95%; height:80vh; z-index:0;"></div> -->
-                <!-- <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center ">
-                    <div class="me-1 mt-1 justify-content-end align-item-end">
-                        <button id="unduh-2-3" type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal3">
-                            <i class="fa-solid fa-download"></i>
-                        </button>
-                    </div>
-                </div> -->
-                <!-- <div style=" height:500px;">
-                    <canvas class="mt-1" id="jp_twowaybar"></canvas>
-                </div>
-                <div class="position-absolute bot-0  end-0 d-flex flex-row justify-content-center align-item-center ">
-                    <div class="form-check form-switch me-3">
-                        <input class="form-check-input" onchange="update_value2(this);" type="checkbox" id="forBarChartLegend" value="show">
-                        <label class="form-check-label" style="margin-left:-3px;" for="flexSwitchCheckChecked">Tampilkan Legend</label>
-                    </div>
-                </div> -->
-            <!-- </div> -->
-        <!-- </div> -->
-    <!-- </div> -->
 </div>
 
 <!-- Tab 2 -->
