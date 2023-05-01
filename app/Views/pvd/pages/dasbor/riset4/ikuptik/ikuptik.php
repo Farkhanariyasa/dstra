e470e70402dca5b2b8d7bd53ac3998d7604a6752
imams <222011830@gmail.com>
2023-05-01 19:03:31 +0700

    A Assets: Menambah file php untuk peta ikuptik

---
+++ app/Views/pvd/pages/dasbor/riset4/ikuptik/ikuptik.php
@@ -0,0 +1,22 @@
+<div class="portfolio-item" onresize="responsivefonts()">
+    <div class="container mt-5 mb-5">
+        <div class="card d-flex justify-content-center flex-column align-item-center flex-md primary pb-5 pt-5 shadow">
+            <p class="card-text text-center">
+                <strong>IKUPTIK Per Kecamatan di Kota Batu</strong>
+            </p>
+            <div class="d-flex flex-row align-item-center justify-content-center">
+                <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center">
+                    <div class="me-1 mt-1 justify-content-end align-item-end">
+                        <button id="" type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="">
+                            <i class="fa-solid fa-download"></i>
+                        </button>
+                    </div>
+                </div>
+                <div id="peta-batu" style="width: 95%; height:80vh; z-index: 0;"></div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Modal Riset 4  -->
+<?php include('modal_ikuptik.php') ?>
