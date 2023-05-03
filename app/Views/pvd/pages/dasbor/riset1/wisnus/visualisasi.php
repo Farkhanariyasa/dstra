99f5d4f1868d7aa13840485d36f4b8b3c17335e3
main <222011356@stis.ac.id>
2023-05-03 17:05:10 +0700

    M Views : import js pie chart

--- app/Views/pvd/pages/dasbor/riset1/wisnus/visualisasi.php
+++ app/Views/pvd/pages/dasbor/riset1/wisnus/visualisasi.php
@@ -55,6 +55,7 @@
 <!-- Impor Chart -->
 <script src="<?= base_url('pvd/js/riset1/hasilSPRiset1_horizontalbar.js') ?>"></script>
 <script src="<?= base_url('pvd/js/riset1/hasilWisnus_Output1.js') ?>"></script>
+<script src="<?= base_url('pvd/js/riset1/hasilWisnus_Output2.js') ?>"></script>
 <script src="<?= base_url('pvd/js/riset1/hasilSPRiset1_mpw_bar.js') ?>"></script>
 <script src="<?= base_url('pvd/js/riset1/hasilSPRiset1_simcard.js') ?>"></script>
 
