ba6d5202bad11db82e40c8153b0178bcc1637872
NickoRagasukma <gabrielprimaditya@gmail.com>
2023-05-04 22:15:36 +0700

    M Assets : Modif grafik sankey riset 2

--- public/pvd/js/riset2/hasilSPRiset2_pw_sankey.js
+++ public/pvd/js/riset2/hasilSPRiset2_pw_sankey.js
@@ -23,30 +23,130 @@ new Chart(pw_sankey, {
       {
         data: [
           {
-            from: "Sukun",
-            to: "Blimbing",
-            flow: 54,
+            from: "Blimbing",
+            to: " Blimbing",
+            flow: 113,
+          },
+          {
+            from: "Blimbing",
+            to: " Kedungkandang",
+            flow: 563,
+          },
+          {
+            from: "Blimbing",
+            to: " Klojen",
+            flow: 2936,
+          },
+          {
+            from: "Blimbing",
+            to: " Lowokwaru",
+            flow: 724,
+          },
+          {
+            from: "Blimbing",
+            to: " Sukun",
+            flow: 391,
+          },
+          {
+            from: "Kedungkandang",
+            to: " Blimbing",
+            flow: 460,
+          },
+          {
+            from: "Kedungkandang",
+            to: " Kedungkandang",
+            flow: 181,
+          },
+          {
+            from: "Kedungkandang",
+            to: " Klojen",
+            flow: 3666,
+          },
+          {
+            from: "Kedungkandang",
+            to: " Lowokwaru",
+            flow: 346,
+          },
+          {
+            from: "Kedungkandang",
+            to: " Sukun",
+            flow: 393,
+          },
+          {
+            from: "Klojen",
+            to: " Blimbing",
+            flow: 768,
+          },
+          {
+            from: "Klojen",
+            to: " Kedungkandang",
+            flow: 736,
+          },
+          {
+            from: "Klojen",
+            to: " Klojen",
+            flow: 3981,
+          },
+          {
+            from: "Klojen",
+            to: " Lowokwaru",
+            flow: 674,
+          },
+          {
+            from: "Klojen",
+            to: " Sukun",
+            flow: 749,
+          },
+          {
+            from: "Lowokwaru",
+            to: " Blimbing",
+            flow: 905,
+          },
+          {
+            from: "Lowokwaru",
+            to: " Kedungkandang",
+            flow: 415,
+          },
+          {
+            from: "Lowokwaru",
+            to: " Klojen",
+            flow: 3557,
+          },
+          {
+            from: "Lowokwaru",
+            to: " Lowokwaru",
+            flow: 511,
+          },
+          {
+            from: "Lowokwaru",
+            to: " Sukun",
+            flow: 560,
           },
           {
             from: "Sukun",
-            to: "Kedungkandang",
-            flow: 39,
+            to: " Blimbing",
+            flow: 267,
           },
           {
             from: "Sukun",
-            to: "Klojen",
-            flow: 332,
+            to: " Kedungkandang",
+            flow: 366,
           },
           {
             from: "Sukun",
-            to: "Lowokwaru",
-            flow: 15,
+            to: " Klojen",
+            flow: 2510,
           },
-          // {
-          //   from: "Sukun",
-          //   to: "Sukun",
-          //   flow: 25,
-          // },
+          {
+            from: "Sukun",
+            to: " Lowokwaru",
+            flow: 218,
+          },
+          {
+            from: "Sukun",
+            to: " Sukun",
+            flow: 40,
+          },
         ],
         colorFrom: (c) => c2(c.dataset.data[c.dataIndex].from),
         colorTo: (c) => c2(c.dataset.data[c.dataIndex].to),
@@ -63,10 +163,13 @@ new Chart(pw_sankey, {
         align: "center",
         color: "black",
         display: true,
-        text: ["Estimasi Jumlah Perjalanan Wisata Menurut Kecamatan Asal dan", "Kecamatan Tujuan Wisata di Kota Malang Bulan Oktober-Desember 2022"],
+        text: [
+          "Estimasi Jumlah Perjalanan Wisata Menurut Kecamatan Asal dan",
+          "Kecamatan Tujuan Wisata di Kota Malang Bulan Oktober-Desember 2022",
+        ],
         font: {
           size: 16,
-          style: "normal"
+          style: "normal",
         },
       },
     },
