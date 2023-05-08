d1271dfe16f2e891ba1fe59387bb65bdb306630e
NickoRagasukma <gabrielprimaditya@gmail.com>
2023-05-08 22:32:02 +0700

    M Views : Tambah fitur pilih chart riset 2

--- app/Views/pvd/pages/dasbor/riset2/hasil_penelitian/dummy_tujuan2.php
+++ app/Views/pvd/pages/dasbor/riset2/hasil_penelitian/dummy_tujuan2.php
@@ -31,6 +31,14 @@
             <div class="card shadow rounded-5">
                 <div class="card-body">
                     <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center ">
+                        <div class="selectBox mt-2 me-2">
+                            <form action="">
+                                <select class="dropdownunduh1" id="forBarChart">
+                                    <option value="1">Satuan Malam</option>
+                                    <option value="2">Satuan Jam</option>
+                                </select>
+                            </form>
+                        </div>
                         <div class="me-1 mt-1 justify-content-end align-item-end">
                             <button id="unduh-2-4" type="button" class="tombol btn-for" data-bs-toggle="modal"
                                 data-bs-target="#exampleModal4">
@@ -104,7 +112,7 @@
                     </div>
 
                     <div style=" height:500px;">
-                        <canvas class="mt1" id="t2_bar2"></canvas>
+                        <canvas class="mt1" id=""></canvas>
                     </div>
                 </div>
             </div>
