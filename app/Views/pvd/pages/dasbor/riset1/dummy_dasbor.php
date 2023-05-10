d9b6181bab9d4e3aab7d82bdc9bbf356f70c12fc
Thomson <222011365@stis.ac.id>
2023-05-11 01:44:38 +0700

    M Views: tampilan wisnus dasbor

--- app/Views/pvd/pages/dasbor/riset1/dummy_dasbor.php
+++ app/Views/pvd/pages/dasbor/riset1/dummy_dasbor.php
@@ -39,13 +39,13 @@
                                 <canvas class="mt1" id="riset1_hasil1"></canvas>
                             </div><br>
                             <div>
-                        <p class="card-text text-justify">
-                            Grafik menunjukkan banyaknya rumah tangga berdasarkan
-                            jumlah anggota rumah tangga yang melakukan perjalanan wisata.
-                            Terlihat bahwa hanya 5 rumah tangga (5,9%) yang
-                            jumlah anggotanya sebesar 5 orang yang melakukan perjalanan wisata.
-                        </p>
-                    </div>
+                                <p class="card-text text-justify">
+                                    Grafik menunjukkan banyaknya rumah tangga berdasarkan
+                                    jumlah anggota rumah tangga yang melakukan perjalanan wisata.
+                                    Terlihat bahwa hanya 5 rumah tangga (5,9%) yang
+                                    jumlah anggotanya sebesar 5 orang yang melakukan perjalanan wisata.
+                                </p>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -136,6 +136,7 @@
     </div>
 </div>
 
+<!-- Family Grouping -->
 <div class="portfolio-item filter-riset1-fg " onresize="responsivefonts()">
     <div class="isi-tujuan shadow mt-0 mb-0">
         <h4 class="card-title judul-card">Family Grouping</h4>
@@ -188,34 +189,47 @@
     </div>
 </div>
 
-
+<!-- Wisnus -->
 <div class="portfolio-item filter-riset1-wisnus" onresize="responsivefonts()">
-    <!-- < class="row"> -->
-    <div class="col-lg-12 grid-margin stretch-card">
-        <div class="card shadow">
-            <div class="card-body">
-                <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center ">
-                    <div class="me-1 mt-1 justify-content-end align-item-end">
-                        <button type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModalwisnus">
-                            <i class="fa-solid fa-download"></i>
-                        </button>
+    <div class="isi-tujuan shadow mt-0 mb-0">
+        <h4 class="card-title judul-card">Statistik Wisatawan Nusantara</h4>
+        <div class="card-body">
+            <div class="row">
+                <div class="col-lg-6 grid-margin stretch-card h-100">
+                    <div class="card shadow rounded-4">
+                        <div class="card-body">
+                            <div class="chartBox" style="height:400px">
+                                <canvas class="mt1" id="riset1_hasil4"></canvas>
+                            </div>
+                            <div class="position-absolute bot-0  end-0 d-flex flex-row justify-content-center align-item-center ">
+                                <div class="form-check form-switch me-3">
+                                    <input class="form-check-input" onchange="update_value_mpw(this);" type="checkbox" id="forBarChartAxis_mpw" value="show" checked>
+                                    <label class="form-check-label" style="margin-left:-3px;" for="flexSwitchCheckChecked">Tampilkan Sumbu</label>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div style=" height:500px;">
-                    <canvas class="mt1" id="riset1_hasil4"></canvas>
-                </div>
-                <div class="position-absolute bot-0  end-0 d-flex flex-row justify-content-center align-item-center ">
-                    <div class="form-check form-switch me-3">
-                        <input class="form-check-input" onchange="update_value_data(this);" type="checkbox" id="forBarChartAxis_data" value="show" checked>
-                        <label class="form-check-label" style="margin-left:-3px;" for="flexSwitchCheckChecked">Tampilkan Sumbu</label>
+                <div class="col-lg-6 grid-margin stretch-card h-100">
+                    <div class="card bg-secondary">
+                        <div class="card-body">
+                            <div class="chartBox" style="height: 400px;">
+                                <canvas class="mt1" id="riset1_hasil5"></canvas>
+                            </div>
+                        </div>
                     </div>
                 </div>
-
+                <div class="position-relative bottom-0 end-0 mt-0 mr-4 d-flex flex-row justify-content-end align-items-end">
+                    <a id="tombol" href="<?= base_url('hasil-pkl/riset1/wisnus-visualisasi') ?>" class="tombol btn-for text-decoration-none">
+                        <span>Selengkapnya</span>
+                        <i class="fas fa-arrow-circle-right"></i>
+                    </a>
+                </div>
             </div>
         </div>
     </div>
-    <!-- <> -->
 </div>
+
 <!-- </div> -->
 
 <!-- Modal Dasbor -->
