7a043bdf48a06c724c487514cb37ae3bbe25c6de
Thomson <222011365@stis.ac.id>
2023-05-11 01:47:29 +0700

    M Views: statistik relawan sby header

--- app/Views/pvd/pages/dasbor/riset1/mpd/statistikrelawansby.php
+++ app/Views/pvd/pages/dasbor/riset1/mpd/statistikrelawansby.php
@@ -1,4 +1,20 @@
 <?= $this->extend('pvd/layout/dasbor/template'); ?>
 <?= $this->section('content'); ?>
 
+<section id="portfolio" class="portfolio section-bg">
+    <div class="container">
+        <header class="section-header">
+            <p>Statistik Relawan Surabaya</p>
+        </header>
+        <hr class="hrjudul">
+    </div>
+
+
+
+
+
+</section>
+
+
+
 <?= $this->endSection(); ?>
