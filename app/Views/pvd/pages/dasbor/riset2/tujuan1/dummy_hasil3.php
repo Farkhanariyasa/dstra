6551444166dd7c5d17ebaa741a3ed8a5b15a7600
NickoRagasukma <gabrielprimaditya@gmail.com>
2023-05-11 22:58:33 +0700

    M Views : Modif tampilan tujuan 1 hasil 3 riset 2

--- app/Views/pvd/pages/dasbor/riset2/tujuan1/dummy_hasil3.php
+++ app/Views/pvd/pages/dasbor/riset2/tujuan1/dummy_hasil3.php
@@ -1,104 +1,46 @@
-<!-- Visualisasi 1 -->
-<div class="portfolio-item filter-riset2-hasilkajian3-1">
-    <!-- <div class="row mt-5">
-        <div class="col-sm-6 my-2">
-            <div class="card">
-                <div class="card-body">
-                    <p class="card-text text-center">Grafik<br><br><br><br><br><br><br><br><br><br><br></p>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-sm-6 my-2">
-            <div class="card">
-                <div class="card-body">
-                    <p class="card-text text-center">Grafik<br><br><br><br><br><br><br><br><br><br><br></p>
-                </div>
-            </div>
-        </div>
-    </div> -->
-
+<!-- Tab 3 new -->
+<div class="portfolio-item filter-riset2-hasiltujuan1-3">
     <div class="row mt-3">
-        <div class="col-lg-12 grid-margin stretch-card">
-            <div class="card shadow">
+        <div class="col-sm-5 my-2">
+            <div class="card shadow rounded-5">
                 <div class="card-body">
-                    <div
-                        class="position-absolute top-0 end-0 d-flex flex-row justify-content-center align-item-center ">
+                    <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center ">
                         <div class="me-1 mt-1 justify-content-end align-item-end">
-                            <button id="unduh-3-2" type="button" class="tombol btn-for" data-bs-toggle="modal"
-                                data-bs-target="#exampleModal2">
+                            <button id="unduh-2-2" type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal2">
                                 <i class="fa-solid fa-download"></i>
                             </button>
                         </div>
                     </div>
-
                     <div style=" height:500px;">
-                        <canvas class="mt1" id="riset2_hasil1"></canvas>
+                        <canvas class="mt-1" id="t1_pie"></canvas>
+                    </div>
+                    <div class="position-absolute bot-0  end-0 d-flex flex-row justify-content-center align-item-center ">
+                        <div class="form-check form-switch me-3">
+                            <input class="form-check-input" onchange="update_value1(this);" type="checkbox" id="forPieChartLegend" value="show">
+                            <label class="form-check-label" style="margin-left:-3px;" for="flexSwitchCheckChecked">Tampilkan Legend</label>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-
-    <div class="row mt-3">
-        <div class="col-lg-12 grid-margin stretch-card">
-            <div class="card shadow">
-                <div class="card-body">
-                    <div
-                        class="position-absolute top-0 end-0 d-flex flex-row justify-content-center align-item-center ">
+        <div class="col-sm-7 my-2">           
+            <div class="card d-flex justify-content-center flex-column align-item-center flex-md primary pb-5 pt-5 shadow rounded-5">
+                <div class="d-flex flex-row align-item-center justify-content-center">
+                    <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center ">
                         <div class="me-1 mt-1 justify-content-end align-item-end">
-                            <button id="unduh-3-3" type="button" class="tombol btn-for" data-bs-toggle="modal"
-                                data-bs-target="#exampleModal3">
+                            <button id="unduh-2-3" type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal3">
                                 <i class="fa-solid fa-download"></i>
                             </button>
                         </div>
                     </div>
-
                     <div style=" height:500px;">
-                        <canvas class="mt1" id="riset2_hasil2"></canvas>
+                        <canvas class="mt-1" id="t1_twowaybar"></canvas>
                     </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-
-<!-- Visualisasi 2 -->
-<div class="portfolio-item filter-riset2-hasilkajian3-2">
-    <div class="row mt-3">
-        <div class="col-lg-12 grid-margin stretch-card">
-            <div class="card shadow">
-                <div class="card-body">
-                    <div class="position-absolute top-0 end-0 d-flex flex-row justify-content-center align-item-center ">
-                        <div class="me-1 mt-1 justify-content-end align-item-end">
-                            <button id="unduh-3-3" type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal3">
-                                <i class="fa-solid fa-download"></i>
-                            </button>
+                    <div class="position-absolute bot-0  end-0 d-flex flex-row justify-content-center align-item-center ">
+                        <div class="form-check form-switch me-3">
+                            <input class="form-check-input" onchange="update_value2(this);" type="checkbox" id="forBarChartLegend" value="show">
+                            <label class="form-check-label" style="margin-left:-3px;" for="flexSwitchCheckChecked">Tampilkan Legend</label>
                         </div>
-                    </div>
-
-                    <div style=" height:500px;">
-                        <canvas class="mt1" id="riset2_hasil3"></canvas>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="row mt-3">
-        <div class="col-lg-12 grid-margin stretch-card">
-            <div class="card shadow">
-                <div class="card-body">
-                    <div class="position-absolute top-0 end-0 d-flex flex-row justify-content-center align-item-center ">
-                        <div class="me-1 mt-1 justify-content-end align-item-end">
-                            <button id="unduh-3-3" type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal3">
-                                <i class="fa-solid fa-download"></i>
-                            </button>
-                        </div>
-                    </div>
-
-                    <div style=" height:500px;">
-                        <canvas class="mt1" id="riset2_hasil4"></canvas>
                     </div>
                 </div>
             </div>
