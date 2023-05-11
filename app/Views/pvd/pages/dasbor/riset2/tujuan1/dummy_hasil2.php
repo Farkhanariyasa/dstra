39c5c278c16d6a58442c3bf0f96e9d66b9929bdb
NickoRagasukma <gabrielprimaditya@gmail.com>
2023-05-11 22:38:32 +0700

    M Views : Modif tampilan tujuan 1 hasil 2 riset 2

--- app/Views/pvd/pages/dasbor/riset2/tujuan1/dummy_hasil2.php
+++ app/Views/pvd/pages/dasbor/riset2/tujuan1/dummy_hasil2.php
@@ -23,141 +23,58 @@
     </div>
 </div> -->
 
-<!-- Visualisasi 1 new -->
-<div class="portfolio-item filter-riset2-hasilkajian2-1">
+<!-- Tujuan 1 Hasil 2 -->
+<div class="portfolio-item filter-riset2-hasiltujuan1-2">
     <div class="row mt-3">
-        
-        <div class="col-sm-8 my-2 py-5">
+        <div class="col-sm-4 my-2 ">
             <div class="card shadow rounded-5">
                 <div class="card-body">
-                    <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center">
-                        <div class="selectBox mt-2 me-2">
-                            <form action="">
-                                <select class="dropdownunduh1" id="forBarChart">
-                                    <option value="1">Satuan Malam</option>
-                                    <option value="2">Satuan Jam</option>
-                                </select>
-                            </form>
+                    <div class="text-center">
+                        <i class="fas fa-user-alt col-1 ikon" style="font-size: 30px; line-height:30px;"></i>
+                        <div class="card-text text-center">
+                            Estimasi Total Jumlah Perjalanan
                         </div>
-                        <div class="me-1 mt-1 justify-content-end align-item-end">
-                            <button id="unduh-2-4" type="button" class="tombol btn-for" data-bs-toggle="modal"
-                                data-bs-target="#exampleModal4">
-                                <i class="fa-solid fa-download"></i>
-                            </button>
+                        <div>
+                            <div class="" style="color: #4b395f; font-size: 50px;"><strong>2180</strong></div>perjalanan 
                         </div>
-                    </div>
-                    <div style=" height:500px;">
-                        <canvas class="mt1" id="t2_bar1"></canvas>
                     </div>
                 </div>
             </div>
         </div>
-
-        <div class="col-sm-4 my-2">
-            <div class="card shadow rounded-5">
-                <!-- Turis -->
-                <div class="card-body">
-                    <div style="line-height: 40px; color: #4b395f; font-size:25px;">
-                        <i class="fas fa-city col-1 ikon"></i><strong> &nbsp; Turis</strong>
-                    </div>
-                    <div style="font-size:15px;">
-                        Turis merupakan wisatawan dalam kota yang melakukan perjalanan terakhir lebih dari 1 malam.
-                    </div>
-                </div>
-            </div>
-
-            <div class="card shadow rounded-5 my-4">
-                <!-- Pelancong -->
-                <div class="card-body">
-                    <div style="line-height: 40px; color: #4b395f; font-size:25px;">
-                        <i class="fas fa-city col-1 ikon"></i><strong> &nbsp; Pelancong</strong>
-                    </div>
-                    <div style="font-size:15px;">
-                        Pelancong merupakan wisatawan nusantara yang melakukan perjalanan terakhir kurang dari 1 malam.
-                    </div>
-                </div>
-            </div>
-
-            <div class="card shadow rounded-5">
-                <!-- Malam -->
-                <div class="card-body">
-                    <div style="line-height: 40px; color: #4b395f; font-size:25px;">
-                        <i class="fas fa-city col-1 ikon"></i><strong> &nbsp; Malam</strong>
-                    </div>
-                    <div style="font-size:15px;">
-                        Lama perjalanan dan kunjungan wisata diukur dalam malam atau jam. Dalam hal ini, malam didefinisikan ketika terjadi pergantian tanggal atau melewati pukul 00.00.
-                    </div>
-                </div>
-            </div>
-
-        </div>
-
-        
-    </div>
-</div>
-
-<!-- Visualisasi 2 new -->
-<div class="portfolio-item filter-riset2-hasilkajian2-2">
-    <div class="row mt-3">
-        
-        <div class="col-sm-8 my-2 py-5">
+        <div class="col-sm-8 my-2">
             <div class="card shadow rounded-5">
                 <div class="card-body">
                     <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center ">
                         <div class="me-1 mt-1 justify-content-end align-item-end">
-                            <button id="unduh-2-6" type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal6">
+                            <button id="unduh-2-2" type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal2">
                                 <i class="fa-solid fa-download"></i>
                             </button>
                         </div>
                     </div>
-
                     <div style=" height:500px;">
-                        <canvas class="mt1" id=""></canvas>
+                        <canvas class="mt-1" id="t1_sankey"></canvas>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
 
-        <div class="col-sm-4 my-2">
-            <div class="card shadow rounded-5">
-                <!-- Turis -->
-                <div class="card-body">
-                    <div style="line-height: 40px; color: #4b395f; font-size:25px;">
-                        <i class="fas fa-city col-1 ikon"></i><strong> &nbsp; Turis</strong>
-                    </div>
-                    <div style="font-size:15px;">
-                        Turis merupakan wisatawan dalam kota yang melakukan perjalanan terakhir lebih dari 1 malam.
+    <div class="container mb-5 mt-2">
+        <div class="card d-flex justify-content-center flex-column align-item-center flex-md primary pb-5 pt-5 shadow rounded-5">
+            <p class="card-text text-center">
+                <strong>Jumlah Perjalanan dalam Kota Malang menurut Kecamatan Asal <br>Wisatawan di Kota Malang Bulan Oktober-Desember 2022</strong>
+            </p>
+            <div class="d-flex flex-row align-item-center justify-content-center">
+                <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center">
+                    <div class="me-1 mt-1 justify-content-end align-item-end">
+                        <button id="" type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="">
+                            <i class="fa-solid fa-download"></i>
+                        </button>
                     </div>
                 </div>
+                <div id="petatematik2" style="width: 95%; height:80vh; z-index:0;"></div>
             </div>
-
-            <div class="card shadow rounded-5 my-4">
-                <!-- Pelancong -->
-                <div class="card-body">
-                    <div style="line-height: 40px; color: #4b395f; font-size:25px;">
-                        <i class="fas fa-city col-1 ikon"></i><strong> &nbsp; Pelancong</strong>
-                    </div>
-                    <div style="font-size:15px;">
-                        Pelancong merupakan wisatawan nusantara yang melakukan perjalanan terakhir kurang dari 1 malam.
-                    </div>
-                </div>
-            </div>
-
-            <div class="card shadow rounded-5">
-                <!-- Malam -->
-                <div class="card-body">
-                    <div style="line-height: 40px; color: #4b395f; font-size:25px;">
-                        <i class="fas fa-city col-1 ikon"></i><strong> &nbsp; Malam</strong>
-                    </div>
-                    <div style="font-size:15px;">
-                        Lama perjalanan dan kunjungan wisata diukur dalam malam atau jam. Dalam hal ini, malam didefinisikan ketika terjadi pergantian tanggal atau melewati pukul 00.00.
-                    </div>
-                </div>
-            </div>
-
         </div>
-
-        
     </div>
 </div>
 
