525310dbe7b3908b6067fcfbcf37d641fc78725a
NickoRagasukma <gabrielprimaditya@gmail.com>
2023-05-15 21:22:26 +0700

    M Views : Tambah script js fungsi tabulasi riset 2

--- app/Views/pvd/pages/dasbor/riset2/tujuan1/tabulasi.php
+++ app/Views/pvd/pages/dasbor/riset2/tujuan1/tabulasi.php
@@ -27,24 +27,10 @@
 
 <!-- Istope -->
 <script src="https://unpkg.com/isotope-layout@3/dist/isotope.pkgd.js"></script>
-<!-- Leaflet JS -->
-<script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js" integrity="sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM=" crossorigin=""></script>
-<!-- Chart JS -->
-<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/chartjs-plugin-datalabels/2.2.0/chartjs-plugin-datalabels.min.js" integrity="sha512-JPcRR8yFa8mmCsfrw4TNte1ZvF1e3+1SdGMslZvmrzDYxS69J7J49vkFL8u6u8PlPJK+H3voElBtUCzaXj+6ig==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-<!-- Chart JS Plugin Sankey Chart -->
-<script src="https://cdn.jsdelivr.net/npm/chartjs-chart-sankey"></script>
-<!-- Data Chart -->
-<script>
-    const datajeniskelamin = <?= json_encode($jk); ?>;
-    const datajenispekerjaanutama = <?= json_encode($jpu); ?>;
-    const datapendidikantertinggi_riset2 = <?= json_encode($pt_2); ?>;
-</script>
-<!-- Impor Chart -->
-<script src="<?= base_url('pvd/js/riset2/hasilSPRiset2_jk_bar.js') ?>"></script>
-<script src="<?= base_url('pvd/js/riset2/hasilSPRiset2_barhorizontal.js') ?>"></script>
-<script src="<?= base_url('pvd/js/riset2/hasilSPRiset2_pw_sankey.js') ?>"></script>
-<script src="<?= base_url('pvd/js/riset2/hasilSPRiset2_peta.js') ?>"></script>
+
+<!-- Datatable Instal -->
+<script src="<?= base_url('pvd/js/riset2/hasilSPRiset2_tabelkajian1.js'); ?>"></script>
+
 <!-- Fungsi Unduh Chart -->
 <script src="<?= base_url('pvd/js/riset2/btn_Download.js') ?>"></script>
 
