daed85981c0bd7c5b3ce692e959a2366ecec0b33
imams <222011830@gmail.com>
2023-05-16 22:45:13 +0700

    M View: Menambah grafik-grafik anades anafer

--- app/Views/pvd/pages/dasbor/riset4/dasbor_riset4.php
+++ app/Views/pvd/pages/dasbor/riset4/dasbor_riset4.php
@@ -170,6 +170,30 @@
                     </div>
                 </div>
 
+                <!-- Part 2a -->
+                <div class="col-lg-12 grid-margin stretch-card">
+                    <div class="card shadow">
+                        <div class="card-body">
+                            <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center ">
+                                <div class="me-1 mt-1 justify-content-end align-item-end">
+                                    <button type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal4-doughnut">
+                                        <i class="fa-solid fa-download"></i>
+                                    </button>
+                                </div>
+                            </div>
+                            <div class="chartBox" style=" height:300px;">
+                                <canvas class="mt1" id="bar-kota-batu-pendidikan"></canvas>
+                            </div>
+                            <!-- <div class="position-absolute bot-0  end-0 d-flex flex-row justify-content-center align-item-center ">
+                                <div class="form-check form-switch me-3">
+                                    <input class="form-check-input" onchange="update_value_legend_bar_pendidikan(this);" type="checkbox" id="bar-kota-batu-pendidikan-legend" value="show" checked>
+                                    <label class="form-check-label" style="margin-left:-3px;" for="flexSwitchCheckChecked">Tampilkan Legenda</label>
+                                </div>
+                            </div> -->
+                        </div>
+                    </div>
+                </div>
+
                 <!-- Part 3 -->
                 <div class="col-lg-12 grid-margin stretch-card">
                     <div class="card shadow">
@@ -286,6 +310,76 @@
                     </div>
                 </div>
 
+                <!-- Part 3a -->
+                <div class="col-lg-12 grid-margin stretch-card">
+                    <div class="card shadow">
+                        <div class="card-body">
+                            <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center ">
+                                <div class="me-1 mt-1 justify-content-end align-item-end">
+                                    <button type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal4-doughnut">
+                                        <i class="fa-solid fa-download"></i>
+                                    </button>
+                                </div>
+                            </div>
+                            <div class="chartBox" style=" height:300px;">
+                                <canvas class="mt1" id="pie-kota-batu-pemanfaatan"></canvas>
+                            </div>
+                            <div class="position-absolute bot-0  end-0 d-flex flex-row justify-content-center align-item-center ">
+                                <div class="form-check form-switch me-3">
+                                    <input class="form-check-input" onchange="update_value_pemanfaatan1(this);" type="checkbox" id="pie-kota-batu-pemanfaatan-legend" value="show" checked>
+                                    <label class="form-check-label" style="margin-left:-3px;" for="flexSwitchCheckChecked">Tampilkan Legenda</label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-6 grid-margin stretch-card ">
+                    <div class="card shadow">
+                        <div class="card-body">
+                            <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center ">
+                                <div class="me-1 mt-1 justify-content-end align-item-end">
+                                    <button type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal4-doughnut">
+                                        <i class="fa-solid fa-download"></i>
+                                    </button>
+                                </div>
+                            </div>
+                            <div class="chartBox" style=" height:300px;">
+                                <canvas class="mt1" id="pie-kota-batu-pemanfaatan-jenis"></canvas>
+                            </div>
+                            <div class="position-absolute bot-0  end-0 d-flex flex-row justify-content-center align-item-center ">
+                                <div class="form-check form-switch me-3">
+                                    <input class="form-check-input" onchange="update_value_pemanfaatan_jenis1(this);" type="checkbox" id="pie-kota-batu-pemanfataan-jenis-legend" value="show" checked>
+                                    <label class="form-check-label" style="margin-left:-3px;" for="flexSwitchCheckChecked">Tampilkan Legenda</label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-6 grid-margin stretch-card ">
+                    <div class="card shadow">
+                        <div class="card-body">
+                            <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center ">
+                                <div class="me-1 mt-1 justify-content-end align-item-end">
+                                    <button type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal4-doughnut">
+                                        <i class="fa-solid fa-download"></i>
+                                    </button>
+                                </div>
+                            </div>
+                            <div class="chartBox" style=" height:300px;">
+                                <canvas class="mt1" id="pie-kota-batu-pemanfaatan-skala"></canvas>
+                            </div>
+                            <div class="position-absolute bot-0  end-0 d-flex flex-row justify-content-center align-item-center ">
+                                <div class="form-check form-switch me-3">
+                                    <input class="form-check-input" onchange="update_value_pemanfaatan_skala1(this);" type="checkbox" id="pie-kota-batu-pemanfaatan-skala-legend" value="show" checked>
+                                    <label class="form-check-label" style="margin-left:-3px;" for="flexSwitchCheckChecked">Tampilkan Legenda</label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
                 <!-- Part 4 -->
                 <div class="col-12 grid-margin stretch-card">
                     <div class="card shadow">
@@ -349,6 +443,23 @@
                     </div>
                 </div>
 
+                <!-- Part 5 -->
+                <div class="col-12 grid-margin stretch-card">
+                    <div class="card shadow">
+                        <div class="card-body">
+                            <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center ">
+                                <div class="me-1 mt-1 justify-content-end align-item-end">
+                                    <button type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal4-barplot">
+                                        <i class="fa-solid fa-download"></i>
+                                    </button>
+                                </div>
+                            </div>
+                            <div style=" height:400px;">
+                                <canvas id="bar-kota-batu-pemanfaatan"></canvas>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
