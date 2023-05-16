61a8c86499396941af7a719f5254df6afcc88803
renatadelarosa <222011432@stis.ac.id>
2023-05-16 11:53:44 +0700

    M Assets : revisi chart tujuan 1 riset 2

--- public/pvd/js/riset2/hasilPKLRiset2_t1_bar.js
+++ public/pvd/js/riset2/hasilPKLRiset2_t1_bar.js
@@ -5,11 +5,11 @@ var bar_lamakunjungan = new Chart(t1_bar, {
   type: "bar",
   data: {
     labels: [
-      "Kecamatan Klojen",
-      "Kecamatan Lowokwaru",
-      "Kecamatan Blimbing",
-      "Kecamatan Kedungkandang",
-      "Kecamatan Sukun",
+      "Klojen",
+      "Lowokwaru",
+      "Blimbing",
+      "Kedungkandang",
+      "Sukun",
     ],
     datasets: [
       {
@@ -17,6 +17,11 @@ var bar_lamakunjungan = new Chart(t1_bar, {
         data: [53.48, 53.85, 56.67, 61.9, 67.33],
         borderWidth: 1,
         backgroundColor: "#ffab00",
+        datalabels: {
+          anchor: 'end',
+          align: 'top',
+          offset: 5
+        }
       },
     ],
   },
@@ -75,7 +80,8 @@ var bar_lamakunjungan = new Chart(t1_bar, {
         display: true,
         text: [
           "Persentase Penduduk yang Berwisata dalam Kota Malang",
-          "menurut Kecamatan Asal Wisatawan di Kota Malang Bulan Oktober-Desember 2022",
+          "menurut Kecamatan Asal Wisatawan di Kota Malang",
+          "Bulan Oktober-Desember 2022",
         ],
         font: {
           size: 16,
