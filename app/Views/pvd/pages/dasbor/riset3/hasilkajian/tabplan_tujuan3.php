b62a2c3e25448ccdf25e37f9c5d6a3260f50d990
putriramatillah <212011283@stis.ac.id>
2023-05-17 00:27:27 +0700

    A Views: Tabulasi tabplan tujuan 3 riset 3

---
+++ app/Views/pvd/pages/dasbor/riset3/hasilkajian/tabplan_tujuan3.php
@@ -0,0 +1,198 @@
+<?php
+function tabplan3_1() { 
+    ?>
+<div class="card">
+    <h3><b>Tabulasi</b></h3>
+    <div class="card-body table-responsive border rounded" style="padding: 0.5rem 1rem;">
+        <table class="table table-hover table-bordered table-responsive display" style="width:100%">
+            <thead>
+                <tr>
+                    <th scope="col">Jenis Kelamin</th>
+                    <th scope="col">Pekerja Penuh</th>
+                    <th scope="col">Pekerja Tidak Penuh</th>
+                    <th scope="col">Total</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">Laki-laki</th>
+                    <td>69.69</td>
+                    <td>30.31</td>
+                    <td>100</td>
+                </tr>
+                <tr>
+                    <th scope="row">Perempuan</th>
+                    <td>72.08</td>
+                    <td>27.92</td>
+                    <td>100</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
+<?php }
+
+function tabplan3_2() { 
+    ?>
+<div class="card">
+    <h3><b>Tabulasi</b></h3>
+    <div class="card-body table-responsive border rounded" style="padding: 0.5rem 1rem;">
+        <table class="table table-hover table-bordered table-responsive display" style="width:100%">
+            <thead>
+                <tr>
+                    <th scope="col">Pelatihan</th>
+                    <th scope="col">Pekerja Penuh</th>
+                    <th scope="col">Pekerja Tidak Penuh</th>
+                    <th scope="col">Total</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">Di Bawah SMA/SMK</th>
+                    <td>65.79</td>
+                    <td>34.21</td>
+                    <td>100</td>
+                </tr>
+                <tr>
+                    <th scope="row">SMA/SMK</th>
+                    <td>74.57</td>
+                    <td>25.43</td>
+                    <td>100</td>
+                </tr>
+                <tr>
+                    <th scope="row">Di Atas SMA/SMK</th>
+                    <td>73.08</td>
+                    <td>26.92</td>
+                    <td>100</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
+<?php }
+
+function tabplan3_3() { 
+    ?>
+<div class="card">
+    <h3><b>Tabulasi</b></h3>
+    <div class="card-body table-responsive border rounded" style="padding: 0.5rem 1rem;">
+        <table class="table table-hover table-bordered table-responsive display" style="width:100%">
+            <thead>
+                <tr>
+                    <th scope="col">Status Perkawinan</th>
+                    <th scope="col">Pekerja Penuh</th>
+                    <th scope="col">Pekerja Tidak Penuh</th>
+                    <th scope="col">Total</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">Belum Pernah Kawin</th>
+                    <td>76.34</td>
+                    <td>23.66</td>
+                    <td>100</td>
+                </tr>
+                <tr>
+                    <th scope="row">Pernah Kawin</th>
+                    <td>68.60</td>
+                    <td>31.40</td>
+                    <td>100</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
+<?php }
+
+function tabplan3_4() { 
+    ?>
+<div class="card">
+    <h3><b>Tabulasi</b></h3>
+    <div class="card-body table-responsive border rounded" style="padding: 0.5rem 1rem;">
+        <table class="table table-hover table-bordered table-responsive display" style="width:100%">
+            <thead>
+                <tr>
+                    <th scope="col">Tingkat Pendapatan</th>
+                    <th scope="col">Pekerja Penuh</th>
+                    <th scope="col">Pekerja Tidak Penuh</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">< UMR </th>
+                    <td>69.54</td>
+                    <td>30.46</td>
+                </tr>
+                <tr>
+                    <th scope="row">>= UMR</th>
+                    <td>74.53</td>
+                    <td>25.47</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
+<?php }
+
+function tabplan3_5() { 
+    ?>
+<div class="card">
+    <h3><b>Tabulasi</b></h3>
+    <div class="card-body table-responsive border rounded" style="padding: 0.5rem 1rem;">
+        <table class="table table-hover table-bordered table-responsive display" style="width:100%">
+            <thead>
+                <tr>
+                    <th scope="col">Wilayah Tempat Tinggal</th>
+                    <th scope="col">Pekerja Penuh</th>
+                    <th scope="col">Pekerja Tidak Penuh</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">Perkotaan</th>
+                    <td>75.69</td>
+                    <td>24.31</td>
+                </tr>
+                <tr>
+                    <th scope="row">>= UMR</th>
+                    <td>52.90</td>
+                    <td>47.10</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
+<?php }
+
+function tabplan3_6() { 
+    ?>
+<div class="card">
+    <h3><b>Tabulasi</b></h3>
+    <div class="card-body table-responsive border rounded" style="padding: 0.5rem 1rem;">
+        <table class="table table-hover table-bordered table-responsive display" style="width:100%">
+            <thead>
+                <tr>
+                    <th scope="col">Pelatihan</th>
+                    <th scope="col">Pekerja Penuh</th>
+                    <th scope="col">Pekerja Tidak Penuh</th>
+                    <th scope="col">Total</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">Ya</th>
+                    <td>72.17</td>
+                    <td>27.83</td>
+                    <td>100</td>
+                </tr>
+                <tr>
+                    <th scope="row">Tidak</th>
+                    <td>70.36</td>
+                    <td>29.64</td>
+                    <td>100</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
+<?php }
