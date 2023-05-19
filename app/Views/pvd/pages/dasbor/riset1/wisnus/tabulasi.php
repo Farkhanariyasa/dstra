63040711f2ecd18b475c409df47fb849c09d7482
Thomson <222011365@stis.ac.id>
2023-05-19 19:19:44 +0700

    M Views: perubahan tampilan tabulasi wisnus riset 1

--- app/Views/pvd/pages/dasbor/riset1/wisnus/tabulasi.php
+++ app/Views/pvd/pages/dasbor/riset1/wisnus/tabulasi.php
@@ -3,7 +3,7 @@
 
 <?= $this->section('content'); ?>
 <header class="section-header">
-    <p>Kajian Pemanfaatan MPD dalam Menunjang Statistik Pariwisata</p>
+    <p>Statistik Wisatawan Nusantara - Tabulasi</p>
 </header>
 <hr class="hrjudul">
 
@@ -11,17 +11,35 @@
     <div class="container" data-aos="fade-up">
         <div class="row">
             <div class="col-lg-12 d-flex justify-content-center">
-                <ul id="portfolio-flters">
-                    <!-- <li data-filter="*">Semua</li> -->
-                    <li data-filter=".filter-riset1-hasil1" class="filter-active">Tabulasi A</li>
-                    <li data-filter=".filter-riset1-hasil2">Tabulasi B</li>
-                </ul>
+            <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist" style="color:#506396">
+                <li class="nav-item" role="presentation">
+                    <button class="nav-link active" id="pills-output1-tab" data-bs-toggle="pill" data-bs-target="#pills-output1" type="button" role="tab" aria-controls="pills-output1" aria-selected="true">Output 1</button>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <button class="nav-link" id="pills-output2-tab" data-bs-toggle="pill" data-bs-target="#pills-output2" type="button" role="tab" aria-controls="pills-output2" aria-selected="false">Output 2</button>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <button class="nav-link" id="pills-output3-tab" data-bs-toggle="pill" data-bs-target="#pills-output3" type="button" role="tab" aria-controls="pills-output3" aria-selected="false">Output 3</button>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <button class="nav-link" id="pills-output4-tab" data-bs-toggle="pill" data-bs-target="#pills-output4" type="button" role="tab" aria-controls="pills-output4" aria-selected="false">Output 4</button>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <button class="nav-link" id="pills-output5-tab" data-bs-toggle="pill" data-bs-target="#pills-output5" type="button" role="tab" aria-controls="pills-output5" aria-selected="false">Output 5</button>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <button class="nav-link" id="pills-output6-tab" data-bs-toggle="pill" data-bs-target="#pills-output6" type="button" role="tab" aria-controls="pills-output6" aria-selected="false">Output 6</button>
+                </li>
+            </ul>
             </div>
         </div>
-
-        <div class="row portfolio-container">
-            <?php include('dummy_submenu2.php') ?>
+        <div class="row " id="row">
+        <div class="row col-lg-12 d-flex justify-content-center mt-2">
+            <div class="row id="test">
+                <?php include('dummy_submenu2.php') ?>
+            </div>
         </div>
+    </div>
     </div>
 </section>
 
