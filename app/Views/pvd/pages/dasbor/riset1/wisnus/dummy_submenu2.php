efb2a6de2edf1b962764fa47841432c9dbdd4493
Thomson <222011365@stis.ac.id>
2023-05-19 19:17:39 +0700

    M Views: perubahan tampilan tabulasi wisnus

--- app/Views/pvd/pages/dasbor/riset1/wisnus/dummy_submenu2.php
+++ app/Views/pvd/pages/dasbor/riset1/wisnus/dummy_submenu2.php
@@ -1,81 +1,262 @@
-<!-- Tabulasi 1 -->
-<div class="portfolio-item filter-riset1-hasil1">
-    <div class="card">
-        <div class="card-header">
-            <div class="card-title text-center mt-2">Data Responden Hasil Survei Pendahuluan Riset 1 A</div>
-            <!-- <button type="button" class="tombol btn-for text-right me-1 mt-1 justify-content-end align-item-end" data-bs-toggle="modal" data-bs-target="#exampleModal4-doughnut">
-                            <i class="fa-solid fa-download"></i>
-   </button> -->
-        </div>
-        <div class="card-body table-responsive" style="padding: 0.5rem 1rem;">
-            <table class="table table-hover table-bordered table-responsive" id="k1_tabel1" style="width:100%;overflow-x:auto;">
-                <thead style="color: #fff; font-weight:bold;">
-                    <tr>
-                        <th scope="col">No.</th>
-                        <th scope="col">Nama</th>
-                        <th scope="col">Desa/Kelurahan</th>
-                        <th scope="col">Kecamatan</th>
-                        <th scope="col">Indosat</th>
-                        <th scope="col">Telkomsel</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php
-                    $i = 1;
-                    foreach ($dataresponden as $v) :
-                    ?>
+<div class="tab-content" id="pills-tabContent">
+    <!-- Tabulasi 1 -->
+    <div class="tab-pane fade show active" id="pills-output1" role="tabpanel" aria-labelledby="pills-output1-tab" tabindex="0">
+        <div class="card">
+            <div class="card-header">
+                <div class="card-title text-center mt-2">Jumlah Perjalanan Wisata Asal Kota Malang Triwulan IV 2022</div>
+            </div>
+            <div class="card-body table-responsive" style="padding: 0.5rem 1rem;">
+                <table class="table table-hover table-bordered table-responsive" id="k1_tabel1" style="width:100%;overflow-x:auto;">
+                    <thead style="color: #fff; font-weight:bold;">
                         <tr>
-                            <th scope="row"><?php echo $i; ?></th>
-                            <td><?php echo $v["b108"]; ?></td>
-                            <td><?php echo $v["b104_deskel"]; ?></td>
-                            <td><?php echo $v["b103_kec"]; ?></td>
-                            <td><?php echo $v["b201"]; ?></td>
-                            <td><?php echo $v["b203"]; ?></td>
+                            <th scope="col">Bulan</th>
+                            <th scope="col">Data MPD</th>
+                            <th scope="col">Data Survei Konvensional</th>
                         </tr>
-                    <?php
-                        $i++;
-                    endforeach;
-                    ?>
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Oktober</td>
+                            <td>-</td>
+                            <td>497.328</td>
+                        </tr>
+                        <tr>
+                            <td>November</td>
+                            <td>-</td>
+                            <td>60.817</td>
+                        </tr>
+                        <tr>
+                            <td>Desember</td>
+                            <td>-</td>
+                            <td>467.514</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
-</div>
 
-<div class="portfolio-item filter-riset1-hasil2">
-    <div class="card">
-        <div class="card-header">
-            <div class="card-title text-center mt-2">Data Responden Hasil Survei Pendahuluan Riset 1 B</div>
-        </div>
-        <div class="card-body table-responsive" style="padding: 0.5rem 1rem;">
-            <table class="table table-hover table-bordered table-responsive" id="k1_tabel2" style="width:100%;overflow-x:auto;">
-                <thead style="color: #fff; font-weight:bold;">
-                    <tr>
-                        <th scope="col">No.</th>
-                        <th scope="col">Nama</th>
-                        <th scope="col">Desa/Kelurahan</th>
-                        <th scope="col">Kecamatan</th>
-                        <th scope="col">Blok Sensus</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php
-                    $i = 1;
-                    foreach ($dataresponden as $v) :
-                    ?>
+    <div class="tab-pane fade " id="pills-output2" role="tabpanel" aria-labelledby="pills-output2-tab" tabindex="0">
+        <div class="card">
+            <div class="card-header">
+                <div class="card-title text-center mt-2">Persentase Perjalanan Wisata Asal Kota Malang Berdasarkan Kategori Perjalanan Wisata Triwulan IV 2022</div>
+            </div>
+            <div class="card-body table-responsive" style="padding: 0.5rem 1rem;">
+                <table class="table table-hover table-bordered table-responsive" id="k1_tabel2" style="width:100%;overflow-x:auto;">
+                    <thead style="color: #fff; font-weight:bold;">
                         <tr>
-                            <th scope="row"><?php echo $i; ?></th>
-                            <td><?php echo $v["b108"]; ?></td>
-                            <td><?php echo $v["b104_deskel"]; ?></td>
-                            <td><?php echo $v["b103_kec"]; ?></td>
-                            <td><?php echo $v["b105"]; ?></td>
+                            <th scope="col">Bulan</th>
+                            <th scope="col">Berkunjung ke Objek Wisata</th>
+                            <th scope="col">Menginap di Akomodasi Komersial</th>
+                            <th scope="col">Berkunjung ke Luar Kota untuk Tujuan Lain</th>
                         </tr>
-                    <?php
-                        $i++;
-                    endforeach;
-                    ?>
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Oktober</td>
+                            <td>115.214</td>
+                            <td>14.149</td>
+                            <td>112.235</td>
+                        </tr>
+                        <tr>
+                            <td>November</td>
+                            <td>131.703</td>
+                            <td>15.860</td>
+                            <td>124.481</td>
+                        </tr>
+                        <tr>
+                            <td>Desember</td>
+                            <td>250.412</td>
+                            <td>30.808</td>
+                            <td>230.797</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+    <div class="tab-pane fade " id="pills-output3" role="tabpanel" aria-labelledby="pills-output3-tab" tabindex="0">
+        <div class="card">
+            <div class="card-header">
+                <div class="card-title text-center mt-2">Jumlah Kepemilikan Kartu SIM Aktif yang Digunakan Wisatawan Nusantara Berdasarkan Provider di Kota Malang Triwulan IV 2022</div>
+            </div>
+            <div class="card-body table-responsive" style="padding: 0.5rem 1rem;">
+                <table class="table table-hover table-bordered table-responsive" id="k1_tabel3" style="width:100%;overflow-x:auto;">
+                    <thead style="color: #fff; font-weight:bold;">
+                        <tr>
+                            <th scope="col">Provider</th>
+                            <th scope="col">Jumlah Kartu SIM Aktif yang Digunakan Wisatawan Nusantara Asal Kota Malang</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Telkomsel</td>
+                            <td>187.873</td>
+                        </tr>
+                        <tr>
+                            <td>XL Axiata</td>
+                            <td>63.958</td>
+                        </tr>
+                        <tr>
+                            <td>Indosat Ooredo</td>
+                            <td>72.854</td>
+                        </tr>
+                        <tr>
+                            <td>3 (Three)</td>
+                            <td>52.276</td>
+                        </tr>
+                        <tr>
+                            <td>Smartfren</td>
+                            <td>13.985</td>
+                        </tr>
+                        <tr>
+                            <td>Lainnya</td>
+                            <td>2.208</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+    <div class="tab-pane fade " id="pills-output4" role="tabpanel" aria-labelledby="pills-output4-tab" tabindex="0">
+        <div class="card">
+            <div class="card-header">
+                <div class="card-title text-center mt-2">Jumlah Wisatawan Nusantara dengan Kepemilikan Ganda Kartu SIM Aktif dalam Satu Ponsel Utama Berdasarkan Kombinasi Provider di Kota Malang Triwulan IV 2022</div>
+            </div>
+            <div class="card-body table-responsive" style="padding: 0.5rem 1rem;">
+                <table class="table table-hover table-bordered table-responsive" id="k1_tabel4" style="width:100%;overflow-x:auto;">
+                    <thead style="color: #fff; font-weight:bold;">
+                        <tr>
+                            <th scope="col">Kombinasi Provider</th>
+                            <th scope="col">Jumlah Wisatawan Nusantara dengan Kepemilikan Ganda Kartu SIM Aktif dalam Satu Ponsel Utama</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Telkomsel - Telkomsel</td>
+                            <td>6.092</td>
+                        </tr>
+                        <tr>
+                            <td>Telkomsel - indosat</td>
+                            <td>6.841</td>
+                        </tr>
+                        <tr>
+                            <td>Telkomsel - XL Axiata</td>
+                            <td>9.611</td>
+                        </tr>
+                        <tr>
+                            <td>Telkomsel - Smartfren</td>
+                            <td>4.752</td>
+                        </tr>
+                        <tr>
+                            <td>Indosat - Indosat</td>
+                            <td>616</td>
+                        </tr>
+                        <tr>
+                            <td>Indosat - XL Axiata</td>
+                            <td>2.178</td>
+                        </tr>
+                        <tr>
+                            <td>Indosat - 3 (Three)</td>
+                            <td>1.178</td>
+                        </tr>
+                        <tr>
+                            <td>Indosat - Smartfren</td>
+                            <td>433</td>
+                        </tr>
+                        <tr>
+                            <td>XL Axiata - XL Axiata</td>
+                            <td>1.146</td>
+                        </tr>
+                        <tr>
+                            <td>XL Axiata - 3 (Three)</td>
+                            <td>2.000</td>
+                        </tr>
+                        <tr>
+                            <td>XL Axiata - Smartfren</td>
+                            <td>553</td>
+                        </tr>
+                        <tr>
+                            <td>3 (Three) - 3 (Three)</td>
+                            <td>890</td>
+                        </tr>
+                        <tr>
+                            <td>3 (Three) - Smartfren</td>
+                            <td>902</td>
+                        </tr>
+                        <tr>
+                            <td>Smartfren - Smartfren</td>
+                            <td>104</td>
+                        </tr>
+                        <tr>
+                            <td>Kombinasi Kartu Lainnya</td>
+                            <td>717</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+    <div class="tab-pane fade " id="pills-output5" role="tabpanel" aria-labelledby="pills-output5-tab" tabindex="0">
+        <div class="card">
+            <div class="card-header">
+                <div class="card-title text-center mt-2"> Jumlah Perjalanan Wisata Asal Kota Malang Berdasarkan Kelompok Umur Triwulan IV 2022</div>
+            </div>
+            <div class="card-body table-responsive" style="padding: 0.5rem 1rem;">
+                <table class="table table-hover table-bordered table-responsive" id="k1_tabel5" style="width:100%;overflow-x:auto;">
+                    <thead style="color: #fff; font-weight:bold;">
+                        <tr>
+                            <th scope="col">Kelompok Umur (Tahun)</th>
+                            <th scope="col">Jumlah Perjalanan Wisata Asal Kota Malang Triwulan IV 2022</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        $i = 1;
+                        foreach ($hasilpklriset1 as $v) :
+                        ?>
+                            <tr>
+                                <td><?php echo $v["Kelompok Umur (Tahun)"]; ?></td>
+                                <td><?php echo $v["Jumlah Perjalanan Wisata Asal Kota Malang Triwulan IV 2022"]; ?></td>
+                            </tr>
+                        <?php
+                            $i++;
+                        endforeach;
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+    <div class="tab-pane fade " id="pills-output6" role="tabpanel" aria-labelledby="pills-output6-tab" tabindex="0">
+        <div class="card">
+            <div class="card-header">
+                <div class="card-title text-center mt-2">Jumlah Wisatawan Nusantara Asal Kota Malang Berdasarkan Kelompok Umur Triwulan IV 2022</div>
+            </div>
+            <div class="card-body table-responsive" style="padding: 0.5rem 1rem;">
+                <table class="table table-hover table-bordered table-responsive" id="k1_tabel6" style="width:100%;overflow-x:auto;">
+                    <thead style="color: #fff; font-weight:bold;">
+                        <tr>
+                            <th scope="col">Kelompok Umur (Tahun)</th>
+                            <th scope="col">Jumlah Wisatawan Nusantara Asal Kota Malang</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        $i = 1;
+                        foreach ($hasilpklriset1 as $v) :
+                        ?>
+                            <tr>
+                                <td><?php echo $v["Kelompok Umur (Tahun)"]; ?></td>
+                                <td><?php echo $v["Jumlah Wisatawan Nusantara Asal Kota Malang"]; ?></td>
+                            </tr>
+                        <?php
+                            $i++;
+                        endforeach;
+                        ?>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 </div>
