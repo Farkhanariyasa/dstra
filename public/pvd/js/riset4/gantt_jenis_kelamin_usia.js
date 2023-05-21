bb2f525a80319d9a2995dd334f9ab0d85f08acc6
imams <222011830@gmail.com>
2023-05-21 10:44:47 +0700

    M View: Revisi gantt chart

--- public/pvd/js/riset4/gantt_jenis_kelamin_usia.js
+++ public/pvd/js/riset4/gantt_jenis_kelamin_usia.js
@@ -1,10 +1,10 @@
 const data = {
     labels: ['Laki-laki', 'Perempuan'],
     datasets: [{
-      label: 'Usia Pemilik UUP',
+      label: 'Rentang usia',
       data: [
-        [20, 40], 
-        [30, 60],
+        [usiaPemilikPengelola['laki-laki.min'], usiaPemilikPengelola['laki-laki.max']], 
+        [usiaPemilikPengelola['perempuan.min'], usiaPemilikPengelola['perempuan.max']],
     ],
       backgroundColor: [
         "#ffab00", "#506396",
@@ -18,7 +18,7 @@ const data = {
         "#ffbc33", "#7382ab",
         "#6f617f", "#c27d90"
       ],
-      barPercentage: 0.75
+      barPercentage: 0.8
     }]
   };
 
@@ -31,7 +31,7 @@ const data = {
       scales: {
         x: {
             min:10,
-            max: 90
+            max: 100
         },
         y: {
           beginAtZero: true
@@ -43,7 +43,7 @@ const data = {
       plugins: {
         title: {
             display: true,
-            text : ['Rentang Usia Pemilik Unit Usaha Pariwisata di Kota Batu'],
+            text : ['Rentang Usia (Tahun) Pemilik Unit Usaha Pariwisata di Kota Batu'],
             font: {
                 size: 18
             }
@@ -66,10 +66,32 @@ const data = {
                     console.log(context[0].label);
                     return context[0].label.replaceAll(',',' ');
                 }
-            }
-        }
+            },
+            enabled: true,
+            // callbacks: {
+            //     label: function(context){
+            //         var label = context.label,
+            //             currentValue = context.raw,
+            //             total = context.chart._metasets[context.datasetIndex].total;
+          
+            //         var percentage = parseFloat((currentValue/total*100).toFixed(1));
+          
+            //         return label + ": " +currentValue + ' (' + percentage + '%)';
+            //     },
+            // },
+        },
+        datalabels: {
+            display: true,
+            color: 'black',
+            // formatter: function(value, context) {
+            //     total = context.chart._metasets[context.datasetIndex].total;
+            //     percentage = parseFloat((value/total*100).toFixed(1));
+            //     return percentage + '%';
+            // },
+        },
       },
-    }
+    },
+    plugins: [ChartDataLabels]
   };
     // render init block
     const myChart = new Chart(
