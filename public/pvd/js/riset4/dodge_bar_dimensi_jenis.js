ef2879b45e1361345da2c3ad5fbc41eab46563db
imams <222011830@gmail.com>
2023-05-21 09:32:55 +0700

    M View: Revisi chart

--- public/pvd/js/riset4/dodge_bar_dimensi_jenis.js
+++ public/pvd/js/riset4/dodge_bar_dimensi_jenis.js
@@ -1,14 +1,14 @@
-const hasil2 = document.getElementById('hasil2');
+const bar2 = document.getElementById('bar-dimensi-jenis-1');
 
-const bar = new Chart(hasil2, {
+const bar_2 = new Chart(bar2, {
     type: 'bar',
     data: {
         labels: [['Jasa', 'Transportasi'],
-                ['Jasa', 'Penyedia','Makanan', 'dan', 'Minuman'],
+                ['Jasa', 'Penyedia Makanan', 'dan Minuman'],
                 ['Jasa', 'Akomodasi']
             ],
         datasets: [{
-            label: 'dimensi1_jenis',
+            label: 'Nilai',
             data: [
                 dimensi1_jenis.transportasi,
                 dimensi1_jenis.makanminum,
@@ -34,10 +34,18 @@ const bar = new Chart(hasil2, {
         plugins: {
             title: {
                 display: true,
-                text : ['dimensi1_jenis'],
+                text : ['Nilai IKUPTIK Dimensi Infrastruktur dan Konektivitas'],
                 font: {
-                    size: 20
+                    size: 18
                 }
+            },
+            subtitle: {
+                display: true,
+                text: 'Berdasarkan Jenis Usaha Pariwisata di Kota Batu',
+                font: {
+                    size: 16,
+                    weight: 'bold',
+                },
             },
             legend:{
                 display: false,
@@ -56,115 +64,115 @@ const bar = new Chart(hasil2, {
     plugins: [ChartDataLabels]
 });
 
-const barChart = document.getElementById('barChart');
-// const barChartLegend = document.getElementById('barChartLegend');
+const barJenis1 = document.getElementById('bar-jenis-1');
+// const barJenis1Legend = document.getElementById('bar-jenis-1-legend');
 
-barChart.addEventListener('change', tampilData);
-// barChartLegend.addEventListener('click', update_value_legend_bar_jenis(chk_bx));
+barJenis1.addEventListener('change', tampilData);
+// barJenis1Legend.addEventListener('click', update_value_legend_bar_jenis(chk_bx));
 
 // function update_value_legend_bar_jenis(chk_bx){
   
 //     if(chk_bx.checked)
 //     {
 //         console.log('check');
-//         bar.options.plugins.legend.display=true;
-//         bar.update();
+//         bar_2.options.plugins.legend.display=true;
+//         bar_2.update();
 //     }
         
 //     else{
 //       console.log('uncheck');
-//       bar.options.plugins.legend.display=false;
-//       bar.update();
+//       bar_2.options.plugins.legend.display=false;
+//       bar_2.update();
   
 //     }
 //   }
 
 function tampilData(){
-    if (barChart.value =='dimensi1') {
-        console.log(barChart.value);
-        bar.data.datasets[0].data = 
+    if (barJenis1.value =='dimensi1') {
+        console.log(barJenis1.value);
+        bar_2.data.datasets[0].data = 
             [
                 dimensi1_jenis.transportasi,
                 dimensi1_jenis.makanminum,
                 dimensi1_jenis.akomodasi
             ];
-            bar.data.labels =   [['Jasa', 'Transportasi'],
-                ['Jasa', 'Penyedia','Makanan', 'dan', 'Minuman'],
+            bar_2.data.labels =   [['Jasa', 'Transportasi'],
+                ['Jasa', 'Penyedia Makanan', 'dan Minuman'],
                 ['Jasa', 'Akomodasi']
             ];
-            bar.options.plugins.title.text='dimensi1_jenis';
-            bar.update();
+            bar_2.options.plugins.title.text='Nilai IKUPTIK Dimensi Infrastruktur dan Konektivitas';
+            bar_2.update();
 
-    } if (barChart.value =='dimensi2') {
-            console.log(barChart.value);
-            bar.data.datasets[0].data = 
+    } if (barJenis1.value =='dimensi2') {
+            console.log(barJenis1.value);
+            bar_2.data.datasets[0].data = 
             [   
                 dimensi2_jenis.transportasi,
                 dimensi2_jenis.makanminum,
                 dimensi2_jenis.akomodasi
             ];
-            bar.data.labels =   [['Jasa', 'Transportasi'],
-                ['Jasa', 'Penyedia','Makanan', 'dan', 'Minuman'],
+            bar_2.data.labels =   [['Jasa', 'Transportasi'],
+                ['Jasa', 'Penyedia Makanan', 'dan Minuman'],
                 ['Jasa', 'Akomodasi']
             ];
-            bar.options.plugins.title.text='dimensi2_jenis';
-            bar.update();
-    } if (barChart.value =='dimensi3'){
-        console.log(barChart.value);
-        bar.data.datasets[0].data = 
+            bar_2.options.plugins.title.text='Nilai IKUPTIK Dimensi Sumber Daya Manusia';
+            bar_2.update();
+    } if (barJenis1.value =='dimensi3'){
+        console.log(barJenis1.value);
+        bar_2.data.datasets[0].data = 
             [   
                 dimensi3_jenis.transportasi,
                 dimensi3_jenis.makanminum,
                 dimensi3_jenis.akomodasi
             ];
-            bar.data.labels =   [['Jasa', 'Transportasi'],
-                ['Jasa', 'Penyedia','Makanan', 'dan', 'Minuman'],
+            bar_2.data.labels =   [['Jasa', 'Transportasi'],
+                ['Jasa', 'Penyedia Makanan', 'dan Minuman'],
                 ['Jasa', 'Akomodasi']
             ];
-            bar.options.plugins.title.text= 'dimensi3_jenis';
-            bar.update();
-    } if (barChart.value =='dimensi4') {
-        console.log(barChart.value);
-        bar.data.datasets[0].data = 
+            bar_2.options.plugins.title.text= 'Nilai IKUPTIK Dimensi Manajemen Organisasi';
+            bar_2.update();
+    } if (barJenis1.value =='dimensi4') {
+        console.log(barJenis1.value);
+        bar_2.data.datasets[0].data = 
             [
                 dimensi4_jenis.transportasi,
                 dimensi4_jenis.makanminum,
                 dimensi4_jenis.akomodasi
             ];
-            bar.data.labels =   [['Jasa', 'Transportasi'],
-                ['Jasa', 'Penyedia','Makanan', 'dan', 'Minuman'],
+            bar_2.data.labels =   [['Jasa', 'Transportasi'],
+                ['Jasa', 'Penyedia Makanan', 'dan Minuman'],
                 ['Jasa', 'Akomodasi']
             ];
-            bar.options.plugins.title.text='dimensi4_jenis';
-            bar.update();
+            bar_2.options.plugins.title.text='Nilai IKUPTIK Dimensi Penggunaan TIK';
+            bar_2.update();
 
-    } if (barChart.value =='dimensi5') {
-            console.log(barChart.value);
-            bar.data.datasets[0].data = 
+    } if (barJenis1.value =='dimensi5') {
+            console.log(barJenis1.value);
+            bar_2.data.datasets[0].data = 
             [   
                 dimensi5_jenis.transportasi,
                 dimensi5_jenis.makanminum,
                 dimensi5_jenis.akomodasi
             ];
-            bar.data.labels =   [['Jasa', 'Transportasi'],
-                ['Jasa', 'Penyedia','Makanan', 'dan', 'Minuman'],
+            bar_2.data.labels =   [['Jasa', 'Transportasi'],
+                ['Jasa', 'Penyedia Makanan', 'dan Minuman'],
                 ['Jasa', 'Akomodasi']
             ];
-            bar.options.plugins.title.text='dimensi5_jenis';
-            bar.update();
-    } if (barChart.value =='dimensi6'){
-        console.log(barChart.value);
-        bar.data.datasets[0].data = 
+            bar_2.options.plugins.title.text='Nilai IKUPTIK Dimensi Eksternal';
+            bar_2.update();
+    } if (barJenis1.value =='dimensi6'){
+        console.log(barJenis1.value);
+        bar_2.data.datasets[0].data = 
             [   
                 dimensi6_jenis.transportasi,
                 dimensi6_jenis.makanminum,
                 dimensi6_jenis.akomodasi
             ];
-            bar.data.labels =   [['Jasa', 'Transportasi'],
-                ['Jasa', 'Penyedia','Makanan', 'dan', 'Minuman'],
+            bar_2.data.labels =   [['Jasa', 'Transportasi'],
+                ['Jasa', 'Penyedia Makanan', 'dan Minuman'],
                 ['Jasa', 'Akomodasi']
             ];
-            bar.options.plugins.title.text= 'dimensi6_jenis';
-            bar.update();
+            bar_2.options.plugins.title.text= 'Nilai IKUPTIK Dimensi Kendala';
+            bar_2.update();
     }
 }
