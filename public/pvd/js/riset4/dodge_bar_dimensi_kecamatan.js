024c79e93f86fdbaff320311906ed63af2f6067d
imams <222011830@gmail.com>
2023-05-21 09:33:17 +0700

    M View: Revisi chart

--- public/pvd/js/riset4/dodge_bar_dimensi_kecamatan.js
+++ public/pvd/js/riset4/dodge_bar_dimensi_kecamatan.js
@@ -1,11 +1,11 @@
-const hasil1 = document.getElementById('hasil1');
+const bar1 = document.getElementById('bar-dimensi-kecamatan-1');
 
-const pie = new Chart(hasil1, {
+const bar_1 = new Chart(bar1, {
     type: 'bar',
     data: {
         labels: ["Batu", "Junrejo", "Bumiaji"],
         datasets: [{
-            label: 'dimensi1_kecamatan',
+            label: 'Nilai',
             data: [
                 dimensi1_kecamatan.batu,
                 dimensi1_kecamatan.junrejo,
@@ -31,10 +31,18 @@ const pie = new Chart(hasil1, {
         plugins: {
             title: {
                 display: true,
-                text : ['dimensi1_kecamatan'],
+                text : 'Nilai IKUPTIK Dimensi Infrastruktur dan Konektivitas',
                 font: {
-                    size: 20
-                }
+                    size: 18
+                },
+            },
+            subtitle: {
+                display: true,
+                text: 'Berdasarkan Kecamatan di Kota Batu',
+                font: {
+                    size: 16,
+                    weight: 'bold',
+                },
             },
             legend:{
                 display: false,
@@ -53,97 +61,97 @@ const pie = new Chart(hasil1, {
     plugins: [ChartDataLabels]
 });
 
-const pieChart = document.getElementById('pieChart');
-// const pieChartLegend = document.getElementById('pieChartLegend');
+const barKecamatan1 = document.getElementById('bar-kecamatan-1');
+// const barKecamatan1Legend = document.getElementById('bar-kecamatan-1-legend');
 
-pieChart.addEventListener('change', tampilData);
-// pieChartLegend.addEventListener('click', update_value_legend_bar_kecamatan(chk_bx));
+barKecamatan1.addEventListener('change', tampilData);
+// barKecamatan1Legend.addEventListener('click', update_value_legend_bar_kecamatan(chk_bx));
 
 // function update_value_legend_bar_kecamatan(chk_bx){
   
 //     if(chk_bx.checked)
 //     {
 //         console.log('check');
-//         pie.options.plugins.legend.display=true;
-//         pie.update();
+//         bar_1.options.plugins.legend.display=true;
+//         bar_1.update();
 //     }
         
 //     else{
 //       console.log('uncheck');
-//       pie.options.plugins.legend.display=false;
-//       pie.update();
+//       bar_1.options.plugins.legend.display=false;
+//       bar_1.update();
   
 //     }
 //   }
 
 function tampilData(){
-    if (pieChart.value =='dimensi1') {
-        console.log(pieChart.value);
-        pie.data.datasets[0].data = 
+    if (barKecamatan1.value =='dimensi1') {
+        console.log(barKecamatan1.value);
+        bar_1.data.datasets[0].data = 
             [
                 dimensi1_kecamatan.batu,
                 dimensi1_kecamatan.junrejo,
                 dimensi1_kecamatan.bumiaji
             ];
-            pie.data.labels = ["Batu", "Junrejo", "Bumiaji"];
-            pie.options.plugins.title.text='dimensi1_kecamatan';
-            pie.update();
+            bar_1.data.labels = ["Batu", "Junrejo", "Bumiaji"];
+            bar_1.options.plugins.title.text='Nilai IKUPTIK Dimensi Infrastruktur dan Konektivitas';
+            bar_1.update();
 
-    } if (pieChart.value =='dimensi2') {
-            console.log(pieChart.value);
-            pie.data.datasets[0].data = 
+    } if (barKecamatan1.value =='dimensi2') {
+            console.log(barKecamatan1.value);
+            bar_1.data.datasets[0].data = 
             [   
                 dimensi2_kecamatan.batu,
                 dimensi2_kecamatan.junrejo,
                 dimensi2_kecamatan.bumiaji
             ];
-            pie.data.labels = ["Batu", "Junrejo", "Bumiaji"];
-            pie.options.plugins.title.text='dimensi2_kecamatan';
-            pie.update();
-    } if (pieChart.value =='dimensi3'){
-        console.log(pieChart.value);
-        pie.data.datasets[0].data = 
+            bar_1.data.labels = ["Batu", "Junrejo", "Bumiaji"];
+            bar_1.options.plugins.title.text='Nilai IKUPTIK Dimensi Sumber Daya Manusia';
+            bar_1.update();
+    } if (barKecamatan1.value =='dimensi3'){
+        console.log(barKecamatan1.value);
+        bar_1.data.datasets[0].data = 
             [   
                 dimensi3_kecamatan.batu,
                 dimensi3_kecamatan.junrejo,
                 dimensi3_kecamatan.bumiaji
             ];
-            pie.data.labels = ["Batu", "Junrejo", "Bumiaji"];
-            pie.options.plugins.title.text= 'dimensi3_kecamatan';
-            pie.update();
-    } if (pieChart.value =='dimensi4') {
-        console.log(pieChart.value);
-        pie.data.datasets[0].data = 
+            bar_1.data.labels = ["Batu", "Junrejo", "Bumiaji"];
+            bar_1.options.plugins.title.text= 'Nilai IKUPTIK Dimensi Manajemen Organisasi ';
+            bar_1.update();
+    } if (barKecamatan1.value =='dimensi4') {
+        console.log(barKecamatan1.value);
+        bar_1.data.datasets[0].data = 
             [
                 dimensi4_kecamatan.batu,
                 dimensi4_kecamatan.junrejo,
                 dimensi4_kecamatan.bumiaji
             ];
-            pie.data.labels = ["Batu", "Junrejo", "Bumiaji"];
-            pie.options.plugins.title.text='dimensi4_kecamatan';
-            pie.update();
+            bar_1.data.labels = ["Batu", "Junrejo", "Bumiaji"];
+            bar_1.options.plugins.title.text='Nilai IKUPTIK Dimensi Penggunaan TIK ';
+            bar_1.update();
 
-    } if (pieChart.value =='dimensi5') {
-            console.log(pieChart.value);
-            pie.data.datasets[0].data = 
+    } if (barKecamatan1.value =='dimensi5') {
+            console.log(barKecamatan1.value);
+            bar_1.data.datasets[0].data = 
             [   
                 dimensi5_kecamatan.batu,
                 dimensi5_kecamatan.junrejo,
                 dimensi5_kecamatan.bumiaji
             ];
-            pie.data.labels = ["Batu", "Junrejo", "Bumiaji"];
-            pie.options.plugins.title.text='dimensi5_kecamatan';
-            pie.update();
-    } if (pieChart.value =='dimensi6'){
-        console.log(pieChart.value);
-        pie.data.datasets[0].data = 
+            bar_1.data.labels = ["Batu", "Junrejo", "Bumiaji"];
+            bar_1.options.plugins.title.text='Nilai IKUPTIK Dimensi Eksternal';
+            bar_1.update();
+    } if (barKecamatan1.value =='dimensi6'){
+        console.log(barKecamatan1.value);
+        bar_1.data.datasets[0].data = 
             [   
                 dimensi6_kecamatan.batu,
                 dimensi6_kecamatan.junrejo,
                 dimensi6_kecamatan.bumiaji
             ];
-            pie.data.labels = ["Batu", "Junrejo", "Bumiaji"];
-            pie.options.plugins.title.text= 'dimensi6_kecamatan';
-            pie.update();
+            bar_1.data.labels = ["Batu", "Junrejo", "Bumiaji"];
+            bar_1.options.plugins.title.text= 'Nilai IKUPTIK Dimensi Kendala';
+            bar_1.update();
     }
 }
