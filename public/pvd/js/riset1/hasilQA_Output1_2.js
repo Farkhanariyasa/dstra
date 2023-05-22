e5593d9e08bf39eb30eaf214d5d436e5d2e86dd7
main <222011356@stis.ac.id>
2023-05-22 22:16:51 +0700

    M Assets : qa output 1_2

---
+++ public/pvd/js/riset1/hasilQA_Output1_2.js
@@ -0,0 +1,112 @@
+const qa_output1_2 = document.getElementById("qa_output1_2");
+
+const data_qa_output1_2 = {
+    labels: ['Dec-21', 'Jan-22', 'Feb-22', 'Mar-22', 'Apr-22', 'May-22', 'Jun-22', 'Jul-22', 'Aug-22', 'Sep-22', 'Oct-22', 'Nov-22'],
+    datasets: [{
+      label: 'Distribusi Jumlah Records',
+      data: [119679
+        ,120693
+        ,57942
+        ,154320
+        ,147745
+        ,108846
+        ,78633
+        ,55227
+        ,295571
+        ,169165
+        ,186546
+        ,253670], 
+      fill: false,
+      borderColor: "#506396",
+      pointStyle: 'rectRot',
+      pointRadius: 1,
+      pointHoverRadius: 10,
+    },
+
+]
+  };
+
+const line_output1_2 = new Chart(qa_output1_2, {
+    type: 'line',
+    data: data_qa_output1_2,
+    options: {
+        animations: {
+            // tension: {
+            //     duration: 1000,
+            //     easing: 'linear',
+            //     from: 1,
+            //     to: 0,
+            //     loop: true
+            // }
+        },
+        scales: {
+            y: { // defining min and max so hiding the dataset does not change scale range
+                min: 0,
+                max: 300000,
+                ticks: {
+                    stepSize: 100000,
+                },
+                grid: {
+                    display: true
+                } 
+            },
+            x: {
+                grid: {
+                    display: false
+                }
+            }
+        },
+        responsive: true,
+        maintainAspectRatio: false,
+        interaction: {
+            intersect: false,
+            mode: 'index',
+        },
+        plugins: {
+            legend: {
+                display: false,
+                position: 'top',
+                labels: {
+                    usePointStyle: true,
+                    boxWidth: 10,
+                    boxHeight: 10,
+                    padding: 20,
+                    font: {
+                        size: 14,
+                        weight: 'bold',
+                    }
+                }
+            },
+            title: {
+                display: true,
+                text: 'Distribusi Jumlah Records Provider Indosat',
+                font: {
+                    size: 16,
+                },
+                color:'#493a5a',
+                padding: {
+                    top: 10,
+                    bottom: 35
+                }
+            },
+
+        }
+    }
+});
+
+const lineChartAxis_output1_2 = document.getElementById("forLineChartAxis_qa_output1_2");
+lineChartAxis_output1_2.addEventListener("click", update_value_qa_output1_2(chk_bx));
+
+function update_value_qa_output1_2(chk_bx) {
+    if (chk_bx.checked) {
+        console.log("check");
+        line_output1_2.options.scales.x.display = true;
+        line_output1_2.options.scales.y.display = true;
+        line_output1_2.update();
+    } else {
+        console.log("uncheck");
+        line_output1_2.options.scales.x.display = false;
+        line_output1_2.options.scales.y.display = false;
+        line_output1_2.update();
+    }
+};
