4a2972a081db4464c6c6ef58f135983e2fa2a109
Thomson <222011365@stis.ac.id>
2023-05-22 15:17:34 +0700

    A Views: Add tampilan QA

---
+++ app/Views/pvd/pages/dasbor/riset1/mpd/dummy_qa.php
@@ -0,0 +1,310 @@
+<div class="tab-content" id="pills-tabContent">
+
+    <div class="tab-pane fade show active" id="pills-output1" role="tabpanel" aria-labelledby="pills-output1-tab" tabindex="0">
+        <div class="isi-tujuan shadow mt-0 mb-0">
+            <h4 class="card-title judul-card">Output 1</h4>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-lg-6 grid-margin stretch-card h-100">
+                        <div class="card shadow rounded-4">
+                            <div class="card-body">
+                                <div class="chartBox" style="height:400px">
+                                    <canvas class="mt1" id="riset1_hasil4"></canvas>
+                                </div>
+                                <div class="position-absolute bot-0  end-0 d-flex flex-row justify-content-center align-item-center ">
+                                    <div class="form-check form-switch me-3">
+                                        <input class="form-check-input" onchange="update_value_mpw(this);" type="checkbox" id="forBarChartAxis_mpw" value="show" checked>
+                                        <label class="form-check-label" style="margin-left:-3px;" for="flexSwitchCheckChecked">Tampilkan Sumbu</label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- <div class="d-grid" style="height: 200px;">
+                        <div class="vr"></div>
+                    </div> -->
+                    <div class="col-lg-6 grid-margin stretch-card h-100">
+                        <!-- <div class="card bg-secondary"> -->
+                        <div class="card shadow rounded-4">
+                            <div class="card-body">
+                                <div class="chartBox" style="height: 400px;">
+                                    <!-- <canvas class="mt1" id="riset1_hasil5"></canvas> -->
+                                </div>
+                            </div>
+                        </div>
+                        <!-- </div> -->
+                    </div>
+                    <div class="col-lg-6 grid-margin stretch-card h-100">
+                        <!-- <div class="card bg-secondary"> -->
+                        <div class="card-body">
+                            <div class="card border rounded-4" style="padding: 2rem 1rem;">
+                                <h3><b>Story</b></h3>
+                                <div class="card-body border rounded">
+                                    <!-- <canvas class="mt1" id="riset1_hasil5"></canvas> -->
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus recusandae voluptatum temporibus suscipit animi hic minus! Exercitationem ut vitae sunt consequuntur modi? Quisquam incidunt eligendi sed provident natus beatae! Obcaecati, libero esse vel quia consequatur error quasi sit eaque porro enim corporis aperiam doloribus tempora perferendis alias, quos non temporibus dolorem dolorum adipisci voluptate laudantium reiciendis, ab ipsa. At, quia.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- </div> -->
+                    </div>
+                    <div class="col-lg-6 grid-margin stretch-card h-100">
+                        <!-- <div class="card bg-secondary"> -->
+                        <div class="card-body">
+                            <div class="card border rounded-4" style="padding: 2rem 1rem;">
+                                <h3><b>Story</b></h3>
+                                <div class="card-body border rounded">
+                                    <!-- <canvas class="mt1" id="riset1_hasil5"></canvas> -->
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus recusandae voluptatum temporibus suscipit animi hic minus! Exercitationem ut vitae sunt consequuntur modi? Quisquam incidunt eligendi sed provident natus beatae! Obcaecati, libero esse vel quia consequatur error quasi sit eaque porro enim corporis aperiam doloribus tempora perferendis alias, quos non temporibus dolorem dolorum adipisci voluptate laudantium reiciendis, ab ipsa. At, quia.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- </div> -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="tab-pane fade " id="pills-output2" role="tabpanel" aria-labelledby="pills-output2-tab" tabindex="0">
+        <div class="isi-tujuan shadow mt-0 mb-0">
+            <h4 class="card-title judul-card">Output 2</h4>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-lg-6 grid-margin stretch-card h-100">
+                        <div class="card shadow rounded-4">
+                            <div class="card-body">
+                                <div class="chartBox" style="height:400px">
+                                    <canvas class="mt1" id="riset1_hasil4"></canvas>
+                                </div>
+                                <div class="position-absolute bot-0  end-0 d-flex flex-row justify-content-center align-item-center ">
+                                    <div class="form-check form-switch me-3">
+                                        <input class="form-check-input" onchange="update_value_mpw(this);" type="checkbox" id="forBarChartAxis_mpw" value="show" checked>
+                                        <label class="form-check-label" style="margin-left:-3px;" for="flexSwitchCheckChecked">Tampilkan Sumbu</label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 grid-margin stretch-card h-100">
+                        <!-- <div class="card bg-secondary"> -->
+                        <div class="card shadow rounded-4">
+                            <div class="card-body">
+                                <div class="chartBox" style="height: 400px;">
+                                    <!-- <canvas class="mt1" id="riset1_hasil5"></canvas> -->
+                                </div>
+                            </div>
+                        </div>
+                        <!-- </div> -->
+                    </div>
+                    <div class="col-lg-6 grid-margin stretch-card h-100">
+                        <!-- <div class="card bg-secondary"> -->
+                        <div class="card-body">
+                            <div class="card border rounded-4" style="padding: 2rem 1rem;">
+                                <h3><b>Story</b></h3>
+                                <div class="card-body border rounded">
+                                    <!-- <canvas class="mt1" id="riset1_hasil5"></canvas> -->
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus recusandae voluptatum temporibus suscipit animi hic minus! Exercitationem ut vitae sunt consequuntur modi? Quisquam incidunt eligendi sed provident natus beatae! Obcaecati, libero esse vel quia consequatur error quasi sit eaque porro enim corporis aperiam doloribus tempora perferendis alias, quos non temporibus dolorem dolorum adipisci voluptate laudantium reiciendis, ab ipsa. At, quia.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- </div> -->
+                    </div>
+                    <div class="col-lg-6 grid-margin stretch-card h-100">
+                        <!-- <div class="card bg-secondary"> -->
+                        <div class="card-body">
+                            <div class="card border rounded-4" style="padding: 2rem 1rem;">
+                                <h3><b>Story</b></h3>
+                                <div class="card-body border rounded">
+                                    <!-- <canvas class="mt1" id="riset1_hasil5"></canvas> -->
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus recusandae voluptatum temporibus suscipit animi hic minus! Exercitationem ut vitae sunt consequuntur modi? Quisquam incidunt eligendi sed provident natus beatae! Obcaecati, libero esse vel quia consequatur error quasi sit eaque porro enim corporis aperiam doloribus tempora perferendis alias, quos non temporibus dolorem dolorum adipisci voluptate laudantium reiciendis, ab ipsa. At, quia.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- </div> -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="tab-pane fade " id="pills-output3" role="tabpanel" aria-labelledby="pills-output3-tab" tabindex="0">
+        <div class="isi-tujuan shadow mt-0 mb-0">
+            <h4 class="card-title judul-card">Output 3</h4>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-lg-6 grid-margin stretch-card h-100">
+                        <div class="card shadow rounded-4">
+                            <div class="card-body">
+                                <div class="chartBox" style="height:400px">
+                                    <canvas class="mt1" id="riset1_hasil4"></canvas>
+                                </div>
+                                <div class="position-absolute bot-0  end-0 d-flex flex-row justify-content-center align-item-center ">
+                                    <div class="form-check form-switch me-3">
+                                        <input class="form-check-input" onchange="update_value_mpw(this);" type="checkbox" id="forBarChartAxis_mpw" value="show" checked>
+                                        <label class="form-check-label" style="margin-left:-3px;" for="flexSwitchCheckChecked">Tampilkan Sumbu</label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 grid-margin stretch-card h-100">
+                        <!-- <div class="card bg-secondary"> -->
+                        <div class="card shadow rounded-4">
+                            <div class="card-body">
+                                <div class="chartBox" style="height: 400px;">
+                                    <!-- <canvas class="mt1" id="riset1_hasil5"></canvas> -->
+                                </div>
+                            </div>
+                        </div>
+                        <!-- </div> -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="tab-pane fade " id="pills-output4" role="tabpanel" aria-labelledby="pills-output4-tab" tabindex="0">
+        <div class="isi-tujuan shadow mt-0 mb-0">
+            <h4 class="card-title judul-card">Output 4</h4>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-lg-6 grid-margin stretch-card h-100">
+                        <div class="card shadow rounded-4">
+                            <div class="card-body">
+                                <div class="chartBox" style="height:400px">
+                                    <canvas class="mt1" id="riset1_hasil4"></canvas>
+                                </div>
+                                <div class="position-absolute bot-0  end-0 d-flex flex-row justify-content-center align-item-center ">
+                                    <div class="form-check form-switch me-3">
+                                        <input class="form-check-input" onchange="update_value_mpw(this);" type="checkbox" id="forBarChartAxis_mpw" value="show" checked>
+                                        <label class="form-check-label" style="margin-left:-3px;" for="flexSwitchCheckChecked">Tampilkan Sumbu</label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 grid-margin stretch-card h-100">
+                        <!-- <div class="card bg-secondary"> -->
+                        <div class="card shadow rounded-4">
+                            <div class="card-body">
+                                <div class="chartBox" style="height: 400px;">
+                                    <!-- <canvas class="mt1" id="riset1_hasil5"></canvas> -->
+                                </div>
+                            </div>
+                        </div>
+                        <!-- </div> -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="tab-pane fade " id="pills-output5" role="tabpanel" aria-labelledby="pills-output5-tab" tabindex="0">
+        <div class="isi-tujuan shadow mt-0 mb-0">
+            <h4 class="card-title judul-card">Output 5</h4>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-lg-6 grid-margin stretch-card h-100">
+                        <div class="card shadow rounded-4">
+                            <div class="card-body">
+                                <div class="chartBox" style="height:400px">
+                                    <canvas class="mt1" id="riset1_hasil4"></canvas>
+                                </div>
+                                <div class="position-absolute bot-0  end-0 d-flex flex-row justify-content-center align-item-center ">
+                                    <div class="form-check form-switch me-3">
+                                        <input class="form-check-input" onchange="update_value_mpw(this);" type="checkbox" id="forBarChartAxis_mpw" value="show" checked>
+                                        <label class="form-check-label" style="margin-left:-3px;" for="flexSwitchCheckChecked">Tampilkan Sumbu</label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 grid-margin stretch-card h-100">
+                        <!-- <div class="card bg-secondary"> -->
+                        <div class="card shadow rounded-4">
+                            <div class="card-body">
+                                <div class="chartBox" style="height: 400px;">
+                                    <!-- <canvas class="mt1" id="riset1_hasil5"></canvas> -->
+                                </div>
+                            </div>
+                        </div>
+                        <!-- </div> -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="tab-pane fade " id="pills-output6" role="tabpanel" aria-labelledby="pills-output6-tab" tabindex="0">
+        <div class="isi-tujuan shadow mt-0 mb-0">
+            <h4 class="card-title judul-card">Output 6</h4>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-lg-6 grid-margin stretch-card h-100">
+                        <div class="card shadow rounded-4">
+                            <div class="card-body">
+                                <div class="chartBox" style="height:400px">
+                                    <canvas class="mt1" id="riset1_hasil4"></canvas>
+                                </div>
+                                <div class="position-absolute bot-0  end-0 d-flex flex-row justify-content-center align-item-center ">
+                                    <div class="form-check form-switch me-3">
+                                        <input class="form-check-input" onchange="update_value_mpw(this);" type="checkbox" id="forBarChartAxis_mpw" value="show" checked>
+                                        <label class="form-check-label" style="margin-left:-3px;" for="flexSwitchCheckChecked">Tampilkan Sumbu</label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 grid-margin stretch-card h-100">
+                        <!-- <div class="card bg-secondary"> -->
+                        <div class="card shadow rounded-4">
+                            <div class="card-body">
+                                <div class="chartBox" style="height: 400px;">
+                                    <!-- <canvas class="mt1" id="riset1_hasil5"></canvas> -->
+                                </div>
+                            </div>
+                        </div>
+                        <!-- </div> -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="tab-pane fade " id="pills-output7" role="tabpanel" aria-labelledby="pills-output7-tab" tabindex="0">
+        <div class="isi-tujuan shadow mt-0 mb-0">
+            <h4 class="card-title judul-card">Output 7</h4>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-lg-6 grid-margin stretch-card h-100">
+                        <div class="card shadow rounded-4">
+                            <div class="card-body">
+                                <div class="chartBox" style="height:400px">
+                                    <canvas class="mt1" id="riset1_hasil4"></canvas>
+                                </div>
+                                <div class="position-absolute bot-0  end-0 d-flex flex-row justify-content-center align-item-center ">
+                                    <div class="form-check form-switch me-3">
+                                        <input class="form-check-input" onchange="update_value_mpw(this);" type="checkbox" id="forBarChartAxis_mpw" value="show" checked>
+                                        <label class="form-check-label" style="margin-left:-3px;" for="flexSwitchCheckChecked">Tampilkan Sumbu</label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 grid-margin stretch-card h-100">
+                        <!-- <div class="card bg-secondary"> -->
+                        <div class="card shadow rounded-4">
+                            <div class="card-body">
+                                <div class="chartBox" style="height: 400px;">
+                                    <!-- <canvas class="mt1" id="riset1_hasil5"></canvas> -->
+                                </div>
+                            </div>
+                        </div>
+                        <!-- </div> -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
