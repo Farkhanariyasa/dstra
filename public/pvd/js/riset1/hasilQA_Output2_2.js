2f57268790d2d95bb517b11ca303a1cbdb9f1e03
main <222011356@stis.ac.id>
2023-05-22 22:17:23 +0700

    M Assets : qa output 2_2

--- public/pvd/js/riset1/hasilQA_Output2_2.js
+++ public/pvd/js/riset1/hasilQA_Output2_2.js
@@ -17,8 +17,8 @@ const data_qa_output2_2 = {
         ,34812
         ,50873], 
       fill: false,
-      borderColor: "#506396",
-      pointStyle: 'rect',
+      borderColor: "#ffab00",
+      pointStyle: 'rectRot',
       pointRadius: 1,
       pointHoverRadius: 10,
     },
