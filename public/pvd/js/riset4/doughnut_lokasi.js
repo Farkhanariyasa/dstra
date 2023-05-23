62afdf67174aca1abad8272ebdc9a320da3a4b59
imams <222011830@gmail.com>
2023-05-23 17:12:29 +0700

    A Assets: Menambah chart 6

---
+++ public/pvd/js/riset4/doughnut_lokasi.js
@@ -0,0 +1,84 @@
+const doughnut3 = document.getElementById('chart-3');
+
+const doughnut_3 = new Chart(doughnut3, {
+    type: 'doughnut',
+    data: {
+        labels: [ ["Berada di bangunan fisik rumah tangga"],
+            ["Tidak berada di bangunan fisik rumah tangga"],
+        ],
+
+        datasets: [{
+            label: 'Jumlah UUP',
+            data: [
+                uupKotaBatuLokasi.ya,
+                uupKotaBatuLokasi.tidak,
+            ],
+            borderwidth: 1,
+            backgroundColor: [
+                "#ffdd99",
+                "#96a1c0",
+            ],
+        }]
+    },
+    options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+            title: {
+                display: true,
+                text : ['Persentase Unit Usaha Pariwisata di Kota Batu Berdasarkan Lokasi Usaha'],
+                font: {
+                    size: 20
+                }
+            },
+            legend:{
+                display: true,
+                position: 'bottom',
+            },
+            tooltip:{
+                enabled: true,
+                callbacks: {
+                    label: function(context){
+                        var label = context.label,
+                            currentValue = context.raw,
+                            total = context.chart._metasets[context.datasetIndex].total;
+              
+                        var percentage = parseFloat((currentValue/total*100).toFixed(1));
+              
+                        return label + ": " +currentValue + ' (' + percentage + '%)';
+                    }
+                }
+            },
+            datalabels: {
+                display: true,
+                color: 'black',
+                formatter: function(value, context) {
+                    total = context.chart._metasets[context.datasetIndex].total;
+                    percentage = parseFloat((value/total*100).toFixed(1));
+                    return percentage + '%';
+                }
+            }
+        },
+    },
+    plugins: [ChartDataLabels]
+});
+
+const doughnutKotaBatuLokasiLegend = document.getElementById('doughnut-kota-batu-lokasi-legend');
+doughnutKotaBatuLokasiLegend.addEventListener('click', update_value1(chk_bx));
+
+function update_value1(chk_bx){
+    if(chk_bx.checked)
+    {
+        console.log('check');
+        doughnut_3.options.plugins.legend.display=true;
+        doughnut_3.update();
+    }
+        
+    else
+    {
+        console.log('uncheck');
+        doughnut_3.options.plugins.legend.display=false;
+        doughnut_3.update();
+  
+    }
+}
