bede6aa9b3043c5f03c26efa506a44c532f3cf3f
prasetyofmalik <222011325@stis.ac.id>
2023-05-25 00:44:10 +0700

    M Asset: isi chart js no. 1-2 tujuan 2 riset 3

--- public/pvd/js/riset3/hasilPKLRiset3_tujuan3.js
+++ public/pvd/js/riset3/hasilPKLRiset3_tujuan3.js
@@ -1,24 +1,121 @@
 //data untuk chart ini diambil dari hasil visualisasi subdiv vdd riset 3 yg diinput manual ketika visualisasinya dibuat di r studio
 
+const tujuan3_1 = document.getElementById('tujuan3_1');
+new Chart(tujuan3_1, {
+  type: 'bar',
+  data: {
+  labels: ["Laki-laki",  "Perempuan"],
+  datasets: [
+    {
+      label: 'Pekerja Penuh',
+      data: [69.69, 72.08],
+      backgroundColor: ["#506396"],
+    },
+    {
+      label: 'Pekerja Tidak Penuh',
+      data: [30.31, 27.92],
+      backgroundColor: ["#ffab00"],
+    }
+    ]},
+  options: {
+  indexAxis: 'y',
+  responsive:true,
+  scales: {
+      x: {
+        stacked: true,
+      },
+      y: {
+        stacked: true
+      }  
+   },
+  plugins:{
+      title: {
+          display: false,
+          text : ["Persentase jam kerja menurut tingkat pendapatan"],
+          font: {
+              size: 18
+          }
+        },
+      legend:{
+          display: true,
+          position: "right"
+        },
+      datalabels:{
+          display:true,
+          color:'black'
+        },
+    },
+  maintainAspectRatio: false
+  }, 
+  plugins:[ChartDataLabels]
+});
+
+const tujuan3_2 = document.getElementById('tujuan3_2');
+new Chart(tujuan3_2, {
+  type: 'bar',
+  data: {
+  labels: ["Di Bawah SMA/SMK",  "SMA/SMK", "Di Atas SMA/SMK"],
+  datasets: [
+    { 
+      label: 'Pekerja Penuh',
+      data: [65.79, 74.57, 73.08],
+      backgroundColor: ["#506396"],
+    },
+    {
+      label: 'Pekerja Tidak Penuh',
+      data: [34.21, 25.43, 26.92],
+      backgroundColor: ["#ffab00"],
+    }
+    ]},
+  options: {
+  indexAxis: 'y',
+  responsive:true,
+  scales: {
+      x: {
+        stacked: true,
+      },
+      y: {
+        stacked: true
+      }  
+   },
+  plugins:{
+      title: {
+          display: false,
+          text : ["Persentase jam kerja menurut tingkat pendapatan"],
+          font: {
+              size: 18
+          }
+        },
+      legend:{
+          display: true,
+          position: "right"
+        },
+      datalabels:{
+          display:true,
+          color:'black'
+        },
+    },
+  maintainAspectRatio: false
+  }, 
+  plugins:[ChartDataLabels]
+});
+
 const tujuan3_3 = document.getElementById('tujuan3_3');
-
-
 new Chart(tujuan3_3, {
     type: 'bar',
     data: {
     labels: ["Belum Pernah Kawin",  "Pernah Kawin"],
     datasets: [
       {
-        
-        label: 'Pekerja Tidak Penuh',
+        label: 'Pekerja Penuh',
         data: [76.34, 68.6 ],
+        backgroundColor: ["#ffab00"],
+      },
+      {  
+        label: 'Pekerja Tidak Penuh',
+        data: [23.66 ,31.4],
         backgroundColor: ["#506396"],
       },
-      {
-        label: 'Pekerja Penuh',
-        data: [23.66 ,31.4],
-        backgroundColor: ["#b35c74"],
-      }
       ]},
     options: {
     indexAxis: 'y',
@@ -59,19 +156,18 @@ const tujuan3_4 = document.getElementById('tujuan3_4');
 new Chart(tujuan3_4, {
   type: 'bar',
   data: {
-  labels: [">=UMR",  "<UMR"],
+  labels: ["< UMR", ">= UMR"],
   datasets: [
     {
-      
+      label: 'Pekerja Penuh',
+      data: [69.54, 74.52],
+      backgroundColor: ["#ffab00"],
+    },
+    {  
       label: 'Pekerja Tidak Penuh',
-      data: [74.52, 69.54 ],
+      data: [30.46, 25.48],
       backgroundColor: ["#506396"],
     },
-    {
-      label: 'Pekerja Penuh',
-      data: [25.48 ,30.46],
-      backgroundColor: ["#b35c74"],
-    }
     ]},
   options: {
   indexAxis: 'y',
@@ -112,19 +208,18 @@ const tujuan3_5 = document.getElementById('tujuan3_5');
 new Chart(tujuan3_5, {
   type: 'bar',
   data: {
-  labels: [">=UMR",  "<UMR"],
+  labels: ["Perkotaan", "Perdesaan"],
   datasets: [
     {
-      
+      label: 'Pekerja Penuh',
+      data: [75.69, 52.9],
+      backgroundColor: ["#ffab00"],
+    },
+    { 
       label: 'Pekerja Tidak Penuh',
-      data: [52.9, 75.69 ],
+      data: [24.31, 47.1],
       backgroundColor: ["#506396"],
     },
-    {
-      label: 'Pekerja Penuh',
-      data: [47.1 ,24.31],
-      backgroundColor: ["#b35c74"],
-    }
     ]},
   options: {
   indexAxis: 'y',
@@ -165,19 +260,18 @@ const tujuan3_6 = document.getElementById('tujuan3_6');
 new Chart(tujuan3_6, {
   type: 'bar',
   data: {
-  labels: ["Tidak",  "Ya"],
+  labels: ["Ya", "Tidak"],
   datasets: [
     {
-      
+      label: 'Pekerja Penuh',
+      data: [72.17, 70.36],
+      backgroundColor: ["#ffab00"],
+    },
+    { 
       label: 'Pekerja Tidak Penuh',
-      data: [70.36, 72.17 ],
+      data: [27.83, 29.64],
       backgroundColor: ["#506396"],
     },
-    {
-      label: 'Pekerja Penuh',
-      data: [29.64,28.83],
-      backgroundColor: ["#b35c74"],
-    }
     ]},
   options: {
   indexAxis: 'y',
