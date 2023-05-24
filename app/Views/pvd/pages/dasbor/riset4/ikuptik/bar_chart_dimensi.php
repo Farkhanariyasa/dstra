ca619e6fea313c8ec105233ba3a56cf698606891
imams <222011830@gmail.com>
2023-05-24 10:18:35 +0700

    M View: Revisi chart

--- app/Views/pvd/pages/dasbor/riset4/ikuptik/bar_chart_dimensi.php
+++ app/Views/pvd/pages/dasbor/riset4/ikuptik/bar_chart_dimensi.php
@@ -78,11 +78,12 @@
     </div>
 </div> -->
 
-<div class="portfolio-item filter-riset4-fg" onresize="responsivefonts()">
+<!-- <div class="portfolio-item filter-riset4-fg" onresize="responsivefonts()">
     <div class="row mt-3">
         <div class="col-sm-6 my-2">
             <div class="card shadow">
             <div class="card-body">
+                <div class="row">
                     <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center ">
                         <div class="selectBox mt-2 me-2">
                             <form action="">
@@ -105,17 +106,18 @@
                     </div>
                     <div class="chartBox" style=" height:400px;">
                         <canvas class="mt1" id="bar-dimensi-kecamatan-1"></canvas>
-                    </div>
+                    </div> -->
                     <!-- <div class="position-absolute bot-0  end-0 d-flex flex-row justify-content-center align-item-center ">
                         <div class="form-check form-switch me-3">
                             <input class="form-check-input" onchange="update_value_legend_bar_kecamatan(this);" type="checkbox" id="bar-kecamatan-1-legend" value="show" checked>
                             <label class="form-check-label" style="margin-left:-3px;" for="flexSwitchCheckChecked">Tampilkan Legenda</label>
                         </div>
                     </div> -->
-                </div>
+                <!-- </div>
             </div>
-        </div>
-        <div class="col-sm-6 my-2">
+            </div>
+        </div> -->
+        <!-- <div class="col-sm-6 my-2">
             <div class="card shadow">
             <div class="card-body">
                     <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center ">
@@ -140,15 +142,186 @@
                     </div>
                     <div class="chartBox" style=" height:400px;">
                         <canvas class="mt1" id="bar-dimensi-jenis-1"></canvas>
-                    </div>
+                    </div> -->
                     <!-- <div class="position-absolute bot-0  end-0 d-flex flex-row justify-content-center align-item-center ">
                         <div class="form-check form-switch me-3">
                             <input class="form-check-input" onchange="update_value_legend_bar_jenis(this);" type="checkbox" id="bar-jenis-1-legend" value="show" checked>
                             <label class="form-check-label" style="margin-left:-3px;" for="flexSwitchCheckChecked">Tampilkan Legenda</label>
                         </div>
                     </div> -->
-                </div>
+                <!-- </div>
             </div>
         </div>
     </div>
-</div>
+</div> -->
+
+<div class="row portfolio-container">
+          <style>
+            .konten-teks {
+                padding: 10%;
+                color: #506396;
+                /* font-family: 'Poppins', 'Courier', 'monospace'; */
+            }
+
+            .konten-teks .card-title {
+                font-weight: bold;
+                font-size: 50px;
+                /* font-family: 'Poppins', 'Courier', 'monospace'; */
+                color: #493a5a;
+
+            }
+
+            .konten-teks .subs {
+                font-weight: bold;
+                font-size: 30px;
+
+            }
+
+            .card-text {
+                /* font-family: 'Poppins', 'Courier', 'monospace'; */
+                color: #506396;
+            }
+          </style>
+
+          <!-- style untuk gantt chart -->
+          <style>
+            /* * {
+              margin: 0;
+              padding: 0;
+              font-family: sans-serif;
+            } */
+            /* .chartMenu {
+              width: 100vw;
+              height: 40px;
+              background: #1A1A1A;
+              color: rgba(54, 162, 235, 1);
+            }
+            .chartMenu p {
+              padding: 10px;
+              font-size: 20px;
+            }
+            .chartCard {
+              width: 100vw;
+              height: calc(100vh - 40px);
+              background: rgba(54, 162, 235, 0.2);
+              display: flex;
+              align-items: center;
+              justify-content: center;
+            }
+            .chartBox {
+              width: 700px;
+              padding: 20px;
+              border-radius: 20px;
+              border: solid 3px rgba(54, 162, 235, 1);
+              background: white;
+            } */
+          </style>
+
+          <!-- Charts and Interpretations -->
+          <div class="portfolio-item filter-riset4-semua" onresize="responsivefonts()">
+              <div class="isi-tujuan shadow mt-0 mb-0">
+                  <h4 class="card-title judul-card">Statistik Pemilik atau Pengelola Usaha Pariwisata di Kota Batu</h4>
+                  <div class="card-body">
+                      <div class="row">
+                          <!-- Part 1 -->
+                          <div class="col-12 grid-margin stretch-card">
+                              <div class="card shadow">
+                                  <div class="pb-2 card-body">
+                                      <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center ">
+                                      <div class="selectBox mt-2 me-2">
+                            <form action="">
+                                <select style="width: 250px" class="dropdownunduh1" id="bar-kecamatan-1">
+                                    <option value="dimensi1">Pilih Dimensi:</option>
+                                    <option value="dimensi1">Infrastruktur dan Konektivitas</option>
+                                    <option value="dimensi2">Sumber Daya Manusia</option>
+                                    <option value="dimensi3">Manajemen Organisasi</option>
+                                    <option value="dimensi4">Penggunaan TIK</option>
+                                    <option value="dimensi5">Eksternal</option>
+                                    <option value="dimensi6">Kendala</option>
+                                </select>
+                            </form>
+                        </div>
+                        <div class="me-1 mt-1 justify-content-end align-item-end">
+                                              <button type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal4-barplot">
+                                                  <i class="fa-solid fa-download"></i>
+                                              </button>
+                                          </div>
+                                      </div>
+                                      <div class="row">
+                                          <div class="mb-0 col-lg-7 grid-margin">
+                                              <div>
+                                                  <div class = "chartBox" style=" height:300px;"> 
+                                                      <canvas id="bar-dimensi-kecamatan-1"></canvas>
+                                                  </div>
+                                              </div>
+                                          </div>
+                                          <div class="mb-0 col-lg-5 grid-margin stretch-card">
+                                              <div>
+                                                  <h4>Dodge Pemilik atau pengelola unit usaha pariwisata berdasarkan jenis kelamin dan usia</h4>
+                                                  <p id="interpretasi-1" class="text-justify">
+                                                      Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.
+                                                      Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.
+                                                      Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.
+                                                      Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.
+                                                  </p>
+                                              </div>
+                                          </div>
+                                      </div>
+                                  </div>
+                              </div>
+                          </div>
+                          <!-- Part 2 -->
+                          <div class="col-12 grid-margin stretch-card">
+                              <div class="card shadow">
+                                  <div class="pb-2 card-body">
+                                      <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center ">
+                                      <div class="selectBox mt-2 me-2">
+                            <form action="">
+                                <select style="width: 250px" class="dropdownunduh1" id="bar-jenis-1">
+                                    <option value="dimensi1">Pilih Dimensi:</option>
+                                    <option value="dimensi1">Infrastruktur dan Konektivitas</option>
+                                    <option value="dimensi2">Sumber Daya Manusia</option>
+                                    <option value="dimensi3">Manajemen Organisasi</option>
+                                    <option value="dimensi4">Penggunaan TIK</option>
+                                    <option value="dimensi5">Eksternal</option>
+                                    <option value="dimensi6">Kendala</option>
+                                </select>
+                            </form>
+                        </div>
+                        <div class="me-1 mt-1 justify-content-end align-item-end">
+                                              <button type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal4-barplot">
+                                                  <i class="fa-solid fa-download"></i>
+                                              </button>
+                                          </div>
+                                      </div>
+                                      <div class="row">
+                                          <div class="mb-0 col-lg-7 grid-margin">
+                                              <div>
+                                                  <div class = "chartBox" style=" height:300px;"> 
+                                                      <canvas id="bar-dimensi-jenis-1"></canvas>
+                                                  </div>
+                                              </div>
+                                          </div>
+                                          <div class="mb-0 col-lg-5 grid-margin stretch-card">
+                                              <div>
+                                                  <h4>Dodge Pemilik atau pengelola unit usaha pariwisata berdasarkan jenis kelamin dan tingkat pendidikan.</h4>
+                                                  <p id="interpretasi-2" class="text-justify">
+                                                      Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.
+                                                      Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.
+                                                      Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.
+                                                      Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.
+                                                  </p>
+                                              </div>
+                                          </div>
+                                      </div>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+
+          <!-- Modal Dasbor -->
+          <!-- < ?php include('modal_pemilik_atau_pengelola_usaha_pariwisata.php') ?> -->
+        </div>
