ccb0b3608d93f40832e6406b9e461c0c12f77d3e
prasetyofmalik <222011325@stis.ac.id>
2023-05-24 21:44:31 +0700

    M View: import chart js tujuan 2 riset 3

--- app/Views/pvd/pages/dasbor/riset3/hasilkajian/tujuan2.php
+++ app/Views/pvd/pages/dasbor/riset3/hasilkajian/tujuan2.php
@@ -406,8 +406,11 @@
 <!-- dropdown show/hide content-->
 <script src="<?= base_url('pvd/js/riset3/dropdown_showhide.js') ?>"></script>
 <!-- Datatable Instal -->
-<script src="<?= base_url('pvd/js/riset3/datatables_export_tujuan2.js'); ?>"></script>
+<!-- <script src="<?= base_url('pvd/js/riset3/datatables_export_tujuan2.js'); ?>"></script> -->
 
+<!-- Chart JS -->
+<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/chartjs-plugin-datalabels/2.2.0/chartjs-plugin-datalabels.min.js" integrity="sha512-JPcRR8yFa8mmCsfrw4TNte1ZvF1e3+1SdGMslZvmrzDYxS69J7J49vkFL8u6u8PlPJK+H3voElBtUCzaXj+6ig==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <!-- Impor Chart -->
 <script src="<?= base_url('pvd/js/riset3/hasilPKLRiset3_tujuan2.js') ?>"></script>
 
