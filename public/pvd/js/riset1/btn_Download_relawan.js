305a2cd6d56952079b399bfd36b7f12da730de22
Thomson <222011365@stis.ac.id>
2023-05-28 07:08:08 +0700

    A Public: btn download chart stats relawan sby

---
+++ public/pvd/js/riset1/btn_Download_relawan.js
@@ -0,0 +1,145 @@
+// StartValidasi
+let idbutton=''
+let idinput=''
+let mailRegex = /^[a-zA-Z][a-zA-Z0-9\-\_\.]+@[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}$/;
+
+for (let i = 1; i < 7; i++) {
+    document.getElementById('relawan-'+[i]).addEventListener('click',(e) => {
+        idbutton='relawan'+[i]
+        console.log("relawan-"+[i]+" diklik")
+        idinput=i-1
+    });
+  }
+
+// FUNCTION GET VALUE WISNUS
+function getvalue(){
+
+    
+    let nama =document.getElementsByClassName('nama');
+    let email = document.getElementsByClassName('email');
+    let instansi =document.getElementsByClassName('instansi');
+    let konfirmasiButton= document.getElementById(idbutton);
+    console.log(idbutton)
+
+    if(nama[idinput].value != "" && email[idinput].value !="" && email[idinput].value.match(mailRegex) && instansi[idinput].value != ""){
+        
+        konfirmasiButton.disabled=false;
+        konfirmasiButton.style.backgroundColor="#7382ab";
+        konfirmasiButton.style.color="#fff";
+        document.getElementById('erroremail').innerHTML=""
+        document.getElementById('errornama').innerHTML=""
+        document.getElementById('errorinstansi').innerHTML=""
+        console.log('dah aman formnya')
+    }else{
+        if(nama[idinput].value===""){
+            document.getElementById('errornama').innerHTML="Nama harus diisi"
+            document.getElementById('errornama').style.color='#fe7c96'
+            console.log('nama kosong')
+        }else if(instansi[idinput].value ===""){
+            document.getElementById('errorinstansi').innerHTML="Instansi harus diisi"
+            document.getElementById('errorinstansi').style.color='#fe7c96'
+            console.log('instansi kosong')
+        }else if(!email[idinput].value.match(mailRegex)){
+            document.getElementById('erroremail').innerHTML="Format email salah"
+            document.getElementById('erroremail').style.color='#fe7c96'
+            console.log('email salah')
+        }else if(email[idinput].value===""){
+            document.getElementById('erroremail').innerHTML="Email harus diisi"
+            document.getElementById('erroremail').style.color='#fe7c96'
+            console.log('email kosong')
+        }
+        
+        konfirmasiButton.disabled=true;
+        konfirmasiButton.style.backgroundColor="#8d8d8d";
+    }
+}
+
+document.getElementById('relawan1').addEventListener('click', function(e) {
+    // Convert our canvas to a data URL
+    let canvasUrl = document.getElementById('relawan_output1').toDataURL();
+    // Create an anchor, and set the href value to our data URL
+    const createEl = document.createElement('a');
+    createEl.href = canvasUrl;
+
+    // This is the name of our downloaded file
+    createEl.download = "Statistik Relawan Surabaya Output 1";
+
+    // Click the download button, causing a download, and then remove it
+    createEl.click();
+    createEl.remove();
+});
+
+document.getElementById('relawan2').addEventListener('click', function(e) {
+    // Convert our canvas to a data URL
+    let canvasUrl = document.getElementById('relawan_output2_1').toDataURL();
+    // Create an anchor, and set the href value to our data URL
+    const createEl = document.createElement('a');
+    createEl.href = canvasUrl;
+
+    // This is the name of our downloaded file
+    createEl.download = "Statistik Relawan Surabaya Output 2.1";
+
+    // Click the download button, causing a download, and then remove it
+    createEl.click();
+    createEl.remove();
+});
+
+document.getElementById('relawan3').addEventListener('click', function(e) {
+    // Convert our canvas to a data URL7
+    let canvasUrl = document.getElementById('relawan_output2_2').toDataURL();
+    // Create an anchor, and set the href value to our data URL
+    const createEl = document.createElement('a');
+    createEl.href = canvasUrl;
+
+    // This is the name of our downloaded file
+    createEl.download = "Wisnus Output 3";
+
+    // Click the download button, causing a download, and then remove it
+    createEl.click();
+    createEl.remove();
+});
+
+document.getElementById('relawan4').addEventListener('click', function(e) {
+    // Convert our canvas to a data URL
+    let canvasUrl = document.getElementById('relawan_output3_1').toDataURL();
+    // Create an anchor, and set the href value to our data URL
+    const createEl = document.createElement('a');
+    createEl.href = canvasUrl;
+
+    // This is the name of our downloaded file
+    createEl.download = "Statistik Relawan Surabaya Output 3.1";
+
+    // Click the download button, causing a download, and then remove it
+    createEl.click();
+    createEl.remove();
+});
+
+document.getElementById('relawan5').addEventListener('click', function(e) {
+    // Convert our canvas to a data URL
+    let canvasUrl = document.getElementById('relawan_output3_2').toDataURL();
+    // Create an anchor, and set the href value to our data URL
+    const createEl = document.createElement('a');
+    createEl.href = canvasUrl;
+
+    // This is the name of our downloaded file
+    createEl.download = "Statistik Relawan Surabaya Output 3.2";
+
+    // Click the download button, causing a download, and then remove it
+    createEl.click();
+    createEl.remove();
+});
+
+document.getElementById('relawan6').addEventListener('click', function(e) {
+    // Convert our canvas to a data URL
+    let canvasUrl = document.getElementById('relawan_output4').toDataURL();
+    // Create an anchor, and set the href value to our data URL
+    const createEl = document.createElement('a');
+    createEl.href = canvasUrl;
+
+    // This is the name of our downloaded file
+    createEl.download = "Statistik Relawan Surabaya Output 4";
+
+    // Click the download button, causing a download, and then remove it
+    createEl.click();
+    createEl.remove();
+});
