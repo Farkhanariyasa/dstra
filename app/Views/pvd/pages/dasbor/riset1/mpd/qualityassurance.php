6da57a5842c17ccf0ca4b5901d65d543f3926eb2
Thomson <222011365@stis.ac.id>
2023-05-29 14:46:46 +0700

    M Views: add fitur download chart qa

--- app/Views/pvd/pages/dasbor/riset1/mpd/qualityassurance.php
+++ app/Views/pvd/pages/dasbor/riset1/mpd/qualityassurance.php
@@ -43,7 +43,9 @@
             </div>
         </div>
     </div>
+</section>
 
+<section>
  <!-- Chart JS -->
 <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/chartjs-plugin-datalabels/2.2.0/chartjs-plugin-datalabels.min.js" integrity="sha512-JPcRR8yFa8mmCsfrw4TNte1ZvF1e3+1SdGMslZvmrzDYxS69J7J49vkFL8u6u8PlPJK+H3voElBtUCzaXj+6ig==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
@@ -88,6 +90,8 @@
 <script src="<?= base_url('pvd/js/riset1/hasilQA_Output7_1.js') ?>"></script>
 <script src="<?= base_url('pvd/js/riset1/hasilQA_Output7_2.js') ?>"></script>
 
+<!-- Fungsi Unduh Chart -->
+<script src="<?= base_url('pvd/js/riset1/btn_Download_qa.js') ?>"></script>
 
 
 
