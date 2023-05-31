689b04fb626a7f1183cb2e2a8a19e3452b257fa6
LuthfilKA <222011321@stis.ac.id>
2023-05-31 19:53:31 +0700

    M Views : button download

--- app/Views/pvd/pages/dasbor/riset3/hasilkajian/tujuan2.php
+++ app/Views/pvd/pages/dasbor/riset3/hasilkajian/tujuan2.php
@@ -26,7 +26,7 @@
 <?= $this->endSection(); ?>
 
 <?= $this->section('content'); ?>
-
+<?php include('modal_riset3.php') ?>
 <div class="container px-0">
   <header class="section-header">
     <h6 style="color: #4b395f;"><strong>Menganalisis dinamika tenaga kerja di industri pariwisata.</strong>
@@ -404,40 +404,7 @@
 
 <!-- Modal Riset 3  -->
 <!-- 1 -->
-<div class="modal fade" id="Modal2_1" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h4 class="modal-title" id="exampleModalLabel">Unduh Hasil PKL</h4>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <!-- <button type="button" id="btndownload13" class="tombolkonf">Konfirmasiowko</button> -->
-      <form name="unduhVisualisasi" action="<?= base_url(); ?>/hasil-pkl/riset3/hasilkajian2" method="POST">
-        <div class="modal-body">
-          <div class="mb-1">
-            <label for="recipient-name" class="col-form-label">Nama <span class="text-danger">*</span></label>
-            <input oninput="getvalue()" type="text" class="form-control nama" name="nama" id="nama" placeholder="Nama Anda" required>
-            <div id="errornama" class="errorvlds"></div>
-          </div>
-          <div class="mb-1">
-            <label for="recipient-name" class="col-form-label" name="emailunduh">Email <span class="text-danger">*</span></label>
-            <input oninput="getvalue()" type="email" class="form-control email" name="email" id="email" placeholder="Email Anda" required>
-            <div id="erroremail" class="errorvlds"></div>
-          </div>
-          <div class="mb-1">
-            <label for="recipient-name" class="col-form-label">Instansi <span class="text-danger">*</span></label>
-            <input oninput="getvalue()" type="text" class="form-control instansi" name="instansi" id="instansi" placeholder="Instansi Asal" required>
-            <div id="errorinstansi" class="errorvlds"></div>
-          </div>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="tombol btn-danger" data-bs-dismiss="modal">Kembali</button>
-          <button disabled type="submit" id="btndownload2_1" class="tombolkonf">Konfirmasiowko</button>
-        </div>
-      </form>
-    </div>
-  </div>
-</div>
+
 
 
 <!-- Jquery -->
