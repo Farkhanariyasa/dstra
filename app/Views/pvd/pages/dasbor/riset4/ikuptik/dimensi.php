7f066e67f9e333e4d757b4d21f9cf6560b0e7830
imams <222011830@gmail.com>
2023-06-01 18:14:45 +0700

    M View: Revisi css

--- app/Views/pvd/pages/dasbor/riset4/ikuptik/dimensi.php
+++ app/Views/pvd/pages/dasbor/riset4/ikuptik/dimensi.php
@@ -3,7 +3,7 @@
 <?= $this->section('content'); ?>
 
 <section id="portfolio" class="portfolio section-bg">
-  <div class="container px-0">
+  <div class="container">
     <header class="section-header">
         <h6 style="color: #4b395f;"><strong>Kesiapan Pemanfaatan TIK Unit Usaha Pariwisata Skala Mikro dan Kecil di Kota Batu dalam Menerapkan 
           <i>Smart Tourism Destination</i></strong>
