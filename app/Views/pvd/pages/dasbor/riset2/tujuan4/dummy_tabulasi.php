46a01b65fd2ad874532763438ebc9fc719ba2731
NickoRagasukma <gabrielprimaditya@gmail.com>
2023-06-02 16:17:26 +0700

    A Views : Tambah tampilan tabulasi tujuan 4 riset 2

---
+++ app/Views/pvd/pages/dasbor/riset2/tujuan4/dummy_tabulasi.php
@@ -0,0 +1,52 @@
+<!-- <div class="card">
+    <div class="card-header">
+        <strong>
+            Tabulasi Analisis Inferensia
+        </strong>
+    </div>
+    <div class="card-body table-responsive" style="padding: 0.5rem 1rem;">
+        <table class="table table-hover table-bordered table-responsive" id="k4_tabel1" style="width:100%">
+            <thead>
+                <tr>
+                    <th scope="col">Total Perjalanan Kedungkandang</th>
+                    <th scope="col">Total Perjalanan Blimbing</th>
+                    <th scope="col">Total Perjalanan Sukun</th>
+                    <th scope="col">Total Perjalanan Lowokwaru</th>
+                    <th scope="col">Total Perjalanan Klojen</th>
+                    <th scope="col">Total Perjalanan</th>
+                    <th scope="col">Jenis Kelamin</th>
+                    <th scope="col">Umur</th>
+                    <th scope="col">Tingkat Pendidikan</th>
+                    <th scope="col">Pekerjaan KRT</th>
+                    <th scope="col">b503</th>
+                    <th scope="col">Kerja 1</th>
+                    <th scope="col">Kerja 2</th>
+                    <th scope="col">Kegiatan Utama</th>
+
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">Kecamatan Blimbing</th>
+                    <td>1.887</td>
+                </tr>
+                <tr>
+                    <th scope="row">Kecamatan Kedungkandang</th>
+                    <td>1.306</td>
+                </tr>
+                <tr>
+                    <th scope="row">Kecamatan Klojen</th>
+                    <td>1.84</td>
+                </tr>
+                <tr>
+                    <th scope="row">Kecamatan Lowokwaru</th>
+                    <td>1.737</td>
+                </tr>
+                <tr>
+                    <th scope="row">Kecamatan Sukun</th>
+                    <td>1.686</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div> -->
