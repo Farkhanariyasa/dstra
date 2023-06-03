94824253418426d1ab7c2364c7ccec6c463ab405
NickoRagasukma <gabrielprimaditya@gmail.com>
2023-06-03 13:16:58 +0700

    M Views : Tambah tabulasi anaferen riset 2

--- app/Views/pvd/pages/dasbor/riset2/tujuan4/dummy_tabulasi.php
+++ app/Views/pvd/pages/dasbor/riset2/tujuan4/dummy_tabulasi.php
@@ -1,52 +1,43 @@
-<!-- <div class="card">
+<div class="card">
     <div class="card-header">
         <strong>
             Tabulasi Analisis Inferensia
         </strong>
     </div>
     <div class="card-body table-responsive" style="padding: 0.5rem 1rem;">
-        <table class="table table-hover table-bordered table-responsive" id="k4_tabel1" style="width:100%">
+        <table class="table table-hover table-bordered table-responsive" id="k4_tabel1" style="width:100%;">
             <thead>
                 <tr>
-                    <th scope="col">Total Perjalanan Kedungkandang</th>
-                    <th scope="col">Total Perjalanan Blimbing</th>
-                    <th scope="col">Total Perjalanan Sukun</th>
-                    <th scope="col">Total Perjalanan Lowokwaru</th>
-                    <th scope="col">Total Perjalanan Klojen</th>
-                    <th scope="col">Total Perjalanan</th>
+                    <th scope="col">Jumlah Perjalanan Wisata Dalam Kota</th>
                     <th scope="col">Jenis Kelamin</th>
-                    <th scope="col">Umur</th>
+                    <th scope="col">Usia</th>
                     <th scope="col">Tingkat Pendidikan</th>
                     <th scope="col">Pekerjaan KRT</th>
-                    <th scope="col">b503</th>
-                    <th scope="col">Kerja 1</th>
-                    <th scope="col">Kerja 2</th>
-                    <th scope="col">Kegiatan Utama</th>
-
+                    <th scope="col">Pekerjaan ART</th>
+                    <th scope="col">Disabilitas</th>
+                    <th scope="col">Kepemilikan Mobil Ruta</th>
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <th scope="row">Kecamatan Blimbing</th>
-                    <td>1.887</td>
-                </tr>
-                <tr>
-                    <th scope="row">Kecamatan Kedungkandang</th>
-                    <td>1.306</td>
-                </tr>
-                <tr>
-                    <th scope="row">Kecamatan Klojen</th>
-                    <td>1.84</td>
-                </tr>
-                <tr>
-                    <th scope="row">Kecamatan Lowokwaru</th>
-                    <td>1.737</td>
-                </tr>
-                <tr>
-                    <th scope="row">Kecamatan Sukun</th>
-                    <td>1.686</td>
-                </tr>
+                <?php
+                $i = 1;
+                foreach ($hasil_anaferen_riset2 as $v) :
+                ?>
+                    <tr>
+                        <td><?php echo $v["Jumlah Perjalanan Wisata Dalam Kota"]; ?></td>
+                        <td><?php echo $v["Jenis Kelamin"]; ?></td>
+                        <td><?php echo $v["Usia"]; ?></td>
+                        <td><?php echo $v["Tingkat Pendidikan"]; ?></td>
+                        <td><?php echo $v["Pekerjaan KRT"]; ?></td>
+                        <td><?php echo $v["Pekerjaan ART"]; ?></td>
+                        <td><?php echo $v["Disabilitas"]; ?></td>
+                        <td><?php echo $v["Kepemilikan Mobil Ruta"]; ?></td>
+                    </tr>
+                <?php
+                    $i++;
+                endforeach;
+                ?>
             </tbody>
         </table>
     </div>
-</div> -->
+</div>
