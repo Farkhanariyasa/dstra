21690ccabcf9c5d132df4633834f59641f7c42f0
LuthfilKA <222011321@stis.ac.id>
2023-06-04 10:46:48 +0700

    M Views : grafik tujuan 1s

--- app/Views/pvd/pages/dasbor/riset3/hasilkajian/tujuan1.php
+++ app/Views/pvd/pages/dasbor/riset3/hasilkajian/tujuan1.php
@@ -3,8 +3,8 @@
 <section id="portfolio" class="portfolio section-bg">
   <div class="container">
     <header class="section-header">
-    <h6 style="color: #4b395f;"><strong>Memperoleh gambaran sosiodemografi dan kualitas tenaga kerja di industri pariwisata.</strong>
-    </h6>
+      <h6 style="color: #4b395f;"><strong>Memperoleh gambaran sosiodemografi dan kualitas tenaga kerja di industri pariwisata.</strong>
+      </h6>
     </header>
     <hr class="hrjudul">
     <div class="row">
@@ -19,56 +19,47 @@
     <div class="row portfolio-container">
       <div class="portfolio-item filter-tujuan1-tabulasi">
         <div class="col-lg-12 grid-margin stretch-card">
-            <div class="card shadow">
-                <div class="card-body">
-                <div class="row">
-                  <div class="col-lg-12 grid-margin stretch-card h-100">
-                    <?php include('tabplan_tujuan1.php')?>
-                  </div>
-                  </div>
-                </div>
-             </div>
-           </div>
-        </div>
-      <div class="portfolio-item filter-tujuan1-grafik">
-        <div class="col-lg-12 grid-margin stretch-card">
-        <div class="card shadow">
-                <div class="card-body">
-                <div class="row">
-                  <div class="col-lg-12 grid-margin stretch-card h-100">
-          <div class="card">
-              <h3><b>Grafik</b></h3>
-              <div class="card-body border rounded">
-                <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center ">
-                  <div class="me-1 mt-1 justify-content-end align-item-end">
-                    <button id="unduh-3-4" type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal4">
-                      <i class="fa-solid fa-download"></i>
-                    </button>
-                  </div>
-                </div>
-                <div class="chartBox bar">
-                  <canvas id="tujuan1_blok3_1" style="width: 95%; height:80vh;"></canvas>
-                </div>
-              </div>
-            </div>
-           </div>
-        </div>
-        <div class="col-lg-12 grid-margin stretch-card">
-            <div class="card">
-              <h3><b>Interpretasi</b></h3>
-              <div class="card-body border rounded" style="padding: 1.5rem 0.5rem;">
-                <div class="col-12 col-sm-12">
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus recusandae voluptatum temporibus suscipit animi hic minus! Exercitationem ut vitae sunt consequuntur modi? Quisquam incidunt eligendi sed provident natus beatae! Obcaecati, libero esse vel quia consequatur error quasi sit eaque porro enim corporis aperiam doloribus tempora perferendis alias, quos non temporibus dolorem dolorum adipisci voluptate laudantium reiciendis, ab ipsa. At, quia.</p>
+          <div class="card shadow">
+            <div class="card-body">
+              <div class="row">
+                <div class="col-lg-12 grid-margin stretch-card h-100">
+                  <?php include('tabplan_tujuan1.php') ?>
                 </div>
               </div>
             </div>
           </div>
+        </div>
+      </div>
+      <div class="portfolio-item filter-tujuan1-grafik">
+        <div class="col-lg-12 grid-margin stretch-card">
+          <div class="card shadow">
+            <div class="card-body">
+
+              <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center ">
+                <div class="selectBox mt-2 me-2">
+                  <form action="">
+                    <select class="dropdownunduh1" id="filterValue">
+                      <option value="T1_1">T1_1</option>
+                      <option value="T1_2">T1_2</option>
+                      <option value="T1_3">T1_3</option>
+                    </select>
+                  </form>
+                </div>
+                <div class="me-1 mt-1 justify-content-end align-item-end">
+                  <button id="unduh-3-1" type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                    <i class="fa-solid fa-download"></i>
+                  </button>
+                </div>
+              </div>
+              <div class="chartBox bar">
+                <canvas class="mt1" id="tujuan1" style="width: 95%; height:80vh;"></canvas>
               </div>
             </div>
-           </div>
+          </div>
         </div>
+      </div>
     </div>
-</div>
+  </div>
 </section>
 
 <!-- Istope -->
@@ -86,8 +77,9 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/chartjs-plugin-datalabels/2.2.0/chartjs-plugin-datalabels.min.js" integrity="sha512-JPcRR8yFa8mmCsfrw4TNte1ZvF1e3+1SdGMslZvmrzDYxS69J7J49vkFL8u6u8PlPJK+H3voElBtUCzaXj+6ig==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
 <!-- Impor Chart -->
-<script src="<?= base_url('pvd/js/riset3/hasilPKLRiset3_1_5_pie.js') ?>"></script>
-<script src="<?= base_url('pvd/js/riset3/hasilPKLRiset3_stackedbarchart.js') ?>"></script>
+<!-- <script src="<?= base_url('pvd/js/riset3/hasilPKLRiset3_1_5_pie.js') ?>"></script>
+<script src="<?= base_url('pvd/js/riset3/hasilPKLRiset3_stackedbarchart.js') ?>"></script> -->
+<script src="<?= base_url('pvd/js/riset3/hasilPKLRiset3_tujuan1.js') ?>"></script>
 
 <!-- Fungsi Unduh Chart -->
 <script src="<?= base_url('pvd/js/riset3/btn_Download.js') ?>"></script>
