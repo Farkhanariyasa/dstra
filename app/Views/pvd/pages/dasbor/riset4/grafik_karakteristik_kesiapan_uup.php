154c136ba90a609d2243a2307b76d75b0e2b7f5c
imams <222011830@gmail.com>
2023-06-05 16:22:12 +0700

    A Assets: Grafik menu 3

---
+++ app/Views/pvd/pages/dasbor/riset4/grafik_karakteristik_kesiapan_uup.php
@@ -0,0 +1,205 @@
+<div class="row portfolio-container">
+          <style>
+            .konten-teks {
+                padding: 10%;
+                color: #506396;
+                /* font-family: 'Poppins', 'Courier', 'monospace'; */
+            }
+
+            .konten-teks .card-title {
+                font-weight: bold;
+                font-size: 50px;
+                /* font-family: 'Poppins', 'Courier', 'monospace'; */
+                color: #493a5a;
+
+            }
+
+            .konten-teks .subs {
+                font-weight: bold;
+                font-size: 30px;
+
+            }
+
+            .card-text {
+                /* font-family: 'Poppins', 'Courier', 'monospace'; */
+                color: #506396;
+            }
+          </style>
+
+          <!-- Charts and Interpretations -->
+          <div class="portfolio-item filter-riset4-semua" onresize="responsivefonts()">
+              <!-- <div class="isi-tujuan shadow mt-0 mb-0">
+                  <h4 class="card-title judul-card">Statistik Pemilik atau Pengelola Usaha Pariwisata di Kota Batu</h4> -->
+                  <div class="card-body">
+                      <div class="row">
+
+                            <!-- Part x -->
+                            <div class="col-12 grid-margin stretch-card">
+                              <div class="card shadow">
+                                  <div class="pb-2 card-body">
+                                      <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center">
+                                            <div class="me-1 mt-1 justify-content-end align-item-end">
+                                              <button id="download-1" type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#modal-download-1">
+                                                  <i class="fa-solid fa-download"></i>
+                                              </button>
+                                          </div>
+                                      </div>
+                                      <div class="row">
+                                          <div class="mb-0 col-lg-7 grid-margin">
+                                              <div>
+                                                  <div class = "chartBox" style="position: relative; height:65vh;"> 
+                                                      <canvas id="chart-1"></canvas>
+                                                  </div>
+                                              </div>
+                                          </div>
+                                          <div class="mb-0 col-lg-5 grid-margin stretch-card">
+                                              <div>
+                                                  <h4>spider chart dimensi 1</h4>
+                                                  <p id="interpretasi-0" class="text-justify">
+                                                      Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.
+                                                      Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.
+                                                      Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.
+                                                      Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.
+                                                  </p>
+                                              </div>
+                                          </div>
+                                      </div>
+                                  </div>
+                              </div>
+                            </div>
+
+                            <!-- Tabulasi 1 -->
+                            <!-- <div class="portfolio-item filter-riset2-hasil1"> -->
+                            <div class="col-12 grid-margin stretch-card">
+                                <div class="card shadow">
+                                    <div class="card-header">
+                                        <strong>
+                                            Sebaran Wisatawan dan Jumlah Perjalanan dalam Kota Malang menurut Kecamatan Asal Wisatawan di Kota
+                                            Malang Bulan Oktober-Desember 2022
+                                        </strong>
+                                    </div>
+                                    <div class="card-body table-responsive">//
+                                        <table class="table table-hover table-bordered table-responsive" id="k1_tabel1" style="width:100%">
+                                            <thead>
+                                                <tr>
+                                                    <th scope="col">Kecamatan Asal</th>
+                                                    <th scope="col">Jumlah Wisatawan</th>
+                                                    <th scope="col">Jumlah Perjalanan</th>
+                                                    <th scope="col">Rata-rata Perjalanan</th>
+                                                    <th scope="col">Jumlah Penduduk</th>
+                                                    <th scope="col">Berwisata (%)</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <tr>
+                                                    <th scope="row">Kecamatan Blimbing</th>
+                                                    <td>26921.65</td>
+                                                    <td>84308.41</td>
+                                                    <td>3.13</td>
+                                                    <td>182693</td>
+                                                    <td>14.74</td>
+                                                </tr>
+                                                <tr>
+                                                    <th scope="row">Kecamatan Kedungkandang</th>
+                                                    <td>33381.04</td>
+                                                    <td>102937.72</td>
+                                                    <td>3.08</td>
+                                                    <td>208741</td>
+                                                    <td>15.99</td>
+                                                </tr>
+                                                <tr>
+                                                    <th scope="row">Kecamatan Klojen</th>
+                                                    <td>14481.36</td>
+                                                    <td>65470.65</td>
+                                                    <td>4.52</td>
+                                                    <td>94039</td>
+                                                    <td>15.4</td>
+                                                </tr>
+                                                <tr>
+                                                    <th scope="row">Kecamatan Lowokwaru</th>
+                                                    <td>30840.04</td>
+                                                    <td>135566.29</td>
+                                                    <td>4.40</td>
+                                                    <td>163964</td>
+                                                    <td>18.81</td>
+                                                </tr>
+                                                <tr>
+                                                    <th scope="row">Kecamatan Sukun</th>
+                                                    <td>25472.20</td>
+                                                    <td>62256.85</td>
+                                                    <td>2.44</td>
+                                                    <td>196689</td>
+                                                    <td>12.95</td>
+                                                </tr>
+                                                <tr>
+                                                    <th scope="row">Kota Malang</th>
+                                                    <td>131096.29</td>
+                                                    <td>450539.92</td>
+                                                    <td>3.44</td>
+                                                    <td>846126</td>
+                                                    <td>15.49</td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- </div> -->
+
+                            <!-- Part x -->
+                            <div class="col-12 grid-margin stretch-card">
+                                <div class="card shadow">
+                                    <div class="pb-2 card-body">
+                                        <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center ">
+                                            <div class="selectBox mt-2 me-2">
+                                                <form action="">
+                                                    <select style="width: 250px" class="dropdownunduh1" id="bar-kecamatan-1">
+                                                        <option value="total">Pilih Dimensi:</option>
+                                                        <option value="total">Total</option>
+                                                        <option value="dimensi1">Infrastruktur dan Konektivitas</option>
+                                                        <option value="dimensi2">Penggunaan TIK</option>
+                                                        <option value="dimensi3">Sumber Daya Manusia</option>
+                                                        <option value="dimensi4">Manajemen Organisasi</option>
+                                                        <option value="dimensi5">Ketersediaan Prasarana TIK</option>
+                                                        <option value="dimensi6">Motivasi Penggunaan TIK</option>
+                                                    </select>
+                                                </form>
+                                            </div>
+                                            <div class="me-1 mt-1 justify-content-end align-item-end">
+                                                <button id="download-2" type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#modal-download-2">
+                                                    <i class="fa-solid fa-download"></i>
+                                                </button>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="mb-0 col-lg-7 grid-margin">
+                                                <div>
+                                                    <div class = "chartBox" style="position: relative; height:65vh;"> 
+                                                        <canvas id="chart-2"></canvas>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="mb-0 col-lg-5 grid-margin stretch-card">
+                                                <div>
+                                                    <h4 style="margin-top: 1.5rem;">Dodge Pemilik atau pengelola unit usaha pariwisata berdasarkan jenis kelamin dan usia</h4>
+                                                    <p id="interpretasi-1" class="text-justify">
+                                                        Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.
+                                                        Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.
+                                                        Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.
+                                                        Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                          
+                      </div>
+                  </div>
+              <!-- </div> -->
+          </div>
+
+          <!-- Modal Dasbor -->
+          <?php include('modal_karakteristik_kesiapan_uup.php') ?>
+        </div>
