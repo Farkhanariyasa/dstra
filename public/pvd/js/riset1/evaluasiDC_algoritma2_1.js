ad4f59083cf6a88db50b6413dd809e76da92b1d6
main <222011356@stis.ac.id>
2023-06-07 21:41:06 +0700

    A Assets : dc algo 2

---
+++ public/pvd/js/riset1/evaluasiDC_algoritma2_1.js
@@ -0,0 +1,194 @@
+let delayed_dc_algoritma2_1;
+const dc_algoritma2_1 = document.getElementById("dc_algoritma2_1");
+const bar_dc_algoritma2_1 = new Chart(dc_algoritma2_1, {
+  type: "line",
+  data: {
+    labels: [
+        "1", 
+        "2", 
+        "3", 
+        "4", 
+        "5", 
+        "6", 
+        "7", 
+        "8", 
+        "9", 
+        "10", 
+        "11", 
+        "12"
+    ],
+    datasets: [
+      {
+        label: "Yes",
+        data: [52
+            ,27
+            ,37
+            ,41
+            ,44
+            ,53
+            ,52
+            ,49
+            ,25
+            ,24
+            ,26
+            ,25
+            
+            
+
+        ],
+        // backgroundColor: ["#516296",],
+        // borderWidth: 1,
+        // borderRadius: 8,
+        fill: false,
+        borderColor: "#516296",
+        pointStyle: 'circle',
+        pointRadius: 1,
+        pointHoverRadius: 10,
+    },
+    {
+        label: "No",
+        data: [97
+            ,98
+            ,98
+            ,97
+            ,97
+            ,98
+            ,98
+            ,97
+            ,97
+            ,97
+            ,97
+            ,97
+            
+            
+        ],
+        // backgroundColor: [ "#C27D90"],
+        // borderWidth: 1,
+        // borderRadius: 8,
+        fill: false,
+        borderColor: "#C27D90",
+        pointStyle: 'circle',
+        pointRadius: 1,
+        pointHoverRadius: 10,
+    },
+
+      
+    ],
+  },
+  options: {
+    responsive: true,
+    maintainAspectRatio: false,
+    animation: {
+      onComplete: () => {
+        delayed_dc_algoritma2_1 = true;
+      },
+      delay: (context) => {
+        let delay = 0;
+        if (context.type === "data" && context.mode === "default" && !delayed_dc_algoritma2_1) {
+          delay = context.dataIndex * 300 + context.datasetIndex * 100;
+        }
+        return delay;
+      },
+    },
+    scales: {
+      x: {
+        ticks: {
+          display: true,
+        },
+      },
+      y: {
+        beginAtZero: true,
+        max: 100,
+        },
+    }, 
+    plugins: {
+      title: {
+        display: true,
+        text: ["F1-Score (%)"],
+        font: {
+          size: 16,
+          // family: "Poppins",
+        },
+        color: "#493a5a", 
+        padding: {
+          top: 5,
+          bottom: 25,
+          right: 10,
+          left: 10,
+        },
+      },
+      legend: {
+        display: true,
+        position: "bottom",
+      },
+      tooltip: {
+        enabled: true,
+      },
+      datalabels: {
+        display: false,
+        color: "black",
+        anchor: 'end',
+        align: 'top',
+      },
+    },
+    maintainAspectRatio: false,
+    scales: {
+      x: {
+        grid: {
+          display: false,
+        },
+        title: {
+          display: true,
+          text: "Resolusi",
+          font: {
+            size: 14,
+            style: "normal",
+            lineHeight: 2,
+          },
+        }
+      },
+      y: {
+        grid: {
+          display: false,
+        },
+        title: {
+          display: false,
+          text: "F1-Score (%)",
+          font: {
+            size: 14,
+            style: "normal",
+            lineHeight: 2,
+          },
+        }
+      },
+    },
+  },
+  plugins: [ChartDataLabels],
+});
+
+function responsivefonts() {
+  if (window.outerWidth < 414) {
+    console.log(window.outerWidth);
+    Chart.defaults.font.size = 5;
+  }
+  if (window.outerWidth < 380) {
+    Chart.defaults.font.size = 1;
+  }
+}
+
+// const barChartAxisRelawan_output1 = document.getElementById("forBarChartAxisRelawan_output1");
+// barChartAxisRelawan_output1.addEventListener("click", update_value_relawan_output1(chk_bx));
+
+// function update_value_relawan_output1(chk_bx) {
+//   if (chk_bx.checked) {
+//     console.log("check");
+//     bar_relawan1.options.scales.x.display = true;
+//     bar_relawan1.options.scales.y.display = true;
+//     bar_relawan1.update();
+//   } else {
+//     console.log("uncheck");
+//     bar_relawan1.options.scales.x.display = false;
+//     bar_relawan1.options.scales.y.display = false;
+//     bar_relawan1.update();
+//   }
+// }
---
+++ public/pvd/js/riset1/evaluasiDC_algoritma2_4.js
@@ -0,0 +1,167 @@
+let delayed_dc_algoritma2_4;
+const dc_algoritma2_4 = document.getElementById("dc_algoritma2_4");
+const bar_dc_algoritma2_4 = new Chart(dc_algoritma2_4, {
+  type: "line",
+  data: {
+    labels: [
+        "1", 
+        "2", 
+        "3", 
+        "4", 
+        "5", 
+        "6", 
+        "7", 
+        "8", 
+        "9", 
+        "10", 
+        "11", 
+        "12"
+    ],
+    datasets: [
+      {
+        label: "Accuracy",
+        data: [95
+            ,95
+            ,96
+            ,95
+            ,95
+            ,96
+            ,95
+            ,95
+            ,95
+            ,95
+            ,95
+            ,95
+            
+            
+
+        ],
+        // backgroundColor: ["#516296",],
+        // borderWidth: 1,
+        // borderRadius: 8,
+        fill: false,
+        borderColor: "#516296",
+        pointStyle: 'circle',
+        pointRadius: 1,
+        pointHoverRadius: 10,
+    },
+      
+    ],
+  },
+  options: {
+    responsive: true,
+    maintainAspectRatio: false,
+    animation: {
+      onComplete: () => {
+        delayed_dc_algoritma2_4 = true;
+      },
+      delay: (context) => {
+        let delay = 0;
+        if (context.type === "data" && context.mode === "default" && !delayed_dc_algoritma2_4) {
+          delay = context.dataIndex * 300 + context.datasetIndex * 100;
+        }
+        return delay;
+      },
+    },
+    scales: {
+      x: {
+        ticks: {
+          display: true,
+        },
+      },
+      y: {
+        beginAtZero: true,
+        max: 100,
+        },
+    }, 
+    plugins: {
+      title: {
+        display: true,
+        text: ["Accuracy (%)"],
+        font: {
+          size: 16,
+          // family: "Poppins",
+        },
+        color: "#493a5a", 
+        padding: {
+          top: 5,
+          bottom: 25,
+          right: 10,
+          left: 10,
+        },
+      },
+      legend: {
+        display: false,
+        position: "bottom",
+      },
+      tooltip: {
+        enabled: true,
+      },
+      datalabels: {
+        display: false,
+        color: "black",
+        anchor: 'end',
+        align: 'top',
+      },
+    },
+    maintainAspectRatio: false,
+    scales: {
+      x: {
+        grid: {
+          display: false,
+        },
+        title: {
+          display: true,
+          text: "Resolusi",
+          font: {
+            size: 14,
+            style: "normal",
+            lineHeight: 2,
+          },
+        }
+      },
+      y: {
+        grid: {
+          display: false,
+        },
+        title: {
+          display: false,
+          text: "F1-Score (%)",
+          font: {
+            size: 14,
+            style: "normal",
+            lineHeight: 2,
+          },
+        }
+      },
+    },
+  },
+  plugins: [ChartDataLabels],
+});
+
+function responsivefonts() {
+  if (window.outerWidth < 414) {
+    console.log(window.outerWidth);
+    Chart.defaults.font.size = 5;
+  }
+  if (window.outerWidth < 380) {
+    Chart.defaults.font.size = 1;
+  }
+}
+
+// const barChartAxisRelawan_output1 = document.getElementById("forBarChartAxisRelawan_output1");
+// barChartAxisRelawan_output1.addEventListener("click", update_value_relawan_output1(chk_bx));
+
+// function update_value_relawan_output1(chk_bx) {
+//   if (chk_bx.checked) {
+//     console.log("check");
+//     bar_relawan1.options.scales.x.display = true;
+//     bar_relawan1.options.scales.y.display = true;
+//     bar_relawan1.update();
+//   } else {
+//     console.log("uncheck");
+//     bar_relawan1.options.scales.x.display = false;
+//     bar_relawan1.options.scales.y.display = false;
+//     bar_relawan1.update();
+//   }
+// }
