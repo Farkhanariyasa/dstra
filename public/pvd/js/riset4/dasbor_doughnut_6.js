b47733dad3e7b7a0a7af4b0e81835a90b9b13042
imams <222011830@gmail.com>
2023-06-07 07:37:17 +0700

    M View: Revisi doughnut chart 6

--- public/pvd/js/riset4/dasbor_doughnut_6.js
+++ public/pvd/js/riset4/dasbor_doughnut_6.js
@@ -66,7 +66,7 @@ new Chart(A1, {
             },
             datalabels: {
                 display: true,
-                color: 'white',
+                color: 'black',
                 formatter: function(value, context) {
                     total = context.chart._metasets[context.datasetIndex].total;
                     percentage = parseFloat((value/total*100).toFixed(2));
