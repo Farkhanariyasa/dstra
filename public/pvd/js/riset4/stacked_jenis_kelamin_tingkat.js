3ec3159a2ee5c3d675c02096ec118f3ca9442ea8
imams <222011830@gmail.com>
2023-06-07 17:14:40 +0700

    M View: Stacked bar 1 menu 3

--- public/pvd/js/riset4/stacked_jenis_kelamin_tingkat.js
+++ public/pvd/js/riset4/stacked_jenis_kelamin_tingkat.js
@@ -2,20 +2,19 @@ const stacked1 = document.getElementById('chart-1');
 const stacked_1 = new Chart(stacked1, {
     type:'bar',
     data: {
-        labels: [ ['Jasa', 'Transportasi Wisata'],
-            ['Jasa', 'Penyedia Makanan', 'dan Minuman'],
-            ['Penyedia', 'Akomodasi']
+        labels: [ ['Laki-laki'],
+            ['Perempuan'],
         ],
         datasets: [{
-            label: 'Tidak Siap',
+            label: 'Siap',
             data:[
-                (jenisKelaminTingkat.lakiTidak/475*100).toFixed(2),
-                (jenisKelaminTingkat.perempuanTidak/450*100).toFixed(2),
+                (jenisKelaminTingkat.lakiSiap/475*100).toFixed(2),
+                (jenisKelaminTingkat.perempuanSiap/450*100).toFixed(2),
             ],
-            backgroundColor:"#ffab00",
+            backgroundColor:"#b35c74",
             borderWidth:1
         },{
-            label: 'Tidak berada di bangunan fisik rumah tangga',
+            label: 'Cukup Siap',
             data:[
                 (jenisKelaminTingkat.lakiCukup/475*100).toFixed(2),
                 (jenisKelaminTingkat.perempuanCukup/450*100).toFixed(2),
@@ -23,22 +22,16 @@ const stacked_1 = new Chart(stacked1, {
             backgroundColor:"#506396",
             borderWidth:1
         },{
-            label: 'Siap',
+            label: 'Kurang Siap',
             data:[
-                (jenisKelaminTingkat.lakiSiap/475*100).toFixed(2),
-                (jenisKelaminTingkat.perempuanSiap/450*100).toFixed(2),
+                (jenisKelaminTingkat.lakiTidak/475*100).toFixed(2),
+                (jenisKelaminTingkat.perempuanTidak/450*100).toFixed(2),
             ],
-            backgroundColor:"#506396",
+            backgroundColor:"#ffab00",
             borderWidth:1
         },
         ]
     },options:{
-        indexAxis: 'y',
-        elements: {
-            bar: {
-              borderWidth: 3,
-            }
-          },
         responsive: true,
         maintainAspectRatio: false,
         scales:{
@@ -91,21 +84,29 @@ const stacked_1 = new Chart(stacked1, {
             },
             datalabels:{
                 display:true,
-                color:'black'
+                color:'black',
+                formatter: function(value, context) {
+                    return  value + '%';
+                },
             }
         },maintainAspectRatio: false,
-        scales:{
-            x:{
-                grid:{
-                    display:false
-                },
+        scales: {
+            x: {
+              stacked: true,
+              ticks: {
+                display: true,
+              },
+              grid:{
+                display:false
             },
-            y:{
-                grid:{
-                    display:false
-                },
+            },
+            y: {
+              stacked: true,
+              grid:{
+                display:false
+            },
             }
-        }
+          },
     },plugins:[ChartDataLabels]
 });
 
@@ -120,34 +121,34 @@ function responsivefonts(){
     }
 };
 
-const dodgeBar5Axis = document.getElementById("dodge-bar-5-axis");
-dodgeBar5Axis.addEventListener("click", update_value_axis_dodge_bar_5(chk_bx));
+// const dodgeBar5Axis = document.getElementById("dodge-bar-5-axis");
+// dodgeBar5Axis.addEventListener("click", update_value_axis_dodge_bar_5(chk_bx));
 
-function update_value_axis_dodge_bar_5(chk_bx) {
-  if (chk_bx.checked) {
-    console.log("check");
-    stacked_1.options.scales.x.display = true;
-    stacked_1.options.scales.y.display = true;
-    stacked_1.update();
-  } else {
-    console.log("uncheck");
-    stacked_1.options.scales.x.display = false;
-    stacked_1.options.scales.y.display = false;
-    stacked_1.update();
-  }
-};
+// function update_value_axis_dodge_bar_5(chk_bx) {
+//   if (chk_bx.checked) {
+//     console.log("check");
+//     stacked_1.options.scales.x.display = true;
+//     stacked_1.options.scales.y.display = true;
+//     stacked_1.update();
+//   } else {
+//     console.log("uncheck");
+//     stacked_1.options.scales.x.display = false;
+//     stacked_1.options.scales.y.display = false;
+//     stacked_1.update();
+//   }
+// };
 
-const dodgeBar5Legend = document.getElementById("dodge-bar-5-legend");
-dodgeBar5Legend.addEventListener("click", update_value_legend_dodge_bar_5(chk_bx));
+// const dodgeBar5Legend = document.getElementById("dodge-bar-5-legend");
+// dodgeBar5Legend.addEventListener("click", update_value_legend_dodge_bar_5(chk_bx));
 
-function update_value_legend_dodge_bar_5(chk_bx) {
-    if (chk_bx.checked) {
-      console.log("check");
-      stacked_1.options.plugins.legend.display=true;
-      stacked_1.update();
-    } else {
-      console.log("uncheck");
-      stacked_1.options.plugins.legend.display=false;
-      stacked_1.update();
-    }
-};
+// function update_value_legend_dodge_bar_5(chk_bx) {
+//     if (chk_bx.checked) {
+//       console.log("check");
+//       stacked_1.options.plugins.legend.display=true;
+//       stacked_1.update();
+//     } else {
+//       console.log("uncheck");
+//       stacked_1.options.plugins.legend.display=false;
+//       stacked_1.update();
+//     }
+// };
