8238b5d5059e39247812db531d96c11d248870d2
main <222011356@stis.ac.id>
2023-06-07 21:26:27 +0700

    A Assets : evaluasi dc algo 1

---
+++ public/pvd/js/riset1/evaluasiDC_algoritma1_1.js
@@ -0,0 +1,151 @@
+let delayed_dc_algoritma1_1;
+const dc_algoritma1_1 = document.getElementById("dc_algoritma1_1");
+const bar_dc_algoritma1_1 = new Chart(dc_algoritma1_1, {
+  type: "bar",
+  data: {
+    labels: ["Sebulan", "Setahun"],
+    datasets: [
+      {
+        label: "Yes",
+        data: [69
+            ,78
+            
+            
+
+
+        ],
+        backgroundColor: ["#516296"],
+        borderWidth: 1,
+        borderRadius: 8,
+      },
+        {
+            label: "No",
+            data: [99
+                ,99
+                
+                
+            ],
+            backgroundColor: ["#C27D90"],
+            borderWidth: 1,
+            borderRadius: 8,
+        },
+      
+    ],
+  },
+  options: {
+    responsive: true,
+    maintainAspectRatio: false,
+    animation: {
+      onComplete: () => {
+        delayed_dc_algoritma1_1 = true;
+      },
+      delay: (context) => {
+        let delay = 0;
+        if (context.type === "data" && context.mode === "default" && !delayed_dc_algoritma1_1) {
+          delay = context.dataIndex * 300 + context.datasetIndex * 100;
+        }
+        return delay;
+      },
+    },
+    scales: {
+      x: {
+        ticks: {
+          display: true,
+        },
+      },
+      y: {
+        beginAtZero: true,
+        max: 100,
+        },
+    }, 
+    plugins: {
+      title: {
+        display: true,
+        text: ["F1-Score (%)"],
+        font: {
+          size: 16,
+          // family: "Poppins",
+        },
+        color: "#493a5a", 
+        padding: {
+          top: 5,
+          bottom: 25,
+          right: 10,
+          left: 10,
+        },
+      },
+      legend: {
+        display: false,
+        position: "bottom",
+      },
+      tooltip: {
+        enabled: true,
+      },
+      datalabels: {
+        display: true,
+        color: "black",
+        anchor: 'end',
+        align: 'top',
+      },
+    },
+    maintainAspectRatio: false,
+    scales: {
+      x: {
+        grid: {
+          display: false,
+        },
+        title: {
+          display: false,
+          text: "Umur",
+          font: {
+            size: 14,
+            style: "normal",
+            lineHeight: 2,
+          },
+        }
+      },
+      y: {
+        grid: {
+          display: false,
+        },
+        title: {
+          display: false,
+          text: "Jumlah Relawan",
+          font: {
+            size: 14,
+            style: "normal",
+            lineHeight: 2,
+          },
+        }
+      },
+    },
+  },
+  plugins: [ChartDataLabels],
+});
+
+function responsivefonts() {
+  if (window.outerWidth < 414) {
+    console.log(window.outerWidth);
+    Chart.defaults.font.size = 5;
+  }
+  if (window.outerWidth < 380) {
+    Chart.defaults.font.size = 1;
+  }
+}
+
+// const barChartAxisRelawan_output1 = document.getElementById("forBarChartAxisRelawan_output1");
+// barChartAxisRelawan_output1.addEventListener("click", update_value_relawan_output1(chk_bx));
+
+// function update_value_relawan_output1(chk_bx) {
+//   if (chk_bx.checked) {
+//     console.log("check");
+//     bar_relawan1.options.scales.x.display = true;
+//     bar_relawan1.options.scales.y.display = true;
+//     bar_relawan1.update();
+//   } else {
+//     console.log("uncheck");
+//     bar_relawan1.options.scales.x.display = false;
+//     bar_relawan1.options.scales.y.display = false;
+//     bar_relawan1.update();
+//   }
+// }
