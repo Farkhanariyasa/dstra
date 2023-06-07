ed83788cce3b000f2d7dfb2d6547e1a6f4460291
LuthfilKA <222011321@stis.ac.id>
2023-06-07 08:25:17 +0700

    M Controller : tambah fungsi untuk download dari riset3

--- app/Controllers/Pvd/Dasbor/Riset3.php
+++ app/Controllers/Pvd/Dasbor/Riset3.php
@@ -5,6 +5,7 @@ namespace App\Controllers\Pvd\Dasbor;
 use App\Controllers\BaseController;
 use App\Models\Pvd\Riset3HasilSpModel;
 use App\Models\Pvd\Riset3HasilPKLModel;
+use App\Models\Pvd\UnduhHasilPklModel;
 
 class Riset3 extends BaseController
 {
@@ -89,101 +90,12 @@ class Riset3 extends BaseController
     // menu Hasil Kajian
     public function hasilkajian1()
     {
-        // jenis usaha
-        $ji = [
-            'ji1' => $this->jenisindustri->getByJenisIndustri("1"),
-            'ji2' => $this->jenisindustri->getByJenisIndustri("2"),
-            //'ji3' => $this->jenisindustri->getByJenisIndustri("3"),
-            'ji4' => $this->jenisindustri->getByJenisIndustri("4"),
-            //'ji5' => $this->jenisindustri->getByJenisIndustri("5"),
-            //'ji6' => $this->jenisindustri->getByJenisIndustri("6"),
-            //'ji7' => $this->jenisindustri->getByJenisIndustri("7"),
-            //'ji8' => $this->jenisindustri->getByJenisIndustri("8"),
-            'ji9' => $this->jenisindustri->getByJenisIndustri("9"),
-            'ji10' => $this->jenisindustri->getByJenisIndustri("10"),
-            'ji11' => $this->jenisindustri->getByJenisIndustri("11", "3", "5", "6", "7", "8")
-        ];
-        // pendidikan tertinggi
-        $pt = [
-            'pt1' => $this->pendidikantertinggi->getByPendidikanTertinggi("1"),
-            'pt2' => $this->pendidikantertinggi->getByPendidikanTertinggi("2"),
-            'pt3' => $this->pendidikantertinggi->getByPendidikanTertinggi("3"),
-            'pt4' => $this->pendidikantertinggi->getByPendidikanTertinggi("4"),
-            'pt5' => $this->pendidikantertinggi->getByPendidikanTertinggi("5"),
-            'pt6' => $this->pendidikantertinggi->getByPendidikanTertinggi("6"),
-            'pt7' => $this->pendidikantertinggi->getByPendidikanTertinggi("7"),
-            'pt8' => $this->pendidikantertinggi->getByPendidikanTertinggi("8"),
-            'pt9' => $this->pendidikantertinggi->getByPendidikanTertinggi("9"),
-        ];
-        // status pekerja
-        $jk_1 = [
-            'jk_11' => $this->jeniskelamin_riset3->getByJenisKelamin_Riset3("1"),
-            'jk_12' => $this->jeniskelamin_riset3->getByJenisKelamin_Riset3("2")
-        ];
-        $pd_1 = $this->pendapatan_riset3->getPendapatan_Riset3();
         $menu = getMenu();
-        $data = [
-            'judul' => 'Hasil Kajian Tujuan 1 Riset 3',
-            'menu' => $menu['riset3'],
-            'ji' => $ji,
-            'pt' => $pt,
-            'jk_1' => $jk_1,
-            'pd_1' => $pd_1
-        ];
-
-        // jenis usaha
-        $jenisindustri_1 = [
-            'ji1_1' => $this->HasilPKL->getByJenisIndustri("1", "1"),
-            'ji2_1' => $this->HasilPKL->getByJenisIndustri("2", "1"),
-            'ji3_1' => $this->HasilPKL->getByJenisIndustri("3", "1"),
-            'ji4_1' => $this->HasilPKL->getByJenisIndustri("4", "1"),
-            'ji5_1' => $this->HasilPKL->getByJenisIndustri("5", "1"),
-            'ji6_1' => $this->HasilPKL->getByJenisIndustri("6", "1"),
-
-            'ji1_2' => $this->HasilPKL->getByJenisIndustri("1", "2"),
-            'ji2_2' => $this->HasilPKL->getByJenisIndustri("2", "2"),
-            'ji3_2' => $this->HasilPKL->getByJenisIndustri("3", "2"),
-            'ji4_2' => $this->HasilPKL->getByJenisIndustri("4", "2"),
-            'ji5_2' => $this->HasilPKL->getByJenisIndustri("5", "2"),
-            'ji6_2' => $this->HasilPKL->getByJenisIndustri("6", "2"),
-
-            'ji1_3' => $this->HasilPKL->getByJenisIndustri("1", "3"),
-            'ji2_3' => $this->HasilPKL->getByJenisIndustri("2", "3"),
-            'ji3_3' => $this->HasilPKL->getByJenisIndustri("3", "3"),
-            'ji4_3' => $this->HasilPKL->getByJenisIndustri("4", "3"),
-            'ji5_3' => $this->HasilPKL->getByJenisIndustri("5", "3"),
-            'ji6_3' => $this->HasilPKL->getByJenisIndustri("6", "3"),
-
-
-            // 'total1_1_1' => 100 / ('ji1_1' + 'ji1_2' + 'ji1_3'),
-            // 'total1_1_2' => 100 / ('ji2_1' + 'ji2_2' + 'ji2_3'),
-            // 'total1_1_3' => 100 / ('ji3_1' + 'ji3_2' + 'ji3_3'),
-            // 'total1_1_4' => 100 / ('ji4_1' + 'ji4_2' + 'ji4_3'),
-            // 'total1_1_5' => 100 / ('ji5_1' + 'ji5_2' + 'ji5_3'),
-            // 'total1_1_6' => 100 / ('ji6_1' + 'ji6_2' + 'ji6_3')
-        ];
-
-
-
-        $data_1_5 = [
-            'rl_jk_1_1' => $this->tujuan1_5->getByRL_JK("1", "1"),
-            'rl_jk_1_2' => $this->tujuan1_5->getByRL_JK("1", "2"),
-            'rl_jk_2_1' => $this->tujuan1_5->getByRL_JK("2", "1"),
-            'rl_jk_2_2' => $this->tujuan1_5->getByRL_JK("2", "2")
-        ];
-
         $data1 = [
             'judul' => 'Hasil Kajian Tujuan 1 Riset 3',
             'menu' => $menu['riset3'],
-            'ji' => $jenisindustri_1,
-            'pt' => $pt,
-            'jk_1' => $jk_1,
-            'pd_1' => $pd_1,
-            'ji1' => $jenisindustri_1,
-            'data_1_5' => $data_1_5,
         ];
         return view('pvd/pages/dasbor/riset3/hasilkajian/tujuan1', $data1);
-        return view('pvd/pages/dasbor/riset3/hasilkajian/tujuan1', $data);
     }
 
     public function hasilkajian2()
@@ -207,30 +119,12 @@ class Riset3 extends BaseController
             't2_6_2' => $this->HasilPKL->get_2_2("6", "2")
 
         ];
-
-        $data_2_3 = [];
-
-        $data_2_4 = [];
-
-        $data_2_5 = [];
-
-        $data_2_6 = [];
-
-        $data_2_7 = [];
-
-        $data_2_8 = [];
-
         $data = [
             'judul' => 'Hasil Kajian Tujuan 2 Riset 3',
             'menu' => $menu['riset3'],
             'data_2_1' => $data_2_1,
             'data_2_2' => $data_2_2,
-            'data_2_3' => $data_2_3,
-            'data_2_4' => $data_2_4,
-            'data_2_5' => $data_2_5,
-            'data_2_6' => $data_2_6,
-            'data_2_7' => $data_2_7,
-            'data_2_8' => $data_2_8,
+
         ];
 
 
@@ -277,5 +171,97 @@ class Riset3 extends BaseController
             'menu' => $menu['riset3'],
         ];
         return view('pvd/pages/dasbor/riset3/dokumen/bukupedoman', $data);
+    }
+
+    public function unduhtujuan1()
+    {
+        $datapengunduh = new UnduhHasilPklModel();
+        $menu = getMenu();
+        $data = [
+            'judul' => 'Hasil Kajian Tujuan 1 Riset 3',
+            'menu' => $menu['riset3'],
+        ];
+
+
+        $data1 = [
+            'email' => $this->request->getVar('email'),
+            'nama' => $this->request->getVar('nama'),
+            'instansi' => $this->request->getVar('instansi')
+        ];
+        $datapengunduh->insert($data1);
+        return view('pvd/pages/dasbor/riset3/hasilkajian/tujuan1', $data);
+    }
+    public function unduhtujuan2()
+    {
+        $menu = getMenu();
+        $data_2_1 = [];
+
+        $data_2_2 = [
+            't2_1_1' => $this->HasilPKL->get_2_2("1", "1") + $this->HasilPKL->get_2_2("1.0", "1"),
+            't2_2_1' => $this->HasilPKL->get_2_2("2", "1"),
+            't2_3_1' => $this->HasilPKL->get_2_2("3", "1"),
+            't2_4_1' => $this->HasilPKL->get_2_2("4", "1"),
+            't2_5_1' => $this->HasilPKL->get_2_2("5", "1"),
+            't2_6_1' => $this->HasilPKL->get_2_2("6", "1"),
+
+            't2_1_2' => $this->HasilPKL->get_2_2("1", "2") + $this->HasilPKL->get_2_2("1", "") + $this->HasilPKL->get_2_2("1.0", "2") + $this->HasilPKL->get_2_2("1.0", ""),
+            't2_2_2' => $this->HasilPKL->get_2_2("2", "2"),
+            't2_3_2' => $this->HasilPKL->get_2_2("3", "2"),
+            't2_4_2' => $this->HasilPKL->get_2_2("4", "2"),
+            't2_5_2' => $this->HasilPKL->get_2_2("5", "2"),
+            't2_6_2' => $this->HasilPKL->get_2_2("6", "2")
+
+        ];
+
+        $data = [
+            'judul' => 'Hasil Kajian Tujuan 2 Riset 3',
+            'menu' => $menu['riset3'],
+            'data_2_1' => $data_2_1,
+            'data_2_2' => $data_2_2,
+
+        ];
+        $datapengunduh = new UnduhHasilPklModel();
+        $data2 = [
+            'email' => $this->request->getVar('email'),
+            'nama' => $this->request->getVar('nama'),
+            'instansi' => $this->request->getVar('instansi')
+        ];
+        $datapengunduh->insert($data2);
+
+        return view('pvd/pages/dasbor/riset3/hasilkajian/tujuan2', $data);
+    }
+    public function unduhtujuan3()
+    {
+        $datapengunduh = new UnduhHasilPklModel();
+        $data3 = [
+            'email' => $this->request->getVar('email'),
+            'nama' => $this->request->getVar('nama'),
+            'instansi' => $this->request->getVar('instansi')
+        ];
+        $datapengunduh->insert($data3);
+
+        $menu = getMenu();
+        $data = [
+            'judul' => 'Hasil Kajian Tujuan 3 Riset 3',
+            'menu' => $menu['riset3'],
+        ];
+        return view('pvd/pages/dasbor/riset3/hasilkajian/tujuan3', $data);
+    }
+
+    public function unduhtujuan4()
+    {
+        $datapengunduh = new UnduhHasilPklModel();
+        $data4 = [
+            'email' => $this->request->getVar('email'),
+            'nama' => $this->request->getVar('nama'),
+            'instansi' => $this->request->getVar('instansi')
+        ];
+        $datapengunduh->insert($data4);
+        $menu = getMenu();
+        $data = [
+            'judul' => 'Hasil Kajian 4 Riset 3',
+            'menu' => $menu['riset3'],
+        ];
+        return view('pvd/pages/dasbor/riset3/hasilkajian/tujuan4', $data);
     }
 }
