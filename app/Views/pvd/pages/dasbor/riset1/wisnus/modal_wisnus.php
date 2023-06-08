97def12b4585801d280504983eeb29d37cef7bac
Thomson <222011365@stis.ac.id>
2023-06-08 22:00:53 +0700

    M Views: perubahan action modal wisnus

--- app/Views/pvd/pages/dasbor/riset1/wisnus/modal_wisnus.php
+++ app/Views/pvd/pages/dasbor/riset1/wisnus/modal_wisnus.php
@@ -6,7 +6,7 @@
                 <h4 class="modal-title" id="exampleModalLabel">Unduh Hasil PKL</h4>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <form name="unduhVisualisasi" action="<?= base_url(); ?>/hasil-pkl/riset1/unduh" method="POST">
+            <form name="unduhVisualisasi" action="<?= base_url(); ?>/hasil-pkl/riset1/wisnus-visualisasi" method="POST">
                 <div class="modal-body">
                     <div class="mb-1">
                         <label for="recipient-name" class="col-form-label">Nama <span class="text-danger">*</span></label>
