bfcbbb26b3b3c97df1363e8660309da9e7a9057f
LuthfilKA <222011321@stis.ac.id>
2023-06-09 10:39:37 +0700

    M Views : responsive grafik tujuan 1

--- app/Views/pvd/pages/dasbor/riset3/hasilkajian/tujuan1.php
+++ app/Views/pvd/pages/dasbor/riset3/hasilkajian/tujuan1.php
@@ -22,7 +22,7 @@
         <div class="col-lg-12 grid-margin stretch-card">
           <div class="card shadow">
             <div class="card-body">
-              <div class="row">
+              <div style="height:600px;" class="row ">
                 <div class="col-lg-12 grid-margin stretch-card h-100">
                   <?php include('tabplan_tujuan1.php') ?>
                 </div>
