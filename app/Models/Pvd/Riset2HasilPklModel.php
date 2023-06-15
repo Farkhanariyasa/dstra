0e192c9afbaa0577ec03038b98435010d8a04ae9
I Putu Agus Wahyu Dupayana <75474419+putuwahyu29@users.noreply.github.com>
2023-06-15 20:56:46 +0700

    M Model : Ubah nama model

--- app/Models/Pvd/Riset2HasilPklModel.php
+++ app/Models/Pvd/Riset2HasilPklModel.php
@@ -4,7 +4,7 @@ namespace App\Models\Pvd;
 
 use CodeIgniter\Model;
 
-class Riset2HasilAnaferenModel extends Model
+class Riset2HasilPklModel extends Model
 {
     protected $table            = 'pvd_riset2_hasilpkl';
     protected $primaryKey       = 'id';
