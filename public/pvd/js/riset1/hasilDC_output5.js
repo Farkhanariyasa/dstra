c86c6f7541385159597d4bb7dc5314b9d654c976
main <222011356@stis.ac.id>
2023-07-10 20:58:41 +0800

    A Assets : DC visualisasi

---
+++ public/pvd/js/riset1/hasilDC_output5.js
@@ -0,0 +1,221 @@
+let delayed_dc_output5;
+const dc_output5 = document.getElementById("dc_output5");
+const bar_dc_output5 = new Chart(dc_output5, {
+  type: "line",
+  data: {
+    labels: [
+        "1", 
+        "2", 
+        "3", 
+        "4", 
+        "5", 
+        "6", 
+        "7", 
+        "8", 
+        "9", 
+        "10", 
+        "11", 
+        "12"
+    ],
+    datasets: [
+      {
+        label: "Treshold",
+        data: [1.5
+            ,3.5
+            ,8.5
+            ,18.5
+            ,42.5
+            ,78.5
+            ,112.5
+            ,182.5
+            ,206.5
+            ,217
+            ,218.5
+            ,26.5
+            
+        ],
+        // backgroundColor: ["#516296",],
+        // borderWidth: 1,
+        // borderRadius: 8,
+        fill: false,
+        borderColor: "#FDAB01",
+        pointStyle: 'circle',
+        pointRadius: 1,
+        pointHoverRadius: 10,
+    },
+    {
+        label: "True DC",
+        data: [1.77
+            ,2.65
+            ,6.32
+            ,15.03
+            ,32.26
+            ,56.58
+            ,91.23
+            ,135.19
+            ,138.52
+            ,110.74
+            ,88.45
+            ,73.84
+            
+            
+            
+        ],
+        // backgroundColor: [ "#C27D90"],
+        // borderWidth: 1,
+        // borderRadius: 8,
+        fill: false,
+        borderColor: "#516296",
+        pointStyle: 'circle',
+        pointRadius: 1,
+        pointHoverRadius: 10,
+    },
+    {
+        label: "False DC",
+        data: [1.10
+            ,2.09
+            ,4.84
+            ,9.75
+            ,18.71
+            ,31.17
+            ,51.25
+            ,82.32
+            ,89.07
+            ,82.75
+            ,75.49
+            ,67.69
+            
+            
+            
+        ],
+        // backgroundColor: [ "#C27D90"],
+        // borderWidth: 1,
+        // borderRadius: 8,
+        fill: false,
+        borderColor: "#C27D90",
+        pointStyle: 'circle',
+        pointRadius: 1,
+        pointHoverRadius: 10,
+    },
+
+
+      
+    ],
+  },
+  options: {
+    responsive: true,
+    maintainAspectRatio: false,
+    animation: {
+      onComplete: () => {
+        delayed_dc_output5 = true;
+      },
+      delay: (context) => {
+        let delay = 0;
+        if (context.type === "data" && context.mode === "default" && !delayed_dc_output5) {
+          delay = context.dataIndex * 300 + context.datasetIndex * 100;
+        }
+        return delay;
+      },
+    },
+    scales: {
+      x: {
+        ticks: {
+          display: true,
+        },
+      },
+      y: {
+        beginAtZero: true,
+        max: 100,
+        },
+    }, 
+    plugins: {
+      title: {
+        display: true,
+        text: ["Grafik Perbandingan Rata-Rata", "Antar Label (no staypoint)"],
+        font: {
+          size: 16,
+          // family: "Poppins",
+        },
+        color: "#493a5a", 
+        padding: {
+          top: 5,
+          bottom: 25,
+          right: 10,
+          left: 10,
+        },
+      },
+      legend: {
+        display: true,
+        position: "bottom",
+      },
+      tooltip: {
+        enabled: true,
+      },
+      datalabels: {
+        display: false,
+        color: "black",
+        anchor: 'end',
+        align: 'top',
+      },
+    },
+    maintainAspectRatio: false,
+    scales: {
+      x: {
+        grid: {
+          display: false,
+        },
+        title: {
+          display: true,
+          text: "Resolusi",
+          font: {
+            size: 14,
+            style: "normal",
+            lineHeight: 2,
+          },
+        }
+      },
+      y: {
+        grid: {
+          display: false,
+        },
+        title: {
+          display: true,
+          text: "Jumlah Irisan",
+          font: {
+            size: 14,
+            style: "normal",
+            lineHeight: 2,
+          },
+        }
+      },
+    },
+  },
+  plugins: [ChartDataLabels],
+});
+
+function responsivefonts() {
+  if (window.outerWidth < 414) {
+    console.log(window.outerWidth);
+    Chart.defaults.font.size = 5;
+  }
+  if (window.outerWidth < 380) {
+    Chart.defaults.font.size = 1;
+  }
+}
+
+// const barChartAxisRelawan_output1 = document.getElementById("forBarChartAxisRelawan_output1");
+// barChartAxisRelawan_output1.addEventListener("click", update_value_relawan_output1(chk_bx));
+
+// function update_value_relawan_output1(chk_bx) {
+//   if (chk_bx.checked) {
+//     console.log("check");
+//     bar_relawan1.options.scales.x.display = true;
+//     bar_relawan1.options.scales.y.display = true;
+//     bar_relawan1.update();
+//   } else {
+//     console.log("uncheck");
+//     bar_relawan1.options.scales.x.display = false;
+//     bar_relawan1.options.scales.y.display = false;
+//     bar_relawan1.update();
+//   }
+// }
