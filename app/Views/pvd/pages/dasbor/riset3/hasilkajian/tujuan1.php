a2bee66256732269dde7099bc272669f236b3b90
prasetyofmalik <222011325@stis.ac.id>
2023-07-11 11:25:08 +0700

    M Views: benerin interpretasi tujuan 1 riset 3

--- app/Views/pvd/pages/dasbor/riset3/hasilkajian/tujuan1.php
+++ app/Views/pvd/pages/dasbor/riset3/hasilkajian/tujuan1.php
@@ -89,10 +89,57 @@
                 <div class="card">
                   <h3><b>Interpretasi</b></h3>
                   <div class="card-body p-2 border rounded">
-                      <p>Terlihat bahwa dimensi Kenangan memiliki nilai skor tertinggi dibandingkan dimensi lainnya, yaitu sebesar 66,37. Sementara itu, nilai skor terendah dimiliki oleh dimensi Sejuk-Indah-Ramah dengan nilai sebesar 48,65, lebih rendah 17,72 poin dibandingkan dimensi Kenangan. Hal ini menunjukkan bahwa upaya untuk meningkatkan sadar wisata tenaga kerja di industri pariwisata di Kabupaten Malang dapat difokuskan mulai dari dimensi Sejuk-Indah-Ramah.</p>
-                      <p>Dimensi Sejuk-Indah-Ramah merupakan dimensi yang memiliki nilai indeks yang paling rendah. Hal ini disebabkan karena kesejukan dan keindahan yang sudah khas di daerah Kabupaten Malang menjadikan tenaga kerja di sana tidak terlalu menerapkan penjagaan kesejukan dan keindahan. Selain itu, rendahnya nilai dimensi ini disebabkan karena keramahan pada wisatawan yang masih kurang dari tenaga kerja pariwisata. Nilai dimensi sejuk-indah-ramah yang memiliki nilai paling rendah dibandingkan dimensi lainnya dapat ditingkatkan dengan melakukan sosialisasi kepada para tenaga kerja di bidang pariwisata untuk selalu menerapkan budaya 5S (Senyum, Salam, Sapa, Sopan, dan Santun) dalam melayani wisatawan atau pengunjung, serta meningkatkan kebiasaan untuk menanam dan merawat tanaman di sekitar tempat wisata guna meningkatkan keasrian dan kesejukan.</p>
-                      <p>Di lain sisi, dimensi kenangan menjadi dimensi dengan nilai tertinggi. Hal ini berarti tenaga kerja di sektor pariwisata di Kabupaten Malang turut serta dalam mengenalkan kesenian dan kebudayaan yang berasal dari daerahnya kepada wisatawan dan juga mempromosikan makanan dan minuman khas maupun cindera mata dari kabupaten malang. Dimensi ini tetap perlu diperhatikan karena akan menentukan kesan positif yang akan disampaikan kepada orang lain dan kunjungan kembali dari wisatawan.</p>
-                      <p>Selain itu, nilai indeks dimensi Aman-Tertib-Bersih memiliki nilai yang sedang, yaitu diantara dimensi Kenangan dan dimensi Sejuk-Indah-Ramah. Hal ini berarti tenaga kerja di sektor industri pariwisata sudah menerapkan sadar wisata pada dimensi Aman-Tertib-Bersih akan tetapi masih terdapat kekurangan. Bentuk tindak keamanan seperti sikap tidak mengganggu kenyamanan wisatawan, tidak membuat kegaduhan dan meminimalkan resiko kecelakaan telah dilakukan tenaga kerja di Kabupaten Malang.</p>
+                    <p>
+                      Dari hasil yang diperoleh, mayoritas pekerja dari seluruh jenis industri pariwisata merupakan bukan berstatus kepala rumah tangga (KRT) dengan persentase sebesar 60,97%. Bahkan pekerja yang berstatus KRT tidak lebih dari setengahnya yaitu hanya sebesar 39,03%.
+                    </p>
+                    <p>
+                      Jumlah pekerja terus mengalami peningkatan mulai usia 15 tahun ke atas dan kembali mengalami penurunan pada usia 34 ke atas. Hal tersebut menunjukkan bahwa pada semua jenis industri mayoritas dipenuhi oleh tenaga kerja pada usia produktif dengan kelompok usia hingga 24 tahun memiliki jumlah orang bekerja terbanyak dari keseluruhan jenis industri pariwisata. 
+                    </p>
+                    <p>
+                      Secara umum pekerja di industri pariwisata didominasi oleh orang yang Pernah Kawin sebanyak 73.5%. Dengan pekerja berjenis kelamin laki-laki lebih mendominasi sebanyak 59,69% dibandingkan pekerja berjenis kelamin perempuan sebanyak 40,31%. Hal tersebut terjadi pada beberapa jenis industri antara lain Penyedia jasa angkutan darat untuk penumpang, Penyediaan jasa kebudayaan, olahraga dan rekreasi dan Jenis industri lainnya. Namun pada sektor tertentu seperti penyediaan jasa makan dan minum dan aktivitas perdagangan barang-barang pariwisata lebih didominasi oleh pekerja berjenis kelamin. 
+                    </p>
+                    <p>
+                      Lebih dari separuh pekerja pariwisata di Kabupaten Malang menerima pendapatan dibawah 2,5 juta rupiah setiap bulannya. Sekitar 3 dari 10 pekerja pariwisata di Kabupaten Malang bahkan hanya menerima pendapatan kurang dari 1,5 juta rupiah per bulannya, jauh lebih rendah dari upah minimum Provinsi Jawa Timur. Mayoritas pekerja berpendapatan rendah ini bekerja di industri penyediaan jasa makan dan minum. 
+                    </p>
+                    <p>
+                      Selanjutnya, lebih dari setengah pekerja menyatakan tempat bekerjanya menggunakan internet yaitu sebesar 66,95%. Dari mayoritas industri tersebut, penyediaan jasa makan dan minum menjadi industri dengan tempat bekerja yang menggunakan internet terbanyak yang mencapai 25,53%, lalu disusul oleh industri penyediaan jasa kebudayaan, olahraga, dan rekreasi sebesar 20,21%. Sedangkan persentase paling sedikit adalah tempat bekerja di jenis industri lainnya yaitu hanya 11,49%. 
+                    </p>
+                    <p>
+                      Mayoritas dari seluruh jenis industri pariwisata sudah menggunakan internet dalam pekerjaannya dengan persentase sebesar 66,95%. Dari 66,95% tersebut, 55,85% diantaranya menggunakan internet untuk promosi sedangkan 13,11% tidak menggunakannya untuk promosi. Industri penyediaan jasa makan dan minum dan industri penyediaan jasa kebudayaan, olahraga dan rekreasi merupakan industri pariwisata yang memiliki persentase tertinggi dalam menggunakan internet untuk promosi. Dari tingginya persentase tempat bekerja yang menggunakan internet tersebut, 65,38% diantaranya digunakan untuk komunikasi dan sisanya sebesar 1,57% tidak digunakan untuk komunikasi. Dari 66,95%, 48,29% diantaranya menggunakan internet untuk transaksi dan 18,66% tidak menggunakan internet untuk transaksi. 
+                    </p>
+                    <p>
+                      Secara keseluruhan lebih dari setengah tenaga kerja yang telah bekerja di industri pariwisata tidak ingin menambah pekerjaan. Akan tetapi, industri penyediaan jasa makan dan minum juga merupakan industri pariwisata dengan persentase tenaga kerjanya yang ingin menambah pekerjaan terbanyak yakni sebesar 26,22 %. Sedangkan industri pariwisata dengan persentase tenaga kerjanya yang ingin menambah pekerjaan paling sedikit dan di bawah 10 % adalah industri penyediaan jasa akomodasi bagi wisatawan. 
+                    </p>
+                    <p>
+                      Selanjutnya, 8-9 dari 10 orang yang bekerja di industri pariwisata menjadikan pekerjaan di industri pariwisata sebagai pekerjaan utama. Apabila dilihat dari jenis industri pariwisatanya, mayoritas pekerja di bidang penyediaan jasa makanan dan minuman menjadikan pekerjaannya industri pariwisata sebagai pekerjaan utamanya. 
+                    </p>
+                    <p>
+                      Berdasarkan hasil pendataan, secara keseluruhan lebih dari separuh tenaga kerja telah bekerja di industri pariwisata selama lebih dari tiga tahun. Sekitar satu dari empat dari mereka bekerja di industri Penyediaan Jasa Makan dan Minum. Hanya terdapat sedikit tenaga kerja di industri pariwisata yang bekerja selama kurang dari tiga tahun yang bekerja di industri Penyediaan Jasa Angkutan Darat untuk Penumpang, yaitu 5,8 persen. 
+                    </p>
+                    <p>
+                      Berdasarkan data yang dikumpulkan, hampir semua responden tidak memiliki pekerjaan sebelum melakoni  pekerjaan di industri pariwisata. Hal itu ditunjukkan dengan persentase yang memiliki pekerjaan sebelumnya hanya sebesar 18,38% dari semua responden. Jenis industri pariwisata dari responden yang tidak memiliki pekerjaan sebelumnya tertinggi adalah penyediaan jasa makan dan minum. Selain itu, hanya sedikit dari responden yang memiliki pekerjaan sebelum bekerja di industri pariwisata yaitu pada jenis industri penyediaan jasa angkutan darat untuk penumpang yaitu hanya sebesar 6,2%. Hampir seluruh tenaga kerja tidak memiliki pekerjaan di industri pariwisata sebelumnya. Sementara itu, hanya sebanyak 5,98% yang pernah bekerja di industri pariwisata sebelumnya.   
+                    </p>
+                    <p>
+                      Berdasarkan data yang dikumpulkan, dapat diketahui bahwa jumlah sampel pekerja di industri pariwisata yang memiliki pekerjaan lainnya di samping pekerjaan yang berada pada industri pariwisata lebih dari setengah dari jumlah sampel yaitu sebesar 69,94%. Sedangkan sisanya yaitu sebesar 30,06% memiliki pekerjaan lainnya selain di industri pariwisata. Hal tersebut menunjukkan bahwa pekerja yang bekerja di industri pariwisata cenderung tidak memiliki pekerjaan lain selain di industri pariwisata.	 
+                    </p>
+                    <p>
+                      Banyaknya tenaga kerja laki-laki di industri pariwisata yang memiliki pekerjaan lainnya (Multiple Jobs) adalah sebesar tiga kali banyaknya tenaga kerja perempuan di industri pariwisata yang juga memiliki pekerjaan lainnya (Multiple Jobs). Lebih dari satu per lima tenaga kerja laki-laki di industri pariwisata yang memiliki pekerjaan lainnya (Multiple Jobs) bekerja di jenis industri pariwisata penyediaan jasa kebudayaan, olahraga, dan rekreasi. Sementara itu, sekitar satu per sepuluh dari tenaga kerja laki-laki di Industri pariwisata yang memiliki pekerjaan lainnya (Multiple Jobs) bekerja di jenis industri pariwisata penyediaan jasa akomodasi bagi wisatawan. Lain halnya dengan tenaga kerja perempuan di industri pariwisata, mereka yang memiliki pekerjaan lainnya (Multiple Jobs) hampir separuhnya yaitu 42% bekerja di jenis industri pariwisata penyediaan jasa makan dan minum. Namun, hanya terdapat sedikit tenaga kerja perempuan yang bekerja di jenis industri pariwisata penyedia jasa angkutan darat untuk penumpang yang memiliki pekerjaan lainnya (Multiple Jobs) yaitu hanya sebesar 2% dari jumlah mereka. 
+                    </p>
+                    <p>
+                      Lebih dari ¼ pekerja di sektor pariwisata memiliki pendapatan sampingan lebih besar daripada pendapatan utama. Tiga industri teratas yang memiliki pekerja terbanyak yang memiliki pendapatan sampingan lebih besar daripada pendapatan utama yaitu industri penyediaan jasa makan dan minum dengan 25.42%, lalu diikuti oleh industri penyediaan jasa, kebudayaan, olahraga dan rekreasi dengan 20,34% dan terakhir diikuti oleh penyediaan jasa angkutan darat untuk penumpang dengan nilai 16,95%.  
+                    </p>
+                    <p>
+                      Lebih dari setengah atau sekitar 5-6 dari 10 pekerja dari industri pariwisata menamatkan pendidikan tertingginya SMP atau SMA, yaitu sebesar 52,4%. Dimana untuk pendidikan tertinggi SMP mayoritas bekerja di sektor penyedia jasa makan dan minum, yaitu hampir 40%. Untuk pekerja yang menamatkan pendidikan lebih dari SMA/SMK sangat minim, hanya sekitar 1 dari 10 pekerja saja. Sedangkan untuk pekerja yang belum SD ataupun belum sekolah, hanya sekitar 3%. Hal ini menandakan bahwa hanpir semua pekerja industri pariwisata berpendidikan sekurang-kurangnya SD. 
+                    </p>
+                    <p>
+                      Secara keseluruhan terdapat 16,38 persen pekerja yang pernah mengikuti pelatihan. Dari 16,38 persen diantaranya, jenis industri pariwisata penyedia jasa makan dan minum menjadi yang terbanyak yang sudah pernah mengikuti pelatihan hampir sepertiga atau persen dari semua jenis industri. Artinya pekerja yang pernah mengikuti pelatihan didominasi oleh industri penyedia jasa makan dan minum. Sedangkan jenis industri yang yang tesedikit adalah jenis industri lainya sebesar seperlima atau sebesar 10,43% saja. 
+                    </p>
+                    <p>
+                      Pada semua industri yang dicacah, diketahui bahwa sebagian besar pekerjanya tidak menggunakan bahasa asing di tempat kerja. Secara keseluruhan hanya terdapat 1 dari 5 pekerja atau hanya 20,8 persen pekerja yang menggunakan bahasa asing di tempat kerja. Dari semua industri yang dicacah, hampir sepertiga atau 27,4 persen yang pekerjanya menggunakan bahasa asing berasal dari industri penyedia jasa makan dan minum.  
+                    </p>
+                    <p>
+                      Sebagian besar dari jumlah responden menyatakan bahwa diantara jenis industri pariwisata yang memiliki rumah tidak layak huni sebesar 68,23% dan sisanya rumah layak huni. Industri penyediaan jasa makan dan minum menjadi penyumbang persentase terbesar pada persentase rumah layak huni yaitu sekitar sepertiga dari jenis industri pariwisata lainnya. 
+                    </p>
                   </div>
                 </div>
               </div>
@@ -104,7 +151,7 @@
   </div>
 </section>
 
-<!-- Istope -->
+<!-- Isotope -->
 <script src="https://unpkg.com/isotope-layout@3/dist/isotope.pkgd.js"></script>
 <!-- Leaflet JS -->
 <script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js" integrity="sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM=" crossorigin=""></script>
