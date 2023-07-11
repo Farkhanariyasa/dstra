7539c70512844fbea3efa7edd03ee88214a0d6ed
imams <222011830@gmail.com>
2023-07-11 22:58:57 +0700

    M View :header pemilik

--- app/Views/pvd/pages/dasbor/riset4/karakteristik_umum_unit_usaha/pemilik_atau_pengelola_usaha_pariwisata.php
+++ app/Views/pvd/pages/dasbor/riset4/karakteristik_umum_unit_usaha/pemilik_atau_pengelola_usaha_pariwisata.php
@@ -3,8 +3,7 @@
     <section id="portfolio" class="portfolio section-bg">
       <div class="container">
         <header class="section-header">
-          <h6 style="color: #4b395f;"><strong>Kesiapan Pemanfaatan TIK Unit Usaha Pariwisata Skala Mikro dan Kecil di Kota Batu dalam Menerapkan
-              <i>Smart Tourism Destination</i></strong>
+          <h6 style="color: #4b395f;"><strong>Pemilik atau Pengelola Unit Usaha Pariwisata (UUP)</strong>
           </h6>
         </header>
         <hr class="hrjudul">
@@ -37,81 +36,12 @@
             }
           </style>
 
-          <!-- style untuk gantt chart -->
-          <style>
-            /* * {
-              margin: 0;
-              padding: 0;
-              font-family: sans-serif;
-            } */
-            /* .chartMenu {
-              width: 100vw;
-              height: 40px;
-              background: #1A1A1A;
-              color: rgba(54, 162, 235, 1);
-            }
-            .chartMenu p {
-              padding: 10px;
-              font-size: 20px;
-            }
-            .chartCard {
-              width: 100vw;
-              height: calc(100vh - 40px);
-              background: rgba(54, 162, 235, 0.2);
-              display: flex;
-              align-items: center;
-              justify-content: center;
-            }
-            .chartBox {
-              width: 700px;
-              padding: 20px;
-              border-radius: 20px;
-              border: solid 3px rgba(54, 162, 235, 1);
-              background: white;
-            } */
-          </style>
-
           <!-- Charts and Interpretations -->
           <div class="portfolio-item filter-riset4-semua" onresize="responsivefonts()">
               <div class="isi-tujuan shadow mt-0 mb-0">
                   <h4 class="card-title judul-card">Statistik Pemilik atau Pengelola Usaha Pariwisata di Kota Batu</h4>
                   <div class="card-body">
                       <div class="row">
-                          <!-- Part 1 -->
-                          <!-- <div class="col-12 grid-margin stretch-card">
-                              <div class="card shadow">
-                                  <div class="pb-2 card-body">
-                                      <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center ">
-                                          <div class="me-1 mt-1 justify-content-end align-item-end">
-                                              <button id="unduh-m2sm1-1" type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal-m2sm1-1">
-                                                  <i class="fa-solid fa-download"></i>
-                                              </button>
-                                          </div>
-                                      </div>
-                                      <div class="row">
-                                          <div class="mb-0 col-lg-7 grid-margin">
-                                              <div>
-                                                  <div class ="chartBox" style=" height:350px;"> 
-                                                      <canvas id="chart-1"></canvas>
-                                                  </div>
-                                              </div>
-                                          </div>
-                                          <div class="mb-0 col-lg-5 grid-margin stretch-card">
-                                              <div>
-                                                  <h4>Dodge Pemilik atau pengelola unit usaha pariwisata berdasarkan jenis kelamin dan usia</h4>
-                                                  <p class="text-justify">
-                                                      Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.
-                                                      Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.
-                                                      Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.
-                                                      Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.
-                                                  </p>
-                                              </div>
-                                          </div>
-                                      </div>
-                                  </div>
-                              </div>
-                          </div> -->
-
                           <div class="col-12 grid-margin stretch-card">
                             <div class="card shadow">
                                 <div class="pb-2 card-body">
@@ -148,41 +78,6 @@
                                 </div>
                             </div>
                         </div>
-
-                          <!-- Part 2 -->
-                          <!-- <div class="col-12 grid-margin stretch-card">
-                              <div class="card shadow">
-                                  <div class="pb-2 card-body">
-                                      <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center ">
-                                          <div class="me-1 mt-1 justify-content-end align-item-end">
-                                              <button id="unduh-m2sm1-2" type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal-m2sm1-2">
-                                                  <i class="fa-solid fa-download"></i>
-                                              </button>
-                                          </div>
-                                      </div>
-                                      <div class="row">
-                                          <div class="mb-0 col-lg-7 grid-margin">
-                                              <div>
-                                                  <div class ="chartBox" style=" height:350px;"> 
-                                                      <canvas id="chart-2"></canvas>
-                                                  </div>
-                                              </div>
-                                          </div>
-                                          <div class="mb-0 col-lg-5 grid-margin stretch-card">
-                                              <div>
-                                                  <h4>Dodge Pemilik atau pengelola unit usaha pariwisata berdasarkan jenis kelamin dan tingkat pendidikan.</h4>
-                                                  <p class="text-justify">
-                                                      Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.
-                                                      Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.
-                                                      Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.
-                                                      Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.
-                                                  </p>
-                                              </div>
-                                          </div>
-                                      </div>
-                                  </div>
-                              </div>
-                          </div> -->
 
                           <div class="col-12 grid-margin stretch-card">
                             <div class="card shadow">
