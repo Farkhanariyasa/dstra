064a82d8953c609965c3fc7e27f8961e31572450
Thomson <222011365@stis.ac.id>
2023-07-12 16:07:50 +0700

    M Views: perubahan nama algoritma dan penambahan viz

--- app/Views/pvd/pages/dasbor/riset1/family_grouping/visualisasi.php
+++ app/Views/pvd/pages/dasbor/riset1/family_grouping/visualisasi.php
@@ -30,7 +30,7 @@
 </style>
 
 <section>
-<script>
+<!-- <script>
         window.addEventListener('DOMContentLoaded', (event) => {
             Swal.fire({
                 title: 'Informasi Penting',
@@ -56,7 +56,7 @@
 
 
         });
-    </script>
+    </script> -->
 </section>
 
 <section id="portfolio" class="portfolio section-bg">
@@ -69,13 +69,13 @@
             <div class="col-lg-12 d-flex justify-content-center">
                 <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist" style="color:#506396">
                     <li class="nav-item" role="presentation">
-                        <button class="nav-link active" id="pills-algoritma1-tab" data-bs-toggle="pill" data-bs-target="#pills-algoritma1" type="button" role="tab" aria-controls="pills-algoritma1" aria-selected="true">Jarak Stay Point</button>
+                        <button class="nav-link active" id="pills-algoritma1-tab" data-bs-toggle="pill" data-bs-target="#pills-algoritma1" type="button" role="tab" aria-controls="pills-algoritma1" aria-selected="true">Algoritma Haversine</button>
                     </li>
                     <li class="nav-item" role="presentation">
                         <button class="nav-link" id="pills-algoritma2-tab" data-bs-toggle="pill" data-bs-target="#pills-algoritma2" type="button" role="tab" aria-controls="pills-algoritma2" aria-selected="false">Algoritma H3</button>
                     </li>
                     <li class="nav-item" role="presentation">
-                        <button class="nav-link" id="pills-algoritma3-tab" data-bs-toggle="pill" data-bs-target="#pills-algoritma3" type="button" role="tab" aria-controls="pills-algoritma3" aria-selected="false">DBSCAN Clustering & Pembentukan Matrix Distance</button>
+                        <button class="nav-link" id="pills-algoritma3-tab" data-bs-toggle="pill" data-bs-target="#pills-algoritma3" type="button" role="tab" aria-controls="pills-algoritma3" aria-selected="false">Algoritma Dynamic Time Warping dan Frechet Distance</button>
                     </li>
                 </ul>
             </div>
@@ -102,37 +102,19 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/chartjs-plugin-datalabels/2.2.0/chartjs-plugin-datalabels.min.js" integrity="sha512-JPcRR8yFa8mmCsfrw4TNte1ZvF1e3+1SdGMslZvmrzDYxS69J7J49vkFL8u6u8PlPJK+H3voElBtUCzaXj+6ig==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <!-- Chart JS Plugin Sankey Chart -->
     <script src="https://cdn.jsdelivr.net/npm/chartjs-chart-sankey"></script>
-    <!-- Impor Grafik -->
-    <script src="<?= base_url('pvd/js/riset1/hasilSPRiset1_ja_pie.js') ?>"></script>
-    <script src="<?= base_url('pvd/js/riset1/hasilSPRiset1_sankey.js') ?>"></script>
-    <script src="<?= base_url('pvd/js/riset1/btn_Download.js') ?>"></script>
 
-    <!-- FG Algoritma 1 -->
-    <script src="<?= base_url('pvd/js/riset1/evaluasiFG_algoritma1_1.js') ?>"></script>
-    <script src="<?= base_url('pvd/js/riset1/evaluasiFG_algoritma1_2.js') ?>"></script>
-    <script src="<?= base_url('pvd/js/riset1/evaluasiFG_algoritma1_3.js') ?>"></script>
-    <script src="<?= base_url('pvd/js/riset1/evaluasiFG_algoritma1_4.js') ?>"></script> 
-
-    <!-- FG Algoritma 2 -->
-    <script src="<?= base_url('pvd/js/riset1/evaluasiFG_algoritma2_1.js') ?>"></script>
-    <script src="<?= base_url('pvd/js/riset1/evaluasiFG_algoritma2_2.js') ?>"></script>
-    <script src="<?= base_url('pvd/js/riset1/evaluasiFG_algoritma2_3.js') ?>"></script>
-    <script src="<?= base_url('pvd/js/riset1/evaluasiFG_algoritma2_4.js') ?>"></script>
-    <script src="<?= base_url('pvd/js/riset1/evaluasiFG_algoritma2_5.js') ?>"></script>
-    <script src="<?= base_url('pvd/js/riset1/evaluasiFG_algoritma2_6.js') ?>"></script>
-    <script src="<?= base_url('pvd/js/riset1/evaluasiFG_algoritma2_7.js') ?>"></script>
-    <script src="<?= base_url('pvd/js/riset1/evaluasiFG_algoritma2_8.js') ?>"></script>
- 
-    <!-- FG Algoritma 3 -->
-    <script src="<?= base_url('pvd/js/riset1/evaluasiFG_algoritma3_1.js') ?>"></script>
-    <script src="<?= base_url('pvd/js/riset1/evaluasiFG_algoritma3_2.js') ?>"></script>
-    <script src="<?= base_url('pvd/js/riset1/evaluasiFG_algoritma3_3.js') ?>"></script>
-    <script src="<?= base_url('pvd/js/riset1/evaluasiFG_algoritma3_4.js') ?>"></script>
-    <script src="<?= base_url('pvd/js/riset1/evaluasiFG_algoritma3_5.js') ?>"></script>
-    <script src="<?= base_url('pvd/js/riset1/evaluasiFG_algoritma3_6.js') ?>"></script>
-    <script src="<?= base_url('pvd/js/riset1/evaluasiFG_algoritma3_7.js') ?>"></script>
-    <script src="<?= base_url('pvd/js/riset1/evaluasiFG_algoritma3_8.js') ?>"></script>
     
+    <!-- Algoritma FINAL-->
+    <script src="<?= base_url('pvd/js/riset1/hasilFG_output1.js') ?>"></script>
+    <script src="<?= base_url('pvd/js/riset1/hasilFG_output2.js') ?>"></script>
+    <script src="<?= base_url('pvd/js/riset1/hasilFG_output3.js') ?>"></script>
+    <script src="<?= base_url('pvd/js/riset1/hasilFG_output4.js') ?>"></script>
+    <script src="<?= base_url('pvd/js/riset1/hasilFG_output5.js') ?>"></script>
+    <script src="<?= base_url('pvd/js/riset1/hasilFG_output6.js') ?>"></script>
+    <script src="<?= base_url('pvd/js/riset1/hasilFG_output7.js') ?>"></script>
+    <script src="<?= base_url('pvd/js/riset1/hasilFG_output8.js') ?>"></script>
+    <script src="<?= base_url('pvd/js/riset1/hasilFG_output9.js') ?>"></script>
+    <script src="<?= base_url('pvd/js/riset1/hasilFG_output10.js') ?>"></script>
 
     <!-- Latex Math -->
     <script type="text/javascript" id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js"></script>
