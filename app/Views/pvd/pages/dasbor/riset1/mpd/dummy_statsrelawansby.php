2016a43f2a8f8ef805aa48b3a27a4803b0db599d
main <222011356@stis.ac.id>
2023-07-13 16:44:03 +0800

    M Views: relawan output 5

--- app/Views/pvd/pages/dasbor/riset1/mpd/dummy_statsrelawansby.php
+++ app/Views/pvd/pages/dasbor/riset1/mpd/dummy_statsrelawansby.php
@@ -184,6 +184,70 @@
             <h3 class="card-title judul-card">Kepler</h3>
             <div class="card-body">
                 <div class="row">
+                    <div class="col-lg-8 grid-margin stretch-card h-100">
+                        <div class="card shadow rounded-4">
+                            <div class="card-body">
+                                <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center ">
+                                    <div class="me-1 mt-1 justify-content-end align-item-end">
+                                        <button id="relawan-4" type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal4">
+                                            <i class="fa-solid fa-download"></i>
+                                        </button>
+                                    </div>
+                                </div>
+                                <div class="row-6 d-flex align-items-center justify-content-center"style="height:440px">
+                                    <img src="<?= base_url('pvd/img/Riset1/relawanAsset.png') ?>"
+                                                        class="img-fluid" alt="" style="height: 60vh; width:auto;">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 grid-margin stretch-card h-100" style="margin-top: -1rem;">
+                        <!-- <div class="card bg-secondary"> -->
+                        <div class="card-body">
+                            <div class="card shadow border rounded-4" style="padding: 1.5rem 0rem;">
+                                <h3 class="text-center"><b>Interpretasi</b></h3>
+                                <div class="card-body border rounded">
+                                    <!-- <canvas class="mt1" id="riset1_hasil5"></canvas> -->
+                                    <p class="text-justify">Berdasarkan diagram di atas, terdapat sebanyak 167 relawan yang bersedia data MPD-nya digunakan dalam penelitian mengenai fenomena family grouping dan sebanyak 23 relawan yang bersedia data MPD-nya digunakan dalam penelitian mengenai permasalahan double counting. Selain itu, terdapat sebanyak 32 relawan yang bersedia data MPD-nya digunakan dalam penelitian mengenai keduanya, yakni fenomena family grouping dan permasalahan double counting. </p>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- </div> -->
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-8 grid-margin stretch-card h-100">
+                        <div class="card shadow rounded-4">
+                            <div class="card-body">
+                                <div class="position-absolute top-0  end-0 d-flex flex-row justify-content-center align-item-center ">
+                                    <div class="me-1 mt-1 justify-content-end align-item-end">
+                                        <button id="relawan-4" type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal4">
+                                            <i class="fa-solid fa-download"></i>
+                                        </button>
+                                    </div>
+                                </div>
+                                <div class="row-6 d-flex align-items-center justify-content-center"style="height:440px">
+                                    <img src="<?= base_url('pvd/img/Riset1/nomorAsset.png') ?>"
+                                                        class="img-fluid" alt="" style="height: 60vh; width:auto;">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 grid-margin stretch-card h-100" style="margin-top: -1rem;">
+                        <!-- <div class="card bg-secondary"> -->
+                        <div class="card-body">
+                            <div class="card shadow border rounded-4" style="padding: 1.5rem 0rem;">
+                                <h3 class="text-center"><b>Interpretasi</b></h3>
+                                <div class="card-body border rounded">
+                                    <!-- <canvas class="mt1" id="riset1_hasil5"></canvas> -->
+                                    <p class="text-justify">Dari jumlah relawan yang telah disebutkan sebelumnya, dihasilkan sebanyak 167 nomor yang digunakan untuk penelitian fenomena family grouping dan sebanyak 46 nomor yang digunakan untuk penelitian permasalahan double counting. Selain itu, terdapat sebanyak 64 nomor yang digunakan untuk meneliti keduanya, yaitu fenomena family grouping dan permasalahan double counting. </p>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- </div> -->
+                    </div>
+                </div>
+                <!-- <div class="row">
                     <div class="col-lg-12 grid-margin stretch-card h-100">
                         <div class="card shadow rounded-4">
                         <h3 class="text-center mt-4" style="font-weight:bold">Jumlah Relawan</h3>
@@ -206,7 +270,7 @@
                             </div>
                         </div>
                     </div>
-                    <!-- <div class="col-lg-12 grid-margin stretch-card h-100">
+                    <div class="col-lg-12 grid-margin stretch-card h-100">
                         <div class="card shadow rounded-4">
                         <h3 class="text-center mt-4" style="font-weight:bold">Persebaran Telkomsel - Indosat</h3>
                             <div class="card-body">
@@ -218,8 +282,8 @@
                                 </div>
                             </div>
                         </div>
-                    </div> -->
-                </div>
+                    </div>
+                </div> -->
             </div>
         </div>
     </div>
