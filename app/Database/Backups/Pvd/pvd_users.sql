bc141bfb824eb61b05969723555e23ac1691edd5
I Putu Agus Wahyu Dupayana <75474419+putuwahyu29@users.noreply.github.com>
2023-07-14 18:15:28 +0800

    M Database : Backup Database

---
+++ app/Database/Backups/Pvd/pvd_users.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Jul 13, 2023 at 02:04 PM
+-- Server version: 8.0.30
+-- PHP Version: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pkl62`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pvd_users`
+--
+
+CREATE TABLE `pvd_users` (
+  `id` int UNSIGNED NOT NULL,
+  `platform_id` varchar(25) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `nama_lengkap` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `picture` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `pvd_users`
+--
+
+INSERT INTO `pvd_users` (`id`, `platform_id`, `username`, `nama_lengkap`, `email`, `picture`) VALUES
+(1, '114056809839607846541', '222011460', 'I Putu Agus Wahyu D.', '222011460@stis.ac.id', 'https://lh3.googleusercontent.com/a/AAcHTteOL8R6-KBGVnGCy5shFwmDe54Mn1arLtCEd8qVfDoKF5A=s96-c');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `pvd_users`
+--
+ALTER TABLE `pvd_users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `pvd_users`
+--
+ALTER TABLE `pvd_users`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
