1ac93d85a355359431b5bd9a11eeafd49868be47
I Putu Agus Wahyu Dupayana <75474419+putuwahyu29@users.noreply.github.com>
2023-07-14 22:42:31 +0800

    M View : Auth

--- app/Views/pvd/pages/auth/index.php
+++ app/Views/pvd/pages/auth/index.php
@@ -21,7 +21,7 @@
   <div class="container-scroller ">
     <div class="container-fluid page-body-wrapper full-page-wrapper">
       <div class="content-wrapper d-flex align-items-center auth forbody">
-        <div class="m-auto p-5 flex-grow row auth-form-light">
+        <div class="m-auto p-3 flex-grow row auth-form-light">
           <div class="col-lg-7 text-center my-5">
             <div class="brand-logo">
               <a href="<?= base_url('hasil-pkl'); ?>"><img src="<?= base_url('pvd/img/logo_pkl.png'); ?>" alt="logo" /></a>
@@ -34,15 +34,17 @@
             <h4 class="font-weight-light mt-4">Masuk untuk melanjutkan.</h4>
             <div class="mt-4">
               <a href="<?= base_url('hasil-pkl/loginstis'); ?>" class="btn btn-block btn-login auth-form-btn mb-2">
-                <img src="<?= base_url('pvd/img/landingpage/logostis.png') ?>" class="logo-login" alt=""> Masuk dengan Google STIS </a>
+                <img src="<?= base_url('pvd/img/landingpage/logostis.png') ?>" class="logo-login me-2" alt=""> Google STIS </a>
               <a href="<?= base_url('hasil-pkl/loginbps'); ?>" class="btn btn-block btn-login auth-form-btn mb-2">
-                <img src="<?= base_url('pvd/img/landingpage/logobps.png') ?>" class="logo-login" alt=""> Masuk dengan Google BPS </a>
+                <img src="<?= base_url('pvd/img/landingpage/logobps.png') ?>" class="logo-login me-2" alt=""> Google BPS </a>
             </div>
           </div>
-          <div class="col-lg-3 text-center my-5">
+          <div class="col-lg-4 text-center my-5">
+            <h3>Bersinergi dengan Data, Majukan Pariwisata Nusantara </h3>
             <div class="brand-maskot">
               <img class="img-fluid" src="<?= base_url('pvd/img/maskot_pkl.png') ?>" alt="">
             </div>
+
           </div>
         </div>
       </div>
