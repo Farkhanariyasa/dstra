a919f8ed8595d5645c15d498221f1997a6c009c1
Farkhanariyasa <90878131+Farkhanariyasa@users.noreply.github.com>
2023-07-18 18:13:34 +0700

    M View : menambahkan logo form login manual

--- app/Views/pvd/pages/auth/login_manual.php
+++ app/Views/pvd/pages/auth/login_manual.php
@@ -18,10 +18,14 @@
 <body>
     <div class="container-scroller">
         <div class="container-fluid page-body-wrapper full-page-wrapper">
-            <div class="content-wrapper d-flex align-items-center auth">
+            <div class="content-wrapper d-flex align-items-center auth forbody">
                 <div class="row flex-grow">
-                    <div class="col-lg-4 mx-auto">
-                        <div class="auth-form-light text-left p-5">
+                    <div class="col-lg-4 mx-auto text-center">
+                        <div class="auth-form-light text-left p-5" style="background-color: white;">
+                            <div class="brand-logo">
+                                <a href="<?= base_url('hasil-pkl'); ?>"><img src="<?= base_url('pvd/img/logo_pkl.png'); ?>" alt="logo" /></a>
+                            </div>
+                            <h1>Web Hasil PKL 62 <br />T.A. 2022/2023 </h1>
                             <h4>Masuk dengan Akun Khusus</h4>
                             <?php if (session()->getFlashdata('gagal')) : ?>
                                 <div class="alert alert-danger" role="alert">
@@ -35,9 +39,12 @@
                                 <div class="form-group">
                                     <input type="password" class="form-control form-control-lg" name="password" placeholder="Kata Sandi">
                                 </div>
-                                <div class="mt-3">
+                                <div class="mt-3" style="margin-bottom: 10px;">
                                     <input type="submit" class="btn btn-block btn-login btn-lg auth-form-btn" name="submit" value="Masuk" />
                                 </div>
+                                <a href="<?= base_url('hasil-pkl/masuk'); ?>" class="text-decoration-none">
+                                    <h4 style="color: #4b395f; font-size:medium"> Kembali</h4>
+                                </a>
                             </form>
                         </div>
                     </div>
