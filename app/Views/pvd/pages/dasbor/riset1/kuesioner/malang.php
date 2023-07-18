254c8b6548e3a5145d257d24d67de9274e89961a
Thomson <222011365@stis.ac.id>
2023-07-18 15:29:13 +0700

    M Views: menghapus comment kuesioner

--- app/Views/pvd/pages/dasbor/riset1/kuesioner/malang.php
+++ app/Views/pvd/pages/dasbor/riset1/kuesioner/malang.php
@@ -35,25 +35,6 @@
     </header>
     <hr class="hrjudul">
 
-    <!-- <h2>Kuesioner Listing</h2>
-    <div class="tab-pane active p-2" id="tab1" role="tabpanel" aria-labelledby="tab-1" style="height: auto;">
-        <div class="row">
-            <div style="width: 100%;">
-                <embed src="https://drive.google.com/file/d/1jfvZHFY-ovdka5aRW413RVrplv6zWD0u/preview" type="application/pdf" width=100% height="600px" />
-            </div>
-        </div>
-    </div>
-
-
-    <h2>Kuesioner Pencacahan</h2>
-    <div class="tab-pane active p-2" id="tab1" role="tabpanel" aria-labelledby="tab-1" style="height: auto;">
-        <div class="row">
-            <div style="width: 100%;">
-                <embed src="https://drive.google.com/file/d/1R0pWJ2IbRJr9GJsCkdRzKq_FzCkRN3ox/preview" type="application/pdf" width=100% height="600px" />
-            </div>
-        </div>
-    </div> -->
-
     <div class="third5 par m-auto p-3 p-md-5 shadow box rounded justify-content-center" style="line-height: 30px;" data-aos="zoom-in" data-aos-duration="1000">
         <h2 class="text-center" style="font-weight: bold;">Kuesioner Listing</h2>
         <div class="tab-pane active p-2" id="tab1" role="tabpanel" aria-labelledby="tab-1" style="height: auto; width: 100%">
