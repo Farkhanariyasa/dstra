ad4ee652f6b215144fa612022cf48518ef6e8df4
Thomson <222011365@stis.ac.id>
2023-07-18 09:38:32 +0700

    M Views: js btn download fg

--- app/Views/pvd/pages/dasbor/riset1/family_grouping/visualisasi.php
+++ app/Views/pvd/pages/dasbor/riset1/family_grouping/visualisasi.php
@@ -124,7 +124,8 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.1.0/dist/sweetalert2.min.css">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.1.0/dist/sweetalert2.min.js"></script>
 
-
+    <!-- Fungsi Unduh Chart -->
+    <script src="<?= base_url('pvd/js/riset1/btn_Download_fg.js') ?>"></script>
 </section>
 
 <?= $this->endSection(); ?>
