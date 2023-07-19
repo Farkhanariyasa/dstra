7ce4d66332e7f359a066bf39a125658ff9e71bd4
putriramatillah <212011283@stis.ac.id>
2023-07-19 18:47:27 +0700

    M View: Hapus peta lokus

--- app/Views/pvd/pages/dasbor/riset3/dummy_riset3.php
+++ app/Views/pvd/pages/dasbor/riset3/dummy_riset3.php
@@ -244,55 +244,6 @@
     </div>
 </div>
 
-<!-- peta lokus -->
-<!-- <div class=" portfolio-item filter-riset3-peta" onresize="responsivefonts()">
-    <div class="row">
-        <div class="col-lg-12 grid-margin stretch-card">
-            <div class="card shadow">
-                <div class="card-body">
-                    <h4 class="card-title text-center">Peta Kabupaten Malang</h4>
-                    <div class="d-flex flex-row align-item-center justify-content-center">
-                        <div class="position-absolute top-0 end-0 d-flex flex-row justify-content-center align-item-center">
-                            <div class="me-1 mt-1 justify-content-end align-item-end">
-                                <button type="button" class="tombol btn-for" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                                    <i class="fa-solid fa-download"></i>
-                                </button>
-                            </div>
-                        </div>
-                        <div id="map" style="width: 95%; height:80vh;"></div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div> -->
-
-<!-- Tabel dummy
-<div class="portfolio-item filter-riset3-tabeldummy">
-    <div class="">
-        <table id="example" class="table">
-            <thead>
-                <tr>
-                    <th scope="col">No</th>
-                    <th scope="col">Nama</th>
-                    <th scope="col">Jenis Kelamin</th>
-                    <th scope="col">Jurusan</th>
-                    <th scope="col">Tingkat</th>
-                </tr>
-            </thead>
-            <tbody>
-                    <tr>
-                        <th></th>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-            </tbody>
-        </table>
-    </div><br><br>
-    <div id="map" class="mt-4"></div>            
-</div> -->
 
 
 <!-- Modal Riset 3  -->
