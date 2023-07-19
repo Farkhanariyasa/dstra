8617ba888f53b0d504c957f986d6ea4c934fd95c
putriramatillah <212011283@stis.ac.id>
2023-07-19 20:51:41 +0700

    D Assets: menghapus 2 chart hasil sp riset 3

--- public/pvd/js/riset3/hasilSPRiset3_peta.js
+++
@@ -1,8 +0,0 @@
-var map = L.map("map").setView([-7.983908, 112.621391], 15);
-
-L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  maxZoom: 19,
-  minZoom: 12,
-  attribution:
-    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-}).addTo(map);
