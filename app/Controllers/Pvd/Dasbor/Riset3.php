148413015361290f9c95318626d5907756044dfc
prasetyofmalik <222011325@stis.ac.id>
2023-07-23 21:34:37 +0700

    M Controller: hapus model sp di controller Riset3

--- app/Controllers/Pvd/Dasbor/Riset3.php
+++ app/Controllers/Pvd/Dasbor/Riset3.php
@@ -3,7 +3,6 @@
 namespace App\Controllers\Pvd\Dasbor;
 
 use App\Controllers\BaseController;
-use App\Models\Pvd\Riset3HasilSpModel;
 use App\Models\Pvd\Riset3HasilPklModel;
 use App\Models\Pvd\UnduhHasilPklModel;
 
@@ -21,11 +20,6 @@ class Riset3 extends BaseController
 
     public function __construct()
     {
-        $this->jenisindustri = new Riset3HasilSpModel();
-        $this->pendidikantertinggi = new Riset3HasilSpModel();
-        $this->jeniskelamin_riset3 = new Riset3HasilSpModel();
-        $this->pendapatan_riset3 = new Riset3HasilSpModel();
-
         $this->HasilPKL = new Riset3HasilPklModel();
         $this->tujuan1_5 = new Riset3HasilPklModel();
         $this->datapengunduh = new UnduhHasilPklModel();
@@ -38,55 +32,6 @@ class Riset3 extends BaseController
             'menu' => $menu['riset3'],
         ];
         return view('pvd/pages/dasbor/riset3/index', $data);
-    }
-    public function tempviz()
-    {
-        // jenis usaha
-        $ji = [
-            'ji1' => $this->jenisindustri->getByJenisIndustri("1"),
-            'ji2' => $this->jenisindustri->getByJenisIndustri("2"),
-            //'ji3' => $this->jenisindustri->getByJenisIndustri("3"),
-            'ji4' => $this->jenisindustri->getByJenisIndustri("4"),
-            //'ji5' => $this->jenisindustri->getByJenisIndustri("5"),
-            //'ji6' => $this->jenisindustri->getByJenisIndustri("6"),
-            //'ji7' => $this->jenisindustri->getByJenisIndustri("7"),
-            //'ji8' => $this->jenisindustri->getByJenisIndustri("8"),
-            'ji9' => $this->jenisindustri->getByJenisIndustri("9"),
-            'ji10' => $this->jenisindustri->getByJenisIndustri("10"),
-            'ji11' => $this->jenisindustri->getByJenisIndustri("11", "3", "5", "6", "7", "8")
-        ];
-        // pendidikan tertinggi
-        $pt = [
-            'pt1' => $this->pendidikantertinggi->getByPendidikanTertinggi("1"),
-            'pt2' => $this->pendidikantertinggi->getByPendidikanTertinggi("2"),
-            'pt3' => $this->pendidikantertinggi->getByPendidikanTertinggi("3"),
-            'pt4' => $this->pendidikantertinggi->getByPendidikanTertinggi("4"),
-            'pt5' => $this->pendidikantertinggi->getByPendidikanTertinggi("5"),
-            'pt6' => $this->pendidikantertinggi->getByPendidikanTertinggi("6"),
-            'pt7' => $this->pendidikantertinggi->getByPendidikanTertinggi("7"),
-            'pt8' => $this->pendidikantertinggi->getByPendidikanTertinggi("8"),
-            'pt9' => $this->pendidikantertinggi->getByPendidikanTertinggi("9"),
-        ];
-        // status pekerja
-        $jk_1 = [
-            'jk_11' => $this->jeniskelamin_riset3->getByJenisKelamin_Riset3("1"),
-            'jk_12' => $this->jeniskelamin_riset3->getByJenisKelamin_Riset3("2")
-        ];
-        $pd_1 = $this->pendapatan_riset3->getPendapatan_Riset3();
-        $menu = getMenu();
-        $data = [
-            'judul' => 'Dasbor Riset 3',
-            'menu' => $menu['riset3'],
-            'ji' => $ji,
-            'pt' => $pt,
-            'jk_1' => $jk_1,
-            'pd_1' => $pd_1
-        ];
-        return view('pvd/pages/dasbor/riset3/tempviz', $data);
-    }
-
-    public function tujuan1()
-    {
     }
 
     // menu Hasil Kajian
@@ -172,13 +117,11 @@ class Riset3 extends BaseController
 
     public function unduhtujuan1()
     {
-
         $menu = getMenu();
         $data = [
             'judul' => 'Hasil Kajian Tujuan 1',
             'menu' => $menu['riset3'],
         ];
-
 
         $data1 = [
             'email' => $this->request->getVar('email'),
@@ -215,7 +158,6 @@ class Riset3 extends BaseController
             'menu' => $menu['riset3'],
             'data_2_1' => $data_2_1,
             'data_2_2' => $data_2_2,
-
         ];
 
         $data2 = [
@@ -229,6 +171,11 @@ class Riset3 extends BaseController
     }
     public function unduhtujuan3()
     {
+        $menu = getMenu();
+        $data = [
+            'judul' => 'Hasil Kajian Tujuan 3',
+            'menu' => $menu['riset3'],
+        ];
 
         $data3 = [
             'email' => $this->request->getVar('email'),
@@ -237,16 +184,16 @@ class Riset3 extends BaseController
         ];
         $this->datapengunduh->insert($data3);
 
-        $menu = getMenu();
-        $data = [
-            'judul' => 'Hasil Kajian Tujuan 3',
-            'menu' => $menu['riset3'],
-        ];
         return view('pvd/pages/dasbor/riset3/hasilkajian/tujuan3', $data);
     }
 
     public function unduhtujuan4()
     {
+        $menu = getMenu();
+        $data = [
+            'judul' => 'Hasil Kajian 4',
+            'menu' => $menu['riset3'],
+        ];
 
         $data4 = [
             'email' => $this->request->getVar('email'),
@@ -254,11 +201,7 @@ class Riset3 extends BaseController
             'instansi' => $this->request->getVar('instansi')
         ];
         $this->datapengunduh->insert($data4);
-        $menu = getMenu();
-        $data = [
-            'judul' => 'Hasil Kajian 4',
-            'menu' => $menu['riset3'],
-        ];
+        
         return view('pvd/pages/dasbor/riset3/hasilkajian/tujuan4', $data);
     }
 }
