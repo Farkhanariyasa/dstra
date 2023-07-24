6a675bfac8d83dad39ed26e47c8f54ef9a425b91
NickoRagasukma <gabrielprimaditya@gmail.com>
2023-07-24 19:23:34 +0700

    M Views : Hapus komen riset 2

--- app/Views/pvd/pages/dasbor/riset2/tujuan3/dummy_hasil2.php
+++ app/Views/pvd/pages/dasbor/riset2/tujuan3/dummy_hasil2.php
@@ -1,4 +1,3 @@
-<!-- Visualisasi 2 -->
 <div class="portfolio-item filter-riset2-hasilkajian3-2">
     <div class="row t3h2 mt-3 tujuan3hasil2">
         <div class="col-sm-8 my-2 grid-margin stretch-card">
@@ -12,7 +11,7 @@
                         </div>
                     </div>
 
-                    <div style=" height:500px;">
+                    <div style="height:500px;">
                         <canvas class="mt1" id="riset2_t3_hasil1"></canvas>
                     </div>
                 </div>
@@ -38,60 +37,9 @@
                     </select>
                 </form>
             </div>
-
-            <!-- <div class="row">
-                <div class="col-lg-12 d-flex justify-content-center">
-                    <ul class=" nav-pills mb-3 flex-column" id="pills-tab" role="tablist" style="color:#506396">
-                        <li class="nav-item" role="presentation">
-                            <button class="nav-link active" id="pills-bepergian-tab" data-bs-toggle="pill" data-bs-target="#pills-bepergian" type="button" role="tab" aria-controls="pills-bepergian" aria-selected="true">Maksud Utama</button>
-                        </li>
-                        <li class="nav-item" role="presentation">
-                            <button class="nav-link" id="pills-kunjungan-tab" data-bs-toggle="pill" data-bs-target="#pills-kunjungan" type="button" role="tab" aria-controls="pills-kunjungan" aria-selected="false">Akomodasi Utama</button>
-                        </li>
-                        <li class="nav-item" role="presentation">
-                            <button class="nav-link" id="pills-kunjungan-tab" data-bs-toggle="pill" data-bs-target="#pills-kunjungan" type="button" role="tab" aria-controls="pills-kunjungan" aria-selected="false">Angkutan Utama</button>
-                        </li>
-                        <li class="nav-item" role="presentation">
-                            <button class="nav-link" id="pills-kunjungan-tab" data-bs-toggle="pill" data-bs-target="#pills-kunjungan" type="button" role="tab" aria-controls="pills-kunjungan" aria-selected="false">Jenis Kegiatan Wisata</button>
-                        </li>
-                        <li class="nav-item" role="presentation">
-                            <button class="nav-link" id="pills-kunjungan-tab" data-bs-toggle="pill" data-bs-target="#pills-kunjungan" type="button" role="tab" aria-controls="pills-kunjungan" aria-selected="false">Media Informasi Utama</button>
-                        </li>
-                        <li class="nav-item" role="presentation">
-                            <button class="nav-link" id="pills-kunjungan-tab" data-bs-toggle="pill" data-bs-target="#pills-kunjungan" type="button" role="tab" aria-controls="pills-kunjungan" aria-selected="false">Rata-rata Biaya yang Dikeluarkan Turis</button>
-                        </li>
-                        <li class="nav-item" role="presentation">
-                            <button class="nav-link" id="pills-kunjungan-tab" data-bs-toggle="pill" data-bs-target="#pills-kunjungan" type="button" role="tab" aria-controls="pills-kunjungan" aria-selected="false">Rata-rata Biaya yang Dikeluarkan Pelancong</button>
-                        </li>
-                    </ul>
-                </div>
-            </div> -->
-
         </div>
 
     </div>
-
-    <!-- <div class="row mt-3">
-        <div class="col-lg-12 grid-margin stretch-card">
-            <div class="card shadow">
-                <div class="card-body">
-                    <div
-                        class="position-absolute top-0 end-0 d-flex flex-row justify-content-center align-item-center ">
-                        <div class="me-1 mt-1 justify-content-end align-item-end">
-                            <button id="unduh-3-3" type="button" class="tombol btn-for" data-bs-toggle="modal"
-                                data-bs-target="#exampleModal3">
-                                <i class="fa-solid fa-download"></i>
-                            </button>
-                        </div>
-                    </div>
-
-                    <div style=" height:500px;">
-                        <canvas class="mt1" id="riset2_hasil4"></canvas>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div> -->
 </div>
 
 <!-- Modal Riset 2 -->
