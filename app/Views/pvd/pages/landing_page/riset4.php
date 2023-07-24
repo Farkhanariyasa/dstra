11b99fb2e9f8421677fd0d9b416a0cc77b4d7c6d
imams <222011830@gmail.com>
2023-07-24 07:48:14 +0700

    M View: update infografis

--- app/Views/pvd/pages/landing_page/riset4.php
+++ app/Views/pvd/pages/landing_page/riset4.php
@@ -439,7 +439,7 @@
                                     <img class="shawdow-lg" id="viz-1" src="<?= base_url('pvd/img/landingpage/riset4/infografis_riset4_gambaranumum_rev1-4_1-01.png') ?>" class="img-fluid text-center" alt="Infografis 1" style="width: auto; height: 325px;">
                                     <div class="overlay rounded-3 d-block mx-0 d-flex justify-content-center">
                                         <div class="d-flex justify-content-center my-auto">
-                                            <a href="<?= 'https://drive.google.com/file/d/1YbfzYss9zXyjo1VhOY1Uk_xUTqYFTmtv/view?usp=sharing' ?>" class="bukulaporan mx-auto">
+                                            <a href="<?= 'https://drive.google.com/file/d/1znN325vYddiaETgUiZJGCdvVaxIRXwg-/view?usp=sharing' ?>" class="bukulaporan mx-auto">
                                                 <div class="text-center">
                                                     <button class="btn btn-primary gdrive" type="button">
                                                         <div>
@@ -462,7 +462,7 @@
                                     <img class="shawdow-lg" id="viz-2" src="<?= base_url('pvd/img/landingpage/riset4/infografis_riset4_IKUPTIK_6-01.png') ?>" class="img-fluid text-center" alt="Infografis 2" style="width: auto; height: 325px;">
                                     <div class="overlay rounded-3 d-block mx-0 d-flex justify-content-center">
                                         <div class="d-flex justify-content-center my-auto">
-                                            <a href="<?= 'https://drive.google.com/file/d/1aqfBBiA_GQUOOISatV-cQ9Q_LSrW5FtX/view?usp=sharing' ?>" class="bukulaporan mx-auto">
+                                            <a href="<?= 'https://drive.google.com/file/d/1r7MfzH35AAO7IBTYoGFTRF6qlGC2qSrt/view?usp=sharing' ?>" class="bukulaporan mx-auto">
                                                 <div class="text-center">
                                                     <button class="btn btn-primary gdrive" type="button">
                                                         <div>
